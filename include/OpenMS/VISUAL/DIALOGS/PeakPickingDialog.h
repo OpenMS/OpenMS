7ffc272ad5face82c90ab6345cb0c2ef40310499
Eva Lange <elange@users.sourceforge.net>
2006-07-17 17:51:09 +0000

    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@179 6adb6e08-d915-0410-941f-83917bcadc18

---
+++ include/OpenMS/VISUAL/DIALOGS/PeakPickingDialog.h
@@ -0,0 +1,72 @@
+// -*- Mode: C++; tab-width: 2; -*-
+// vi: set ts=2:
+//
+// --------------------------------------------------------------------------
+//                   OpenMS Mass Spectrometry Framework
+// --------------------------------------------------------------------------
+//  Copyright (C) 2003-2006 -- Oliver Kohlbacher, Knut Reinert
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// --------------------------------------------------------------------------
+// $Maintainer: Timo Sachsenberg $
+// --------------------------------------------------------------------------
+
+
+#ifndef OPENMS_VISUAL_DIALOGS_PeakPickingDialog_H
+#define OPENMS_VISUAL_DIALOGS_PeakPickingDialog_H
+
+#include <OpenMS/VISUAL/DIALOGS/UIC/PeakPickingDialogTemplate.h>
+#include <OpenMS/FORMAT/Param.h>
+
+namespace OpenMS
+{
+
+  /**
+  	@brief GoTo dialog used to zoom to a m/z and retention time range.
+  	
+  	@ingroup Dialogs
+  */
+  class PeakPickingDialog: public PeakPickingDialogTemplate
+  {
+    Q_OBJECT
+
+  public:
+    PeakPickingDialog(QWidget* parent = 0, const char* name = 0, WFlags fl = 0);
+    ~PeakPickingDialog();
+    void setPeakHeight(float h);
+    float getPeakHeight();
+    void setPeakHeightMs2(float h2);
+    float getPeakHeightMs2();
+    void setSignalToNoise(float sn);
+    float getSignalToNoise();
+    void setFwhm(float fwhm);
+    float getFwhm();
+    void setOptimization(bool opt);
+    bool getOptimization();
+  protected:
+    bool optimization_;
+    float height_;
+    float height_ms2_;
+    float signal_to_noise_;
+    float fwhm_;
+  protected slots:
+    virtual void resetButton_clicked();
+    virtual void startButton_clicked();
+  };
+
+}
+#endif // OPENMS_VISUAL_DIALOGS_PeakPickingDialog_H
+
