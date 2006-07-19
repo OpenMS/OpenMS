5cb823b695b0dd9eebec271751fcb9df639b0287
Eva Lange <elange@users.sourceforge.net>
2006-07-19 08:48:30 +0000

    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@210 6adb6e08-d915-0410-941f-83917bcadc18

--- include/OpenMS/TRANSFORMATIONS/RAW2PEAK/PeakShape.h
+++ include/OpenMS/TRANSFORMATIONS/RAW2PEAK/PeakShape.h
@@ -30,6 +30,7 @@
 
 #include <math.h>
 
+#include <OpenMS/KERNEL/DPickedPeak.h>
 #include <OpenMS/CONCEPT/Types.h>
 
 namespace OpenMS
@@ -37,14 +38,14 @@ namespace OpenMS
 
   /** @brief This class is a internal representation (used by the PeakPickerCWT) of a peak shape.
 
-  It defines an asymmetric lorentzian and asymmetric hyperbolic squared secan function. 
+      It defines an asymmetric lorentzian and asymmetric hyperbolic squared secan function. 
   */
-  //@{
 
   class PeakShape
   {
   public:
 
+    /// Constructor
     PeakShape()
         : height(0),
         mz_position(0),
@@ -55,41 +56,53 @@ namespace OpenMS
         signal_to_noise(0.),
         type(PeakShapeType::UNDEFINED)
     {}
-    ///
+    /// Constructor
     PeakShape(double height_,
               double mz_position_,
               double left_width_,
               double right_width_,
               double area_,
               PeakShapeType::Enum type_);
-    ///
+    /// Copy constructor
     PeakShape(const PeakShape& peakshape);
-    ///
+    /// Destructor
     virtual ~PeakShape(){}
-    ///
+    /// Assignment operator
     PeakShape& operator = (const PeakShape& peakshape);
 
-    /// compute the real value of the fitted Peak at position x
+    /// Compute the intensity of the peaks shape at position x
     double operator() (const double x) const;
-    ///
+    /// Computes symmetry measure of the peak shape, which is corresponds to th ratio of the left and right width parameters.
     double getSymmetricMeasure() const;
-    ///
+    /// Estimates the full width at half maximum.
     double getFWHM() const;
-    ///
+    /// Maximum intensity of the peak shape
     double height;
-    ///
+    /// Centroid position
     double mz_position;
-    ///
+    /// Left width parameter
     double left_width;
-    ///
+    /// Right width parameter
     double right_width;
-    ///
+    /// Area of the peak shape
     double area;
-    ///
+    /** @brief Correlation coefficient.
+      
+      It represents the squared pearson correlation coefficient with the original data (0 <= r_value <= 1).
+    */
     double r_value;
-    ///
+    /// The signal to noise ratio at the mz_position
     double signal_to_noise;
-    ///
+    /** @brief Peak shape type (asymmetric lorentzian or asymmetric hyperbolic secans squared).
+
+        The peak shape can represent an asymmetric lorentzian function, given by 
+        l(x) = height/(1.+pow(left_width*(x - mz_position), 2)) (x<=mz_position)
+        l(x) = height/(1.+pow(right_width*(x - mz_position), 2)) (x>mz_position)
+        
+        or an asymmetric hyperbolic secans squared function 
+        s(x) = height/pow(cosh(left_width*(x-mz_position)), 2) (x<=mz_position)
+        s(x) = height/pow(cosh(right_width*(x-mz_position)), 2) (x>mz_position)
+    */
     PeakShapeType::Enum type;
 
 
