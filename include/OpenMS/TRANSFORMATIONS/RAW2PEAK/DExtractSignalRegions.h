86327c870210014fbbe3e2feb3f18702a2695a58
Eva Lange <elange@users.sourceforge.net>
2006-07-20 14:45:53 +0000

    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@232 6adb6e08-d915-0410-941f-83917bcadc18

--- include/OpenMS/TRANSFORMATIONS/RAW2PEAK/DExtractSignalRegions.h
+++ include/OpenMS/TRANSFORMATIONS/RAW2PEAK/DExtractSignalRegions.h
@@ -29,8 +29,7 @@
 
 #include <OpenMS/CONCEPT/Macros.h>
 
-#include <OpenMS/KERNEL/DPeakArrayNonPolymorphic.h>
-
+#include <OpenMS/KERNEL/MSSpectrum.h>
 #include <OpenMS/FORMAT/Param.h>
 
 #include <OpenMS/KERNEL/DimensionDescription.h>
@@ -49,34 +48,23 @@ namespace OpenMS
 
     To improve the run time of the peak picker the raw mass spectra are decomposed into
     smaller parts.
-
-    @todo fix and add test (Eva)
   */
 
-  template <Size D, typename ContainerType = DPeakArrayNonPolymorphic<D,DRawDataPoint<D> > >
+  template <Size D = 1, typename IteratorType = typename MSSpectrum<DRawDataPoint<D> >::const_iterator >
   class DExtractSignalRegions
   {
   public:
-    /** @name Type definitions
-     */
-    //@{
+    /// Dimension description type
     typedef DimensionDescription < DimensionDescriptionTagLCMS > DimensionDescription;
-    ///
-    typedef typename ContainerType::PeakType PeakType;
-    ///
-    typedef typename ContainerType::const_iterator ConstIterator;
-    ///
-    typedef std::vector<ConstIterator> IteratorVector;
-    ///
+    /// Peak type
+    typedef typename IteratorType::value_type PeakType;
+    /// Iterator vector type
+    typedef std::vector<IteratorType> IteratorVector;
+    /// Container iterator iterator
     typedef typename IteratorVector::iterator IteratorIterator;
-    ///
-    typedef typename IteratorVector::const_iterator IteratorConstIterator;
-    //@}
+    ///Container iterator const iterator
+    typedef typename IteratorVector::const_iterator IteratorIteratorType;
 
-
-    /** @name Constructors and Destructor
-     */
-    //@{
     /// Default constructor
     inline DExtractSignalRegions()
         : dalton_per_split_(10)
@@ -93,7 +81,7 @@ namespace OpenMS
           mz_dim_ = DimensionDescription::MZ;
         }
     }
-    ///
+    /// Constructor given a param object
     inline DExtractSignalRegions(const Param& parameters)
     {
       param_ = parameters;
@@ -124,16 +112,21 @@ namespace OpenMS
 
     /// Copy constructor
   inline DExtractSignalRegions(const DExtractSignalRegions&  e) : mz_dim_(e.mz_dim_), rt_dim_(e.rt_dim_), dalton_per_split_(e.dalton_per_split_) {}
+
     /// Destructor
     virtual ~DExtractSignalRegions()
     {}
-    //@}
 
-    /** @name Assignment
-     */
-    //@{
+
+    /// Assignment operator
     inline DExtractSignalRegions& operator=(const DExtractSignalRegions& e)
     {
+      // take care of self assignments
+      if (this == &e)
+      {
+        return *this;
+      }
+
       mz_dim_=e.mz_dim_;
       rt_dim_=e.rt_dim_;
       dalton_per_split_ = e.dalton_per_split_;
@@ -141,11 +134,7 @@ namespace OpenMS
 
       return *this;
     }
-    //@}
 
-    /** Accessors
-     */
-    //@{
     /// Non-mutable access to he mz dimension
     inline const int& getMZdim() const { return mz_dim_; }
     /// Mutable access to the mz dimensin
@@ -175,15 +164,16 @@ namespace OpenMS
     inline void setParam(const Param& param) { param_ = param; }
     //@}
 
-    /// The filtering method.
-    /** Splits the array defined by begin/end into independent areas where peaks are picked independently.
-        This reduces the runtime of finding peaks.
+
+    /** @brief Splits the array defined by begin/end into independent areas.
+     
+        This can for example reduces the runtime of the peak picking algorithm.
         As criterium, a signal is splitted into parts if more than points_for_split
         points are less than maxIntensity_*signalToNoiseThreshold_ (means that a quite large area of the signal is
         unimportant)
-        The output is written to splitted_array_ (pairs of beginning/end iterators)
+        The begin and end iterators of each split are written into an iterator vector.
     */
-    void splitScan(ConstIterator it_begin, ConstIterator it_end, double noise_level, IteratorVector &splitted_array)
+    void splitScan(IteratorType it_begin, IteratorType it_end, double noise_level, IteratorVector &splitted_array)
     {
 #ifdef DEBUG_PEAK_PICKING
       std::cout << "start splitScan " << dalton_per_split_ << " with noise_level " << noise_level
@@ -191,54 +181,63 @@ namespace OpenMS
 #endif
       splitted_array.clear();
 
-      ConstIterator end_point = it_end-1;
-      ConstIterator first=it_begin,last=it_end-1;
+      IteratorType end_point = it_end-1;
+      IteratorType first=it_begin,last=it_end-1;
 
-      /** First find the first point of interest. **/
+      // Search (from both ends) for the first raw data points which exeed the noise level
       while((first < end_point) && (first->getIntensity() < noise_level))
       {
         first++;
       }
 
-      /** There are no datapoints greater than the noise level **/
       if (first==last)
         return;
 
-      /** Then find the last point of interest. **/
+
       while((last > first) && (last->getIntensity() < noise_level))
       {
         last--;
       }
 
-      first = ((first-3) < it_begin) ? it_begin : (first-3);
-      last = ((last+3) > it_end) ? it_end : (last+3);
+      first = ((first-10) < it_begin) ? it_begin : (first-10);
+      last = ((last+10) > it_end) ? it_end : (last+10);
 
-      ConstIterator new_end = first+2;
+      // if the split contains no data points with intensity greater than noise, discard it
+      IteratorType greater_noise = it_end;
+      IteratorType new_end = first+2;
       while(new_end != (last-1))
       {
 #ifdef DEBUG_PEAK_PICKING
         std::cout << "while : first " << first->getPosition()[mz_dim_] << " while : new_end " << new_end->getPosition()[mz_dim_]
         <<  " " << ((new_end+1)->getPosition()[mz_dim_] - new_end->getPosition()[mz_dim_])<< std::endl;
 #endif
-        /**is there a gap between new_end and the next data point, which is greater than one dalton? **/
+
+        if (new_end->getIntensity() > noise_level)
+        {
+          greater_noise = new_end;
+        }
+
+        // is there a gap between new_end and the next data point, which is greater than one dalton?
         if (((new_end+1)->getPosition()[mz_dim_] - new_end->getPosition()[mz_dim_]) > 1)
         {
 #ifdef DEBUG_PEAK_PICKING
           std::cout << "gap " << new_end->getPosition()[mz_dim_] << " und first " << first->getPosition()[mz_dim_] << " dist: " << distance(first,new_end)<<std::endl;
 #endif
-          // are there enough datapoints between first and new_end?
-          if (distance(first,(new_end+1)) > 3)
+          // and are there enough datapoints between first and new_end?
+          if ((distance(first,(new_end+1)) > 3) && (greater_noise != it_end))
           {
             // cut the signal
             splitted_array.push_back(first);
             splitted_array.push_back(new_end+1);
 #ifdef DEBUG_PEAK_PICKING
             std::cout << " find the end x da away: " <<  std::endl
-											<< " push first " << first->getPosition()[mz_dim_] 
-											<< " push last " << (new_end)->getPosition()[mz_dim_]
-											<< " distance " << distance(first,(new_end+1))
-											<< std::endl;
+            << " push first " << first->getPosition()[mz_dim_]
+            << " push last " << (new_end)->getPosition()[mz_dim_]
+            << " distance " << distance(first,(new_end+1))
+            << std::endl;
 #endif
+            greater_noise = it_end;
+
           }
           //else throw away this split
           first = new_end+1;
@@ -249,77 +248,63 @@ namespace OpenMS
           continue;
         }
 
-        // compute the moving average
-        if((distance(first,new_end) >= 5) && average_(new_end,it_begin,5) < noise_level)
-        {
-          // are there enough datapoints between first and new_end?
-          if (distance(first,new_end-1) > 3)
-          {
-            // if the signal is homogeneous cut
-            splitted_array.push_back(first);
-            splitted_array.push_back(new_end-1);
-#ifdef DEBUG_PEAK_PICKING
-            std::cout << " average find the end x da away: " << std::endl
-											<< " push first " << first->getPosition()[mz_dim_] 
-											<< " last " << (new_end-2)->getPosition()[mz_dim_]
-											<< " distance " << distance(first,(new_end+1))
-											<< std::endl;
-#endif
-
-          }
-          first = new_end-1;
-#ifdef DEBUG_PEAK_PICKING
-          std::cout << "after : first " << first->getPosition()[mz_dim_] << std::endl;
-#endif
-          ++new_end;
-          continue;
-        }
-
-
-        /** if the split has already  a length of dalton_per_split_ cut search for a minimum **/
+        // if the split has already  a length of dalton_per_split_ search for a minimum and cut
         if (((new_end)->getPosition()[mz_dim_] - first->getPosition()[mz_dim_]) > dalton_per_split_)
         {
 #ifdef DEBUG_PEAK_PICKING
           std::cout << "dalton_per_split_ reached " << first->getPosition()[mz_dim_]  << " " << (new_end)->getPosition()[mz_dim_] << std::endl;
 #endif
-          bool found = false;
           int  search_radius = 5;
 
-          /** Search to the left for a minimum **/
-          for (int i=0; ((i < search_radius) && (found == false)); i++)
+          // if the split contains no data point with intensity greater than noise, discard the split
+          if (greater_noise == it_end)
           {
-            ConstIterator left_temp, right_temp;
-            left_temp = new_end - i;
-
-            if ((average_(left_temp-1,it_begin, 2) < average_(left_temp-2,it_begin, 2))
-                &&(average_(left_temp-1,it_begin,2) < average_(left_temp,it_begin,2))
-                && left_temp->getIntensity() < noise_level)
+            first = new_end;
+            std::cout << "No greater_noise " << std::endl;
+          }
+          else
+          {
+            // Search to the left for a minimum
+            for (int i=0; i < search_radius; i++)
             {
-              splitted_array.push_back(first);
-              splitted_array.push_back(left_temp);
+              IteratorType left_temp, right_temp;
+              left_temp = new_end - i;
+
+              if (((average_(left_temp-1,it_begin, 2) < average_(left_temp-2,it_begin, 2))
+                   &&(average_(left_temp-1,it_begin,2) < average_(left_temp,it_begin,2))
+                   && left_temp->getIntensity() < noise_level) && (greater_noise <= left_temp))
+              {
+                // if the signal is still falling to the left go on until a minimum is found
+                while ((left_temp-2)->getIntensity() < (left_temp-1)->getIntensity())
+                {
+                  --left_temp;
+                }
+                
+                splitted_array.push_back(first);
+                splitted_array.push_back(left_temp);
 #ifdef DEBUG_PEAK_PICKING
-              std::cout << "minimum found " << std::endl
-												<< " push " << first->getPosition()[mz_dim_] 
-												<< " and " << (left_temp-1)->getPosition()[mz_dim_] << std::endl;
+                std::cout << "minimum found " << std::endl
+                << " push " << first->getPosition()[mz_dim_]
+                << " and " << (left_temp-1)->getPosition()[mz_dim_] << std::endl;
 #endif
-              new_end = left_temp;
-              first = new_end;
-              found = true;
-              break;
+                new_end = left_temp;
+                first = new_end;
+                greater_noise = it_end;
+
+                break;
+              }
             }
           }
         }
-        /** If there is no minimum to be found inside one search_radius,
-        *  go on
-        */
         ++new_end;
       }
 
-      if (distance(first, new_end) > 3)
+      // does the last split contain enough data points and does the split contain any data value with an intensity greater then noise?
+      if ((distance(first, new_end) > 3) && (greater_noise != it_end))
       {
 #ifdef DEBUG_PEAK_PICKING
         std::cout << " push the last split: first " << first->getPosition()[mz_dim_] << std::endl
-        << "push last " << (new_end+1)->getPosition()[mz_dim_]
+        << "push last " << (new_end)->getPosition()[mz_dim_]
         << "distance " << distance(first,(new_end+2)) << std::endl;
 #endif
         splitted_array.push_back(first);
@@ -335,24 +320,25 @@ namespace OpenMS
 
   protected:
     ///
-    // MZ dimension
+    /// MZ dimension
     int mz_dim_;
     ///
-    // RT dimension
+    /// RT dimension
     int rt_dim_;
     ///
-    // length of the boxes the signal is decomposed to
+    /// Length of the regions the signal is decomposed to
     float dalton_per_split_;
     ///
-    // Parameter object
+    /// Parameter object
     Param param_;
-    //
-    inline double average_(ConstIterator act_pos, ConstIterator first, int number)
+
+    /// compute the average of the act_pos intensity and the intensities of its left number data points
+    inline double average_(IteratorType act_pos, IteratorType first, int number)
     {
       int i = 0;
       int k = 1;
       double mean = act_pos->getIntensity();
-      for (i = 1; i < number; ++i)
+      for (i = 1; ((i < number) && (act_pos - i) >= first) ; ++i)
       {
         if ((act_pos-i) >= first)
         {
