368f4bb1d2286698363b9472ba94f973bb5f587a
Ole Schulz-Trieglaff <ole_st@users.sourceforge.net>
2006-07-21 15:17:48 +0000

    MapMatcher TOPP Module.
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@271 6adb6e08-d915-0410-941f-83917bcadc18

--- source/APPLICATIONS/TOPP/MapMatcher.C
+++ source/APPLICATIONS/TOPP/MapMatcher.C
@@ -70,269 +70,145 @@ using namespace std;
 	@ingroup TOPP
 */
 
-//-------------------------------------------------------------
-// command line name of this tool
-//-------------------------------------------------------------
-const char* tool_name = "MapMatcher";
+// We do not want this class to show up in the docu -> @cond
+/// @cond TOPPCLASSES
 
-//-------------------------------------------------------------
-// description of the usage of this TOPP tool
-//-------------------------------------------------------------
-
-void print_usage()
+class TOPPMapMatcher
+            : public TOPPBase
 {
-	cerr << endl
-       << tool_name << " -- estimates a transformation for pairs of features in different LC/MS maps" << endl
-       << endl
-       << "Usage:" << endl
-			 << " " << tool_name << " [options]" << endl
-			 << endl
-			 << "Options are:" << endl
-			 << " -grid <file>   grid covering the map to be transformed (default read from INI file)" << endl
-			 << " -pairs <file>  feature pairs (default read from INI file)" << endl
-			 << " -q <float>  	 minimum quality of pairs considered (default read from INI file)" << endl
-			 << " -out <file>  	 output file (default read from INI file)" << endl
-			 << endl
-			 << "Common TOPP options are:" << endl
-			 << "  -ini <file>       TOPP INI file (default: TOPP.ini)" << endl
-			 << "  -log <file>       log file (default: TOPP.log)" << endl
-			 << "  -n <int>          instance number (default: 1)" << endl
-			 << "  -d <level>        sets debug level (default: 0)" << endl
-			 << "  --help            shows this help" << endl
-       		 << "  --help-opt        shows help on the INI options accepted" << endl
-			 << endl ;
-}
 
-//-------------------------------------------------------------
-// main program
-//-------------------------------------------------------------
+public:
+    TOPPMapMatcher()
+            : TOPPBase("MapMatcher")
+    {}
+
+protected:
+
+    void printToolUsage_()
+    {
+        cerr << endl
+        << tool_name_ << " -- estimates a transformation for pairs of features in different LC/MS maps" << endl
+        << endl
+        << "Usage:" << endl
+        << " " << tool_name_ << " [options]" << endl
+        << endl
+        << "Options are:" << endl
+        << " -grid <file>   grid covering the map to be transformed (default read from INI file)" << endl
+        << " -pairs <file>  feature pairs (default read from INI file)" << endl
+        << " -q <float>  	 minimum quality of pairs considered (default read from INI file)" << endl
+        << " -out <file>  	 output file (default read from INI file)" << endl
+        << endl ;
+
+    }
+
+    void printToolHelpOpt_()
+    {
+        cerr << endl
+        << tool_name_ << endl
+        << endl
+        << "INI options:" << endl
+        << "  in <file>                  either feat or mzData (default read from INI file)" << endl
+        << "  out <file>                output mzData file name" << endl
+        << "  in_type <file_type>  either feat or mzData (default read from INI file)" << endl
+        << endl
+        << "INI File example section:" << endl
+        << "<ITEM name=\"pairs\" value=\"04111717_pairs.xml\" type=\"string\"/>" << endl
+        << "<ITEM name=\"grid\" value=\"the_grid.xml\" type=\"string\"/>" << endl
+        << "<ITEM name=\"min_quality\" value=\"0.5\" type=\"float\"/>" << endl
+        << "<ITEM name=\"out\" value=\"grid_wtransform.xml\" type=\"string\"/>" << endl;
+    }
+
+    void setOptionsAndFlags_()
+    {
+        options_["-grid"] = "grid";
+        options_["-pairs"] = "pairs";
+        options_["-out"] = "out";
+        options_["-q"] = "min_quality";
+    }
+
+    ExitCodes main_(int , char**)
+    {
+        //-----------------------------------------------------------
+        // parsing parameters
+        //-------------------------------------------------------------
+        //File names
+        String gridfile, pairsfile, outfile;
+
+        gridfile = getParamAsString_("grid");
+        writeDebug_(String("Grid file: ") + gridfile, 1);
+
+        pairsfile = getParamAsString_("pairs");
+        writeDebug_(String("Pairs file: ") + pairsfile, 1);
+
+        outfile = getParamAsString_("out");
+        writeDebug_(String("Output file: ") + outfile, 1);
+
+        //parameters
+        double min_quality = 0;
+
+        //length of the structuring element
+        String qualstr = getParamAsString_("min_quality");
+        writeDebug_(String("min_quality") + qualstr, 1);
+
+        try
+        {
+            //resampling
+            if (qualstr != "")
+            {
+                min_quality = qualstr.toFloat();
+            }
+        }
+        catch(Exception::ConversionError& e)
+        {
+            writeLog_(String("Invalid value for the minimum quality '") + qualstr  + "' given. Aborting!");
+            printUsage_();
+            return ILLEGAL_PARAMETERS;
+        }
+
+        //-------------------------------------------------------------
+        // reading input
+        //-------------------------------------------------------------
+
+        DGridFile grid_file;
+        DGrid<2> the_grid;
+        grid_file.load(gridfile,the_grid);
+
+        DFeaturePairsFile pairs_file;
+        DFeaturePairVector<2> pairs_vector;
+        pairs_file.load(pairsfile,pairs_vector);
+
+        //-------------------------------------------------------------
+        // calculations
+        //-------------------------------------------------------------
+        DMapMatcherRegression<2> map_matcher;
+        map_matcher.setFeaturePairs(pairs_vector);
+        map_matcher.setGrid(the_grid);
+        map_matcher.setMinQuality(min_quality);
+
+        map_matcher.estimateTransform();
+
+        //-------------------------------------------------------------
+        // writing output
+        //-------------------------------------------------------------
+
+        DGrid<2> grid_with_transform;
+        grid_with_transform = map_matcher.getGrid();
+
+        grid_file.store(outfile,grid_with_transform);
+
+        return OK;
+
+    }
+};
+
+/// @endcond
 
 int main( int argc, char ** argv )
 {
-	// instance specific location of settings in INI file (e.g. 'TOPP_Skeleton:1:')
-	String ini_location;
-	// path to the log file
-	String logfile = "";
-	// debug level
-	int debug_level = 0;
-	// log filestream (as long as the real logfile is not setermined yet)
-	ofstream log;
-	log.open("TOPP.log", ofstream::out | ofstream::app);
-	
-	//-------------------------------------------------------------
-	// command line parsing
-	//-------------------------------------------------------------
-	
-	//list of all the valid options
-	map<string,string> valid_options;
-	valid_options["-grid"] = "grid";
-	valid_options["-pairs"] = "pairs";
-	valid_options["-q"] = "min_quality";
-	valid_options["-out"] = "out";
-	valid_options["-ini"] = "ini";
-	valid_options["-log"] = "log";
-	valid_options["-n"] = "instance";
-	valid_options["-d"] = "debug";
-	valid_options["--help"] = "help";
-	//for debugging
-	valid_options["unknown"] = "unknown";
-	valid_options["misc"] = "misc";
-	
-	Param param;
-	param.parseCommandLine(argc, argv, valid_options);
-
-	//-------------------------------------------------------------
-	// read debug level from command line if set 
-	//-------------------------------------------------------------
-	if (!param.getValue("debug").isEmpty())
-	{
-		debug_level = (int)(param.getValue("debug"));
-	}
-	
-	//-------------------------------------------------------------
-	// determine instance number
-	//-------------------------------------------------------------
-
-	if (param.getValue("instance").isEmpty())
-	{
-		param.setValue("instance",1);
-	}
-	ini_location = String(tool_name) + ":" + param.getValue("instance").toString() + ":";
-	if (debug_level>0) log << Date::now() << " " << ini_location << " Instance number: " << param.getValue("instance") << endl;
-
-	//-------------------------------------------------------------
-	// check command line options
-	//-------------------------------------------------------------
-	
-	// '--help' given
-	if (!(param.getValue("help").isEmpty()))
-	{
-		print_usage();
-		return OK;
-	}
-	
-	// test if unknown options were given
-	if (!param.getValue("unknown").isEmpty())
-	{
-		log << Date::now() << " " << ini_location << " Unknown option '" << (string)(param.getValue("unknown")) << "' given. Aborting!" << endl;
-		cout << "Unknown option '" << (string)(param.getValue("unknown")) << "' given. Aborting!" << endl;
-		print_usage();
-		return ILLEGAL_PARAMETERS;
-	}
-	
-	// test if unknown text argument were given (we do not use them)
-	if (!param.getValue("misc").isEmpty())
-	{
-		log << Date::now() << " " << ini_location << " Trailing text argument '" << (string)(param.getValue("misc")) << "' given. Aborting!" << endl;
-		cout << "Trailing text argument '" << (string)(param.getValue("misc")) << "' given. Aborting!" << endl;
-		print_usage();
-		return ILLEGAL_PARAMETERS;
-	}
-
-	try
-	{
-	
-		//-------------------------------------------------------------
-		// loading INI file
-		//-------------------------------------------------------------
-		if (param.getValue("ini").isEmpty())
-		{
-			param.setValue("ini",string("TOPP.ini"));
-		}
-		param.load((string)(param.getValue("ini")));
-		if (debug_level>0) log << Date::now() << " " << ini_location << " INI file: " << param.getValue("ini") << endl;
-	
-		
-		//-------------------------------------------------------------
-		// determine and open log file
-		//-------------------------------------------------------------
-		if (!(param.getValue("log").isEmpty()))
-		{
-			logfile = (string)(param.getValue("log"));
-		}
-		if (param.getValue("log").isEmpty() && !(param.getValue(ini_location+"log").isEmpty()))
-		{
-			logfile = (string)(param.getValue(ini_location+"log"));
-		}
-		if (param.getValue("log").isEmpty() && !(param.getValue("common:log").isEmpty()))
-		{
-			logfile = (string)(param.getValue("common:log"));
-		}
-		if (debug_level>0) log << Date::now() << " " << ini_location << " log file: " << logfile << endl;
-		log.close();
-		log.open(logfile.c_str(), ofstream::out | ofstream::app);
-
-		//-------------------------------------------------------------
-		// parsing parameters
-		//-------------------------------------------------------------
-		//File names
-		String gridfile, pairsfile, outfile;
-		
-		if (!(param.getValue("grid").isEmpty())) //from command line
-		{
-			gridfile = (string)(param.getValue("grid"));
-		}
-		else if (!(param.getValue(ini_location+"grid").isEmpty())) //from INI file
-		{
-			gridfile = (string)(param.getValue(ini_location+"grid"));
-		}
-		if (debug_level>0) log << Date::now() << " " << ini_location << " grid file: " << gridfile << endl;	
-		
-		if (!(param.getValue("pairs").isEmpty())) //from command line
-		{
-			pairsfile = (string)(param.getValue("pairs"));
-		}
-		else if (!(param.getValue(ini_location+"pairs").isEmpty())) //from INI file
-		{
-			pairsfile = (string)(param.getValue(ini_location+"pairs"));
-		}
-		if (debug_level>0) log << Date::now() << " " << ini_location << " pairs file: " << pairsfile << endl;	
-	
-		//determine output file name
-		if (!(param.getValue("out").isEmpty())) //from command line
-		{
-			outfile = (string)(param.getValue("out"));
-		}
-		else if (!(param.getValue(ini_location+"out").isEmpty())) //from INI file
-		{
-			outfile = (string)(param.getValue(ini_location+"out"));
-		}
-		if (debug_level>0) log << Date::now() << " " << ini_location << " output file: " << outfile << endl;	
-		
-		//parameters
-		double min_quality = 0;
-			
-		if (!(param.getValue("min_quality").isEmpty())) //from command line
-		{
-			min_quality = (double)(param.getValue("min_quality"));
-		}
-		else if (!(param.getValue(ini_location+"min_quality").isEmpty())) //from INI file
-		{
-			min_quality = (double)(param.getValue(ini_location+"min_quality"));
-		}
-		if (debug_level>1) log << Date::now() << " " << ini_location << " min_quality: " << min_quality << endl;	
-
-		//-------------------------------------------------------------
-		// reading input
-		//-------------------------------------------------------------
-		
-		DGridFile grid_file;
-		DGrid<2> the_grid;
-		grid_file.load(gridfile,the_grid);
-				
-		DFeaturePairsFile pairs_file;
-		DFeaturePairVector<2> pairs_vector;
-		pairs_file.load(pairsfile,pairs_vector);
-		
-		//-------------------------------------------------------------
-		// calculations
-		//-------------------------------------------------------------
-		DMapMatcherRegression<2> map_matcher;
-		map_matcher.setFeaturePairs(pairs_vector);
-		map_matcher.setGrid(the_grid);
-		map_matcher.setMinQuality(min_quality);
-		
-		map_matcher.estimateTransform();
-		
-		//-------------------------------------------------------------
-		// writing output
-		//-------------------------------------------------------------
-		
-		DGrid<2> grid_with_transform;
-		grid_with_transform = map_matcher.getGrid();
-		
-		grid_file.store(outfile,grid_with_transform);
-	
-	}
-	catch(Exception::UnableToCreateFile& e)
-	{
-		cout << "Error: Unable to write file (" << e.what() <<")"<< endl;
-		log << Date::now() << " " << ini_location << " Error: Unable to write file (" << e.what() <<")"<< endl;
-		return CANNOT_WRITE_OUTPUT_FILE;
-	}	
-	catch(Exception::FileNotFound& e)
-	{
-		cout << "Error: File not found (" << e.what() <<")"<< endl;
-		log << Date::now() << " " << ini_location << " Error: File not found (" << e.what() <<")"<< endl;
-		return INPUT_FILE_NOT_FOUND;
-	}
-	catch(Exception::ParseError& e)
-	{
-		cout << "Error: Unable to read file (" << e.what() <<")"<< endl;
-		log << Date::now() << " " << ini_location << " Error: Unable to read file (" << e.what() <<")"<< endl;
-		return INPUT_FILE_CORRUPT;
-	}
-	catch(Exception::Base& e)
-	{
-		cout << "Error: Unexpected error (" << e.what() <<")"<< endl;
-		log << Date::now() << " " << ini_location << " Error: Unexpected error (" << e.what() <<")"<< endl;
-		return UNKNOWN_ERROR;
-	}
-  
-	log.close();
-	
-	return OK;
+    TOPPMapMatcher tool;
+    return tool.main(argc,argv);
 }
+
 
 
 
