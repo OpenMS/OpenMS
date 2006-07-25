c3f163bbc0556f2a4acb6fdeab99a3bb3a5782f2
Martin Langwisch <martinlangwisch@users.sourceforge.net>
2006-07-25 12:35:17 +0000

    renamed some functions
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@344 6adb6e08-d915-0410-941f-83917bcadc18

--- include/OpenMS/FORMAT/InspectOutfile.h
+++ include/OpenMS/FORMAT/InspectOutfile.h
@@ -1,50 +1,36 @@
-/// -*- Mode: C++; tab-width: 2; -*-
-/// vi: set ts=2:
-///
-/// --------------------------------------------------------------------------
-///                   OpenMS Mass Spectrometry Framework
-/// --------------------------------------------------------------------------
-///  Copyright (C) 2003-2006 -- Oliver Kohlbacher, Knut Reinert
-///
-///  This library is free software; you can redistribute it and/or
-///  modify it under the terms of the GNU Lesser General Public
-///  License as published by the Free Software Foundation; either
-///  version 2.1 of the License, or (at your option) any later version.
-///
-///  This library is distributed in the hope that it will be useful,
-///  but WITHOUT ANY WARRANTY; without even the implied warranty of
-///  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-///  Lesser General Public License for more details.
-///
-///  You should have received a copy of the GNU Lesser General Public
-///  License along with this library; if not, write to the Free Software
-///  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-///
-/// --------------------------------------------------------------------------
-/// $Id: InspectOutfile.h,v 1.0 2006/07/12 15:58:59 martinlangwisch Exp $
-/// $Author: martinlangwisch $
-/// $Maintainer: Martin Langwisch $
-/// --------------------------------------------------------------------------
+// -*- Mode: C++; tab-width: 2; -*-
+// vi: set ts=2:
+//
+// --------------------------------------------------------------------------
+//                   OpenMS Mass Spectrometry Framework
+// --------------------------------------------------------------------------
+//  Copyright (C) 2003-2006 -- Oliver Kohlbacher, Knut Reinert
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// --------------------------------------------------------------------------
+// $Id: InspectOutfile.h,v 1.0 2006/07/12 15:58:59 martinlangwisch Exp $
+// $Author: martinlangwisch $
+// $Maintainer: Martin Langwisch $
+// --------------------------------------------------------------------------
 
 #ifndef OPENMS_FORMAT_INSPECTOUTFILE_H
 #define OPENMS_FORMAT_INSPECTOUTFILE_H
 
-
-/*#include <OpenMS/KERNEL/KernelTraits.h>
-#include <OpenMS/METADATA/Identification.h>
-#include <OpenMS/METADATA/ProteinIdentification.h>
-#include <OpenMS/METADATA/PeptideHit.h>
-#include <OpenMS/METADATA/ProteinHit.h>
-#include <OpenMS/DATASTRUCTURES/Date.h>*/
 #include <OpenMS/FORMAT/Outfile.h>
 #include <OpenMS/FORMAT/InspectFile.h>
-
-
-/*#include <sstream>
-#include <string>
-#include <map>
-#include <set>
-#include <cstring>*/
 
 namespace OpenMS
 {
@@ -61,12 +47,6 @@ namespace OpenMS
 		InspectFile
   {
     public:
-			/// empty constructor
-			InspectOutfile();
-			
-			/// copy constructor
-			InspectOutfile(const InspectOutfile& source);
-		
       /// Constructor
       InspectOutfile(const std::string& result_filename, const std::string& database_filename, const std::string& database_path, std::string index_filename = "") throw (Exception::FileNotFound, Exception::ParseError);
       
