5ba752fbe7aa1f7c063dfe1697d6a980611e44f5
Martin Langwisch <martinlangwisch@users.sourceforge.net>
2006-07-25 12:35:39 +0000

    renamed some functions
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@345 6adb6e08-d915-0410-941f-83917bcadc18

--- include/OpenMS/FORMAT/InspectFile.h
+++ include/OpenMS/FORMAT/InspectFile.h
@@ -1,30 +1,30 @@
-/// -*- Mode: C++; tab-width: 2; -*-
-/// vi: set ts=2:
+// -*- Mode: C++; tab-width: 2; -*-
+// vi: set ts=2:
 ///
-/// --------------------------------------------------------------------------
-///                   OpenMS Mass Spectrometry Framework
-/// --------------------------------------------------------------------------
-///  Copyright (C) 2003-2006 -- Oliver Kohlbacher, Knut Reinert
+// --------------------------------------------------------------------------
+//                   OpenMS Mass Spectrometry Framework
+// --------------------------------------------------------------------------
+//  Copyright (C) 2003-2006 -- Oliver Kohlbacher, Knut Reinert
 ///
-///  This library is free software; you can redistribute it and/or
-///  modify it under the terms of the GNU Lesser General Public
-///  License as published by the Free Software Foundation; either
-///  version 2.1 of the License, or (at your option) any later version.
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
 ///
-///  This library is distributed in the hope that it will be useful,
-///  but WITHOUT ANY WARRANTY; without even the implied warranty of
-///  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-///  Lesser General Public License for more details.
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
 ///
-///  You should have received a copy of the GNU Lesser General Public
-///  License along with this library; if not, write to the Free Software
-///  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ///
-/// --------------------------------------------------------------------------
-/// $Id: InspectFile.h,v 1.0 2006/07/12 15:58:59 martinlangwisch Exp $
-/// $Author: martinlangwisch $
-/// $Maintainer: Martin Langwisch $
-/// --------------------------------------------------------------------------
+// --------------------------------------------------------------------------
+// $Id: InspectFile.h,v 1.0 2006/07/12 15:58:59 martinlangwisch Exp $
+// $Author: martinlangwisch $
+// $Maintainer: Martin Langwisch $
+// --------------------------------------------------------------------------
 
 #ifndef OPENMS_FORMAT_INSPECTFILE_H
 #define OPENMS_FORMAT_INSPECTFILE_H
@@ -44,20 +44,36 @@ namespace OpenMS
 	{
 		public:
 			
-			/// erase all given characters in a string ### in string-klasse einbauen?
-			void erase(std::string& s, const std::string& unwanted_character);
+			/// erase all given characters in a string
 			
-			/// erase all whitespaces in a string ### in string-klasse einbauen?
-			void erase_whitespaces(std::string& s);
+			/// @todo move to String
+			void remove(std::string& s, const std::string& unwanted_character);
+			
+			/// erase all whitespaces in a string
+			/// @todo move to String
+			void removeWhitespaces(std::string& s);
 			
 			/// used to generate a trie database from another kind of database
-			void compressor(const std::string& source_filename, const std::string& source_path, const std::string& database_path, std::vector< unsigned int > wanted_records = std::vector< unsigned int >(), std::string database_filename = "", std::string index_filename = "", bool append = false, std::string species = "None") throw (Exception::FileNotFound, Exception::ParseError);
+			void generateTrieDB(const std::string& source_filename, const std::string& source_path, const std::string& database_path, std::vector< unsigned int > wanted_records = std::vector< unsigned int >(), std::string database_filename = "", std::string index_filename = "", bool append = false, std::string species = "None") throw (Exception::FileNotFound, Exception::ParseError);
 			
+			/// compress a trie database to contain the wanted records only
 			void compressTrieDB(const std::string database_filename, std::string index_filename, std::string database_path, std::vector< unsigned int > wanted_records, std::string second_database_filename = "", std::string second_index_filename = "", std::string second_database_path = "", bool append = false) throw (Exception::FileNotFound, Exception::ParseError);
 			
-			void getSeparators(const std::string& source_filename, std::string& ac_label, std::string& sequence_start_label, std::string& sequence_end_label, std::string& comment_label, std::string& species_label) throw (Exception::FileNotFound, Exception::ParseError);
+			/**
+				@brief retrieve the labels used in a file
+				
+				id line
+				label for the start of the sequence
+				label for the end of the sequence
+				label for the line defining the species
+			*/
+			void getLabels(const std::string& source_filename, std::string& ac_label, std::string& sequence_start_label, std::string& sequence_end_label, std::string& comment_label, std::string& species_label) throw (Exception::FileNotFound, Exception::ParseError);
 			
+			/// retrieve sequences from a trie database
 			void getSequences(std::string database_path, const std::string& database_filename, std::string index_filename, const std::vector< unsigned int > wanted_records, std::vector<std::string>& sequences) throw (Exception::FileNotFound, Exception::ParseError);
+			
+			/// if there's no character marking the name as a directory it is set
+			void ensurePathChar(std::string& path, char path_char = '/');
 			
 			bool setTempDatabaseFilename(const std::string& temp_database_filename);
 			const std::string& getTempDatabaseFilename() const;
@@ -73,30 +89,30 @@ namespace OpenMS
 			
 			void setUseTempFiles(const bool use_temp_files);
 			const bool getUseTempFiles() const;
-			
-			void ensurePathChar(std::string& path, char path_char = '/');
 		
 		protected:
-			/* can convert any file format fullfilling the following conditions to the ".trie" database format (and creating a corresponding index file)
-			for each record in the file there has to be
-			- a unique* label for the id line (the label and the id have to be seperated by at least one whitespace)
-			- a unique* label for the start of the sequence (a line preceeding the sequence lines!)
-			- a unique* label for the end of the sequence (a line succeeding the sequence lines!)
-			- there may be a unique label for the line defining the species
-			the lines labeled correspondingly have to start with the label (whitespaces before are allowed)
-			* unique means there must not be more than one line with this label, there may be one line (and thus one label) for several things, though; e.g. in a FASTA file a ">" marks the id, the start and the end of a sequence:
-			> very_nice_protein_indeed
-			MYVERYNICEPRQTEIN
-			> some_other_protein
-			...
+			/**
+				@brief can convert any file format fullfilling the following conditions to the ".trie" database format (and creating a corresponding index file)
+				
+				for each record in the file there has to be
+				- a unique* label for the id line (the label and the id have to be seperated by at least one whitespace)
+				- a unique* label for the start of the sequence (a line preceeding the sequence lines!)
+				- a unique* label for the end of the sequence (a line succeeding the sequence lines!)
+				- there may be a unique label for the line defining the species
+				the lines labeled correspondingly have to start with the label (whitespaces before are allowed)
+				* unique means there must not be more than one line with this label, there may be one line (and thus one label) for several things, though; e.g. in a FASTA file a ">" marks the id, the start and the end of a sequence:
+				> very_nice_protein_indeed
+				MYVERYNICEPRQTEIN
+				> some_other_protein
+				...
 			*/
-			void compressor_(const std::string& source_filename, const std::string& source_path, const std::string& database_path, const std::string& ac_label, const std::string& sequence_start_label, const std::string& sequence_end_label, const std::string& comment_label, std::string species_label = "", std::string species = "None", std::vector< unsigned int > wanted_records = std::vector< unsigned int >(), std::string database_filename = "", std::string index_filename = "", bool append = false) throw (Exception::FileNotFound, Exception::ParseError);
+			void generateTrieDB_(const std::string& source_filename, const std::string& source_path, const std::string& database_path, const std::string& ac_label, const std::string& sequence_start_label, const std::string& sequence_end_label, const std::string& comment_label, std::string species_label = "", std::string species = "None", std::vector< unsigned int > wanted_records = std::vector< unsigned int >(), std::string database_filename = "", std::string index_filename = "", bool append = false) throw (Exception::FileNotFound, Exception::ParseError);
 			
-			static const unsigned int index_peptide_name_length_;
-			static const unsigned int index_db_record_length_;
-			static const unsigned int index_trie_record_length_;
-			static const unsigned int index_record_length_;
-			static const char trie_delimiter_;
+			static const unsigned int index_peptide_name_length_; ///< the length of a peptide name in the index file
+			static const unsigned int index_db_record_length_; ///< the length of the originial database position in the index file
+			static const unsigned int index_trie_record_length_; ///< the length of the trie database position in the index file
+			static const unsigned int index_record_length_; ///< the length of a record (the sum of the three parameters above)
+			static const char trie_delimiter_; ///< the character that delimits protein sequences in a trie database
 			static const std::string score_type_;
 			
 			static std::string temp_database_filename_;
