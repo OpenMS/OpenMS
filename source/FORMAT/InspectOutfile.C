fec404904817833d155bdbb3acc512a927144357
Martin Langwisch <martinlangwisch@users.sourceforge.net>
2006-07-28 10:43:27 +0000

    newest version
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@395 6adb6e08-d915-0410-941f-83917bcadc18

--- source/FORMAT/InspectOutfile.C
+++ source/FORMAT/InspectOutfile.C
@@ -30,11 +30,14 @@
 
 namespace OpenMS 
 {
-  InspectOutfile::InspectOutfile(const std::string& result_filename, const std::string& database_filename_, const std::string& database_path, const double& p_value_threshold, std::string index_filename_)
-  	throw (Exception::FileNotFound, Exception::ParseError, Exception::IllegalArgument)
-    : Outfile()
+	InspectOutfile::InspectOutfile()
+	{}
+	
+  void InspectOutfile::load(const std::string& result_filename, std::vector< Identification >&	identifications, ProteinIdentification&	protein_identification, std::vector< float >& 	precursor_retention_times, std::vector< float >& precursor_mz_values, const double& p_value_threshold, const std::string& database_filename, const std::string& database_path, const std::string& sequence_filename, std::string index_filename) throw (Exception::FileNotFound, Exception::ParseError, Exception::IllegalArgument)
   {
 		// (0) preparations
+		std::vector< PeptideHit > peptide_hits;
+		std::vector< ProteinHit > protein_hits;
 
 		// check whether the p_value is correct
 		if ( (p_value_threshold < 0) || (p_value_threshold > 1) )
@@ -42,89 +45,19 @@ namespace OpenMS
 			throw Exception::IllegalArgument(__FILE__, __LINE__, __PRETTY_FUNCTION__, "p_value_threshold");
 		}
 
-		// open the result and database file
-		String database_filename(database_filename_);
-		String index_filename(index_filename_);
+		// open the result and database file(s) ####
+		//String database_filename(database_filename_);
+		//String index_filename(index_filename_);
 		std::ifstream result_file( result_filename.c_str());
 		if ( !result_file )
 		{
 			throw Exception::FileNotFound(__FILE__, __LINE__, __PRETTY_FUNCTION__, result_filename);
 		}
-		std::string path_and_file = database_path;
-		ensurePathChar(path_and_file);
-		path_and_file.append(database_filename);
-		std::ifstream database_file( path_and_file.c_str(), std::ios::in | std::ios::binary );
-		if ( !database_file )
-		{
-			throw Exception::FileNotFound(__FILE__, __LINE__, __PRETTY_FUNCTION__, database_filename);
-		}
-		
-		std::string start_seperator, buffer;
-		getLabels(path_and_file, buffer, start_seperator, buffer, buffer, buffer);
-		database_file.close();
-		database_file.clear();
 		
 		// get the header
 		String line;
 		//unsigned int number_of_columns;
 		std::vector<String> substrings;
-		/*if ( getline(result_file, line) )
-		{
-			if ( !line.empty() ) line.resize(line.length()-1);
-			line.split('\t', substrings);
-			number_of_columns = substrings.size();
-		}
-		else // Inspect search was not successfull
-		{
-			return;
-		}
-		
-		// get the number of some columns
-		int spectrum_file_column = -1;
-		int scan_column = -1;
-		int peptide_column = -1;
-		int protein_column = -1;
-		int charge_column = -1;
-		int MQScore_column = -1;
-		int record_number_column = -1;
-		
-		for ( std::vector< String >::const_iterator iter = substrings.begin(); iter != substrings.end(); ++iter)
-		{
-			if ( (*iter) == "#SpectrumFile" )
-			{
-				spectrum_file_column = (iter - substrings.begin());
-			}
-			else if ( (*iter) == "Scan#" )
-			{
-				scan_column = (iter - substrings.begin());
-			}
-			else if ( (*iter) == "Annotation" )
-			{
-				peptide_column = (iter - substrings.begin());
-			}
-			else if ( (*iter) == "Protein" )
-			{
-				protein_column = (iter - substrings.begin());
-			}
-			else if ( (*iter) == "Charge" )
-			{
-				charge_column = (iter - substrings.begin());
-			}
-			else if ( (*iter) == "MQScore" )
-			{
-				MQScore_column = (iter - substrings.begin());
-			}
-			else if ( (*iter) == "RecordNumber" )
-			{
-				record_number_column = (iter - substrings.begin());
-			}
-		}
-		
-		// check whether the columns are available in the header
-		if ( (spectrum_file_column == -1) || (scan_column == -1) || (peptide_column == -1) || (protein_column == -1) || (charge_column == -1) || (MQScore_column == -1) ||  (record_number_column == -1))
-		{
-			throw Exception::ParseError(__FILE__, __LINE__, __PRETTY_FUNCTION__, "at least one of the columns '#SpectrumFile', 'Scan#', 'Annotation', 'Protein', 'Charge', 'MQScore' or  'RecordNumber' is missing!" , result_filename);
-		}*/
 		
 		enum columns
 		{
@@ -146,10 +79,16 @@ namespace OpenMS
 			spec_file_pos_column
 		};
 		unsigned int number_of_columns = 16;
+		bool from_fasta = (!sequence_filename.empty());
+		bool missing_column;
+		if ( (!database_filename.empty()) && from_fasta )
+		{
+			throw Exception::ParseError(__FILE__, __LINE__, __PRETTY_FUNCTION__, "trie AND FASTA database given (only one of both is allowed).", result_filename);
+		}
 		
 		// map the protein hits according to their record number in the result file
-		//					record number			position in protein_hits_
-		std::map< unsigned int, unsigned int > rn_position_map;
+		//					record number			position in protein_hits
+		std::map< std::pair< bool, int >, int > rn_position_map;
 		Identification* query;
 		PeptideHit peptide_hit;
 		std::vector< PeptideHit >::iterator pep_hit_i;
@@ -160,8 +99,11 @@ namespace OpenMS
 		std::string accession, accession_type, spectrum_file;
 		unsigned int record_number, scan_number, start, end;
 		unsigned int rank = 0;
-		unsigned int max_record_number = 0;
+		//unsigned int max_record_number = 0;
 		unsigned int line_number = 0; // used to report in which line an error occured
+		
+		// get all the proteins and make a vector of those who come from a FASTA file and those who come from a trie database
+		std::vector< unsigned int > FASTA_proteins, trie_proteins;
 		
 		while ( getline(result_file, line) )
 		{
@@ -169,8 +111,9 @@ namespace OpenMS
 			++line_number;
 			line.split('\t', substrings);
 			
-			// check whether the line has enough columns
-			if (substrings.size() < number_of_columns )
+			// check whether the line has enough columns (a line from a fasta-db does not include the protein name)
+			missing_column = ( substrings.size() == number_of_columns-1 );
+			if ( substrings.size() < number_of_columns - missing_column )
 			{
 				char buffer[10];
 				sprintf(buffer, "%i", line_number);
@@ -190,41 +133,116 @@ namespace OpenMS
 			if ( substrings[0] == "#SpectrumFile" ) continue;
 			
 			// take only those peptides whose p-value is less or equal the given threshold
-			if ( atof(substrings[p_value_column].c_str()) > p_value_threshold ) continue;			
+			if ( (substrings[p_value_column - missing_column] != "nan") && (atof(substrings[p_value_column - missing_column].c_str()) > p_value_threshold) ) continue;
+			
+			// if there's a missing column, the record number is one too high
+			record_number = atoi(substrings[record_number_column - missing_column].c_str()) - missing_column;
+			
+			// (1.1)  if a new protein is found, get the rank and insert it
+			if ( rn_position_map.find(std::make_pair(from_fasta, record_number)) == rn_position_map.end() )
+			{
+				//max_record_number = std::max(max_record_number, record_number);
+				
+				protein_hit.clear();
+				//protein_hit.setScore(0.0);
+				//protein_hit.setScoreType(score_type_);
+				rn_position_map.insert(std::make_pair(std::make_pair(from_fasta, record_number), protein_hits.size()));
+				
+				// get accession number and type
+				if ( !from_fasta )
+				{
+					getACAndACType(substrings[protein_column], accession, accession_type);
+					protein_hit.setAccession(accession);
+					protein_hit.setAccessionType(accession_type);
+				}
+				
+				protein_hit.setRank(rn_position_map.size());
+				protein_hits.push_back(protein_hit);
+			}
+		} // result file read
+		result_file.close();
+		result_file.clear();
+		
+		for ( std::map< std::pair< bool, int >, int >::const_iterator i = rn_position_map.begin(); i != rn_position_map.end(); ++i )
+		{
+			if ( i->first.first ) FASTA_proteins.push_back(i->first.second);
+			else trie_proteins.push_back(i->first.second);
+		}
+		
+		// search the sequence, accession and accession type of the proteins from a FASTA file
+		if ( !FASTA_proteins.empty() )
+		{
+			std::vector< std::vector< String > > protein_info;
+			std::string ac_label, sequence_start_label, sequence_end_label, comment_label, species_label;
+			
+			getLabels(sequence_filename, ac_label, sequence_start_label, sequence_end_label, comment_label, species_label);
+			
+			getSequenceAndACandACType(sequence_filename, FASTA_proteins, protein_info, ac_label, sequence_start_label, sequence_end_label, comment_label, species_label);
+			
+			// set the retrieved protein info
+			std::vector< std::vector< String > >::const_iterator p_i = protein_info.begin();
+			for ( std::vector< unsigned int >::const_iterator i = FASTA_proteins.begin(); i != FASTA_proteins.end(); ++i, ++p_i )
+			{
+				protein_hits[rn_position_map[std::make_pair(true, *i)]].setSequence((*p_i)[0]);
+				getACAndACType((*p_i)[1], accession, accession_type);
+				protein_hits[rn_position_map[std::make_pair(true, *i)]].setAccession(accession);
+				protein_hits[rn_position_map[std::make_pair(true, *i)]].setAccessionType(accession_type);
+			}
+			
+			protein_info.clear();
+			FASTA_proteins.clear();
+		}
+		
+		result_file.open( result_filename.c_str());
+		if ( !result_file )
+		{
+			throw Exception::FileNotFound(__FILE__, __LINE__, __PRETTY_FUNCTION__, result_filename);
+		}
+		
+		while ( getline(result_file, line) )
+		{
+			if ( !line.empty() ) line.resize(line.length()-1);
+			++line_number;
+			line.split('\t', substrings);
+			
+			// check whether the line has enough columns (a line from a fasta-db does not include the protein name)
+			missing_column = ( substrings.size() == number_of_columns-1 );
+			if ( substrings.size() < number_of_columns - missing_column )
+			{
+				char buffer[10];
+				sprintf(buffer, "%i", line_number);
+				std::string error_message = "wrong number of columns in row ";
+				error_message.append(buffer);
+				error_message.append("! (");
+				sprintf(buffer, "%i", substrings.size());
+				error_message.append(buffer);
+				error_message.append(" present, should be ");
+				sprintf(buffer, "%i", number_of_columns);
+				error_message.append(buffer);
+				error_message.append(")");
+				throw Exception::ParseError(__FILE__, __LINE__, __PRETTY_FUNCTION__, error_message.c_str() , result_filename);
+			}
+			
+			// if the version Inspect.20060620.zip is used, there is a header
+			if ( substrings[0] == "#SpectrumFile" ) continue;
+			
+			// take only those peptides whose p-value is less or equal the given threshold, nans are included
+			if ( (substrings[p_value_column - missing_column] != "nan") && (atof(substrings[p_value_column - missing_column].c_str()) > p_value_threshold) ) continue;			
 
-			// (1.0) if a new query is found, insert it into the vector and start a new one
+			// (1.0) if a new query is found, insert it into the vector
 			if ( (substrings[spectrum_file_column] != spectrum_file) || ((unsigned int) atoi(substrings[scan_column].c_str()) != scan_number) )
 			{
-				queries_.push_back(Identification());
-				query = &queries_.back();
+				identifications.push_back(Identification());
+				query = &identifications.back();
 				spectrum_file = substrings[spectrum_file_column];
 				scan_number = atoi(substrings[scan_column].c_str());
 				
-				query->setCharge(atoi(substrings[charge_column].c_str()));
+				query->setCharge(atoi(substrings[charge_column - missing_column].c_str()));
 				query->setPeptideSignificanceThreshold(p_value_threshold);
 				rank = 0;
 			}
 			
-			record_number = atoi(substrings[record_number_column].c_str());
-			// get accession number and type
-			getACAndACType(substrings[protein_column], accession, accession_type);
-			
-			// (1.1)  if a new protein is found, get all the information and insert it
-			if ( rn_position_map.find(record_number) == rn_position_map.end() )
-			{
-				max_record_number = std::max(max_record_number, record_number);
-				
-				protein_hit.clear();
-				protein_hit.setAccession(accession);
-				protein_hit.setAccessionType(accession_type);
-// ### score einfach zusammenrechnen?
-				//protein_hit.setScore(0.0);
-				//protein_hit.setScoreType(score_type_);
-				
-				rn_position_map.insert(std::make_pair(record_number, protein_hits_.size()));
-				protein_hit.setRank(rn_position_map.size());
-				protein_hits_.push_back(protein_hit);
-			}
+			record_number = atoi(substrings[record_number_column - missing_column].c_str()) - missing_column;
 			
 			// (1.2) get the peptide infos from the new peptide and insert it
 			peptide_hit.clear();
@@ -234,70 +252,145 @@ namespace OpenMS
 			end = substrings[peptide_column].find_last_of('.');
 			peptide_hit.setSequence(substrings[peptide_column].substr(start, end-start));
 			peptide_hit.setRank(++rank);
-			peptide_hit.addProteinIndex(datetime, accession);
+			peptide_hit.addProteinIndex(datetime, protein_hits[rn_position_map[std::make_pair(from_fasta, record_number)]].getAccession());
 			
 			rank -= updatePeptideHits(peptide_hit, query->getPeptideHits());
-			updatePeptideHits(peptide_hit, peptide_hits_);
+			updatePeptideHits(peptide_hit, peptide_hits);
 		} // result file read
+		result_file.close();
 		
-		// get the sequences of the protein
-		// make a vector of the records to get the sequences of the proteins
-		std::vector< unsigned int > record_vector;
-		for ( std::map< unsigned int, unsigned int >::iterator i = rn_position_map.begin(); i != rn_position_map.end(); ++i)
+		// get the sequences of the trie proteins
+		if ( !trie_proteins.empty() )
 		{
-			record_vector.push_back(i->first);
-		}
-		// it it's no trie database generate one from the database and use this one
-		if ( start_seperator != std::string(1, trie_delimiter_) )
-		{
-			database_file.close();
-			database_file.clear();
-			std::string old_database_filename = database_filename;
-			database_filename = getTempDatabaseFilename();
-			index_filename = getTempIndexFilename();
+			std::vector< std::string > sequences;
+			getSequences(database_path, database_filename, index_filename, trie_proteins, sequences);
 			
-			generateTrieDB(old_database_filename, database_path, database_path, record_vector, database_filename, index_filename);
-			
-			// change the vector so it contains the positions of the wanted records in the new database (which is just their number, but this way the record vector can be used for 'old', as well as just generated trie databases)
-			for ( unsigned int i = 0; i < record_vector.size(); ++i)
+			std::vector< std::string >::const_iterator p_i = sequences.begin();
+			for ( std::vector< unsigned int >::const_iterator i = trie_proteins.begin(); i != trie_proteins.end(); ++i, ++p_i)
 			{
-				record_vector[i] = i;
+				protein_hits[rn_position_map[std::make_pair(true, *i)]].setSequence(*p_i);
 			}
+			sequences.clear();
+			trie_proteins.clear();
 		}
-		
-		std::vector<std::string> sequences;
-		getSequences(database_path, database_filename, index_filename, record_vector, sequences);
-		
-		unsigned int i = 0;
-		for ( std::map< unsigned int, unsigned int >::iterator protein_i = rn_position_map.begin(); protein_i != rn_position_map.end(); ++protein_i)
-		{
-			protein_hits_[protein_i->second].setSequence(sequences[i]);
-			++i;
-		}
-		sequences.clear();
 		
 		// if there's but one query the protein hits are inserted there instead of a ProteinIdentification object
-		if ( queries_.empty() )
+		if ( identifications.empty() )
 		{
-			query->setProteinHits(protein_hits_);
+			query->setProteinHits(protein_hits);
 			query->setDateTime(datetime);
 		}
 		
-		protein_ids_.setProteinHits(protein_hits_);
-		protein_ids_.setDateTime(datetime);
+		protein_identification.setProteinHits(protein_hits);
+		protein_identification.setDateTime(datetime);
 		
+		peptide_hits.clear();
+		protein_hits.clear();
 		peptide_hit.clear();
 		protein_hit.clear();
 		
 		rn_position_map.clear();
-		record_vector.clear();
-		
-		curr_peptide_hit_ = peptide_hits_.begin();
-		curr_protein_hit_ = protein_hits_.begin();
-		curr_query_ = queries_.begin();
-
-		// now that all information are read
- 		ok_ = true;
   }
+
+	void InspectOutfile::getACAndACType(String line, std::string& accession, std::string& accession_type) throw (Exception::ParseError)
+	{
+		std::pair<std::string, std::string> p;
+		// if it's a FASTA line
+		if ( line.hasPrefix(">") )
+		{
+			line.erase(0,1);
+		}
+		line.trim();
+		
+		// if it's a swissprot accession
+		if ( line.hasPrefix("tr") || line.hasPrefix("sp") )
+		{
+			accession = line.substr(3, line.find('|', 3)-3);
+			accession_type = "SwissProt";
+		}
+		else if ( line.hasPrefix("gi") )
+		{
+			unsigned int snd = line.find('|', 3);
+			unsigned int third = line.find('|', ++snd);
+			++third;
+			accession = line.substr(third, line.find('|', third)-third);
+			accession_type = line.substr(snd, third-1-snd);
+			if ( accession_type == "gb" )
+			{
+				accession_type = "GenBank";
+			}
+			else if ( accession_type == "emb" )
+			{
+				accession_type = "EMBL";
+			}
+			else if ( accession_type == "dbj" )
+			{
+				accession_type = "DDBJ";
+			}
+		}
+		else if ( line.hasPrefix("ref") )
+		{
+			accession = line.substr(4, line.find('|', 4) - 4);
+			accession_type = "NCBI";
+		}
+		// if it's a swissprot line
+		else if ( line.hasPrefix("AC") )
+		{
+			line.erase(0,2);
+			accession = line.trim();
+			accession_type = "SwissProt";
+		}
+		else
+		{
+			accession = line.trim();
+			accession_type = "unknown";
+		}
+		/* GenBank                           gi|gi-number|gb|accession|locus
+ EMBL Data Library                 gi|gi-number|emb|accession|locus
+ DDBJ, DNA Database of Japan       gi|gi-number|dbj|accession|locus
+ NCBI Reference Sequence           gi|gi-number|ref|accession|locus
+ SWISS-PROT                        gi|gi-number|sp|accession|name
+ General database identifier       gnl|database|identifier
+ Local Sequence identifier         lcl|identifier*/
+	}
+	
+	bool InspectOutfile::updatePeptideHits(PeptideHit& peptide_hit, std::vector< PeptideHit >& peptide_hits)
+	{
+		std::vector< PeptideHit >::iterator pep_hit_i;
+		for ( pep_hit_i = peptide_hits.begin(); pep_hit_i != peptide_hits.end(); ++pep_hit_i)
+		{
+			if ( (pep_hit_i->getSequence() == peptide_hit.getSequence()) && (pep_hit_i->getScore() == peptide_hit.getScore()) ) break;
+		}
+		
+		// a peptide hit may only be inserted if it's score type matches the one of the existing hits
+		if ( (peptide_hits.empty()) || (peptide_hits[0].getScoreType() == peptide_hit.getScoreType()) )
+		{
+			// if a new peptide is found, insert it
+			if ( pep_hit_i == peptide_hits.end() )
+			{
+				peptide_hits.push_back(peptide_hit);
+				return false;
+			}
+			// if the peptide has already been inserted, insert additional protein hits
+			else
+			{
+				std::vector< std::pair< String, String > >::iterator prot_hit_i1, prot_hit_i2;
+				// remove all protein hits from the peptide that are already in the list
+				for ( prot_hit_i1 = peptide_hit.getProteinIndices().begin(); prot_hit_i1 != peptide_hit.getProteinIndices().end(); )
+				{
+					prot_hit_i2 = std::find(pep_hit_i->getProteinIndices().begin(), pep_hit_i->getProteinIndices().end(), *prot_hit_i1);
+					if ( prot_hit_i2 != pep_hit_i->getProteinIndices().end() ) peptide_hit.getProteinIndices().erase(prot_hit_i1);
+					else ++prot_hit_i1;
+				}
+				// add the additional protein hits
+				for ( prot_hit_i2 = peptide_hit.getProteinIndices().begin(); prot_hit_i2 != peptide_hit.getProteinIndices().end(); ++prot_hit_i2 )
+				{
+					pep_hit_i->addProteinIndex(*prot_hit_i2);
+				}
+				return true;
+			}
+		}
+		return false;
+	}
 
 } //namespace OpenMS
