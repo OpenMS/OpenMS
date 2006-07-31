783fdc2fd94af3f129581ecfbb39b8b84bb34867
Clemens Gröpl <groepl@users.sourceforge.net>
2006-07-31 22:52:34 +0000

    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@425 6adb6e08-d915-0410-941f-83917bcadc18

--- source/APPLICATIONS/TOPP/Examples/UnlabeledMatcher.sh
+++ source/APPLICATIONS/TOPP/Examples/UnlabeledMatcher.sh
@@ -4,13 +4,13 @@ echo "This is UnlabeledMatcher.sh - a small shell script to illustrate the usage
 echo "The feature map data files can be obtained upon request from the authors."
 
 #--------------------------------------------------
-function run_wrapper()
+function cmd-wrapper()
 {
 # uncomment your choice (for debugging of this script)
-#  yes, do run
-#	time $@
+#  yes, DO run
+	time $@
 #  simulate
-	echo run_wrapper: $@
+#	echo cmd-wrapper: $@
 }
 #--------------------------------------------------
 
@@ -23,8 +23,8 @@ echo "We start with a simple, artificial example.  Two maps, shifted by 3 second
 echo "As was to be expected, both algorithms find all 2521 pairs."
 echo " "
 
-run_wrapper UnlabeledMatcher -ini UnlabeledMatcher.ini -n 1 -d 1000
-run_wrapper UnlabeledMatcher -ini UnlabeledMatcher.ini -n 2 -d 1000
+cmd-wrapper UnlabeledMatcher -ini UnlabeledMatcher.ini -n 1 -d 1000
+cmd-wrapper UnlabeledMatcher -ini UnlabeledMatcher.ini -n 2 -d 1000
 # time UnlabeledMatcher -ini UnlabeledMatcher.ini -n 3 -d 1000
 
 echo " "
@@ -35,17 +35,17 @@ echo "The next example shows two repeat measurements."
 echo "The 'geomhash_shift' algorithm finds 1928, the 'simple' algorithm 1890 feature pairs."
 echo " "
 
-run_wrapper UnlabeledMatcher -ini UnlabeledMatcher.ini -n 10 -d 1
-run_wrapper UnlabeledMatcher -ini UnlabeledMatcher.ini -n 11 -d 1
+cmd-wrapper UnlabeledMatcher -ini UnlabeledMatcher.ini -n 10 -d 1
+cmd-wrapper UnlabeledMatcher -ini UnlabeledMatcher.ini -n 11 -d 1
 
-run_wrapper gnuplot E32R_24h_1_vs_2.geomhash_shift.data.gp - <<EOF
+cmd-wrapper gnuplot E32R_24h_1_vs_2.geomhash_shift.data.gp - <<EOF
 set output 'E32R_24h_1_vs_2.geomhash_shift.data.png'
 set terminal png size 1000,1000
 replot
 quit
 EOF
 
-run_wrapper gnuplot E32R_24h_1_vs_2.simple.data.gp - <<EOF
+cmd-wrapper gnuplot E32R_24h_1_vs_2.simple.data.gp - <<EOF
 set output 'E32R_24h_1_vs_2.simple.data.png'
 set terminal png size 1000,1000
 replot
@@ -60,13 +60,13 @@ echo "The x-axis is the index of the feature pair, ordered by quality,"
 echo "the y-axis is the quality."
 echo " "
 
-run_wrapper FeaturePairSplitter -in E32R_24h_1_vs_2.geomhash_shift.pairs.xml -qual E32R_24h_1_vs_2.geomhash_shift.pairs.qualities
+cmd-wrapper FeaturePairSplitter -in E32R_24h_1_vs_2.geomhash_shift.pairs.xml -qual E32R_24h_1_vs_2.geomhash_shift.pairs.qualities
 LC_ALL=C sort -nr E32R_24h_1_vs_2.geomhash_shift.pairs.qualities > E32R_24h_1_vs_2.geomhash_shift.pairs.qualities.sorted
 
-run_wrapper FeaturePairSplitter -in E32R_24h_1_vs_2.simple.pairs.xml -qual E32R_24h_1_vs_2.simple.pairs.qualities
+cmd-wrapper FeaturePairSplitter -in E32R_24h_1_vs_2.simple.pairs.xml -qual E32R_24h_1_vs_2.simple.pairs.qualities
 LC_ALL=C sort -nr E32R_24h_1_vs_2.simple.pairs.qualities > E32R_24h_1_vs_2.simple.pairs.qualities.sorted
 
-run_wrapper gnuplot - <<EOF
+cmd-wrapper gnuplot - <<EOF
 plot 'E32R_24h_1_vs_2.geomhash_shift.pairs.qualities.sorted' with lines
 replot 'E32R_24h_1_vs_2.simple.pairs.qualities.sorted' with lines
 set output 'E32R_24h_1_vs_2.algorithm.qualities.png'
@@ -79,48 +79,38 @@ echo " "
 echo "============================================================"
 echo " "
 
-#--------------------------------------------------
-function run_wrapper()
-{
-# uncomment your choice (for debugging of this script)
-#  yes, do run
-	time $@
-#  simulate
-#	echo run_wrapper: $@
-}
-#--------------------------------------------------
-
 echo "Finally, we go through the same steps for another two repeat measurements."
 
 echo "Here the 'geomhash_shift' algorithm finds 2182, the 'simple' algorithm 2155 feature pairs."
 echo "Have a look at the  grid file.  It lists the shifts,"
 echo "which change over time in a complex, non-linear way."
+echo "The simple algorithm creates quite a few false pairs in the cluster near RT=500"
 echo " "
 
-run_wrapper UnlabeledMatcher -ini UnlabeledMatcher.ini -n 20 -d 1
-run_wrapper UnlabeledMatcher -ini UnlabeledMatcher.ini -n 21 -d 1
+cmd-wrapper UnlabeledMatcher -ini UnlabeledMatcher.ini -n 20 -d 1
+cmd-wrapper UnlabeledMatcher -ini UnlabeledMatcher.ini -n 21 -d 1
 
-run_wrapper gnuplot E32R_24h_10uMCu_1_vs_2.geomhash_shift.data.gp - <<EOF
+cmd-wrapper gnuplot E32R_24h_10uMCu_1_vs_2.geomhash_shift.data.gp - <<EOF
 set output 'E32R_24h_10uMCu_1_vs_2.geomhash_shift.data.png'
 set terminal png size 1000,1000
 replot
 quit
 EOF
-xs
-run_wrapper gnuplot E32R_24h_10uMCu_1_vs_2.simple.data.gp - <<EOF
+
+cmd-wrapper gnuplot E32R_24h_10uMCu_1_vs_2.simple.data.gp - <<EOF
 set output 'E32R_24h_10uMCu_1_vs_2.simple.data.png'
 set terminal png size 1000,1000
 replot
 quit
 EOF
 
-run_wrapper FeaturePairSplitter -in E32R_24h_10uMCu_1_vs_2.geomhash_shift.pairs.xml -qual E32R_24h_10uMCu_1_vs_2.geomhash_shift.pairs.qualities
+cmd-wrapper FeaturePairSplitter -in E32R_24h_10uMCu_1_vs_2.geomhash_shift.pairs.xml -qual E32R_24h_10uMCu_1_vs_2.geomhash_shift.pairs.qualities
 LC_ALL=C sort -nr E32R_24h_10uMCu_1_vs_2.geomhash_shift.pairs.qualities > E32R_24h_10uMCu_1_vs_2.geomhash_shift.pairs.qualities.sorted
 
-run_wrapper FeaturePairSplitter -in E32R_24h_10uMCu_1_vs_2.simple.pairs.xml -qual E32R_24h_10uMCu_1_vs_2.simple.pairs.qualities
+cmd-wrapper FeaturePairSplitter -in E32R_24h_10uMCu_1_vs_2.simple.pairs.xml -qual E32R_24h_10uMCu_1_vs_2.simple.pairs.qualities
 LC_ALL=C sort -nr E32R_24h_10uMCu_1_vs_2.simple.pairs.qualities > E32R_24h_10uMCu_1_vs_2.simple.pairs.qualities.sorted
 
-run_wrapper gnuplot - <<EOF
+cmd-wrapper gnuplot - <<EOF
 plot 'E32R_24h_10uMCu_1_vs_2.geomhash_shift.pairs.qualities.sorted' with lines
 replot 'E32R_24h_10uMCu_1_vs_2.simple.pairs.qualities.sorted' with lines
 set output 'E32R_24h_10uMCu_1_vs_2.algorithm.qualities.png'
@@ -128,6 +118,10 @@ set term png
 replot
 quit
 EOF
+
+echo " "
+echo "============================================================"
+echo " "
 
 echo " "
 echo "Done."
