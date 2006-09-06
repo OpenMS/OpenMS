6adedea34587fa28042ed24e0138f962f3a244d1
Eva Lange <elange@users.sourceforge.net>
2006-09-06 16:20:09 +0000

    Changed the second template argument of FeatureMap.
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@499 6adb6e08-d915-0410-941f-83917bcadc18

--- include/OpenMS/KERNEL/StandardTypes.h
+++ include/OpenMS/KERNEL/StandardTypes.h
@@ -95,7 +95,7 @@ namespace OpenMS
 	/**
 		 @brief  Two-dimensional map of features, with meta information about experimental settings.
 	*/
-	typedef DFeatureMap<2,KernelTraits> FeatureMap;
+	typedef DFeatureMap<2,Feature> FeatureMap;
 	//@}
 
 }
