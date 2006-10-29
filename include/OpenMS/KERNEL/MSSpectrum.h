455309078ce177f4269fcfc82e7b2bdfe61e6d59
Clemens Gröpl <groepl@users.sourceforge.net>
2006-10-29 18:12:15 +0000

    typo in doc
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@732 6adb6e08-d915-0410-941f-83917bcadc18

--- include/OpenMS/KERNEL/MSSpectrum.h
+++ include/OpenMS/KERNEL/MSSpectrum.h
@@ -2,23 +2,23 @@
 // vi: set ts=2:
 //
 // --------------------------------------------------------------------------
-//                   OpenMS Mass Spectrometry Framework
+//									 OpenMS Mass Spectrometry Framework
 // --------------------------------------------------------------------------
-//  Copyright (C) 2003-2006 -- Oliver Kohlbacher, Knut Reinert
+//	Copyright (C) 2003-2006 -- Oliver Kohlbacher, Knut Reinert
 //
-//  This library is free software; you can redistribute it and/or
-//  modify it under the terms of the GNU Lesser General Public
-//  License as published by the Free Software Foundation; either
-//  version 2.1 of the License, or (at your option) any later version.
-// 
-//  This library is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-//  Lesser General Public License for more details.
+//	This library is free software; you can redistribute it and/or
+//	modify it under the terms of the GNU Lesser General Public
+//	License as published by the Free Software Foundation; either
+//	version 2.1 of the License, or (at your option) any later version.
 //
-//  You should have received a copy of the GNU Lesser General Public
-//  License along with this library; if not, write to the Free Software
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//	This library is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the GNU
+//	Lesser General Public License for more details.
+//
+//	You should have received a copy of the GNU Lesser General Public
+//	License along with this library; if not, write to the Free Software
+//	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA	02111-1307	USA
 //
 // --------------------------------------------------------------------------
 // $Maintainer: Marc Sturm $
@@ -34,167 +34,167 @@
 
 #include <map>
 
-namespace OpenMS 
+namespace OpenMS
 {
 	/**
-		@brief The representation of a 1D spectrum. 
-		
-		It contains the data itself (DSpectrum) and metadata about spectrum specific instrument settings,
-		aquisition settings, description of the meta values used in the peaks and precursor info (SpectrumSettings).
-		
-		Several MSSpectrum instances are contained in MSExperiment e.g. class MSExperiment is essentially
-		a vector of spectra with additional information about the experiment. 
-		
-		Precursor info from SpectrumSettings should only be used if this spectrum is a tandem-MS spectrum.
-		The precursor spectrum is the first spectrum in MSExperiment, that has a lower MS-level than
-		the current spectrum.
-		
-		@note For range operations, see \ref RangeUtils "RangeUtils module"!
-		
-		@ingroup Kernel
+		 @brief The representation of a 1D spectrum.
+
+		 It contains the data itself (DSpectrum) and metadata about spectrum specific instrument settings,
+		 aquisition settings, description of the meta values used in the peaks and precursor info (SpectrumSettings).
+
+		 Several MSSpectrum instances are contained in MSExperiment e.g. class MSExperiment is essentially
+		 a vector of spectra with additional information about the experiment.
+
+		 Precursor info from SpectrumSettings should only be used if this spectrum is a tandem-MS spectrum.
+		 The precursor spectrum is the first spectrum in MSExperiment, that has a lower MS-level than
+		 the current spectrum.
+
+		 @note For range operations, see \ref RangeUtils "RangeUtils module"!
+
+		 @ingroup Kernel
 	*/
 	template <typename PeakT = DPeak<1> >
-  class MSSpectrum 
-  	: public DSpectrum<1, DPeakArray<1, PeakT> >,
-  		public SpectrumSettings,
-  		public PersistentObject
-  {
-    public:
+	class MSSpectrum
+		: public DSpectrum<1, DPeakArray<1, PeakT> >,
+			public SpectrumSettings,
+			public PersistentObject
+	{
+	 public:
 
-			///Comparator for the retention time.
-			struct RTLess
-				: public std::binary_function <MSSpectrum, MSSpectrum, bool>
+		///Comparator for the retention time.
+		struct RTLess
+			: public std::binary_function <MSSpectrum, MSSpectrum, bool>
+		{
+			bool operator () (const MSSpectrum& a, const MSSpectrum& b) const
 			{
-				bool operator () (const MSSpectrum& a, const MSSpectrum& b) const
-				{
-					return (a.getRetentionTime() < b.getRetentionTime());
-				}
-			};
-				
-			/// Peak type
-			typedef PeakT PeakType;
-			
-			/// Spectrum base type
-			typedef DSpectrum<1, DPeakArray<1, PeakT> > BaseSpectrum;
-    	
-    	/// Constructor
-      MSSpectrum():
-				DSpectrum<1, DPeakArray<1, PeakT> >(),
-				SpectrumSettings(),
-				PersistentObject()
-			{
-			  
+				return (a.getRetentionTime() < b.getRetentionTime());
 			}
-      /// Copy constructor
-      MSSpectrum(const MSSpectrum& source):
-				DSpectrum<1, DPeakArray<1, PeakT> >(source),
-				SpectrumSettings(source),
-				PersistentObject(source)
-			{
-			  
-			}
-      /// Destructor
-      ~MSSpectrum()
-			{
-			  
-			}
-      
-      // Assignment operator
-      MSSpectrum& operator= (const MSSpectrum& source)
-			{
-			  if (&source == this) return *this;
-			  
-		  	DSpectrum<1, DPeakArray<1, PeakT> >::operator=(source);
-		   	SpectrumSettings::operator=(source);
-		    PersistentObject::operator=(source);
-			  return *this;
-			}
+		};
 
-      /// Equality operator
-      bool operator== (const MSSpectrum& rhs) const
-		  {
-		  	return
-			   	SpectrumSettings::operator==(rhs) &&
-			    DSpectrum<1, DPeakArray<1, PeakT> >::operator==(rhs)
-		  		;
-		  }
-      /// Equality operator
-      bool operator!= (const MSSpectrum& rhs) const
-		  {
-		  	return !(operator==(rhs));
-		 	}
-			
-			/**
-				@brief Fast search for peak range begin
-				
-				@note Make sure the spectrum is sorted with respect to m/z ratio! Otherwise the result is undefined.
-			*/
-			typename BaseSpectrum::Iterator MZBegin(double mz)
-			{
-				PeakType p;
-				p.getPosition()[0] = mz;
-				return lower_bound(BaseSpectrum::begin(), BaseSpectrum::end(), p, typename PeakType::PositionLess());
-			}
+		/// Peak type
+		typedef PeakT PeakType;
 
-			/**
-				@brief Fast search for peak range end (returns the path-the-end iterator)
-				
-				@note Make sure the spectrum is sorted with respect to m/z ratio. Otherwise the result is undefined.
-			*/
-			typename BaseSpectrum::Iterator MZEnd(double mz)
-			{
-				PeakType p;
-				p.getPosition()[0] = mz;
-				return upper_bound(BaseSpectrum::begin(), BaseSpectrum::end(), p, typename PeakType::PositionLess());
-			}
+		/// Spectrum base type
+		typedef DSpectrum<1, DPeakArray<1, PeakT> > BaseSpectrum;
 
-			/**
-				@brief Fast search for peak range begin
-				
-				@note Make sure the spectrum is sorted with respect to m/z ratio! Otherwise the result is undefined.
-			*/
-			const typename BaseSpectrum::ConstIterator MZBegin(double mz) const
-			{
-				PeakType p;
-				p.getPosition()[0] = mz;
-				return lower_bound(BaseSpectrum::begin(), BaseSpectrum::end(), p, typename PeakType::PositionLess());
-			}
+		/// Constructor
+		MSSpectrum():
+			DSpectrum<1, DPeakArray<1, PeakT> >(),
+			SpectrumSettings(),
+			PersistentObject()
+		{
 
-			/**
-				@brief Fast search for peak range end (returns the path-the-end iterator)
-				
-				@note Make sure the spectrum is sorted with respect to m/z ratio. Otherwise the result is undefined.
-			*/
-			const typename BaseSpectrum::ConstIterator MZEnd(double mz) const
-			{
-				PeakType p;
-				p.getPosition()[0] = mz;
-				return upper_bound(BaseSpectrum::begin(), BaseSpectrum::end(), p, typename PeakType::PositionLess());
-			}
-			
-		protected:
-			// Docu in base class
-	    virtual void clearChildIds_()
-	    {
-	    	//TODO Persistence
-	    };	
-			
-  };
-	
+		}
+		/// Copy constructor
+		MSSpectrum(const MSSpectrum& source):
+			DSpectrum<1, DPeakArray<1, PeakT> >(source),
+			SpectrumSettings(source),
+			PersistentObject(source)
+		{
+
+		}
+		/// Destructor
+		~MSSpectrum()
+		{
+
+		}
+
+		// Assignment operator
+		MSSpectrum& operator= (const MSSpectrum& source)
+		{
+			if (&source == this) return *this;
+
+			DSpectrum<1, DPeakArray<1, PeakT> >::operator=(source);
+			SpectrumSettings::operator=(source);
+			PersistentObject::operator=(source);
+			return *this;
+		}
+
+		/// Equality operator
+		bool operator== (const MSSpectrum& rhs) const
+		{
+			return
+				SpectrumSettings::operator==(rhs) &&
+				DSpectrum<1, DPeakArray<1, PeakT> >::operator==(rhs)
+				;
+		}
+		/// Equality operator
+		bool operator!= (const MSSpectrum& rhs) const
+		{
+			return !(operator==(rhs));
+		}
+
+		/**
+			 @brief Fast search for peak range begin
+
+			 @note Make sure the spectrum is sorted with respect to m/z ratio! Otherwise the result is undefined.
+		*/
+		typename BaseSpectrum::Iterator MZBegin(double mz)
+		{
+			PeakType p;
+			p.getPosition()[0] = mz;
+			return lower_bound(BaseSpectrum::begin(), BaseSpectrum::end(), p, typename PeakType::PositionLess());
+		}
+
+		/**
+			 @brief Fast search for peak range end (returns the past-the-end iterator)
+
+			 @note Make sure the spectrum is sorted with respect to m/z ratio. Otherwise the result is undefined.
+		*/
+		typename BaseSpectrum::Iterator MZEnd(double mz)
+		{
+			PeakType p;
+			p.getPosition()[0] = mz;
+			return upper_bound(BaseSpectrum::begin(), BaseSpectrum::end(), p, typename PeakType::PositionLess());
+		}
+
+		/**
+			 @brief Fast search for peak range begin
+
+			 @note Make sure the spectrum is sorted with respect to m/z ratio! Otherwise the result is undefined.
+		*/
+		const typename BaseSpectrum::ConstIterator MZBegin(double mz) const
+		{
+			PeakType p;
+			p.getPosition()[0] = mz;
+			return lower_bound(BaseSpectrum::begin(), BaseSpectrum::end(), p, typename PeakType::PositionLess());
+		}
+
+		/**
+			 @brief Fast search for peak range end (returns the past-the-end iterator)
+
+			 @note Make sure the spectrum is sorted with respect to m/z ratio. Otherwise the result is undefined.
+		*/
+		const typename BaseSpectrum::ConstIterator MZEnd(double mz) const
+		{
+			PeakType p;
+			p.getPosition()[0] = mz;
+			return upper_bound(BaseSpectrum::begin(), BaseSpectrum::end(), p, typename PeakType::PositionLess());
+		}
+
+	 protected:
+		// Docu in base class
+		virtual void clearChildIds_()
+		{
+			//TODO Persistence
+		};
+
+	};
+
 	///Print the contents to a stream.
 	template <typename PeakT>
-	std::ostream& operator << (std::ostream& os, const MSSpectrum<PeakT>& spec)	
+	std::ostream& operator << (std::ostream& os, const MSSpectrum<PeakT>& spec)
 	{
 		os << "-- MSSPECTRUM BEGIN --"<<std::endl;
 
 		//spectrum settings
 		os <<static_cast<const SpectrumSettings&>(spec);
-		
+
 		//peaklist
 		os <<static_cast<const DSpectrum<1, DPeakArray<1,PeakT> >&>(spec);
 
 		os << "-- MSSPECTRUM END --"<<std::endl;
-	
+
 		return os;
 	}
 
