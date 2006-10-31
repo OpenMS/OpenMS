d642cac3056b772761273399a7eda2e1e9c68b5d
Clemens Gröpl <groepl@users.sourceforge.net>
2006-10-31 23:02:18 +0000

    FeaturePairSplitter -dump will create a Gnuplot script along with the dump
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@762 6adb6e08-d915-0410-941f-83917bcadc18

--- source/APPLICATIONS/TOPP/FeaturePairSplitter.C
+++ source/APPLICATIONS/TOPP/FeaturePairSplitter.C
@@ -47,26 +47,28 @@ using namespace std;
 
 /**
 	@page FeaturePairSplitter FeaturePairSplitter
-	
+
 	@brief Splits a feature pair file into two feature files.
 
 	This is just a small utility.  The features are copied from the pairs.  The
 	relative order of features is preserved.  For example the first two features
 	of each output file belong to each other, then the second two, and so on.
-	The <i>quality</i> information of the feature pairs is written to a third
+	The <i>quality</i> information of the feature pairs can be written to a third
 	file.
-	
+
   The input file is parsed by DFeaturePairsFile; a typical file name extension
   would be ".pairs.xml".
 
   The two output files are written by DFeatureMapFile; a typical file name
   extension would be '.feat.xml'.
-	
+
 	The qualities are written one per line; a typical file name extension would be '.txt'.
+
+	The dump option generates output suitable to be run through Gnuplot.
 
 	@note All options needed to operate this tool can be given in the command
 	line, so no INI file is needed.
-	
+
 	@ingroup TOPP
 
 */
@@ -87,7 +89,7 @@ class TOPPFeaturePairSplitter
   {
     cerr << endl
 				 << getToolName() << " -- split a feature pairs file into two feature files and a qualities file." << endl
-				 << "Version: " << VersionInfo::getVersion() << endl << 
+				 << "Version: " << VersionInfo::getVersion() << endl <<
 			"\n"
 			"Usage:\n"
 			"  " << getToolName() << " [-in <file>] [-out1 <file>] [-out2 <file>] [-qual <file>] [-dump <file>] [-ini <file>] [-log <file>] [-n <int>] [-d <level>]\n\n"
@@ -96,7 +98,7 @@ class TOPPFeaturePairSplitter
 			"  -out1 <file>      first feature output file\n"
 			"  -out2 <file>      second feature output file\n"
 			"  -qual <file>      pair qualtities output file\n"
-			"  -dump <file>      pair dump output file\n"
+			"  -dump <file>      pair dump output file (writes two files: <file> and <file>.gp)\n"
 			"All output options are optional.\n"
 				 << endl;
   }
@@ -111,7 +113,7 @@ class TOPPFeaturePairSplitter
 			"  out1   first feature output file\n"
 			"  out2   second feature output file\n"
 			"  qual   pair qualtities output file\n"
-			"  dump   pair dump output file\n"
+			"  dump   pair dump output file (writes two files: <file> and <file>.gp)\n"
 			"\n"
 			"INI File example section:\n"
 			"  <ITEM name=\"in\" value=\"pairs.xml\" type=\"string\"/>\n"
@@ -179,7 +181,7 @@ class TOPPFeaturePairSplitter
 			if ( write_out1 )
 				first_feature_map.push_back(iter->getFirst());
 			if ( write_out2 )
-			second_feature_map.push_back(iter->getSecond());
+				second_feature_map.push_back(iter->getSecond());
 			if ( write_qual )
 				qualities_vector.push_back(iter->getQuality());
 		}
@@ -200,8 +202,8 @@ class TOPPFeaturePairSplitter
 		{
 			ofstream qualities_file(qual.c_str());
 			copy(qualities_vector.begin(),qualities_vector.end(),
-								ostream_iterator<double>(qualities_file,"\n")
-							 );
+					 ostream_iterator<double>(qualities_file,"\n")
+					);
 		}
 		if ( write_dump )
 		{
@@ -210,17 +212,21 @@ class TOPPFeaturePairSplitter
 					RT = DimensionDescription < DimensionDescriptionTagLCMS >::RT,
 					MZ = DimensionDescription < DimensionDescriptionTagLCMS >::MZ
 				};
-			
+
 			ofstream dump_file(dump.c_str());
+			std::string dump_gp = dump + ".gp";
 
-			dump_file << "# rt1 mz1 it1  rt2 mz2 it2  qual\n";
-
+			dump_file <<
+				"# " << dump << " generated " << Date::now() << ".\n"
+				"# Use 'gnuplot " << dump_gp << "' to view.\n"
+				"# num  rt1 mz1 it1  rt2 mz2 it2  qual\n";
 			for ( DFeaturePairVector<2>::ConstIterator iter = feature_pairs.begin();
 						iter != feature_pairs.end();
 						++iter
 					)
 			{
 				dump_file
+					<< iter - feature_pairs.begin() << ' '
 					<< iter -> getFirst() . getPosition()[RT] << ' '
 					<< iter -> getFirst() . getPosition()[MZ] << ' '
 					<< iter -> getFirst() . getIntensity() << "  "
@@ -229,13 +235,23 @@ class TOPPFeaturePairSplitter
 					<< iter -> getSecond() . getIntensity() << "  "
 					<< iter -> getQuality() << '\n';
 			}
+			dump_file << "# " << dump_gp << " EOF " << Date::now() << std::endl;
 
+			std::ofstream dump_file_gp(dump_gp.c_str());
+			dump_file_gp << "# " << dump_gp << " generated " << Date::now() << std::endl;
+			dump_file_gp <<
+				"# Gnuplot script to view feature pairs\n"
+				"plot   \"" << dump <<"\" using 2:3 title \"map 1\"\n"
+				"replot \"" << dump <<"\" using 5:6 title \"map 2\"\n"
+				"replot \"" << dump <<"\" using 2:3:($5-$2):($6-$3) w vectors nohead title \"pairs\"\n"
+				;
+			dump_file_gp << "# " << dump_gp << " EOF " << Date::now() << std::endl;
 		}
 
 		return EXECUTION_OK;
 
 	} // main_()
-	
+
 }; // TOPPFeaturePairSplitter
 
 
