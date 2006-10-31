a6843e4aaea153f349c8afca44d4da4ebc4208ea
Clemens Gröpl <groepl@users.sourceforge.net>
2006-10-31 23:31:59 +0000

    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@767 6adb6e08-d915-0410-941f-83917bcadc18

--- source/TEST/ClassTest_test.C
+++ source/TEST/ClassTest_test.C
@@ -79,7 +79,7 @@ CHECK()
 		std::string f_dummy( double d, float f, int i, unsigned u, long l, unsigned long ul, char c) { return __PRETTY_FUNCTION__; }
 	} dummy;
 
-STATUS("\n\n\tExample for usage of __PRETTY_FUNCTION__ inside a member function of a nested class in main():\n\t" << dummy.f_dummy(1., 2.F, 3, 4U, 5L, 6UL, '\008') << '\n')
+	STATUS("\n\n\tExample for usage of __PRETTY_FUNCTION__ inside a member function of a nested class in main():\n\t" << dummy.f_dummy(0,0,0,0,0,0,0,0) << '\n')
 RESULT
 
 CHECK()
