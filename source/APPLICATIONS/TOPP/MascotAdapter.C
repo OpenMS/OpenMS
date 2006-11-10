61fa902a6965b44d11ad6c9e922934b86eb23d22
Marc Sturm <marc_sturm@users.sourceforge.net>
2006-11-10 16:02:53 +0000

    docu changes
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@820 6adb6e08-d915-0410-941f-83917bcadc18

--- source/APPLICATIONS/TOPP/MascotAdapter.C
+++ source/APPLICATIONS/TOPP/MascotAdapter.C
@@ -201,14 +201,12 @@ class TOPPMascotAdapter
 					 << " " << getToolName() << " [options]" << endl
 					 << endl
 					 << "Options are:" << endl
-					 << "  -in <file>           input file in mzData/Mascot resultsfile " 
-					 << "(default read from INI file)" << endl
-					 << "  -out <file>          output file in analysisXML/Mascot generic format "
-					 << "(default read from INI file)" << endl
-					 << "  -mascot_in           if this flag is set the MascotAdapter will read in "
-					 << "mzData and write Mascot generic format" << endl
-					 << "  -mascot_out          if this flag is set the MascotAdapter will read in "
-					 << "a Mascot resultsfile and write analysisXML." << endl
+					 << "  -in <file>           input file in mzData format."
+					 << "                       Note: In mode 'mascot_out' a Mascot results file is read." 
+					 << "  -out <file>          output file in analysisXML format."
+					 << "                       Note: In mode 'mascot_in' Mascot generic format is written."
+					 << "  -mascot_in           if this flag is set the MascotAdapter will read in mzData and write Mascot generic format." << endl
+					 << "  -mascot_out          if this flag is set the MascotAdapter will read in a Mascot resultsfile and write analysisXML." << endl
 					 << "  -instr               the instrument that was used to measure the spectra (default read from INI file)" << endl
 					 <<	"  -prcr_m_tol          the precursor mass tolerance (default read from INI file)" << endl
 					 << "  -pk_m_tol            the peak mass tolerance (default read from INI file)" << endl
