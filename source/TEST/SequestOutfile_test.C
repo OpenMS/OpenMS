86207b39515516268851f825954fe78564eea515
Martin Langwisch <martinlangwisch@users.sourceforge.net>
2006-12-13 10:15:20 +0000

    changed test for getColumns
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@1048 6adb6e08-d915-0410-941f-83917bcadc18

--- source/TEST/SequestOutfile_test.C
+++ source/TEST/SequestOutfile_test.C
@@ -109,6 +109,10 @@ CHECK(bool getColumns(const String& line, vector< String >& substrings, Unsigned
 	TEST_EQUAL(file.getColumns("", substrings, 12, 10), false)
 	TEST_EQUAL(file.getColumns(line, substrings, 12, 10), true)
 	TEST_EQUAL((columns == substrings), true)
+	
+	line = "  1.   1/80          0 1967.0013  0.0000  1.5789   310.3 0.05    0 18/64 gi|544379|sp|P35574|GDE RABIT+2   C.ETQAWSIATILETLYDL.-";
+	TEST_EQUAL(file.getColumns(line, substrings, 12, 10), true)
+	TEST_EQUAL((columns == substrings), true)
 RESULT
 
 CHECK(void getSequences(const String& database_filename, const map< String, UnsignedInt >& ac_position_map, vector< String >& sequences, vector< pair< String, UnsignedInt > >& found, map< String, UnsignedInt >& not_found) throw (Exception::FileNotFound))
