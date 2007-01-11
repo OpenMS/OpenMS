631b097a85208eea6da494f4ff6599bb37ffc179
Clemens Gröpl <groepl@users.sourceforge.net>
2007-01-11 22:11:05 +0000

    uses TOPPBase2 now :-)
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@1197 6adb6e08-d915-0410-941f-83917bcadc18

--- source/APPLICATIONS/TOPP/MapMatcher.C
+++ source/APPLICATIONS/TOPP/MapMatcher.C
@@ -22,6 +22,7 @@
 //
 // --------------------------------------------------------------------------
 // $Maintainer: Clemens Groepl $
+// Initial version by Ole Schulz-Trieglaff
 // --------------------------------------------------------------------------
 
 #include <OpenMS/FORMAT/Param.h>
@@ -34,7 +35,7 @@
 #include <OpenMS/FORMAT/DGridFile.h>
 #include <OpenMS/FORMAT/DFeaturePairsFile.h>
 
-#include <OpenMS/APPLICATIONS/TOPPBase.h>
+#include <OpenMS/APPLICATIONS/TOPPBase2.h>
 
 #include <map>
 #include <iostream>
@@ -50,165 +51,101 @@ using namespace std;
 
 /**
 	@page MapMatcher MapMatcher
-	
+
 	@brief Computes a transformation for a list of feature pairs.
-	
+
 	This is the second step in the map matching workflow. This application
 	takes a list of feature pairs as computed by the FeatureMatcher and
-	a grid (partially) covering the LC/MS map. For each grid cell, a 
+	a grid (partially) covering the LC/MS map. For each grid cell, a
 	transformation is computed that maps the feature partners on each
 	other. Currently, this transformation is linear.
-	
+
 	The output of this application is the list of grid cells with the
 	estimated transformation.
-	
-		
+
 */
 
 // We do not want this class to show up in the docu:
 /// @cond TOPPCLASSES
 
 class TOPPMapMatcher
-            : public TOPPBase
+	: public TOPPBase2
 {
 
-public:
-    TOPPMapMatcher()
-            : TOPPBase("MapMatcher")
-    {}
+ public:
+	TOPPMapMatcher()
+		: TOPPBase2("MapMatcher", "estimate a transformation to map a list of pairs of features in different LC/MS maps onto each other")
+	{}
 
-protected:
+ protected:
 
-    void printToolUsage_() const
-    {
-        cerr << endl
-        << getToolName() << " -- estimates a transformation for pairs of features in different LC/MS maps" << endl
-        << "Version: " << VersionInfo::getVersion() << endl
-        << endl
-        << "Usage:" << endl
-        << " " << getToolName() << " [options]" << endl
-        << endl
-        << "Options are:" << endl
-        << " -grid <file>   grid covering the map to be transformed (default read from INI file)" << endl
-        << " -pairs <file>  feature pairs (default read from INI file)" << endl
-        << " -q <float>  	 minimum quality of pairs considered (default read from INI file)" << endl
-        << " -out <file>  	 output file (default read from INI file)" << endl
-        << endl ;
+	void registerOptionsAndFlags_()
+	{
+		registerStringOption_("pairs","<file>","","input feature pairs file");
+		registerStringOption_("grid","<file>","","input grid file");
+		registerStringOption_("out","<file>","","output grid file");
+		registerDoubleOption_("min_quality","<double>",0,"minimum quality of pairs considered",false);
+	}
 
-    }
+	ExitCodes main_(int , char**)
+	{
+		//-----------------------------------------------------------
+		// parsing parameters
+		//-------------------------------------------------------------
 
-    void printToolHelpOpt_() const
-    {
-        cerr << endl
-        << getToolName() << endl
-        << endl
-        << "INI options:" << endl
-        << "  in <file>                  either feat or mzData (default read from INI file)" << endl
-        << "  out <file>                output mzData file name" << endl
-        << "  in_type <file_type>  either feat or mzData (default read from INI file)" << endl
-        << endl
-        << "INI File example section:" << endl
-        << "<ITEM name=\"pairs\" value=\"04111717_pairs.xml\" type=\"string\"/>" << endl
-        << "<ITEM name=\"grid\" value=\"the_grid.xml\" type=\"string\"/>" << endl
-        << "<ITEM name=\"min_quality\" value=\"0.5\" type=\"float\"/>" << endl
-        << "<ITEM name=\"out\" value=\"grid_wtransform.xml\" type=\"string\"/>" << endl;
-    }
+		String grid_filename = getStringOption_("grid");
+		inputFileReadable_(grid_filename);
 
-    void setOptionsAndFlags_()
-    {
-        options_["-grid"] = "grid";
-        options_["-pairs"] = "pairs";
-        options_["-out"] = "out";
-        options_["-q"] = "min_quality";
-    }
+		String pairs_filename = getStringOption_("pairs");
+		inputFileReadable_(pairs_filename);
 
-    ExitCodes main_(int , char**)
-    {
-        //-----------------------------------------------------------
-        // parsing parameters
-        //-------------------------------------------------------------
-        //File names
-        String gridfile, pairsfile, outfile;
+		String out_filename = getStringOption_("out");
+		outputFileWritable_(out_filename);
 
-        gridfile = getParamAsString_("grid");
-        writeDebug_(String("Grid file: ") + gridfile, 1);
+		double min_quality = getDoubleOption_("min_quality");
 
-        pairsfile = getParamAsString_("pairs");
-        writeDebug_(String("Pairs file: ") + pairsfile, 1);
+		//-------------------------------------------------------------
+		// reading input
+		//-------------------------------------------------------------
 
-        outfile = getParamAsString_("out");
-        writeDebug_(String("Output file: ") + outfile, 1);
+		DGridFile grid_file;
+		DGrid<2> grid;
+		writeLog_("Reading grid file " + grid_filename );
+		grid_file.load(grid_filename, grid);
 
-        //parameters
-        double min_quality = 0;
+		DFeaturePairsFile pairs_file;
+		DFeaturePairVector<2> pairs_vector;
+		writeLog_("Reading pairs file " + pairs_filename );
+		pairs_file.load(pairs_filename, pairs_vector);
 
-        //length of the structuring element
-        String qualstr = getParamAsString_("min_quality");
-        writeDebug_(String("min_quality") + qualstr, 1);
+		//-------------------------------------------------------------
+		// calculations
+		//-------------------------------------------------------------
+		DMapMatcherRegression<> map_matcher;
+		map_matcher.setFeaturePairs(pairs_vector);
+		map_matcher.setGrid(grid);
+		map_matcher.setMinQuality(min_quality);
 
-        try
-        {
-            //resampling
-            if (qualstr != "")
-            {
-                min_quality = qualstr.toFloat();
-            }
-        }
-        catch(Exception::ConversionError& e)
-        {
-            writeLog_(String("Invalid value for the minimum quality '") + qualstr  + "' given. Aborting!");
-            printUsage_();
-            return ILLEGAL_PARAMETERS;
-        }
+		// action!
+		map_matcher.estimateTransform();
 
-			  writeLog_(String("Reading grid file ") + gridfile );
-				writeLog_(String("Reading pairs file ") + pairsfile );
-		
-        //-------------------------------------------------------------
-        // reading input
-        //-------------------------------------------------------------
+		//-------------------------------------------------------------
+		// writing output
+		//-------------------------------------------------------------
 
-        DGridFile grid_file;
-        DGrid<2> the_grid;
-        grid_file.load(gridfile,the_grid);
+		DGrid<2> const & grid_with_transform = map_matcher.getGrid();
 
-        DFeaturePairsFile pairs_file;
-        DFeaturePairVector<2> pairs_vector;
-        pairs_file.load(pairsfile,pairs_vector);
+		grid_file.store(out_filename, grid_with_transform);
 
-        //-------------------------------------------------------------
-        // calculations
-        //-------------------------------------------------------------
-        DMapMatcherRegression<> map_matcher;
-        map_matcher.setFeaturePairs(pairs_vector);
-        map_matcher.setGrid(the_grid);
-        map_matcher.setMinQuality(min_quality);
+		return EXECUTION_OK;
 
-        map_matcher.estimateTransform();
-
-        //-------------------------------------------------------------
-        // writing output
-        //-------------------------------------------------------------
-
-        DGrid<2> grid_with_transform;
-        grid_with_transform = map_matcher.getGrid();
-
-        grid_file.store(outfile,grid_with_transform);
-
-        return EXECUTION_OK;
-
-    }
+	}
 };
 
 /// @endcond
 
 int main( int argc, char ** argv )
 {
-    TOPPMapMatcher tool;
-    return tool.main(argc,argv);
+	TOPPMapMatcher tool;
+	return tool.main(argc,argv);
 }
-
-
-
-
-
