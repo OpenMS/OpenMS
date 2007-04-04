2175e19d8b47a25ba7813ee2e2bdc709e07e1954
Stefan Rink <srink@users.sourceforge.net>
2007-04-04 13:24:05 +0000

    bugfix2
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@1851 6adb6e08-d915-0410-941f-83917bcadc18

--- source/VISUAL/ParamEditorDelegate.C
+++ source/VISUAL/ParamEditorDelegate.C
@@ -4,72 +4,74 @@
 #include <QtCore/QAbstractItemModel>
 #include <QtCore/QString>
 #include <QtCore/QStringList>
-#include "ParamEditorDelegate.h"
+namespace OpenMS
+{
+	#include <OpenMS/VISUAL/ParamEditorDelegate.h>
 
-ParamEditorDelegate::ParamEditorDelegate(QObject *parent)
-     : QItemDelegate(parent)
- {
- }
- 
- QWidget *ParamEditorDelegate::createEditor(QWidget *parent,
-     const QStyleOptionViewItem & option,
-     const QModelIndex & index ) const
- {
-	 QString str = index.model()->data(index, Qt::DisplayRole).toString();
-	 if(index.column()==1 &&str.isEmpty()) return 0;
-	 else if (index.column() == 2 && !str.isEmpty())
-	{
-		//  QVariant originalValue = index.model()->data(index, Qt::UserRole);
-		//if (!isSupportedType(originalValue.type()))
-		//  return 0;
-		QComboBox *editor = new QComboBox(parent);
-		QStringList list;
-		list<<"int"<<"float"<<"string";
-		editor->addItems(list);
-		return editor;
-	}
-	else if(index.column()==2 && str.isEmpty()) return 0;
-	 else return QItemDelegate::createEditor(parent,option,index);
-
- }
- 
- void ParamEditorDelegate::setEditorData(QWidget *editor,
-                                     const QModelIndex &index) const
- {
-	 if(index.column()==2)
+	ParamEditorDelegate::ParamEditorDelegate(QObject *parent)
+	     : QItemDelegate(parent)
 	 {
-     QString str = index.model()->data(index, Qt::DisplayRole).toString();
-	int pos = combo_list_.indexOf(str);
-	 QComboBox *combo = static_cast<QComboBox*>(editor);
-			if (pos!=-1)
-			{
-				combo->setCurrentIndex(pos);
-			}
+	 }
+	 
+	 QWidget *ParamEditorDelegate::createEditor(QWidget *parent,
+	     const QStyleOptionViewItem & option,
+	     const QModelIndex & index ) const
+	 {
+		 QString str = index.model()->data(index, Qt::DisplayRole).toString();
+		 if(index.column()==1 &&str.isEmpty()) return 0;
+		 else if (index.column() == 2 && !str.isEmpty())
+		{
+			//  QVariant originalValue = index.model()->data(index, Qt::UserRole);
+			//if (!isSupportedType(originalValue.type()))
+			//  return 0;
+			QComboBox *editor = new QComboBox(parent);
+			QStringList list;
+			list<<"int"<<"float"<<"string";
+			editor->addItems(list);
+			return editor;
 		}
-		else QItemDelegate::setEditorData(editor,index);
- }
- 
- void ParamEditorDelegate::setModelData(QWidget *editor, QAbstractItemModel *model,
-                                    const QModelIndex &index) const
- {
-	 if(index.column()==2)
-	 {
-     QComboBox *combo= static_cast<QComboBox*>(editor);
-     QString str = combo->currentText();
+		else if(index.column()==2 && str.isEmpty()) return 0;
+		 else return QItemDelegate::createEditor(parent,option,index);
 
-     model->setData(index, str);
 	 }
-	 else QItemDelegate::setModelData(editor,model,index);
- }
- 
- void ParamEditorDelegate::updateEditorGeometry(QWidget *editor,
-     const QStyleOptionViewItem &option, const QModelIndex & index) const
- {
-	 if(index.column()==2)
+	 
+	 void ParamEditorDelegate::setEditorData(QWidget *editor,
+					     const QModelIndex &index) const
 	 {
-     editor->setGeometry(option.rect);
+		 if(index.column()==2)
+		 {
+	     QString str = index.model()->data(index, Qt::DisplayRole).toString();
+		int pos = combo_list_.indexOf(str);
+		 QComboBox *combo = static_cast<QComboBox*>(editor);
+				if (pos!=-1)
+				{
+					combo->setCurrentIndex(pos);
+				}
+			}
+			else QItemDelegate::setEditorData(editor,index);
 	 }
-	 else QItemDelegate::updateEditorGeometry(editor,option,index);
+	 
+	 void ParamEditorDelegate::setModelData(QWidget *editor, QAbstractItemModel *model,
+					    const QModelIndex &index) const
+	 {
+		 if(index.column()==2)
+		 {
+	     QComboBox *combo= static_cast<QComboBox*>(editor);
+	     QString str = combo->currentText();
+
+	     model->setData(index, str);
+		 }
+		 else QItemDelegate::setModelData(editor,model,index);
+	 }
+	 
+	 void ParamEditorDelegate::updateEditorGeometry(QWidget *editor,
+	     const QStyleOptionViewItem &option, const QModelIndex & index) const
+	 {
+		 if(index.column()==2)
+		 {
+	     editor->setGeometry(option.rect);
+		 }
+		 else QItemDelegate::updateEditorGeometry(editor,option,index);
+	 }
+	 
  }
- 
- 
