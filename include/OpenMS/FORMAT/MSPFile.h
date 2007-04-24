11c95bfaad8f11ff83305f5a3c41c2f36ea91600
Andreas Bertsch <andreas_bertsch@users.sourceforge.net>
2007-04-24 13:41:41 +0000

    added new support for the NIST MSP format; still uncomplete and the test does not test anything
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@1958 6adb6e08-d915-0410-941f-83917bcadc18

---
+++ include/OpenMS/FORMAT/MSPFile.h
@@ -0,0 +1,135 @@
+// -*- Mode: C++; tab-width: 2; -*-
+// vi: set ts=2:
+//
+// --------------------------------------------------------------------------
+//                   OpenMS Mass Spectrometry Framework
+// --------------------------------------------------------------------------
+//  Copyright (C) 2003-2007 -- Oliver Kohlbacher, Knut Reinert
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// --------------------------------------------------------------------------
+// $Maintainer: Andreas Bertsch $
+// --------------------------------------------------------------------------
+
+#ifndef OPENMS_FORMAT_MSPFILE_H
+#define OPENMS_FORMAT_MSPFILE_H
+
+#include <OpenMS/FORMAT/TextFile.h>
+#include <OpenMS/FORMAT/HANDLERS/MzDataHandler.h>
+#include <OpenMS/CONCEPT/ProgressLogger.h>
+#include <OpenMS/METADATA/Identification.h>
+#include <cctype>
+
+namespace OpenMS
+{
+	/**
+		@brief File adapter for MzData files
+	
+		@TODO add ProgressLogger to File? (andreas)
+		@TODO is TextFile a good idea? (andreas)
+		@ingroup FileIO
+	*/
+	class MSPFile 
+		:	public TextFile/*,
+			public ProgressLogger*/
+	{
+		public:
+
+			///Default constructor
+			MSPFile();
+
+			///Destructor
+			virtual ~MSPFile();
+			
+			/**
+				@brief Loads a map from a MSPFile file.
+
+				@p map has to be a MSExperiment or have the same interface.
+			*/
+			template <typename MapType>
+			void load(const String& filename, std::vector<IdentificationData>& ids, MapType& map) throw (Exception::FileNotFound, Exception::ParseError)
+			{
+				TextFile::load(filename, true); // load a trimmed version of the file
+				typename MapType::SpectrumType spec;
+				for (ConstIterator it = begin(); it != end(); ++it)
+				{
+					String line = *it;
+
+					if (line.hasPrefix("Name:"))
+					{
+						std::vector<String> split, split2;
+						line.split(' ', split);
+						split[1].split('/', split2);
+						Identification id;
+						id.getPeptideHits().push_back(PeptideHit(0, "", 0, split2[1].toInt(), split2[0]));
+						IdentificationData data;
+						data.id = id;
+						ids.push_back(data);
+						std::cerr << split[1] << std::endl;
+					}
+					if (line.hasPrefix("MW:"))
+					{
+						// skip that as it is not necessary and might not be available at all
+					}
+					if (line.hasPrefix("Comment:"))
+					{
+						//spec.setMetaValue("MSPComment", line);
+					}
+					if (line.hasPrefix("Num peaks:"))
+					{
+						++it;
+						if (it != end() && *it != "")
+						{
+							line = *it;
+							while (line.size() > 0 && std::isdigit(line[0]) && it != end() && *it != "")
+							{
+								line = *it;
+								std::vector<String> split;
+								line.split('\t', split);
+								typename MapType::SpectrumType::PeakType peak;
+								if (split.size() != 3)
+								{
+									throw Exception::ParseError(__FILE__, __LINE__, __PRETTY_FUNCTION__, line, "not <mz> <intensity> <comment>");
+								}
+								peak.setMZ(split[0].toFloat());
+								peak.setIntensity(split[1].toFloat());
+								//peak.setMetaValue("MSPPeakInfo", split[2]);
+								++it;
+								spec.push_back(peak);
+							}
+						}
+						map.push_back(spec);
+						spec.clear();
+					}
+				}
+			}
+
+			/**
+				@brief Stores a map in a MSPFile file.
+
+				@p map has to be a MSExperiment or have the same interface.
+			*/
+			template <typename MapType>
+			void store(const String& filename, const MapType& map)
+			const throw (Exception::UnableToCreateFile)
+			{
+			}
+		
+	};
+
+} // namespace OpenMS
+
+#endif // OPENMS_FORMAT_MSPFILE_H
---
+++ source/TEST/MSPFile_test.C
@@ -0,0 +1,74 @@
+// -*- Mode: C++; tab-width: 2; -*-
+// vi: set ts=2:
+//
+// --------------------------------------------------------------------------
+//                   OpenMS Mass Spectrometry Framework
+// --------------------------------------------------------------------------
+//  Copyright (C) 2003-2007 -- Oliver Kohlbacher, Knut Reinert
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// --------------------------------------------------------------------------
+// $Maintainer: Andreas Bertsch $
+// --------------------------------------------------------------------------
+
+#include <OpenMS/CONCEPT/ClassTest.h>
+///////////////////////////
+
+#include <OpenMS/FORMAT/MSPFile.h>
+//#include <OpenMS/KERNEL/MSExperiment.h>
+//#include <OpenMS/KERNEL/MSExperimentExtern.h>
+//#include <OpenMS/KERNEL/StandardTypes.h>
+//#include <OpenMS/FORMAT/AnalysisXMLFile.h>
+//#include <OpenMS/FORMAT/MzDataFile.h>
+
+using namespace OpenMS;
+using namespace std;
+
+///////////////////////////
+
+START_TEST(MSPFile, "$Id: MSPFile_test.C 1911 2007-04-13 09:20:40Z marc_sturm $")
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+MSPFile* ptr = 0;
+CHECK((MSPFile()))
+	ptr = new MSPFile;
+	TEST_NOT_EQUAL(ptr, 0)
+RESULT
+
+CHECK((~MSPFile()))
+	delete ptr;
+RESULT
+
+CHECK((template<typename MapType> void load(const String& filename, MapType& map) throw(Exception::FileNotFound, Exception::ParseError)))
+	/*MSPFile msp_file;
+	PeakMap map;
+	vector<IdentificationData> ids;
+	msp_file.load("/home/andreas/DATA/NIST_PEPLIB/LIBS_TXT/human.msp", ids, map);
+	cerr << "#ids=" << ids.size() << endl;
+	AnalysisXMLFile analysis_file;
+	analysis_file.store("human.analysisXML", vector<ProteinIdentification>(), ids);
+
+
+	MzDataFile map_file;
+	map_file.store("human.mzData", map);
+*/
+RESULT
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+END_TEST
