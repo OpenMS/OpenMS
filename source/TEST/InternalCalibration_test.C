3488a66e97c52f7a304d1d9a5473404820d4eb51
Alexandra Zerck <azerck@users.sourceforge.net>
2007-04-24 09:09:42 +0000

    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@1952 6adb6e08-d915-0410-941f-83917bcadc18

---
+++ source/TEST/InternalCalibration_test.C
@@ -0,0 +1,154 @@
+// -*- Mode: C++; tab-width: 2; -*-
+// vi: set ts=2:
+//
+// --------------------------------------------------------------------------
+//                   OpenMS Mass Spectrometry Framework 
+// --------------------------------------------------------------------------
+//  Copyright (C) 2003-2007 -- Oliver Kohlbacher, Knut Reinert
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// --------------------------------------------------------------------------
+// $Maintainer: Alexandra Zerck $
+// --------------------------------------------------------------------------
+//
+
+#include <OpenMS/CONCEPT/ClassTest.h>
+
+///////////////////////////
+
+#include <OpenMS/FILTERING/CALIBRATION/InternalCalibration.h>
+#include <OpenMS/FORMAT/MzDataFile.h>
+
+using namespace OpenMS;
+using namespace std;
+
+///////////////////////////
+
+START_TEST(InternalCalibration, "$Id$")
+
+/////////////////////////////////////////////////////////////
+
+InternalCalibration* ptr = 0;
+CHECK((InternalCalibration()))
+  ptr = new InternalCalibration;
+  TEST_NOT_EQUAL(ptr, 0)
+RESULT
+
+CHECK((~InternalCalibration()))
+  delete ptr;
+RESULT
+
+ptr = new InternalCalibration();
+
+CHECK((InternalCalibration(const InternalCalibration& source)))
+  InternalCalibration copy(*ptr);
+  TEST_EQUAL(copy.getPeaks()== ptr->getPeaks(),true)
+  TEST_EQUAL(copy.getMonoisotopicPeaks()==ptr->getMonoisotopicPeaks(),true )
+RESULT
+
+CHECK((InternalCalibration& operator=(const InternalCalibration& source)))
+  InternalCalibration copy;
+  copy = *ptr;
+  TEST_EQUAL(copy.getPeaks()== ptr->getPeaks(),true)
+  TEST_EQUAL(copy.getMonoisotopicPeaks()==ptr->getMonoisotopicPeaks(),true )
+RESULT
+
+CHECK((const MSExperiment<PickedPeakType>& getPeaks() const))
+  MSExperiment<PickedPeak1D> exp;
+  MSSpectrum<PickedPeak1D> spec;
+  PickedPeak1D peak;
+  peak.setMZ(100.1);
+  spec.push_back(peak);
+  peak.setMZ(102.1);
+  spec.push_back(peak);
+  exp.push_back(spec);
+  ptr->setPeaks(exp);
+  TEST_EQUAL(ptr->getPeaks()== exp,true)
+RESULT
+  
+CHECK((void setPeaks( MSExperiment<PickedPeakType>& exp) ))
+  MSExperiment<PickedPeak1D> exp;
+  MSSpectrum<PickedPeak1D> spec;
+  PickedPeak1D peak;
+  peak.setMZ(100.1);
+  spec.push_back(peak);
+  peak.setMZ(102.1);
+  spec.push_back(peak);
+  exp.push_back(spec);
+  
+  ptr->setPeaks(exp);
+  TEST_EQUAL(ptr->getPeaks()==exp,true)
+RESULT
+
+CHECK((const std::vector<std::vector<UInt> >& getMonoisotopicPeaks() const))
+  std::vector<std::vector<UInt> > p;
+  std::vector<UInt> vec;
+  vec.push_back(1);
+  vec.push_back(2);
+  vec.push_back(3);
+  p.push_back(vec);
+  
+  p.push_back(vec);
+  ptr->setMonoisotopicPeaks(p);
+  TEST_EQUAL(ptr->getMonoisotopicPeaks()== p,true)
+RESULT
+  
+CHECK((void setMonoisotopicPeaks( std::vector<std::vector<UInt> >& p) ))
+  std::vector<std::vector<UInt> > p;
+  std::vector<UInt> vec;
+  vec.push_back(1);
+  vec.push_back(2);
+  vec.push_back(3);
+  p.push_back(vec);
+  ptr->setMonoisotopicPeaks(p);
+  TEST_EQUAL(ptr->getMonoisotopicPeaks()==p,true)
+RESULT
+
+
+CHECK( template<typename InputPeakType>
+       void calibrate(MSExperiment<InputPeakType>& exp, std::vector<double>& ref_masses) )
+  PRECISION(0.000001)
+  MSExperiment<RawDataPoint1D> exp;
+  MSExperiment<PickedPeak1D> exp_peaks;
+  MzDataFile file;
+  file.load("data/InternalCalibration_test.mzData",exp);
+  std::vector<double> ref_masses;
+  ref_masses.push_back(1296.68476942);
+  ref_masses.push_back(2465.19833942);
+
+  ptr->setPeakBound(800);
+  ptr->calibrate(exp,ref_masses);
+// hier muss nun noch geprüft werden, ob zumindest an den 2 calibranten der fehler null ist
+  PeakPickerCWT pp;
+  pp.setPeakCorrBound(0.0);
+  pp.setPeakBound(800);
+  pp.setFwhmBound(0.0);
+  pp.pickExperiment(exp,exp_peaks);
+  PickedPeak1D peak;
+  peak.setMZ(1296.68476942);
+  MSSpectrum<PickedPeak1D>::Iterator it = lower_bound(exp_peaks[0].begin(),exp_peaks[0].end(),peak,RawDataPoint1D::PositionLess());
+  --it;
+  TEST_REAL_EQUAL(it->getMZ(),1296.68476942)
+  peak.setMZ(2465.19833942);
+  it = lower_bound(exp_peaks[0].begin(),exp_peaks[0].end(),peak,RawDataPoint1D::PositionLess());
+  --it;
+  TEST_REAL_EQUAL(it->getMZ(),2465.19833942)
+RESULT
+  
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+END_TEST
