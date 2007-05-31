6f3714b1bdd1c0888215ce730834b6ce585d1750
Alexandra Zerck <azerck@users.sourceforge.net>
2007-05-31 18:23:42 +0000

    added ExternalCalibration
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@2116 6adb6e08-d915-0410-941f-83917bcadc18

---
+++ source/APPLICATIONS/TOPP/ExternalCalibration.C
@@ -0,0 +1,221 @@
+// -*- Mode: C++; tab-width: 2; -*-
+// vi: set ts=2:
+//
+// --------------------------------------------------------------------------
+//                   OpenMS Mass Spectrometry Framework
+// --------------------------------------------------------------------------
+//  Copyright (C) 2003-2007 -- Oliver Kohlbacher, Knut Reinert
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// --------------------------------------------------------------------------
+// $Maintainer: Alexandra Zerck $
+// --------------------------------------------------------------------------
+
+#include <OpenMS/FORMAT/MzDataFile.h>
+#include <OpenMS/FORMAT/TextFile.h>
+#include <OpenMS/APPLICATIONS/TOPPBase.h>
+#include <OpenMS/FILTERING/CALIBRATION/ExternalCalibration.h>
+
+using namespace OpenMS;
+using namespace std;
+
+
+
+//-------------------------------------------------------------
+//Doxygen docu
+//-------------------------------------------------------------
+
+/**
+	 @page ExternalCalibration ExternalCalibration
+	 
+   @brief Performs an external calibration for tof spectra.
+
+	 Given one or more calibrant spectra containing flight times, the instrument's calibration constants and the
+	 expected masses the quadratic function y_i = a + b*x_i + c*x_i^2 is fitted, where x_i is the ith flight time.
+	 If there are more than one calibrant spectra the coefficients a, b and c are averaged. The fitted function is
+	 then used to convert the flight times of the given experiment to m/z-values.
+
+	 You can choose to calibrate picked or raw data. If you use picked data, set the flag peak_data. If you have
+	 raw data an additional peak picking step for the calibrant spectra is needed, the parameters for the
+	 peak picker can be set in the ini-file.
+
+	
+   @ingroup TOPP
+*/
+
+// We do not want this class to show up in the docu:
+/// @cond TOPPCLASSES
+
+class TOPPExternalCalibration
+      : public TOPPBase
+ {
+ public:
+  TOPPExternalCalibration()
+    : TOPPBase("ExternalCalibration","Apply external calibration.")
+  {
+  }
+
+ protected:
+
+  void registerOptionsAndFlags_()
+  {
+    registerStringOption_("in","<input file>","","input mzData file (peak or raw data)");
+    registerStringOption_("out","<output file>","","output mzData file (peak or raw data)");
+		registerStringOption_("ext_calibrants","<input file>","","mzData file containing the external calibrant spectra (peak or raw data)");
+    registerStringOption_("ref_masses","<reference file>","","file containing reference masses of the external calibrant spectra (one per line)",true);
+		registerStringOption_("tof_const","<file>","","File containing TOF conversion constants."
+													" These can be either two or three constants\n" 
+													"per set, depending on the conversion type. Either one set for all calibrant spectra \n"
+													"(tab separated), or one for each spectrum.\n"
+													"For a detailed description, please have a look at the doxygen documentation."
+													"(one set, tab separated, per line)",true);
+		registerFlag_("peak_data","set this flag, if you have peak data, not raw data");
+		addText_("\nIf you want to calibrate raw data, it is necessary to perform a peak picking step before the "
+							"actual calibration is done. \nThe parameters for the peak picking step can be given "
+							"given in the 'algorithm' part of INI file in the subsection PeakPicker, e.g.:\n"
+							"<NODE name=\"algorithm\">\n"
+						  " <NODE name=\"PeakPicker\">\n"
+							"  <NODE name=\"wavelet_transform\">\n"
+							"    <ITEM name=\"scale\" value=\"0.2\" type=\"float\" />\n"
+							"  </NODE>\n"
+							"  <NODE name=\"thresholds\">\n"
+							"    <ITEM name=\"peak_bound\" value=\"100\" type=\"float\" />\n"
+							"    <ITEM name=\"correlation\" value=\"0.5\" type=\"float\" />\n"
+							"    <ITEM name=\"fwhm_bound\" value=\"0.1\" type=\"float\"/>\n"
+							"  </NODE>\n"
+						  " </NODE>\n"
+						  "</NODE>");
+		 addEmptyLine_();
+		 registerSubsection_("algorithm");
+  }
+
+  ExitCodes main_(int , char**)
+  {
+
+    //-------------------------------------------------------------
+    // parameter handling
+    //-------------------------------------------------------------
+
+    String in = getStringOption_("in");
+    String out = getStringOption_("out");
+		String in_calib = getStringOption_("ext_calibrants");
+    String ref = getStringOption_("ref_masses");
+		String conv = getStringOption_("tof_const");
+		bool peak_data = getFlag_("peak_data");
+    //-------------------------------------------------------------
+    // init ExternalCalibration
+    //-------------------------------------------------------------
+
+    ExternalCalibration calib;
+		calib.setLogType(log_type_);
+		Param param = getParam_().copy("algorithm:",true);
+		calib.setParameters(param);
+    //-------------------------------------------------------------
+    // loading input
+    //-------------------------------------------------------------
+    MSExperiment<RawDataPoint1D > ms_exp_calib,ms_exp_raw;
+		MSExperiment<PickedPeak1D > ms_exp_p,ms_exp_calib_p;
+		MzDataFile mz_data_file;
+		mz_data_file.setLogType(log_type_);
+		if(peak_data)
+			{
+				mz_data_file.load(in_calib,ms_exp_calib_p);
+				mz_data_file.load(in,ms_exp_p);
+			}
+		else
+			{
+				mz_data_file.load(in_calib,ms_exp_calib);
+				mz_data_file.load(in,ms_exp_raw);
+			}
+		vector<double> ref_masses;
+		TextFile ref_file;
+		ref_file.load(ref,true);
+		
+		for(TextFile::Iterator iter = ref_file.begin(); iter != ref_file.end(); ++iter)
+			{
+				ref_masses.push_back(atof(iter->c_str()));
+			}
+		TextFile const_file;
+		const_file.load(conv,true);
+		std::vector<String> vec;
+		TextFile::Iterator iter = const_file.begin();
+		iter->split('\t',vec);
+
+		std::vector<double> ml1,ml2,ml3;
+		ml1.push_back(atof(vec[0].c_str()));
+		ml2.push_back(atof(vec[1].c_str()));
+		if(vec.size()==3)
+			{
+				ml3.push_back(atof(vec[2].c_str()));				
+			}
+		++iter;
+		
+		for(; iter != const_file.end(); ++iter)
+			{
+				iter->split('\t',vec);
+				ml1.push_back(atof(vec[0].c_str()));
+				ml2.push_back(atof(vec[1].c_str()));
+				if(vec.size()==3)
+					{
+						ml3.push_back(atof(vec[2].c_str()));				
+					}
+			}
+
+		if(ml1.size() != 1 && (!(peak_data && ml1.size() == ms_exp_calib_p.size()) && ml1.size() != ms_exp_calib.size()))
+			{
+				writeLog_("Incorrect number of calibration constants given. Aborting!");
+				return INPUT_FILE_CORRUPT;
+			}
+		calib.setML1s(ml1);
+		calib.setML2s(ml2);
+		if(!ml3.empty()) calib.setML3s(ml3);
+    //-------------------------------------------------------------
+    // perform calibration
+    //-------------------------------------------------------------
+		if(peak_data)
+			{
+				calib.calibrate(ms_exp_calib_p,ms_exp_p,ref_masses);
+			}
+		else calib.calibrate(ms_exp_calib,ms_exp_raw,ref_masses);
+    
+    //-------------------------------------------------------------
+    // writing output
+    //-------------------------------------------------------------
+		if(peak_data)
+			{
+				mz_data_file.store(out,ms_exp_p);
+			}
+		else mz_data_file.store(out,ms_exp_raw);
+
+    return EXECUTION_OK;
+  }
+};
+
+Param TOPPBase::getSubsectionDefaults_(const String& /*section*/) const
+{
+  // there is only one subsection: 'algorithm' (s.a) .. and in it belongs the PeakPicker param
+  Param tmp;
+  tmp.insert("PeakPicker:",PeakPickerCWT().getDefaults());
+  return tmp;
+}
+
+int main( int argc, char ** argv )
+{
+  TOPPExternalCalibration tool;
+  return tool.main(argc,argv);
+}
+
+/// @endcond
---
+++ source/TEST/ExternalCalibration_test.C
@@ -0,0 +1,225 @@
+// -*- Mode: C++; tab-width: 2; -*-
+// vi: set ts=2:
+//
+// --------------------------------------------------------------------------
+//                   OpenMS Mass Spectrometry Framework 
+// --------------------------------------------------------------------------
+//  Copyright (C) 2003-2007 -- Oliver Kohlbacher, Knut Reinert
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// --------------------------------------------------------------------------
+// $Maintainer: Alexandra Zerck $
+// --------------------------------------------------------------------------
+//
+
+#include <OpenMS/CONCEPT/ClassTest.h>
+
+///////////////////////////
+
+#include <OpenMS/FILTERING/CALIBRATION/ExternalCalibration.h>
+#include <OpenMS/FORMAT/MzDataFile.h>
+#include <OpenMS/FORMAT/TextFile.h>
+
+using namespace OpenMS;
+using namespace std;
+
+///////////////////////////
+
+START_TEST(ExternalCalibration, "$Id$")
+
+/////////////////////////////////////////////////////////////
+
+ExternalCalibration* ptr = 0;
+CHECK((ExternalCalibration()))
+  ptr = new ExternalCalibration;
+  TEST_NOT_EQUAL(ptr, 0)
+RESULT
+
+CHECK((~ExternalCalibration()))
+  delete ptr;
+RESULT
+
+ptr = new ExternalCalibration();
+
+
+CHECK((const std::vector<double>& getML1s() const))
+  std::vector<double> vec;
+  vec.push_back(0.1);
+  vec.push_back(0.3);
+  ptr->setML1s(vec);
+  TEST_EQUAL(ptr->getML1s()== vec,true)
+RESULT
+
+CHECK((const std::vector<double>& getML2s() const))
+  std::vector<double> vec;
+  vec.push_back(0.1);
+  vec.push_back(0.3);
+  ptr->setML2s(vec);
+  TEST_EQUAL(ptr->getML2s()== vec,true)
+RESULT
+
+CHECK((const std::vector<double>& getML3s() const))
+  std::vector<double> vec;
+  vec.push_back(0.1);
+  vec.push_back(0.3);
+  ptr->setML3s(vec);
+  TEST_EQUAL(ptr->getML3s()== vec,true)
+RESULT  
+
+  
+CHECK((void setML1s( std::vector<double>& ml1s) ))
+  std::vector<double> vec;
+  vec.push_back(0.1);
+  vec.push_back(0.3);
+  ptr->setML1s(vec);
+  TEST_EQUAL(ptr->getML1s()== vec,true)
+RESULT
+
+CHECK((void setML2s( std::vector<double>& ml2s) ))
+  std::vector<double> vec;
+  vec.push_back(0.1);
+  vec.push_back(0.3);
+  ptr->setML2s(vec);
+  TEST_EQUAL(ptr->getML2s()== vec,true)
+RESULT
+
+CHECK((void setML3s( std::vector<double>& ml3s) ))
+  std::vector<double> vec;
+  vec.push_back(0.1);
+  vec.push_back(0.3);
+  ptr->setML3s(vec);
+  TEST_EQUAL(ptr->getML3s()== vec,true)
+RESULT
+  
+
+
+CHECK( template<typename InputPeakType>
+       void calibrate(MSExperiment<RawDataPointType>& calib_spectra,MSExperiment<PeakType >& exp,std::vector<double>& exp_masses) )
+  PRECISION(0.000001)
+  std::cout.precision(12);
+  MSExperiment<RawDataPoint1D> calib_exp;
+  MSExperiment<RawDataPoint1D> exp,res_exp;
+  MzDataFile file;
+  file.load("data/ExternalCalibration_test_calibrants.mzData",calib_exp);
+  file.load("data/ExternalCalibration_test.mzData",exp);
+  file.load("data/ExternalCalibration_test_output.mzData",res_exp);
+  vector<double> ref_masses;
+  TextFile ref_file;
+
+  ref_file.load("data/ExternalCalibration_test_calibrant_masses.txt",true);
+
+  for(TextFile::Iterator iter = ref_file.begin(); iter != ref_file.end(); ++iter)
+   {
+     ref_masses.push_back(atof(iter->c_str()));
+   }
+
+  std::vector<double> ml1;
+                
+
+                                 
+  ml1.push_back(418327.924993827);
+                
+  ml1.push_back(418257.238180361);
+  ml1.push_back(418295.34897904);
+  std::vector<double> ml2;
+      
+
+                
+  ml2.push_back(253.645187196031);
+ 
+ 
+  ml2.push_back(250.532666867861);
+  ml2.push_back(251.878402283764);
+  std::vector<double> ml3;
+
+
+                
+  ml3.push_back(-0.0414243465397252);
+
+  ml3.push_back(-0.0428127107041497);
+  ml3.push_back(-0.0419329877166861);
+  ptr->setML1s(ml1);
+  ptr->setML2s(ml2);
+  ptr->setML3s(ml3);
+
+  Param param;
+  param.setValue("PeakPicker:thresholds:peak_bound",400);
+  param.setValue("PeakPicker:thresholds:correlation",0.0);
+  ptr->setParameters(param);
+  ptr->calibrate(calib_exp,exp,ref_masses);
+
+/// this is caused by the higher precision ExternalCalibration works with
+  file.store("ExternalCalibration_output.tmp",exp);
+  file.load("ExternalCalibration_output.tmp",exp);
+  TEST_EQUAL(exp,res_exp)
+
+RESULT
+
+CHECK( template<typename InputPeakType>
+       void calibrate(MSExperiment<PickedPeakType>& calib_spectra,MSExperiment<PeakType >& exp,
+									 std::vector<double>& exp_masses) )
+
+  PRECISION(0.000001)
+  std::cout.precision(12);
+  MSExperiment<PickedPeak1D> calib_exp;
+  MSExperiment<PickedPeak1D> exp,res_exp;
+  MzDataFile file;
+  file.load("data/ExternalCalibration_test_calibrants2.mzData",calib_exp);
+  file.load("data/ExternalCalibration_test2.mzData",exp);
+  file.load("data/ExternalCalibration_test_output2.mzData",res_exp);
+  vector<double> ref_masses;
+  TextFile ref_file;
+
+  ref_file.load("data/ExternalCalibration_test_calibrant_masses.txt",true);
+
+  for(TextFile::Iterator iter = ref_file.begin(); iter != ref_file.end(); ++iter)
+   {
+     ref_masses.push_back(atof(iter->c_str()));
+   }
+
+  std::vector<double> ml1;
+                
+
+                                 
+  ml1.push_back(418327.924993827);
+  ml1.push_back(418257.238180361);
+  ml1.push_back(418295.348979040);
+  std::vector<double> ml2;
+                
+  ml2.push_back(253.645187196031);
+  ml2.push_back(250.532666867861);
+  ml2.push_back(251.878402283764);
+  std::vector<double> ml3;
+                
+  ml3.push_back(-0.0414243465397252);
+  ml3.push_back(-0.0428127107041497);
+  ml3.push_back(-0.0419329877166861);
+  ptr->setML1s(ml1);
+  ptr->setML2s(ml2);
+  ptr->setML3s(ml3);
+
+  ptr->calibrate(calib_exp,exp,ref_masses);
+
+/// this is caused by the higher precision ExternalCalibration works with
+  file.store("ExternalCalibration_output.tmp",exp);
+  file.load("ExternalCalibration_output.tmp",exp);
+  TEST_EQUAL(exp,res_exp)
+
+RESULT	
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+END_TEST
