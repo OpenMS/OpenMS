1488c3a3d4513c3708b6e3381e95c7b543b363b9
Eva Lange <elange@users.sourceforge.net>
2007-06-11 08:58:33 +0000

    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@2160 6adb6e08-d915-0410-941f-83917bcadc18

---
+++ source/EXAMPLES/Tutorial_SavitzkyGolayFilter.C
@@ -0,0 +1,24 @@
+#include <OpenMS/FILTERING/SMOOTHING/SavitzkyGolaySVDFilter.h>
+#include <OpenMS/FORMAT/DTAFile.h>
+#include <OpenMS/KERNEL/StandardTypes.h>
+#include <iostream>
+
+using namespace OpenMS;
+using namespace std;
+
+Int main()
+{
+  RawSpectrum spec_raw;
+  RawSpectrum spec_filtered;
+  
+  DTAFile dta_file;
+  dta_file.load("../TEST/data/PeakTypeEstimator_test.dta",spec_raw);
+
+  SavitzkyGolaySVDFilter sg;
+  sg.setWindowLength(1.0);
+  sg.setOrder(3);
+  
+  sg.filter(spec_raw,spec_filtered);
+ 
+  return 0;
+} //end of main
