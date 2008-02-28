794337cc2cfeff15e3951484ec62ab57e9c9bfcb
Marcel Grunert <grunert@users.sourceforge.net>
2008-02-28 21:37:20 +0000

    added new test
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@2787 6adb6e08-d915-0410-941f-83917bcadc18

---
+++ source/TEST/ExtendedIsotopeFitter1D_test.C
@@ -0,0 +1,103 @@
+// -*- Mode: C++; tab-width: 2; -*-
+// vi: set ts=2:
+//
+// --------------------------------------------------------------------------
+//                   OpenMS Mass Spectrometry Framework
+// --------------------------------------------------------------------------
+//  Copyright (C) 2003-2008 -- Oliver Kohlbacher, Knut Reinert
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// --------------------------------------------------------------------------
+// $Maintainer: $ Marcel Grunert
+// --------------------------------------------------------------------------
+
+#include <OpenMS/CONCEPT/ClassTest.h>
+
+///////////////////////////
+
+#include <OpenMS/TRANSFORMATIONS/FEATUREFINDER/ExtendedIsotopeFitter1D.h>
+
+///////////////////////////
+
+using namespace OpenMS;
+using namespace std;
+
+START_TEST(ExtendedIsotopeFitter1D, "$Id: ExtendedIsotopeFitter1D_test.C 2783 2008-02-28 22:27:55Z grunert $")
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+ExtendedIsotopeFitter1D* ptr = 0;
+CHECK(ExtendedIsotopeFitter1D())
+{
+	ptr = new ExtendedIsotopeFitter1D();
+        TEST_EQUAL(ptr->getName(), "ExtendedIsotopeFitter1D")
+	TEST_NOT_EQUAL(ptr, 0)
+}
+RESULT
+
+CHECK(~ExtendedIsotopeFitter1D())
+{
+	delete ptr;
+}
+RESULT
+
+CHECK((ExtendedIsotopeFitter1D(const  ExtendedIsotopeFitter1D &source)))
+{
+  // TODO
+}
+RESULT
+
+CHECK((virtual ~ExtendedIsotopeFitter1D()))
+{
+  // TODO
+}
+RESULT
+
+CHECK((virtual ExtendedIsotopeFitter1D& operator=(const  ExtendedIsotopeFitter1D &source)))
+{
+  // TODO
+}
+RESULT
+
+CHECK((QualityType fit1d(const  RawDataArrayType &range, InterpolationModel *&model)))
+{
+  // TODO
+}
+RESULT
+
+CHECK((Fitter1D* create()))
+{
+  Fitter1D* ptr = ExtendedIsotopeFitter1D::create();
+  TEST_EQUAL(ptr->getName(), "ExtendedIsotopeFitter1D")
+  TEST_NOT_EQUAL(ptr, 0)
+}
+RESULT
+
+CHECK((const String getProductName()))
+{
+  TEST_EQUAL(ExtendedIsotopeFitter1D::getProductName(),"ExtendedIsotopeFitter1D")
+  TEST_EQUAL(ExtendedIsotopeFitter1D().getName(),"ExtendedIsotopeFitter1D")
+}
+RESULT
+
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+END_TEST
+
+
+
