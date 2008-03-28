24b1d0d8eb0449c9b9ad1a4625a099ec683674dc
Ole Schulz-Trieglaff <ole_st@users.sourceforge.net>
2008-03-28 17:19:04 +0000

    updated reference to iupac technical report.
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/Release1.1@2948 6adb6e08-d915-0410-941f-83917bcadc18

--- include/OpenMS/CHEMISTRY/ElementDB.h
+++ include/OpenMS/CHEMISTRY/ElementDB.h
@@ -44,13 +44,11 @@ namespace OpenMS
 	  	"Isotopic Compositions of the Elements 1997", Pure Appl. Chem., 70(1), 217-235, 1998.
 	  	(http://www.iupac.org/reports/1998/7001rosman/)
 	 
-	  	The elements isotope distributions (in the default file) are taken from
-	  	"Atomic Weights of the Elements 1999", Pure Appl. Chem. 73(4), 667-683, 2001.
-	 		(http://old.iupac.org/publications/pac/73/4/0667/)
-	 
-	 		ATOMIC WEIGHTS OF THE ELEMENTS: REVIEW 2000 (IUPAC Technical Report)
-	 		Pure Appl. Chem., Vol. 75, No. 6, pp. 683 800, 2003.
-	*/
+	  	The isotope distributions (in the default file) are taken from
+	 		"Atomic weights of the elements. Review 2000" (IUPAC Technical Report)
+			Pure Appl. Chem., 2003, Vol. 75, No. 6, pp. 683-799
+			doi:10.1351/pac200375060683	
+*/
 	
 	class ElementDB
 	{
