9dab1bbb4ba01c8866f4a0c86273d520e06ed195
Chris Bielow <chris.bielow@fu-berlin.de>
2008-03-30 15:20:52 +0000

    minor fix
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/Release1.1@2959 6adb6e08-d915-0410-941f-83917bcadc18

--- source/config/tools/installer/TOPPTest_Assembler.pl
+++ source/config/tools/installer/TOPPTest_Assembler.pl
@@ -1,13 +1,20 @@
 #!perl
 
-if (@ARGV != 1)
+if (@ARGV < 1)
 {
-	die "Usage:\n  $0 <path_to_OpenMS>\n\n";
+	die "Usage:\n  $0 <path_to_OpenMS> [enable DBImporter (1|0=default)]\n\n";
 }
 
 print "Setting $OMS_path to $ARGV[0]!\n";
 
-$OMS_path = '/d/uni/OpenMS_Win/my/OpenMS';
+#$OMS_path = '/d/uni/OpenMS_Win/my/OpenMS';
+$OMS_path = $ARGV[0];
+
+$enable_DB = 0;
+if (@ARGV >=2)
+{
+	$enable_DB = $ARGV[1];
+}
 
 $outfile = "$OMS_path/source/TEST/TOPP/TOPP_test.bat";
 
@@ -37,12 +44,19 @@ foreach $line (@makefile)
 		$rejoin = join(' ', ($file[$#file], @parts[1..$#parts]));
 		print "$rejoin\n\n";
 		
-		push @cmds, $rejoin;
-		push @cmds, "IF ERRORLEVEL 1 ECHO TOPP Command in line $#cmds failed!";
+		if ((!$enable_DB) and ($file[$#file] eq "DBImporter") || ($file[$#file] eq "DBExporter") ) {
+			print "skipping DB importer...\n".$file[$#file]."\n";
+			#skip DBImporter test
+		}
+		else {
+			push @cmds, $rejoin;
+			push @cmds, "IF ERRORLEVEL 1 ECHO TOPP Command in line $#cmds failed! Press any key to continue.";
+			push @cmds, "IF ERRORLEVEL 1 pause";
+		}
 	}	
 }
 
-push @cmds, "\nECHO ALL TESTS RUN!\nECHO  Check for errors above!";
+push @cmds, "\nECHO ALL TESTS RUN!\nECHO Check for errors above!\npause";
 
 open(CMD, ">$outfile");
 print CMD join("\n",@cmds);
@@ -51,7 +65,7 @@ close(CMD);
 print "$outfile written!\n\n";
 
 
-print "Now, copy\n  $OMS_path/source/TEST/TOPP\nto the VM and run it. Good luck!"
+print "Now, copy\n  $OMS_path/source/TEST/TOPP\nto the VM and run it. Good luck!";
 
   # Create a Zip file
  #  use Archive::Zip qw( :ERROR_CODES :CONSTANTS );
