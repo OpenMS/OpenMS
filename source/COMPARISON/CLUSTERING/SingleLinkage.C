58f7e8ee9de7d907ad93046f2ef5230fdde9f819
Mathias Walzer <walzer@informatik.uni-tuebingen.de>
2008-07-04 12:39:10 +0000

    fixed to coding convention
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/Release1.2@3461 6adb6e08-d915-0410-941f-83917bcadc18

--- source/COMPARISON/CLUSTERING/SingleLinkage.C
+++ source/COMPARISON/CLUSTERING/SingleLinkage.C
@@ -34,7 +34,7 @@ namespace OpenMS
 	SingleLinkage::SingleLinkage()
 	  : ClusterFunctor()
 	{
-			setName(SingleLinkage::getProductName());
+		setName(SingleLinkage::getProductName());
 	}
 	
 	SingleLinkage::SingleLinkage(const SingleLinkage& source)
@@ -55,25 +55,25 @@ namespace OpenMS
 		return *this;
 	}
 		
-	void SingleLinkage::cluster(const DistanceMatrix<double>& originalDist, DistanceMatrix<double>& actualDist, vector< vector<UInt> >& clusters, const String filepath /*= ""*/, const double threshold /*=1*/) const throw (Exception::UnableToCreateFile,ClusterFunctor::InsufficientInput)
+	void SingleLinkage::cluster(const DistanceMatrix<double>& original_distance, DistanceMatrix<double>& actual_distance, vector< vector<UInt> >& clusters, const String filepath /*= ""*/, const double threshold /*=1*/) const throw (Exception::UnableToCreateFile,ClusterFunctor::InsufficientInput)
 	{
 		// attention: clustering process is done by clustering the indices 
 		// pointing to elements in inputvector and distances in inputmatrix
 
 		// input MUST have >= 2 elements!
-		if(actualDist.dimensionsize()<2) 
+		if(actual_distance.dimensionsize()<2) 
 		{
 			throw ClusterFunctor::InsufficientInput(__FILE__, __LINE__, __PRETTY_FUNCTION__, "Distance matrix to start from only contains one element");
 		}
 		
-		// in case of preclustering actualDist and clusters have to match
-		if(clusters.size()>0 && actualDist.dimensionsize()!=clusters.size()) 
+		// in case of preclustering actual_distance and clusters have to match
+		if(clusters.size()>0 && actual_distance.dimensionsize()!=clusters.size()) 
 		{
 			throw ClusterFunctor::InsufficientInput(__FILE__, __LINE__, __PRETTY_FUNCTION__, "Distance matrix to start from does not match the clusters to start from");
 		}
 
 		std::ofstream os;
-		//make sure writing is successful
+		//make sure writing is successful and respect evtl. preclustering for the output
 		if(filepath.size()!=0)
 		{
 			os.open(filepath.c_str());
@@ -85,16 +85,16 @@ namespace OpenMS
 			os << "SINGLE LINKAGE" << endl << "dendrogram-file: " << filepath << endl << "timestamp: " << PreciseTime::now() << endl;
 			
 			//clusters inital state
-			if(clusters.size()!=originalDist.dimensionsize())
+			if(clusters.size()!=original_distance.dimensionsize())
 			{
 				os << "preclustering--snip" << endl;
-				UInt mergedCounter = 0;
+				UInt merged_counter = 0;
 				for(UInt i = 0; i < clusters.size(); ++i)
 				{
 					if(clusters[i].size() == 2)
 					{
-						os << "*" << " | " << clusters[i][0]-mergedCounter << " | " << clusters[i][1]-mergedCounter << " | " << "#" << endl;
-						++mergedCounter;
+						os << "*" << " | " << clusters[i][0]-merged_counter << " | " << clusters[i][1]-merged_counter << " | " << "#" << endl;
+						++merged_counter;
 					}
 				}
 				os << "preclustering--snap" << endl;
@@ -104,11 +104,11 @@ namespace OpenMS
 		// when no clustering has been done start atomar
 		if(clusters.size()==0) 
 		{
-			if(actualDist.dimensionsize()!=originalDist.dimensionsize()) 
+			if(actual_distance.dimensionsize()!=original_distance.dimensionsize()) 
 			{
 				throw ClusterFunctor::InsufficientInput(__FILE__, __LINE__, __PRETTY_FUNCTION__, "Distance matrices do not match");
 			}
-			for (UInt i = 0; i < actualDist.dimensionsize(); ++i)
+			for (UInt i = 0; i < actual_distance.dimensionsize(); ++i)
 			{
 				vector<UInt> tmp(1,i);
 				clusters.push_back(tmp);
@@ -116,70 +116,70 @@ namespace OpenMS
 		}
 
 		// Initial minimum-distance pair
-		actualDist.updateMinElement();
-		pair<UInt,UInt> min = actualDist.getMinElementCoordinates();
+		actual_distance.updateMinElement();
+		pair<UInt,UInt> min = actual_distance.getMinElementCoordinates();
 	
-		UInt counter1(0);
-		while(actualDist(min.first,min.second) < threshold && actualDist.dimensionsize() > 2)
+		UInt clustersteps_counter(0);
+		while(actual_distance(min.first,min.second) < threshold && actual_distance.dimensionsize() > 2)
 		{ 
-			++counter1;
+			++clustersteps_counter;
 			
 			if(filepath.size()!=0)
 			{
 				//write in file
-				os << counter1 << " | " << min.first << " | " << min.second << " | " << actualDist(min.first,min.second) << endl;
+				os << clustersteps_counter << " | " << min.first << " | " << min.second << " | " << actual_distance(min.first,min.second) << endl;
 			}
 
-cout << counter1 << " | " << min.first << " | " << min.second << " | " << actualDist(min.first,min.second) << endl;
 		//pick minimum-distance pair i,j and merge them
 			//pushback elements of second to first (and then erase second)
-		 	for (UInt c = 0; c < clusters[min.second].size(); ++c)
-		 	{
-			 	clusters[min.first].push_back(clusters[min.second][c]);
-		 	}
+			for (UInt c = 0; c < clusters[min.second].size(); ++c)
+			{
+				clusters[min.first].push_back(clusters[min.second][c]);
+			}
 
-		 	// erase second one
-		 	clusters.erase(clusters.begin()+min.second,clusters.begin()+min.second+1);
+			// erase second one
+			clusters.erase(clusters.begin()+min.second,clusters.begin()+min.second+1);
 	
-		 	//reduce
-			actualDist.reduce(min.second);
+			//reduce
+			actual_distance.reduce(min.second);
 	
-			//update actualDist matrix (and minimum-distance pair)
+			//update actual_distance matrix (and minimum-distance pair)
 			//single linkage: new distcance between clusteres is the minimum distance between elements of each cluster
 			for (UInt i = 0; i < min.first; ++i)
-		 	{
-			 	actualDist.setValueQuick(i,min.first,getMinDist_(i,min.first,clusters,originalDist));
+			{
+			 	actual_distance.setValueQuick(i,min.first,getMinDist_(i,min.first,clusters,original_distance));
 			}
-			for (UInt j = min.first+1; j < actualDist.dimensionsize(); ++j)
-		 	{
-			 	actualDist.setValueQuick(min.first,j,getMinDist_(min.first,j,clusters,originalDist));
+			for (UInt j = min.first+1; j < actual_distance.dimensionsize(); ++j)
+			{
+				
+				actual_distance.setValueQuick(min.first,j,getMinDist_(min.first,j,clusters,original_distance));
 			}
 			
 			//update min
-			actualDist.updateMinElement();
+			actual_distance.updateMinElement();
 				
 			//get min-pair from triangular matrix
-			min = actualDist.getMinElementCoordinates();
+			min = actual_distance.getMinElementCoordinates();
 			
 		//repeat until only two cluster remains, last step skips matrix operations
 		}
 		
-		if(actualDist(min.first,min.second) < threshold && actualDist.dimensionsize() == 2)
+		if(actual_distance(min.first,min.second) < threshold && actual_distance.dimensionsize() == 2)
 		{
 			if(filepath.size()!=0)
 			{
 				//write in file
-				os << counter1 << " | " << min.first << " | " << min.second << " | " << actualDist(min.first,min.second) << endl;
-			}				
+				os << clustersteps_counter << " | " << min.first << " | " << min.second << " | " << actual_distance(min.first,min.second) << endl;
+			}	
 			//pick minimum-distance pair i,j and merge them
 			//pushback elements of second to first (and then erase second)
-		 	for (UInt c = 0; c < clusters[min.second].size(); ++c)
-		 	{
-			 	clusters[min.first].push_back(clusters[min.second][c]);
-		 	}
-		 	// erase second one
-		 	clusters.erase(clusters.begin()+min.second,clusters.begin()+min.second+1);
-		 }
+			for (UInt c = 0; c < clusters[min.second].size(); ++c)
+			{
+				clusters[min.first].push_back(clusters[min.second][c]);
+			}
+			// erase second one
+			clusters.erase(clusters.begin()+min.second,clusters.begin()+min.second+1);
+		}
 		
 		if(filepath.size()!=0)
 		{
@@ -188,7 +188,7 @@ cout << counter1 << " | " << min.first << " | " << min.second << " | " << actual
 		}
 	}
 
-	double SingleLinkage::getMinDist_(UInt& o, UInt x, vector< vector<UInt> >& clusters, const DistanceMatrix<double>& originalDist) const
+	double SingleLinkage::getMinDist_(UInt& o, UInt x, vector< vector<UInt> >& clusters, const DistanceMatrix<double>& original_distance) const
 	{
 		double min(DBL_MAX);
 		
@@ -198,82 +198,79 @@ cout << counter1 << " | " << min.first << " | " << min.second << " | " << actual
 		
 		if(clusters.size() > o && clusters.size() > x )
 		{
-			
 			#ifdef TO_DEBUG
 			cout << "looking at "<< o << " and " << x << endl;
 			#endif
 			
-			//look only in cells upper the main diagonal!
+			//look only in cells up the main diagonal!
 			//o, x are indices of clusters
 			//wanted is mindist between elements of o to x
 			for (UInt i = 0; i < clusters[x].size(); ++i)
-			{
-							
+			{	
 				for (UInt j = 0; j < clusters[o].size(); ++j)
-		 		{
-			 				
-			 		if(clusters[x][i]<clusters[o][j])
-			 		{
-				 		#ifdef TO_DEBUG
-			 			cout << "comparing element "<< clusters[x][i] << " with element " << clusters[o][j] << endl;
-			 			cout << "distance: "<< originalDist.getValue(clusters[x][i],clusters[o][j]) << endl;
-			 			#endif
-			 			
-				 		if(originalDist.getValue(clusters[x][i],clusters[o][j])< min)
-				 		{
-			 				min = originalDist.getValue(clusters[x][i],clusters[o][j]);
-		 				}
-				 	}
-				 	else
-				 	{
+				{	
+					if(clusters[x][i]<clusters[o][j])
+					{
+						#ifdef TO_DEBUG
+						cout << "comparing element "<< clusters[x][i] << " with element " << clusters[o][j] << endl;
+						cout << "distance: "<< original_distance.getValue(clusters[x][i],clusters[o][j]) << endl;
+						#endif
+						
+						if(original_distance.getValue(clusters[x][i],clusters[o][j])< min)
+						{
+							min = original_distance.getValue(clusters[x][i],clusters[o][j]);
+						}
+					}
+					else
+					{
 					 	#ifdef TO_DEBUG
-			 			cout << "comparing element "<< clusters[o][j] << " with element " << clusters[x][i] << endl;
-			 			cout << "distance: "<< originalDist.getValue(clusters[o][j],clusters[x][i]) << endl;
-			 			#endif
-			 			
-					 	if(originalDist.getValue(clusters[o][j],clusters[x][i])< min)
-			 			{
-				 			min = originalDist.getValue(clusters[o][j],clusters[x][i]);
-			 			}
-				 	}
-		 		}
+						cout << "comparing element "<< clusters[o][j] << " with element " << clusters[x][i] << endl;
+						cout << "distance: "<< original_distance.getValue(clusters[o][j],clusters[x][i]) << endl;
+						#endif
+						
+						if(original_distance.getValue(clusters[o][j],clusters[x][i])< min)
+						{
+							min = original_distance.getValue(clusters[o][j],clusters[x][i]);
+						}
+					}
+				}
 			}
-	 		
 		}
 		return min;
 	}
+	
 /*
-	void SingleLinkage::cluster(const DistanceMatrix<double,1>& originalDist, double& threshold, vector< vector<UInt> >& clusters) const throw (ClusterFunctor::InsufficientInput)
+	void SingleLinkage::cluster(const DistanceMatrix<double,1>& original_distance, double& threshold, vector< vector<UInt> >& clusters) const throw (ClusterFunctor::InsufficientInput)
 	{
 	
 		// attention: clustering process is done by clustering the indices 
 		// pointing to elements in inputvector and distances in inputmatrix
 
 		// input MUST have >= 2 elements!
-		if(originalDist.dimensionsize()<2) 
+		if(original_distance.dimensionsize()<2) 
 		{
 			throw ClusterFunctor::InsufficientInput(__FILE__, __LINE__, __PRETTY_FUNCTION__, "Distance matrix only contains one element");
 		}
 				
 		// first all indices are in separate(single) Cluster
 		clusters.clear();
-		for (UInt i = 0; i < originalDist.dimensionsize(); ++i)
+		for (UInt i = 0; i < original_distance.dimensionsize(); ++i)
 		{
 			vector<UInt> tmp(1,i);
 			clusters.push_back(tmp);
 		}
 				
 		//DistanceMatrix representing the distances between the actual clusters
-		DistanceMatrix<double,1> actualDist(originalDist);
+		DistanceMatrix<double,1> actual_distance(original_distance);
 		
 		// Initial minimum-distance pair
-		pair<UInt,UInt> min = actualDist.getMinElementCoordinates();
+		pair<UInt,UInt> min = actual_distance.getMinElementCoordinates();
 			
 		#ifdef TO_DEBUG 
 		cout << "minpair: " << min.first << min.second << endl;
 		#endif
 
-		while(actualDist(min.first,min.second) < threshold && actualDist.dimensionsize() > 2)
+		while(actual_distance(min.first,min.second) < threshold && actual_distance.dimensionsize() > 2)
 		{ 		
 
 		//pick minimum-distance pair i,j and merge them
@@ -297,32 +294,32 @@ cout << counter1 << " | " << min.first << " | " << min.second << " | " << actual
 			#endif		 	
 			
 			//reduce
-			actualDist.reduce(min.second);
+			actual_distance.reduce(min.second);
 			
-			//update actualDist matrix (and minimum-distance pair)
+			//update actual_distance matrix (and minimum-distance pair)
 			//single linkage: new distcance between clusteres is the minimum distance between elements of each cluster
 			for (UInt i = 0; i < min.first; ++i)
 			{
-			 	actualDist.setValueQuick(i,min.first,getMinDist_(i,min.first,clusters,originalDist));						
+			 	actual_distance.setValueQuick(i,min.first,getMinDist_(i,min.first,clusters,original_distance));						
 			}
-			for (UInt j = min.first+1; j < actualDist.dimensionsize(); ++j)
+			for (UInt j = min.first+1; j < actual_distance.dimensionsize(); ++j)
 			{
-			 	actualDist.setValueQuick(min.first,j,getMinDist_(min.first,j,clusters,originalDist));						
+			 	actual_distance.setValueQuick(min.first,j,getMinDist_(min.first,j,clusters,original_distance));						
 			}
 			
 			//update min
-			actualDist.updateMinElement();
+			actual_distance.updateMinElement();
 							
 			#ifdef TO_DEBUG
-			cout << actualDist << endl;
+			cout << actual_distance << endl;
 			#endif
 		
 			//get min-pair from actual distance matrix
-			min = actualDist.getMinElementCoordinates();
+			min = actual_distance.getMinElementCoordinates();
 			
 			#ifdef TO_DEBUG 
 			cout << "minpair: " << min.first << min.second << endl;
-			cout << "dist.dimensionsize: " << actualDist.dimensionsize() << endl;
+			cout << "dist.dimensionsize: " << actual_distance.dimensionsize() << endl;
 			#endif
 			
 		//repeat until threshhold distance between all clusters is exceeded
@@ -331,7 +328,7 @@ cout << counter1 << " | " << min.first << " | " << min.second << " | " << actual
 		}
 
 		// if threshold still not exceeded and only two clusters left to merge
-		if(actualDist(min.first,min.second) < threshold && actualDist.dimensionsize() == 2)
+		if(actual_distance(min.first,min.second) < threshold && actual_distance.dimensionsize() == 2)
 		{
 			for (UInt c = 0; c < clusters[min.second].size(); ++c)
 		 	{
