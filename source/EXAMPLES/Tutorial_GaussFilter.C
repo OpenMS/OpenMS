4c76aaaaed36c1b6c495ffdc4b96590e67d8785a
Marc Sturm <marc_sturm@users.sourceforge.net>
2008-07-23 12:22:24 +0000

    Updated version to 1.2 and fixed example files
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/Release1.2@3561 6adb6e08-d915-0410-941f-83917bcadc18

--- source/EXAMPLES/Tutorial_GaussFilter.C
+++ source/EXAMPLES/Tutorial_GaussFilter.C
@@ -8,18 +8,17 @@ using namespace std;
 
 Int main()
 {
-  PeakMap exp_raw;
-  PeakMap exp_filtered;
+  PeakMap exp;
   
   MzDataFile mzdata_file;
-  mzdata_file.load("data/Tutorial_GaussFilter.mzData",exp_raw);
+  mzdata_file.load("data/Tutorial_GaussFilter.mzData",exp);
 
   GaussFilter g;
   Param param;
   param.setValue("gaussian_width",1.0);
   g.setParameters(param);
  
-  g.filterExperiment(exp_raw,exp_filtered);
+  g.filterExperiment(exp);
  
   return 0;
 } //end of main
