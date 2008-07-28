13064b339ce7795b8a8c04fd340a8b0d706bc212
Mathias Walzer <walzer@informatik.uni-tuebingen.de>
2008-07-28 15:06:11 +0000

    throw is documented only
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/Release1.2@3581 6adb6e08-d915-0410-941f-83917bcadc18

--- include/OpenMS/COMPARISON/CLUSTERING/SingleLinkage.h
+++ include/OpenMS/COMPARISON/CLUSTERING/SingleLinkage.h
@@ -34,7 +34,7 @@
 #include <OpenMS/DATASTRUCTURES/DistanceMatrix.h>
 #include <OpenMS/COMPARISON/CLUSTERING/ClusterFunctor.h>
 #include <OpenMS/KERNEL/StandardTypes.h>
-#include <OpenMS/DATASTRUCTURES/String.h> 
+#include <OpenMS/DATASTRUCTURES/String.h>
 #include <OpenMS/CONCEPT/TimeStamp.h>
 
 namespace OpenMS
@@ -45,13 +45,13 @@ namespace OpenMS
 		The details of the score can be found in:
 		LU, S. Y. AND FU, K. S. 1978. A sentence-to-sentence clustering procedure for pattern analysis. IEEE Trans. Syst. Man Cybern. 8,381-389.
 		@see ClusterFunctor() base class.
-		
+
 		@ingroup SpectraClustering
 	*/
 	class SingleLinkage : public ClusterFunctor
-	{	  
+	{
 		public:
-	
+
 		/// default constructor
 		SingleLinkage();
 
@@ -60,49 +60,51 @@ namespace OpenMS
 
 		/// destructor
 		virtual ~SingleLinkage();
-	
+
 		/// assignment operator
 		SingleLinkage& operator = (const SingleLinkage& source);
-		
+
 		/**
 			@brief clusters the indices according to their respective element distances
-		
+
 			@param original_distance DistanceMatrix<double> containing the distances of the elements to be clustered
 			@param actual_distance DistanceMatrix<double> containing the distances of the clusters at current stage
 			@param clusters vector< vector<UInt> >, each vector<UInt> represents a cluster, its elements being the indices according to original_dist. @see ClusterFunctor() Base class.
 			@param filepath String&, by default empty, when given, a dendrogram will be written in a file created in that path
 			@param threshold double value, the minimal distance from which on cluster merging is considered unrealistic. By default set to 1, i.e. complete clustering until only one cluster remains
-		
+			@throw UnableToCreateFile thrown if for the given filepath no file can be created
+			@throw ClusterFunctor::InsufficientInput thrown if input is <2
+
 			The clustering method is single linkage, where the updated distances after merging two clusters
-			are each the minimal distance between the elements of their clusters. @see SingleLinkage::getMinDist_ 
+			are each the minimal distance between the elements of their clusters. @see SingleLinkage::getMinDist_
 			Output is the stagewise merged clusters and each clusters average distance to the others
-		*/ 
-		void cluster(const DistanceMatrix<double>& original_distance, DistanceMatrix<double>& actual_distance, std::vector< std::vector<UInt> >& clusters, const String filepath="", const double threshold=1) const throw (Exception::UnableToCreateFile,ClusterFunctor::InsufficientInput);
-	
+		*/
+		void cluster(const DistanceMatrix<double>& original_distance, DistanceMatrix<double>& actual_distance, std::vector< std::vector<UInt> >& clusters, const String filepath="", const double threshold=1) const;
+
 		/// creates a new instance of a SingleLinkage object
-		static ClusterFunctor* create() 
+		static ClusterFunctor* create()
 		{
-			return new SingleLinkage(); 
+			return new SingleLinkage();
 		}
 
 		/// get the identifier for this object
 		static const String getProductName()
 		{
 			return "SingleLinkage";
-		}	
+		}
 	private:
-	
+
 		/**
 			@brief gets the minimum distance between all elements of one cluster and all elements of another cluster.
-			
+
 			@param o first UInt index pointing to a cluster
 			@param x second UInt index pointing to a cluster
 			@param clusters the clusters
 			@param original_dist DistanceMatrix containing the distances between all clustered elements of @p clusters
-				
-			The indices shall point to clusters in the given clustering vector< vector<UInt> > for which the 
+
+			The indices shall point to clusters in the given clustering vector< vector<UInt> > for which the
 			minimal distance between elements from @p o and @p x is to be calculated from the given @p original_dist.
-	            
+
 			@see ClusterFunctor() Base class.
 		*/
 		double getMinDist_(UInt& o, UInt x, std::vector< std::vector<UInt> >& clusters, const DistanceMatrix<double>& original_dist) const;
