c024435e70bd34d3423740a399af3986e67d5cc9
Marc Sturm <marc_sturm@users.sourceforge.net>
2008-11-20 12:22:10 +0000

    Replaced QuantIDMerger by IDMapper (now functional)
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@4090 6adb6e08-d915-0410-941f-83917bcadc18

---
+++ source/APPLICATIONS/TOPP/IDMapper.C
@@ -0,0 +1,134 @@
+// -*- mode: C++; tab-width: 2; -*-
+// vi: set ts=2:
+//
+// --------------------------------------------------------------------------
+//                   OpenMS Mass Spectrometry Framework
+// --------------------------------------------------------------------------
+//  Copyright (C) 2003-2008 -- Oliver Kohlbacher, Knut Reinert
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// --------------------------------------------------------------------------
+// $Maintainer: Marc Sturm $
+// --------------------------------------------------------------------------
+
+#include <OpenMS/config.h>
+
+#include <OpenMS/FORMAT/IdXMLFile.h>
+#include <OpenMS/FORMAT/FeatureXMLFile.h>
+#include <OpenMS/FORMAT/ConsensusXMLFile.h>
+#include <OpenMS/FORMAT/FileHandler.h>
+#include <OpenMS/ANALYSIS/ID/IDMapper.h>
+#include <OpenMS/APPLICATIONS/TOPPBase.h>
+
+using namespace OpenMS;
+using namespace std;
+
+//-------------------------------------------------------------
+//Doxygen docu
+//-------------------------------------------------------------
+
+/**
+	@page IDMapper_TOPP IDMapper_TOPP
+	
+	Assigns protein/peptide identifications to feature or consensus features.
+*/
+
+// We do not want this class to show up in the docu:
+/// @cond TOPPCLASSES
+
+class TOPPIDMapper
+	: public TOPPBase
+{
+
+	public:
+
+		TOPPIDMapper()
+			: TOPPBase("IDMapper", "Assigns protein/peptide identifications to feature or consensus features.")
+		{
+		}
+	
+	protected:
+	
+		void registerOptionsAndFlags_()
+		{	
+			registerInputFile_("id", "<file>", "", "Protein/peptide identifications file");
+			setValidFormats_("id",StringList::create("idXML"));
+			registerInputFile_("in", "<file>", "", "Protein/peptide identifications file");
+			setValidFormats_("in",StringList::create("featureXML,consensusXML"));
+			registerOutputFile_("out", "<file>", "", "Output file (the format depends on the input file format).");		
+			setValidFormats_("out",StringList::create("featureXML,consensusXML"));
+				
+			addEmptyLine_();
+			IDMapper mapper;
+			registerDoubleOption_("rt_delta","<value>",mapper.getRTDelta(), "Maximum allowed RT deviation between identification and feature.", false);
+			setMinFloat_("rt_delta",0.0);
+			registerDoubleOption_("mz_delta","<value>",mapper.getMZDelta(), "Maximum allowed m/z deviation between identification and feature.", false);
+			setMinFloat_("mz_delta",0.0);
+		}
+	
+		ExitCodes main_(int , const char**)
+		{
+			String in = getStringOption_("in");
+			FileHandler::Type in_type = FileHandler::getType(in);
+			String out = getStringOption_("out");
+				
+			//----------------------------------------------------------------
+			// load idXML
+			vector<ProteinIdentification> protein_ids;
+			vector<PeptideIdentification> peptide_ids;
+			IdXMLFile().load(getStringOption_("id"),protein_ids,peptide_ids);
+
+			//----------------------------------------------------------------
+			//create mapper
+			IDMapper mapper;
+			mapper.setRTDelta(getDoubleOption_("rt_delta"));
+			mapper.setMZDelta(getDoubleOption_("mz_delta"));
+
+			//----------------------------------------------------------------
+			// consensusXML
+			if (in_type == FileHandler::CONSENSUSXML)
+			{
+				ConsensusXMLFile file;
+				ConsensusMap map;
+				file.load(in,map);
+				mapper.annotate(map,peptide_ids,protein_ids);
+				file.store(out,map);
+			}
+			
+			//----------------------------------------------------------------
+			// featureXML
+			if (in_type == FileHandler::FEATUREXML)
+			{
+				FeatureMap<> map;
+				FeatureXMLFile file;
+				file.load(in,map);
+				mapper.annotate(map,peptide_ids,protein_ids);
+				file.store(out,map);
+			}
+			
+			return EXECUTION_OK;
+		}
+
+};
+
+
+int main( int argc, const char** argv )
+{
+	TOPPIDMapper tool;
+	return tool.main(argc, argv);
+}
+
+/// @endcond
