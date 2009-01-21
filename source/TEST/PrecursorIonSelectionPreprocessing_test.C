c59779ca5160e5ba6b10c42ebf1bc16a7af04f23
Alexandra Zerck <azerck@users.sourceforge.net>
2009-01-21 15:40:52 +0000

    minor changes
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/Release1.3@4423 6adb6e08-d915-0410-941f-83917bcadc18

--- source/TEST/PrecursorIonSelectionPreprocessing_test.C
+++ source/TEST/PrecursorIonSelectionPreprocessing_test.C
@@ -71,7 +71,7 @@ ptr->dbPreprocessing(OPENMS_GET_TEST_DATA_PATH("PrecursorIonSelectionPreprocessi
 	
 START_SECTION((const std::map<String,std::vector<DoubleReal> >& getProtMasses() const))
 	std::map<String,std::vector<DoubleReal> > prot_map = ptr->getProtMasses();
-	TEST_EQUAL(prot_map.size(), 3)
+	TEST_EQUAL(prot_map.size(), 4)
 END_SECTION
 
 START_SECTION((const std::vector<DoubleReal> & getMasses(String acc) const))
@@ -86,7 +86,7 @@ END_SECTION
 	
 START_SECTION(void dbPreprocessing(String &db_path))
 	std::map<String,std::vector<DoubleReal> > prot_map = ptr->getProtMasses();
-	TEST_EQUAL(prot_map.size(), 3)
+	TEST_EQUAL(prot_map.size(), 4)
 END_SECTION
 
 START_SECTION(DoubleReal getWeight(DoubleReal mass))
@@ -99,7 +99,7 @@ START_SECTION(void loadPreprocessing())
 	param.setValue("preprocessing:preprocessed_db_path",OPENMS_GET_TEST_DATA_PATH("PrecursorIonSelectionPreprocessing_db_10_ppm_1_"));
   ldb.setParameters(param);
   ldb.loadPreprocessing();
-  TEST_EQUAL(ldb.getProtMasses().size(),3)
+  TEST_EQUAL(ldb.getProtMasses().size(),4)
   DoubleReal w = ldb.getWeight(147.113);
   TEST_REAL_SIMILAR(w,0.5)
 
