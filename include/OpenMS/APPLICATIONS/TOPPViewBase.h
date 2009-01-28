ae509bf0c26b1ff0b1b451922528d71a8f4fd581
Chris Bielow <chris.bielow@fu-berlin.de>
2009-01-28 10:25:21 +0000

    added TODO (Drag&Drop)
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/Release1.3@4468 6adb6e08-d915-0410-941f-83917bcadc18

--- include/OpenMS/APPLICATIONS/TOPPViewBase.h
+++ include/OpenMS/APPLICATIONS/TOPPViewBase.h
@@ -70,6 +70,8 @@ namespace OpenMS
   /**
   	@brief Main window of TOPPView tool
 		
+		@improvement Support Drag&Drop of files from Explorer/Konqueror (HiWi)
+		
     @improvement Paint only highest point per pixel, paint only part of the data when moving (Hiwi)
 		
 		@improvement For "Open"/"Save as" Dialogs, use the path of the currently opened file as default (Hiwi)
