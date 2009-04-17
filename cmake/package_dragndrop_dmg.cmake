ccbe06b7b777811ad203c31dfa390e5d535d4e48
Andreas Bertsch <andreas_bertsch@users.sourceforge.net>
2009-04-17 08:49:44 +0000

    added eyecandy for MacOSX DragNDrop installer (cmake-cvs needed)
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/Release1.4@5114 6adb6e08-d915-0410-941f-83917bcadc18

--- cmake/package_dragndrop_dmg.cmake
+++ cmake/package_dragndrop_dmg.cmake
@@ -2,7 +2,9 @@ set(CPACK_GENERATOR "DragNDrop")
 
 # this option to take effect requires at least version 2.6.4 of cmake, which is not released yet
 # therefore only the developer version can be used to make background images
-set(CPACK_BUNDLE_DMG_BACKGROUND_IMAGE ${PROJECT_SOURCE_DIR}/cmake/MacOSX/background.png)
+# The file should be accompanied with a file named background png, stored in the sam folder
+set(CPACK_DMG_DS_STORE ${PROJECT_SOURCE_DIR}/cmake/MacOSX/DS_store)
+
 
 # add start-up script for TOPPView.app into Bundle, and move real TOPPView executable to TOPPView.exe
 # call scripts to fix issues with wrongly referenced Qt libs 
@@ -63,5 +65,8 @@ install(FILES 		${PROJECT_BINARY_DIR}/doc/TOPP_tutorial.pdf   DESTINATION OpenMS
 # install the TOPP command shell
 install(FILES ${PROJECT_SOURCE_DIR}/cmake/MacOSX/TOPP-shell.command DESTINATION OpenMS-1.4/ RENAME TOPP-shell PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ COMPONENT TOPPShell)
 install(FILES ${PROJECT_SOURCE_DIR}/cmake/MacOSX/TOPP_bash_profile  DESTINATION OpenMS-1.4/ RENAME .TOPP_bash_profile COMPONENT TOPPShell)
+
+# install background image for the folder
+install(FILES ${PROJECT_SOURCE_DIR}/cmake/MacOSX/background.png DESTINATION OpenMS-1.4/share/OpenMS/ COMPONENT share)
 
 include(CPack)
