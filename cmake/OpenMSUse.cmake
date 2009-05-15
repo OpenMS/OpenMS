6a05f630691981b139e535182c5a988e40328066
Andreas Bertsch <andreas_bertsch@users.sourceforge.net>
2009-05-15 14:00:37 +0000

    fixed bug reported by Hendrik; compiler flags are now exportet; maybe additional export follow
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@5267 6adb6e08-d915-0410-941f-83917bcadc18

--- cmake/OpenMSUse.cmake
+++ cmake/OpenMSUse.cmake
@@ -3,6 +3,14 @@ include(CMakeImportBuildSettings)
 
 cmake_import_build_settings(${OpenMS_BUILD_SETTINGS_FILE})
 
-include_directories(${OPENMS_INCLUDE_DIRS})
 
+# this is a hack (????), because build settings (compiler flags and Co.
+# seem not to be automatically transferred to projects using the 
+# build settings?!?
+set(CMAKE_CXX_FLAGS ${CMAKE_BUILD_SETTING_CXX_FLAGS})
+set(CMAKE_C_FLAGS ${CMAKE_BUILD_SETTING_C_FLAGS})
+
+
+
+include_directories(${OPENMS_INCLUDE_DIRS})
 link_directories(${OPENMS_LIBRARIES_DIRS})
