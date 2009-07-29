8864c2506101655e4271726f00512d7663bcf792
Andreas Bertsch <andreas_bertsch@users.sourceforge.net>
2009-07-29 20:14:31 +0000

    fixed MSPFile_test
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@5580 6adb6e08-d915-0410-941f-83917bcadc18

--- source/TEST/MSPFile_test.C
+++ source/TEST/MSPFile_test.C
@@ -82,7 +82,7 @@ START_SECTION(void load(const String &filename, std::vector< PeptideIdentificati
 
 		//test DocumentIdentifier addition
 	TEST_STRING_EQUAL(exp.getLoadedFilePath(), OPENMS_GET_TEST_DATA_PATH("MSPFile_test.msp"));
-	TEST_STRING_EQUAL(FileHandler::typeToName(exp.getLoadedFileType()),"Unknown");
+	TEST_STRING_EQUAL(FileHandler::typeToName(exp.getLoadedFileType()),"MSP");
 
 
 	TEST_STRING_EQUAL(exp[0].getNativeID(), "index=0")
