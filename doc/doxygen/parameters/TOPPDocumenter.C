56e391f3ae81910ec35c948724271f8cab470a19
Marc Sturm <marc_sturm@users.sourceforge.net>
2009-08-25 05:15:49 +0000

    Added MRMPairFnider to UTILS docu
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/Release1.5@5848 6adb6e08-d915-0410-941f-83917bcadc18

--- doc/doxygen/parameters/TOPPDocumenter.C
+++ doc/doxygen/parameters/TOPPDocumenter.C
@@ -53,7 +53,7 @@ int main (int , char** )
 	}
 	
 	//UTILS
-	StringList utils_tools = StringList::create("IDMassAccuracy,DecoyDatabase,MapAlignmentEvaluation,CaapConvert,CVInspector,DecoyDatabase,Digestor,FFEval,FuzzyDiff,HistView,IDExtractor,LabeledEval,SemanticValidator,SequenceCoverageCalculator,XMLValidator,IdXMLEvaluation,MSSimulator,ERPairFinder,PeptideIndexer,SpecLibCreator,SpectrumGeneratorNetworkTrainer");
+	StringList utils_tools = StringList::create("IDMassAccuracy,DecoyDatabase,MapAlignmentEvaluation,CaapConvert,CVInspector,DecoyDatabase,Digestor,FFEval,FuzzyDiff,HistView,IDExtractor,LabeledEval,SemanticValidator,SequenceCoverageCalculator,XMLValidator,IdXMLEvaluation,MSSimulator,ERPairFinder,PeptideIndexer,SpecLibCreator,SpectrumGeneratorNetworkTrainer,MRMPairFinder");
 	for (Size i=0; i<utils_tools.size(); ++i)
 	{
 		//start process
