3d827fa63bb896205c47e5f4b9dd0c793fd1914d
Stephan Aiche <stephan.aiche@fu-berlin.de>
2009-10-19 12:01:11 +0000

    added me as maintainer to the KERNEL/CONCEPT/DATASTRUCTURES/MATH tests
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@6132 6adb6e08-d915-0410-941f-83917bcadc18

--- source/TEST/Date_test.C
+++ source/TEST/Date_test.C
@@ -21,9 +21,9 @@
 //  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
 // --------------------------------------------------------------------------
-// $Maintainer: $
+// $Maintainer: Stephan Aiche$
 // $Authors: Marc Sturm $
-//
+// --------------------------------------------------------------------------
 
 #include <OpenMS/CONCEPT/ClassTest.h>
 
