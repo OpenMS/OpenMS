33a1a8ee72bda2bd6de19c295c2f45ec56117c04
Andreas Bertsch <andreas_bertsch@users.sourceforge.net>
2009-11-13 13:46:46 +0000

    fixed ticket #32, TOPPView save file endings
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/Release1.6@6343 6adb6e08-d915-0410-941f-83917bcadc18

--- source/VISUAL/Spectrum2DCanvas.C
+++ source/VISUAL/Spectrum2DCanvas.C
@@ -1998,9 +1998,38 @@ namespace OpenMS
 
 		if (layer.type==LayerData::DT_PEAK) //peak data
 		{
-    	QString file_name = QFileDialog::getSaveFileName(this, "Save file", proposed_name.toQString(),"mzML files (*.mzML);;mzData files (*.mzData);;mzXML files (*.mzXML);;All files (*)");
+			QString selected_filter = "";
+    	QString file_name = QFileDialog::getSaveFileName(this, "Save file", proposed_name.toQString(),"mzML files (*.mzML);;mzData files (*.mzData);;mzXML files (*.mzXML);;All files (*)", &selected_filter);
 			if (!file_name.isEmpty())
 			{
+				// check whether a file type suffix has been given
+				// first check mzData and mzXML then mzML
+				// if the setting is at "All files" 
+				// mzML will be used
+				String upper_filename = file_name;
+				upper_filename.toUpper();
+				if (selected_filter == "mzData files (*.mzData)")
+				{
+					if (!upper_filename.hasSuffix(".MZDATA"))
+					{
+						file_name += ".mzData";
+					}
+				}
+				else if (selected_filter == "mzXML files (*.mzXML)")
+				{
+					if (!upper_filename.hasSuffix(".MZXML"))
+					{
+						file_name += ".mzXML";
+					}
+				}
+				else 
+				{
+					if (!upper_filename.hasSuffix(".MZML"))
+					{
+						file_name += ".mzML";
+					}
+				}
+
 	    	if (visible) //only visible data
 	    	{
 					ExperimentType out;
@@ -2020,6 +2049,13 @@ namespace OpenMS
 			QString file_name = QFileDialog::getSaveFileName(this, "Save file", proposed_name.toQString(),"FeatureXML files (*.featureXML);;All files (*)");
 			if (!file_name.isEmpty())
 			{
+				// add suffix ".featureXML" if not given
+				String upper_filename = file_name;
+				upper_filename.toUpper();
+				if (!upper_filename.hasSuffix(".FEATUREXML"))
+				{
+					file_name += ".featureXML";
+				}
 		  	if (visible) //only visible data
 		  	{
 					FeatureMapType out;
@@ -2038,6 +2074,14 @@ namespace OpenMS
 			QString file_name = QFileDialog::getSaveFileName(this, "Save file", proposed_name.toQString(),"ConsensusXML files (*.consensusXML);;All files (*)");
 			if (!file_name.isEmpty())
 			{
+				// add suffix ".consensusXML" if not given
+				String upper_filename = file_name;
+				upper_filename.toUpper();
+				if (!upper_filename.hasSuffix(".CONSENSUSXML"))
+				{
+					file_name += ".consensusXML";
+				}
+
 		  	if (visible) //only visible data
 		  	{
 					ConsensusMapType out;
