62fa79f0eb23a1a78b12d2a6f031dcf79b44c8e4
Andreas Bertsch <andreas_bertsch@users.sourceforge.net>
2010-01-23 18:11:40 +0000

    updated copyright
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@6566 6adb6e08-d915-0410-941f-83917bcadc18

--- source/COMPARISON/CLUSTERING/EuclideanSimilarity.C
+++ source/COMPARISON/CLUSTERING/EuclideanSimilarity.C
@@ -4,7 +4,7 @@
 // --------------------------------------------------------------------------
 //                   OpenMS Mass Spectrometry Framework
 // --------------------------------------------------------------------------
-//  Copyright (C) 2003-2007 -- Oliver Kohlbacher, Knut Reinert
+//  Copyright (C) 2003-2010 -- Oliver Kohlbacher, Knut Reinert
 //
 //  This library is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU Lesser General Public
@@ -22,7 +22,7 @@
 //
 // --------------------------------------------------------------------------
 // $Maintainer: Mathias Walzer $
-// $Authors: $
+// $Authors: Mathias Walzer $
 // --------------------------------------------------------------------------
 //
 
