e636df65c461b1e2de014d3a05b6f359d32f54b2
Andreas Bertsch <andreas_bertsch@users.sourceforge.net>
2010-03-03 14:32:56 +0000

    protein inference prototype
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@6723 6adb6e08-d915-0410-941f-83917bcadc18

--- source/APPLICATIONS/TOPP/ProteinInference.C
+++ source/APPLICATIONS/TOPP/ProteinInference.C
@@ -29,6 +29,8 @@
 #include <OpenMS/CONCEPT/VersionInfo.h>
 #include <OpenMS/FORMAT/IdXMLFile.h>
 
+#include <set>
+
 using namespace OpenMS;
 using namespace std;
 
@@ -80,100 +82,124 @@ class TOPPProteinInference
 			String in = getStringOption_("in");
 			String out = getStringOption_("out");
 			Size min_peptides_per_protein = getIntOption_("min_peptides_per_protein");
+			bool treat_charge_variants_separately(getFlag_("treat_charge_variants_separately"));
+			bool treat_modification_variants_separately(getFlag_("treat_modification_variants_separately"));
 
+			// load identifications
 			vector<ProteinIdentification> prot_ids;
 			vector<PeptideIdentification> pep_ids;
 			IdXMLFile().load(in, prot_ids, pep_ids);
 
-			vector<ProteinIdentification> prot_id;
-			prot_id.push_back(*prot_ids.begin());
-
-			Map<String, Size> acc_counts;
-			for (vector<ProteinIdentification>::iterator it = prot_ids.begin(); it != prot_ids.end(); ++it)
+			// collect the different proteins (some of the protein hit copies are discarded)
+			Map<String, ProteinHit> acc_to_protein_hit;
+			for (vector<ProteinIdentification>::const_iterator it = prot_ids.begin(); it != prot_ids.end(); ++it)
 			{
-				for (vector<PeptideIdentification>::const_iterator it1 = pep_ids.begin(); it1 != pep_ids.end(); ++it1)
+				for (vector<ProteinHit>::const_iterator pit = it->getHits().begin(); pit != it->getHits().end(); ++pit)
 				{
-					// for all peptide hits
-					for (vector<PeptideHit>::const_iterator it2 = it1->getHits().begin(); it2 != it1->getHits().end(); ++it2)
-					{
-						// for all protein accessions
-						for (vector<String>::const_iterator it3 = it2->getProteinAccessions().begin(); it3 != it2->getProteinAccessions().end(); ++it3)
-						{
-							if (!acc_counts.has(*it3))
-							{
-								acc_counts[*it3] = 1;
-							}
-							else
-							{
-								++acc_counts[*it3];
-							}
-						}
-					}
+					acc_to_protein_hit[pit->getAccession()] = *pit;
 				}
-
-				vector<ProteinHit> hits = it->getHits();
-				it->setHits(vector<ProteinHit>());
-				set<String> accepted_proteins;
-				// for all protein hits for the id run, only accept proteins that have at least 'min_peptides_per_protein' peptides 
-				for (vector<ProteinHit>::const_iterator it1 = hits.begin(); it1 != hits.end(); ++it1)
-				{
-					if (acc_counts.has(it1->getAccession()) && acc_counts[it1->getAccession()] > min_peptides_per_protein)
-					{
-						it->insertHit(*it1);
-						accepted_proteins.insert(it1->getAccession());
-					}
-				}
-
-				// remove peptides that are not accepted
-				for (vector<PeptideIdentification>::iterator it1 = pep_ids.begin(); it1 != pep_ids.end(); ++it1)
-				{
-					vector<PeptideHit> peptide_hits = it1->getHits();
-					it1->setHits(vector<PeptideHit>());
-
-					for (vector<PeptideHit>::const_iterator it2 = peptide_hits.begin(); it2 != peptide_hits.end(); ++it2)
-					{
-						for (vector<String>::const_iterator it3 = it2->getProteinAccessions().begin(); it3 != it2->getProteinAccessions().end(); ++it3)
-						{
-							if (accepted_proteins.find(*it3) != accepted_proteins.end())
-							{
-								it1->insertHit(*it2);
-								break;
-							}
-						}
-					}
-				}
-
-				// remove proteins that are not accepted
-				vector<ProteinHit> protein_hits = it->getHits();
-				it->setHits(vector<ProteinHit>());
-				for (vector<ProteinHit>::const_iterator it1 = protein_hits.begin(); it1 != protein_hits.end(); ++it1)
-				{
-					if (accepted_proteins.find(it1->getAccession()) != accepted_proteins.end())
-					{
-						it->insertHit(*it1);
-					}
-				}
-
-				// fix wrong accessions of the peptides (to proteins that were removed)
-				for (vector<PeptideIdentification>::iterator it1 = pep_ids.begin(); it1 != pep_ids.end(); ++it1)
-        {
-				 	vector<PeptideHit> peptide_ids = it1->getHits();
-          for (vector<PeptideHit>::iterator it2 = peptide_ids.begin(); it2 != peptide_ids.end(); ++it2)
-          {
-						vector<String> valid_accessions;
-            for (vector<String>::const_iterator it3 = it2->getProteinAccessions().begin(); it3 != it2->getProteinAccessions().end(); ++it3)
-            {
-              if (accepted_proteins.find(*it3) != accepted_proteins.end())
-              { 
-								valid_accessions.push_back(*it3);
-              }
-            }
-						it2->setProteinAccessions(valid_accessions);
-					}
-					it1->setHits(peptide_ids);
-        }
 			}
 
+			writeDebug_(String(acc_to_protein_hit.size()) + " different protein accessions in the file.", 1);
+
+
+			// count the sequences that match a protein accession
+			Map<String, Map<Size, set<String> > > acc_peptides;
+			for (vector<PeptideIdentification>::const_iterator it1 = pep_ids.begin(); it1 != pep_ids.end(); ++it1)
+			{
+				// for all peptide hits
+				for (vector<PeptideHit>::const_iterator it2 = it1->getHits().begin(); it2 != it1->getHits().end(); ++it2)
+				{
+					String pep_seq;
+					if (treat_modification_variants_separately)
+					{
+						pep_seq = it2->getSequence().toString();
+					}
+					else
+					{
+						pep_seq = it2->getSequence().toUnmodifiedString();
+					}
+					Size charge = 0;
+					if (treat_charge_variants_separately)
+					{
+						charge = it2->getCharge();
+					}
+
+					// for all protein accessions
+					for (vector<String>::const_iterator it3 = it2->getProteinAccessions().begin(); it3 != it2->getProteinAccessions().end(); ++it3)
+					{
+						acc_peptides[*it3][charge].insert(pep_seq);
+					}
+				}
+			}
+
+			writeDebug_("Peptides from " + String(acc_peptides.size()) + " recorded.", 1);
+	
+			// for all protein hits for the id run, only accept proteins that have at least 'min_peptides_per_protein' peptides 
+			set<String> accepted_proteins;
+			vector<ProteinHit> accepted_protein_hits;
+			for (Map<String, ProteinHit>::ConstIterator it1 = acc_to_protein_hit.begin(); it1 != acc_to_protein_hit.end(); ++it1)
+			{
+				if (acc_peptides.has(it1->first))
+				{	
+					Size num_peps(0);
+					for (Map<Size, set<String> >::ConstIterator it2 = acc_peptides[it1->first].begin(); it2 != acc_peptides[it1->first].end(); ++it2)
+					{
+						num_peps += it2->second.size();	
+					}
+
+					if (num_peps >= min_peptides_per_protein)
+					{
+						accepted_proteins.insert(it1->first);
+						accepted_protein_hits.push_back(it1->second);
+					}
+				}
+			}
+
+			writeDebug_("Accepted " + String(accepted_protein_hits.size()) + " proteins.", 1);
+			writeDebug_("Accepted " + String(accepted_proteins.size()) + " proteins.", 1);
+
+			// remove peptides that are not accepted
+			for (vector<PeptideIdentification>::iterator it1 = pep_ids.begin(); it1 != pep_ids.end(); ++it1)
+			{
+				vector<PeptideHit> peptide_hits = it1->getHits();
+				it1->setHits(vector<PeptideHit>());
+				for (vector<PeptideHit>::const_iterator it2 = peptide_hits.begin(); it2 != peptide_hits.end(); ++it2)
+				{
+					for (vector<String>::const_iterator it3 = it2->getProteinAccessions().begin(); it3 != it2->getProteinAccessions().end(); ++it3)
+					{
+						if (accepted_proteins.find(*it3) != accepted_proteins.end())
+						{
+							it1->insertHit(*it2);
+							break;
+						}
+					}
+				}
+			}
+
+			// remove proteins that are not accepted
+			prot_ids.resize(1);
+			prot_ids[0].setHits(accepted_protein_hits);
+
+			// fix wrong accessions of the peptides (to proteins that were removed)
+			for (vector<PeptideIdentification>::iterator it1 = pep_ids.begin(); it1 != pep_ids.end(); ++it1)
+      {
+			 	vector<PeptideHit> peptide_ids = it1->getHits();
+        for (vector<PeptideHit>::iterator it2 = peptide_ids.begin(); it2 != peptide_ids.end(); ++it2)
+        {
+					vector<String> valid_accessions;
+          for (vector<String>::const_iterator it3 = it2->getProteinAccessions().begin(); it3 != it2->getProteinAccessions().end(); ++it3)
+          {
+            if (accepted_proteins.find(*it3) != accepted_proteins.end())
+            { 
+						valid_accessions.push_back(*it3);
+            }
+          }
+					it2->setProteinAccessions(valid_accessions);
+				}
+				it1->setHits(peptide_ids);
+      }
+			
 			DateTime now = DateTime::now();
 			String identifier(now.get() + "_TOPPProteinInference");
 			for (vector<PeptideIdentification>::iterator it = pep_ids.begin(); it != pep_ids.end(); ++it)
@@ -181,10 +207,10 @@ class TOPPProteinInference
 				it->setIdentifier(identifier);
 			}
 
-			prot_id[0].setIdentifier(identifier);
+			prot_ids[0].setIdentifier(identifier);
 
 			// write output
-			IdXMLFile().store(out, prot_id, pep_ids);
+			IdXMLFile().store(out, prot_ids, pep_ids);
 
 			return EXECUTION_OK;
 		}
