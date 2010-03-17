a0d4d564cf70eea53dc45042bf782073738c4146
Alexandra Zerck <azerck@users.sourceforge.net>
2010-03-17 21:00:18 +0000

    minor changes
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@6797 6adb6e08-d915-0410-941f-83917bcadc18

--- source/TEST/InclusionExclusionList_test.C
+++ source/TEST/InclusionExclusionList_test.C
@@ -21,8 +21,8 @@
 //  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
 // --------------------------------------------------------------------------
-// $Maintainer: $
-// $Authors: $
+// $Maintainer: Alexandra Zerck $
+// $Authors: Alexandra Zerck $
 // --------------------------------------------------------------------------
 
 #include <OpenMS/CONCEPT/ClassTest.h>
