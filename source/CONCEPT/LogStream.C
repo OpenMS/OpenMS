f599263f9373bb374a51476bb0b8061728b3659f
Chris Bielow <chris.bielow@fu-berlin.de>
2010-04-21 16:15:30 +0000

    ** disabled LOG_DEBUG's default association with a stream
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@6915 6adb6e08-d915-0410-941f-83917bcadc18

--- source/CONCEPT/LogStream.C
+++ source/CONCEPT/LogStream.C
@@ -529,6 +529,6 @@ namespace OpenMS
 	OPENMS_DLLAPI  Logger::LogStream Log_error(new Logger::LogStreamBuf("ERROR"), true, &cerr);
 	OPENMS_DLLAPI  Logger::LogStream Log_warn(new Logger::LogStreamBuf("WARNING"), true, &cout);
 	OPENMS_DLLAPI  Logger::LogStream Log_info(new Logger::LogStreamBuf("INFO"), true, &cout);
-	OPENMS_DLLAPI  Logger::LogStream Log_debug(new Logger::LogStreamBuf("DEBUG"), true, &cout);
+	OPENMS_DLLAPI  Logger::LogStream Log_debug(new Logger::LogStreamBuf("DEBUG"), true);
 
 } // namespace OpenMS
