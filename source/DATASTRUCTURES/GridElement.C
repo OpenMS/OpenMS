7da64f8c90b846fc66b0f251ea34a96afa20103f
Steffen Sass <steffensass@users.sourceforge.net>
2010-04-26 14:35:57 +0000

    New data structure for SILACAnalyzer
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@6941 6adb6e08-d915-0410-941f-83917bcadc18

---
+++ source/DATASTRUCTURES/GridElement.C
@@ -0,0 +1,26 @@
+
+#include <OpenMS/DATASTRUCTURES/GridElement.h>
+#include <iostream>
+#include <cmath>
+
+
+/// Default constructor
+
+/// Copy constructor
+
+/// Detailed constructor
+
+
+
+
+//bool SILACData::operator>(const SILACData &rhs) const // required for built-in STL functions like sort
+//{
+//	if ( *this == rhs)
+//		return false;
+//	else
+//		return (!(*this<rhs));
+//}
+
+
+
+
