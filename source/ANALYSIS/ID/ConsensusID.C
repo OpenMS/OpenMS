763aafa7bbde02873cd412bd29d27f3d26b4f57c
Chris Bielow <chris.bielow@fu-berlin.de>
2010-04-28 11:08:10 +0000

    ** cleaned up external code projects (the external libary now has some actual content)
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@6947 6adb6e08-d915-0410-941f-83917bcadc18

--- source/ANALYSIS/ID/ConsensusID.C
+++ source/ANALYSIS/ID/ConsensusID.C
@@ -4,7 +4,7 @@
 // --------------------------------------------------------------------------
 //                   OpenMS Mass Spectrometry Framework
 // --------------------------------------------------------------------------
-//  Copyright (C) 2003-2009 -- Oliver Kohlbacher, Knut Reinert
+//  Copyright (C) 2003-2010 -- Oliver Kohlbacher, Knut Reinert
 //
 //  This library is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU Lesser General Public
@@ -22,7 +22,7 @@
 //
 // --------------------------------------------------------------------------
 // $Maintainer: Sven Nahnsen $
-// $Authors: Sven Nahnsen and others$
+// $Authors: Sven Nahnsen and others $
 // --------------------------------------------------------------------------
 
 #include <OpenMS/ANALYSIS/ID/ConsensusID.h>
--- source/TEST/EXTERNAL/TestExternalCode.C
+++ source/TEST/EXTERNAL/TestExternalCode.C
@@ -1,7 +1,9 @@
 #include <OpenMS/KERNEL/FeatureMap.h>
 #include <OpenMS/CHEMISTRY/AASequence.h>
+#include "ExampleLibraryFile.h"
 
 using namespace OpenMS;
+using namespace OpenMSExternal;
 
 int main(int argc, char *argv[])
 {
@@ -9,7 +11,8 @@ int main(int argc, char *argv[])
 	FeatureMap<> fm;
 	Feature feature;
 	fm.push_back(feature);
-	
+	std::string s = ExampleLibraryFile::printSomething();
+	std::cout << "From external lib: " << s << "\n";
 	std::cout << "All good and well!\n";
 	
 	return 0;
