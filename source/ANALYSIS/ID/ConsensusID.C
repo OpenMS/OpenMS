6fb1d5862a19d9579b31611252e0590003c1b73e
Chris Bielow <chris.bielow@fu-berlin.de>
2010-04-30 15:03:27 +0000

    ** minor stuff
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@6955 6adb6e08-d915-0410-941f-83917bcadc18

--- source/ANALYSIS/ID/ConsensusID.C
+++ source/ANALYSIS/ID/ConsensusID.C
@@ -53,8 +53,6 @@ namespace OpenMS
 		defaults_.setMinInt("considered_hits",1);
 		defaults_.setValue("number_of_runs",0,"The number of runs used as input. This information is used in 'Ranked' and 'Average' to compute the new scores. If not given, the number of input identifications is taken.");
 		defaults_.setMinInt("number_of_runs",0);
-		defaults_.setValue("mz_tolerance", 0.01, "give a a mz tolerance");
-		defaults_.setValue("rt_tolerance", 0.01, "give an rt tolerance");
 		defaults_.setValue("min_number_of_engines", 2, "The minimum number of search engines used to generate peptide lists");
 		defaults_.setMinInt("min_number_of_engines",2);
 //nice value for mz and RT tolerances 0.01
