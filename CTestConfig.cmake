3dc99b1b42f124112ba4a643470b44c8c726bffc
Mathias Walzer <walzer@informatik.uni-tuebingen.de>
2010-07-20 16:47:35 +0000

    changed CTest drop_location to reflect changed CDash-situation in Tuebinger server-topography
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@7111 6adb6e08-d915-0410-941f-83917bcadc18

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -9,6 +9,6 @@ set(CTEST_NIGHTLY_START_TIME "23:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "www-bs2.informatik.uni-tuebingen.de")
-set(CTEST_DROP_LOCATION "/services/OpenMS_services/CDash/submit.php?project=OpenMS")
+set(CTEST_DROP_LOCATION "/services/OpenMS/CDash/submit.php?project=OpenMS")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
