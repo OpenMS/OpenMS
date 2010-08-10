9ebb13b3b64f2ae971a92238440da70addfe5c47
Chris Bielow <chris.bielow@fu-berlin.de>
2010-08-10 14:59:26 +0000

    ** fix #173 and #174
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@7180 6adb6e08-d915-0410-941f-83917bcadc18

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -2,10 +2,11 @@ if (WIN32)
 	set(OPENMS_LIB_INSTALL_PATH "bin")
 else()	## Linux & MacOS
 	## variables we need to distinguish 32Bit/64Bit versions
-	set(OPENMS_LIB_INSTALL_PATH "lib")
 	if (OPENMS_64BIT_ARCHITECTURE)
 		set(OPENMS_LIB_INSTALL_PATH "lib64")
-	endif()
+  else()
+    set(OPENMS_LIB_INSTALL_PATH "lib")
+  endif()
 endif()
 	
 ## CPack installation and packaging procedures
@@ -16,9 +17,10 @@ install(TARGETS OpenMS EXPORT OpenMSLibExportGroup
   COMPONENT library)
 
 ## create script that allows external projects to use our OpenMS lib
-install(EXPORT OpenMSLibExportGroup DESTINATION cmake/ FILE ${CF_LibOpenMSExport})
+get_filename_component(LibOpenMSExportName "${CF_LibOpenMSExport}" NAME)
+install(EXPORT OpenMSLibExportGroup DESTINATION cmake/ FILE ${LibOpenMSExportName})
 		  
-## install utils
+## install UTILS tools (using 'install(TARGETS UTILS...)' as shortcut does not work)
 foreach(util ${UTILS_executables})
   install(TARGETS ${util}
     RUNTIME DESTINATION bin
@@ -40,9 +42,22 @@ INSTALL(DIRECTORY share/		# warning: that slash(/) is important here, otherwise 
   COMPONENT share
   PATTERN ".svn" EXCLUDE)
 
-## install the documentation and the tutorials
-install(FILES     ${PROJECT_BINARY_DIR}/doc/index.html      DESTINATION share/OpenMS/doc COMPONENT doc)
-install(DIRECTORY ${PROJECT_BINARY_DIR}/doc/html            DESTINATION share/OpenMS/doc COMPONENT doc PATTERN ".svn" EXCLUDE)
-install(FILES ${PROJECT_BINARY_DIR}/doc/OpenMS_tutorial.pdf DESTINATION share/OpenMS/doc COMPONENT doc)
-install(FILES ${PROJECT_BINARY_DIR}/doc/TOPP_tutorial.pdf   DESTINATION share/OpenMS/doc COMPONENT doc)
-
+  
+if (DOXYGEN_FOUND)
+  # 'doc' target exists
+  install(CODE "MESSAGE(\"Installing documentation created from 'doc' target. Make sure this target was called before!\")")
+  ## this does not work yet (CMake 'bug') :  add_dependencies(install doc) ## force 'doc' to be build before installing the resulting files
+  install(FILES     ${PROJECT_BINARY_DIR}/doc/index.html      DESTINATION share/OpenMS/doc COMPONENT doc) 
+  install(DIRECTORY ${PROJECT_BINARY_DIR}/doc/html            DESTINATION share/OpenMS/doc COMPONENT doc PATTERN ".svn" EXCLUDE) 
+  if (DOXYGEN_FOUND AND LATEX_COMPILER AND DVIPS_CONVERTER)
+    # 'doc_tutorials' target exists
+    install(CODE "MESSAGE(\"Installing documentation created from 'doc_tutorials' target. Make sure this target was called before!\")")
+    ## this does not work yet (CMake 'bug') :  add_dependencies(install doc_tutorials) ## force 'doc_tutorials' to be build before installing the resulting files
+    install(FILES ${PROJECT_BINARY_DIR}/doc/OpenMS_tutorial.pdf DESTINATION share/OpenMS/doc COMPONENT doc) 
+    install(FILES ${PROJECT_BINARY_DIR}/doc/TOPP_tutorial.pdf   DESTINATION share/OpenMS/doc COMPONENT doc) 
+  else() 
+    Message(STATUS "Latex missing. Disabling 'tutorials' target installation!") 
+  endif() 
+else() 
+  Message(STATUS "Doxygen missing. Disabling all documentation targets installation!") 
+endif() 
