048fb0678b726b71e57f6ed73d14d662b34baa6f
Mathias Walzer <walzer@informatik.uni-tuebingen.de>
2010-11-02 16:42:16 +0000

    set deb dependencies for openms with CPack
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@7529 6adb6e08-d915-0410-941f-83917bcadc18

--- cmake/package_deb.cmake
+++ cmake/package_deb.cmake
@@ -4,6 +4,7 @@ include(cmake/install_common.cmake)
 set(CPACK_DEBIAN_PACKAGE_MAINTAINER "bielow@mi.fu-berlin.de")
 set(CPACK_GENERATOR "DEB")
 set(CPACK_COMPONENTS_ALL applications library share)
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "cmake (>= 2.6), g++ (>= 4.0), autoconf (>= 2.6), qt4-dev-tools, patch, libtool")
 
 ## should be the last include
 include(CPack)
