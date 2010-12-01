5439b2f95e8e96d55c42170a868af46055e744be
Chris Bielow <chris.bielow@fu-berlin.de>
2010-12-01 13:28:46 +0000

    ** added FASTA output to Digestor
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/Sim2@7630 6adb6e08-d915-0410-941f-83917bcadc18

--- source/APPLICATIONS/UTILS/Digestor.C
+++ source/APPLICATIONS/UTILS/Digestor.C
@@ -22,9 +22,10 @@
 //
 // --------------------------------------------------------------------------
 // $Maintainer: Chris Bielow $
-// $Authors: $
+// $Authors: Nico Pfeiffer, Chris Bielow $
 // --------------------------------------------------------------------------
 
+#include <OpenMS/FORMAT/FileHandler.h>
 #include <OpenMS/FORMAT/IdXMLFile.h>
 #include <OpenMS/FORMAT/FASTAFile.h>
 #include <OpenMS/METADATA/ProteinIdentification.h>
@@ -86,7 +87,8 @@ class TOPPDigestor
 			registerInputFile_("in","<file>","","input file");
       setValidFormats_("in",StringList::create("FASTA"));
 			registerOutputFile_("out","<file>","","output file (peptides)\n");
-      setValidFormats_("out",StringList::create("idXML"));
+      registerStringOption_("out_type", "<type>","", "out type", false);
+      setValidStrings_("out_type",StringList::create("idXML,FASTA"));
 
       registerIntOption_("missed_cleavages","<number>",1,"the number of allowed missed cleavages", false);
 			setMinInt_("missed_cleavages", 0);
@@ -101,8 +103,6 @@ class TOPPDigestor
 			vector<PeptideIdentification> identifications;
 			vector< String > peptides;
 			vector<PeptideHit> temp_peptide_hits;
-			std::vector<FASTAFile::FASTAEntry> protein_data;
-			FASTAFile file;
 			EnzymaticDigestion digestor;
 			vector<AASequence> temp_peptides;
 			PeptideIdentification peptide_identification;
@@ -111,25 +111,39 @@ class TOPPDigestor
 			ProteinHit temp_protein_hit;
 			vector<String> protein_accessions;
 			vector<String> parts;
-			String inputfile_name = "";
-			String outputfile_name = "";
-			UInt min_size = 0;
-			UInt missed_cleavages = 0;
 			ProteinIdentification::SearchParameters search_parameters;
 			
 			protein_identifications.push_back(ProteinIdentification());
 			//-------------------------------------------------------------
 			// parsing parameters
 			//-------------------------------------------------------------
-			inputfile_name = getStringOption_("in");			
-			outputfile_name = getStringOption_("out");	
-			min_size = getIntOption_("min_length");
-			missed_cleavages = getIntOption_("missed_cleavages");
+			String inputfile_name = getStringOption_("in");			
+			String outputfile_name = getStringOption_("out");	
+
+		  //input file type
+		  FileHandler fh;
+		  FileTypes::Type out_type = fh.nameToType(getStringOption_("out_type"));
+
+		  if (out_type==FileTypes::UNKNOWN)
+		  {
+			  out_type = fh.getType(outputfile_name);
+			  writeDebug_(String("Output file type: ") + fh.typeToName(out_type), 2);
+		  }
+
+		  if (out_type==FileTypes::UNKNOWN)
+		  {
+			  writeLog_("Error: Could not determine output file type!");
+			  return PARSE_ERROR;
+		  }
+
+			UInt min_size = getIntOption_("min_length");
+			UInt missed_cleavages = getIntOption_("missed_cleavages");
 			
 			//-------------------------------------------------------------
 			// reading input
 			//-------------------------------------------------------------
-			
+			FASTAFile file;
+			std::vector<FASTAFile::FASTAEntry> protein_data;
 			file.load(inputfile_name, protein_data);
 			//-------------------------------------------------------------
 			// calculations
@@ -141,7 +155,10 @@ class TOPPDigestor
 			digestor.setMissedCleavages(missed_cleavages);
 			
 			protein_accessions.resize(1, String(""));
-			for (Size i = 0; i < protein_data.size(); ++i)
+
+			std::vector<FASTAFile::FASTAEntry> all_peptides;
+
+      for (Size i = 0; i < protein_data.size(); ++i)
 			{
 				protein_accessions[0] = protein_data[i].identifier;
 				temp_protein_hit.setSequence(protein_data[i].sequence);
@@ -155,6 +172,9 @@ class TOPPDigestor
 					{
 						temp_peptide_hit.setSequence(temp_peptides[j]);
 						peptide_identification.insertHit(temp_peptide_hit);
+            // for FASTA file output
+            FASTAFile::FASTAEntry pep(protein_data[i].identifier, protein_data[i].description, temp_peptides[j].toString());
+            all_peptides.push_back(pep);
 					}
 				}
 				protein_identifications[0].insertHit(temp_protein_hit);
@@ -173,10 +193,16 @@ class TOPPDigestor
 			//-------------------------------------------------------------
 			// writing output
 			//-------------------------------------------------------------
-			
-			IdXML_file.store(outputfile_name,
-											 protein_identifications,
-											 identifications);
+      if (out_type==FileTypes::FASTA)
+      {
+        file.store(outputfile_name, all_peptides);
+      }
+      else
+      {
+			  IdXML_file.store(outputfile_name,
+											   protein_identifications,
+											   identifications);
+      }
 
 			return EXECUTION_OK;
 		}
