5626dca05aa3e4b7bd33c440b8c41bc3e96a6e4c
Sandro Andreotti <andreott@users.sourceforge.net>
2011-02-04 15:51:11 +0000

    completes commit  7891
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@7896 6adb6e08-d915-0410-941f-83917bcadc18

--- source/ANALYSIS/TARGETED/OfflinePrecursorIonSelection.C
+++ source/ANALYSIS/TARGETED/OfflinePrecursorIonSelection.C
@@ -43,6 +43,8 @@ OfflinePrecursorIonSelection::OfflinePrecursorIonSelection() : DefaultParamHandl
 	defaults_.setMinInt("ms2_spectra_per_rt_bin",1);
 	defaults_.setValue("min_peak_distance",3.,"The minimal distance (in Da) of two peaks in one spectrum so that they can be selected.");
 	defaults_.setMinFloat("min_peak_distance",0.);
+	defaults_.setValue("selection_window",2.,"All peaks within a mass window (in Da) of a selected peak are also selected for fragmentation.");
+	defaults_.setMinFloat("min_peak_distance",0.);
 	defaults_.setValue("exclude_overlapping_peaks","false","If true overlapping or nearby peaks (within min_peak_distance) are excluded for selection.");
 	defaults_.setValidStrings("exclude_overlapping_peaks",StringList::create("true,false"));
 	defaults_.setValue("use_dynamic_exclusion","false","If true dynamic exclusion is applied.");
@@ -55,19 +57,6 @@ OfflinePrecursorIonSelection::OfflinePrecursorIonSelection() : DefaultParamHandl
 OfflinePrecursorIonSelection::~OfflinePrecursorIonSelection()
 {
 
-}
-
-
-void OfflinePrecursorIonSelection::updateExclusionList_(std::vector<std::pair<Size,Size> >& exclusion_list)
-{
-	for(Size i = 0; i < exclusion_list.size();++i)
-		{
-			if(exclusion_list[i].second > 0) --exclusion_list[i].second;
-		}
-	sort(exclusion_list.begin(),exclusion_list.end(),PairComparatorSecondElementMore<std::pair<Size,Size> >());
-	std::vector<std::pair<Size,Size> >::iterator iter = exclusion_list.begin();
-	while(iter != exclusion_list.end() && iter->second != 0) ++iter;
-	exclusion_list.erase(iter,exclusion_list.end());
 }
 
 }
