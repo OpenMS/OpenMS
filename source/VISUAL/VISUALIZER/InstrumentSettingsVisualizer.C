ee9af7b1a5ecfa7a47bb09f52b7818bb47f93bb8
Chris Bielow <chris.bielow@fu-berlin.de>
2011-02-16 16:32:15 +0000

    ** fixed more Copyright header issues
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/Release1.8@7954 6adb6e08-d915-0410-941f-83917bcadc18

--- source/VISUAL/VISUALIZER/InstrumentSettingsVisualizer.C
+++ source/VISUAL/VISUALIZER/InstrumentSettingsVisualizer.C
@@ -4,7 +4,7 @@
 // --------------------------------------------------------------------------
 //                   OpenMS Mass Spectrometry Framework
 // --------------------------------------------------------------------------
-//  Copymain (C) 2003-2010 -- Oliver Kohlbacher, Knut Reinert
+//  Copymain (C) 2003-2011 -- Oliver Kohlbacher, Knut Reinert
 //
 //  This library is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU Lesser General Public
