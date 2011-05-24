7384c378417e8f232aead7daa03ae08b820c1b31
Timo Sachsenberg <sachsenb@informatik.uni-tuebingen.de>
2011-05-24 14:35:59 +0000

    revert to previous: wrong commit
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@8321 6adb6e08-d915-0410-941f-83917bcadc18

--- source/CHEMISTRY/ElementDB.C
+++ source/CHEMISTRY/ElementDB.C
@@ -39,7 +39,7 @@ namespace OpenMS
 {
 	ElementDB::ElementDB()
 	{
-    readFromFile_("CHEMISTRY/Elements.xml");
+		readFromFile_("CHEMISTRY/Elements.xml");
 	}
 
 	ElementDB::~ElementDB()
@@ -97,88 +97,45 @@ namespace OpenMS
 		return (atomic_numbers_.has(atomic_number));
 	}
 	
-  double ElementDB::calculateAvgWeight_(const Map<UInt, double>& Z_to_abundance, const Map<UInt, double>& Z_to_mass)
-  {
-    double avg = 0;
-    // extract Zs
-    vector<UInt> keys;
-    for (Map<UInt, double>::const_iterator it = Z_to_abundance.begin(); it != Z_to_abundance.end(); ++it)
-    {
-      keys.push_back(it->first);
-    }
-
-    // calculate weighted average
-    for (vector<UInt>::iterator it = keys.begin(); it != keys.end(); ++it)
-    {
-      avg += Z_to_mass[*it] * Z_to_abundance[*it];
-    }
-
-    return avg;
-  }
-
-  double ElementDB::calculateMonoWeight_(const Map<UInt, double>& Z_to_mass)
-  {
-    double smallest_weight = 1e10;
-
-    for (Map<UInt, double>::const_iterator it = Z_to_mass.begin(); it != Z_to_mass.end(); ++it)
-    {
-      if (it->second < smallest_weight)
-      {
-        smallest_weight = it->second;
-      }
-    }
-
-    return smallest_weight;
-  }
-
 	void ElementDB::readFromFile_(const String& file_name) 
 	{
 		String file = File::find(file_name);
 
-    // load elements into param object
 		Param param;
 		param.load(file);
 
 		UInt an(0);
+		double avg_weight(0), mono_weight(0);
 		String name, symbol;
 		
-    // determine prefix
+		// build prefix
 		vector<String> split;
-    param.begin().getName().split(':', split);
+		param.begin().getName().split(':',split);
 		String prefix("");
-    for (Size i = 0; i < split.size()-1; ++i)
+		for (Size i=0;i<split.size()-1;++i)
 		{
 			prefix += split[i]+":";
 		}
-    //cout << "first element prefix=" << prefix << endl;
-
-    Map<UInt, double> Z_to_abundancy;
-    Map<UInt, double> Z_to_mass;
 		
-    for (Param::ParamIterator it = param.begin(); it != param.end(); ++it)
+		Map<UInt, double> distribution;
+		
+		for (Param::ParamIterator it=param.begin(); it!=param.end(); ++it)
 		{
-      // new element started?
 			if (!it.getName().hasPrefix(prefix))
 			{
 				// update prefix
 				it.getName().split(':',split);
 				prefix = "";
-        for (Size i = 0;i < split.size()-1; ++i)
+				for (Size i=0;i<split.size()-1;++i)
 				{
 					prefix += split[i]+":";
 				}
-        // cout << "new element prefix=" << prefix << endl;
 
-        // Parsing of previous element is finished. Now store data in Element object
-        IsotopeDistribution isotopes = parseIsotopeDistribution_(Z_to_abundancy);
-        double avg_weight = calculateAvgWeight_(Z_to_abundancy, Z_to_mass);
-        double mono_weight = calculateMonoWeight_(Z_to_mass);
+				IsotopeDistribution isotopes = parseIsotopeDistribution_(distribution);
 
-        /* // print information about elements
-        cout << "Name: " << name << " AtomicNumber: " << an << " AvgWeight: " << avg_weight
-             << " MonoWeight: " << mono_weight << " NIsotopes: " << isotopes.size() << endl;
-        */
+				distribution.clear();
 
+				// new element to be build
 				Element* e = new Element(name, symbol, an, avg_weight, mono_weight, isotopes);
 				names_[name] = e;
 				symbols_[symbol] = e;
@@ -189,35 +146,23 @@ namespace OpenMS
 				{
 					String iso_name = "(" + String(iit->first) + ")" + name;
 					String iso_symbol = "(" + String(iit->first) + ")" + symbol;
-
-          // set avg and mono to same value for isotopes (old hack...)
-          DoubleReal iso_avg_weight = Z_to_mass[iit->first];
+					DoubleReal iso_avg_weight = mono_weight + (iit->first - isotopes.begin()->first); // TODO exact masses
 					DoubleReal iso_mono_weight = iso_avg_weight;
 					IsotopeDistribution iso_isotopes;
 					vector<pair<Size, double> > iso_container;
 					iso_container.push_back(make_pair(iit->first, 1.0));
 					iso_isotopes.set(iso_container);
-
-          /* // print name, symbal and atomic mass of the current isotope
-          cout << "Isotope Name: " << iso_name << " Symbol: " << iso_symbol << " AtomicMass: " << iso_mono_weight << endl;
-          */
-
 					Element* iso_e = new Element(iso_name, iso_symbol, an, iso_avg_weight, iso_mono_weight, iso_isotopes);
 					names_[iso_name] = iso_e;
 					names_[iso_symbol] = iso_e;
 				}
-
-        Z_to_abundancy.clear();
-        Z_to_mass.clear();
 			}
 
-      // top level: read the contents of the element section
+			// read the contents of the element section
 			it.getName().split(':',split);
 			String key = split[2];
 			String value = it->value;
 			value.trim();
-
-      // cout << "Key=" << key << endl;
 
 			if (key == "AtomicNumber")
 			{
@@ -225,46 +170,47 @@ namespace OpenMS
 			}
 			else
 			{
-        if (key == "Isotopes")
-        {
-          UInt Z = UInt(split[3].toInt());
-          String item = split[4];
-          if (item == "RelativeAbundance")
-          {
-            Z_to_abundancy[Z] = double(value.toDouble()/100.0);
-          } else
-          if (item == "AtomicMass")
-          {
-            Z_to_mass[Z] = double(value.toDouble());
-          } else
-          {
-            cerr << "read unknown item in Isotopes: " << item << endl;
-          }
-        }
-        else
-        {
-          if (key == "Name")
-          {
-            name = value;
-          }
-          else
-          {
-            if (key == "Symbol")
-            {
-              symbol = value;
-            }
-            else
-            {
-              cerr << "read unknown tag: " << key << endl;
-            }
-          }
-        }
-      }
-    }
+				if (key == "AverageWeight")
+				{
+					avg_weight = value.toDouble();
+				}
+				else
+				{
+					if (key == "Isotopes")
+					{
+						distribution[UInt(split[3].toInt())] = double(value.toDouble()/100);
+					}
+					else
+					{
+						if (key == "Name")
+						{
+							name = value;
+						}
+						else
+						{
+							if (key == "Symbol")
+							{
+								symbol = value;
+							}
+							else
+							{
+								if (key == "MonoWeight")
+								{
+									mono_weight = value.toDouble();
+								}
+								else
+								{
+									cerr << "read unknown tag: " << key << endl;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 
 		// build last element
-    double avg_weight(0), mono_weight(0);
-    IsotopeDistribution isotopes = parseIsotopeDistribution_(Z_to_abundancy);
+		IsotopeDistribution isotopes = parseIsotopeDistribution_(distribution);
 		Element * e = new Element(name, symbol, an, avg_weight, mono_weight, isotopes);
 		names_[name] = e;
 		symbols_[symbol] = e;
@@ -274,7 +220,7 @@ namespace OpenMS
 	IsotopeDistribution ElementDB::parseIsotopeDistribution_(const Map<UInt, double>& distribution) 
 	{
 		IsotopeDistribution::ContainerType dist;
-    for (Map<UInt, double>::ConstIterator it = distribution.begin(); it != distribution.end(); ++it)
+		for (Map<UInt, double>::ConstIterator it=distribution.begin(); it!=distribution.end(); ++it)
 		{
 			dist.push_back(make_pair(it->first, it->second));
 		}
