64cef33925569eeaa9d3ad3580c9d5b3a0257c30
Sven Nahnsen <sven_nahnsen@users.sourceforge.net>
2011-05-24 12:14:36 +0000

    prefix option added
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@8312 6adb6e08-d915-0410-941f-83917bcadc18

--- source/APPLICATIONS/TOPP/PeptideIndexer.C
+++ source/APPLICATIONS/TOPP/PeptideIndexer.C
@@ -290,6 +290,7 @@ class TOPPPeptideIndexer
 			setValidFormats_("in", StringList::create("IdXML"));
 			registerStringOption_("decoy_string", "<string>", "_rev", "String that was appended to the accession of the protein database to indicate a decoy protein.", false);
 			registerFlag_("write_protein_sequence", "If set, the protein sequences are stored as well.");
+			registerFlag_("prefix", "If set, the decoy_string is supposed to be appended as a prefix.");
 			registerFlag_("keep_unreferenced_proteins", "If set, protein hits which are not referenced by any peptide are kept.");
       registerFlag_("allow_unmatched", "If set, unmatched peptide sequences are allowed. By default (i.e. not set) the program terminates with error status on unmatched peptides.");
       registerIntOption_("aaa_max", "<AA count>", 4, "Maximal number of ambiguous amino acids (AAA) allowed when matching to a protein DB with AAA's. AAA's are 'B', 'Z', and 'X'", false);
@@ -304,6 +305,7 @@ class TOPPPeptideIndexer
 			String in(getStringOption_("in"));
 			String out(getStringOption_("out"));
 			bool write_protein_sequence(getFlag_("write_protein_sequence"));
+			bool prefix(getFlag_("prefix"));
 			bool keep_unreferenced_proteins(getFlag_("keep_unreferenced_proteins"));
       bool allow_unmatched(getFlag_("allow_unmatched"));
       
@@ -488,8 +490,16 @@ class TOPPPeptideIndexer
 					bool matches_decoy(false);
 					for (vector<String>::const_iterator it = it2->getProteinAccessions().begin(); it != it2->getProteinAccessions().end(); ++it)
 					{
-						if (it->hasSuffix(decoy_string)) matches_decoy = true;
-						else matches_target = true;
+						if(prefix)
+						{
+							if (it->hasPrefix(decoy_string)) matches_decoy = true;
+							else matches_target = true;
+						}
+						else
+						{
+							if (it->hasSuffix(decoy_string)) matches_decoy = true;
+							else matches_target = true;
+						}
 					}
 					StringList target_decoy;
 					if (matches_target) target_decoy.push_back("target");
