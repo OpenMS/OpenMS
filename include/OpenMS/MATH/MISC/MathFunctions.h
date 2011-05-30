990d824ce5bd715c7285e5d8bbdbf85d334cb781
Timo Sachsenberg <sachsenb@informatik.uni-tuebingen.de>
2011-05-30 07:52:03 +0000

    imslib math functions integration
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@8329 6adb6e08-d915-0410-941f-83917bcadc18

--- include/OpenMS/MATH/MISC/MathFunctions.h
+++ include/OpenMS/MATH/MISC/MathFunctions.h
@@ -145,6 +145,69 @@ namespace OpenMS
 			return std::abs(a - b) <= tol;
 		}
 
+    /**
+      @brief Returns the greatest common divisor (gcd) of two numbers by applying the Euclidean algorithm.
+      @param a A number.
+      @param b A number.
+      @return The greatest common divisor.
+      @see gcd(T a, T b, T& a1, T& b1)
+      @ingroup MathFunctionsMisc
+     */
+    template <typename T>
+    T gcd(T a, T b)
+    {
+      T c;
+      while (b != 0)
+      {
+        c = a % b;
+        a = b;
+        b = c;
+      }
+      return a;
+    }
+
+
+    /**
+     @brief Returns the greatest common divisor by applying the extended Euclidean algorithm (Knuth TAoCP vol. 2, p342).
+     Calculates u1, u2 and u3 (which is returned) so that a * u1 + b * u2 = u3 = gcd(a, b, u1, u2)
+
+     @param a A number.
+     @param b A number.
+     @param u1 A reference to the number to be returned (see the above formula).
+     @param u2 A reference to the number to be returned (see the above formula).
+     @return The greatest common divisor.
+     @see gcd(T, T)
+     @ingroup MathFunctionsMisc
+     */
+    template <typename T>
+    T gcd(T a, T b, T& u1, T& u2)
+    {
+      u1 = 1;
+      u2 = 0;
+      T u3 = a;
+
+      T v1 = 0;
+      T v2 = 1;
+      T v3 = b;
+
+      while (v3 != 0)
+      {
+        T q = u3 / v3;
+        T t1 = u1 - v1*q;
+        T t2 = u2 - v2*q;
+        T t3 = u3 - v3*q;
+
+        u1 = v1;
+        u2 = v2;
+        u3 = v3;
+
+        v1 = t1;
+        v2 = t2;
+        v3 = t3;
+      }
+
+      return u3;
+    }
 	} // namespace Math
 } // namespace OpenMS
 
