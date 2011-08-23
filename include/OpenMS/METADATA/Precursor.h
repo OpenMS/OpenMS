32f5de7d16feae8a36d67a04df65d55f77d5b820
Mathias Walzer <walzer@informatik.uni-tuebingen.de>
2011-08-23 18:52:38 +0000

    added inline function for convenience: getUnchargedMass, assigned myself as maintainer
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@8650 6adb6e08-d915-0410-941f-83917bcadc18

--- include/OpenMS/METADATA/Precursor.h
+++ include/OpenMS/METADATA/Precursor.h
@@ -10,7 +10,7 @@
 //  modify it under the terms of the GNU Lesser General Public
 //  License as published by the Free Software Foundation; either
 //  version 2.1 of the License, or (at your option) any later version.
-// 
+//
 //  This library is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
@@ -21,8 +21,8 @@
 //  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
 // --------------------------------------------------------------------------
-// $Maintainer: Andreas Bertsch $
-// $Authors: Marc Sturm $
+// $Maintainer: Mathias Walzer $
+// $Authors: Marc Sturm, Mathias Walzer $
 // --------------------------------------------------------------------------
 
 #ifndef OPENMS_METADATA_PRECURSOR_H
@@ -30,28 +30,29 @@
 
 #include <OpenMS/KERNEL/Peak1D.h>
 #include <OpenMS/METADATA/CVTermList.h>
+#include <OpenMS/CONCEPT/Constants.h>
 
 #include <set>
 
-namespace OpenMS 
+namespace OpenMS
 {
 	/**
 		@brief Precursor meta information.
-		
+
 		This class contains precursor information:
     - isolation window
     - activation
     - selected ion (m/z, intensity, charge, possible charge states)
-  
+
 		@ingroup Metadata
-	*/  
+	*/
   class OPENMS_DLLAPI Precursor
     : public CVTermList,
 			public Peak1D
   {
-    
+
     public:
-    
+
     /// Method of activation
       enum ActivationMethod
       {
@@ -72,14 +73,14 @@ namespace OpenMS
       };
 			/// Names of activation methods
 			static const std::string NamesOfActivationMethod[SIZE_OF_ACTIVATIONMETHOD];
-      
+
       /// Constructor
       Precursor();
       /// Copy constructor
       Precursor(const Precursor& source);
       /// Destructor
       virtual ~Precursor();
-      
+
       /// Assignment operator
       Precursor& operator= (const Precursor& source);
 
@@ -87,43 +88,51 @@ namespace OpenMS
       bool operator== (const Precursor& rhs) const;
       /// Equality operator
       bool operator!= (const Precursor& rhs) const;
-			
+
 			/// returns a const reference to the activation methods
       const std::set<ActivationMethod>& getActivationMethods() const;
 			/// returns a mutable reference to the activation methods
       std::set<ActivationMethod>& getActivationMethods();
       /// sets the activation methods
       void setActivationMethods(const std::set<ActivationMethod>& activation_methods);
-			
+
 			/// returns the activation energy (in electronvolt)
       DoubleReal getActivationEnergy() const;
       /// sets the activation energy (in electronvolt)
       void setActivationEnergy(DoubleReal activation_energy);
-      
+
       /// returns the lower offset from the target m/z
       DoubleReal getIsolationWindowLowerOffset() const;
       /// sets the lower offset from the target m/z
       void setIsolationWindowLowerOffset(DoubleReal bound);
-      
+
       /// returns the upper offset from the target m/z
       DoubleReal getIsolationWindowUpperOffset() const;
       /// sets the upper offset from the target m/z
       void setIsolationWindowUpperOffset(DoubleReal bound);
-      
+
       /// Non-mutable access to the charge
       Int getCharge() const;
       /// Mutable access to the charge
       void setCharge( Int charge );
-      
+
       ///Mutable access to possible charge states
       std::vector<Int>& getPossibleChargeStates();
       ///Non-mutable access to possible charge states
       const std::vector<Int>& getPossibleChargeStates() const;
       ///Sets the possible charge states
       void setPossibleChargeStates(const std::vector<Int>& possible_charge_states);
- 
+
+	/// Returns the uncharged mass of the precursor, if charge is unknown, i.e. 0 best guess is its doubly charged
+	inline DoubleReal getUnchargedMass() const
+	{
+		int c = charge_;
+		(c==0)?c=2:c=charge_;
+		return (getMZ()*c - c*Constants::PROTON_MASS_U);
+	}
+
     protected:
-      
+
       std::set<ActivationMethod> activation_methods_;
       DoubleReal activation_energy_;
       DoubleReal window_low_;
