680fe7a02a65223e5310bd53634e36b90e39b4c3
Stephan Aiche <stephan.aiche@fu-berlin.de>
2011-10-06 13:10:07 +0000

    fixed most of the "Inconsistent maintainers ..." warnings #checker
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@8936 6adb6e08-d915-0410-941f-83917bcadc18

--- include/OpenMS/FORMAT/MascotRemoteQuery.h
+++ include/OpenMS/FORMAT/MascotRemoteQuery.h
@@ -21,7 +21,7 @@
 //  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
 // --------------------------------------------------------------------------
-// $Maintainer: Chris Bielow$
+// $Maintainer: Chris Bielow $
 // $Authors: Andreas Bertsch, Daniel Jameson, Chris Bielow$
 // --------------------------------------------------------------------------
 
