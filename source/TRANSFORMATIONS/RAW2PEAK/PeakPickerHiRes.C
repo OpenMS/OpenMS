e500a5868eba41f5276975c9a3e874a98c20f7d7
Erhan Kenar <kenar@informatik.uni-tuebingen.de>
2011-10-13 13:48:04 +0000

    yet another PeakPickerHiRes fix... switch on/off SNT estimation
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@8986 6adb6e08-d915-0410-941f-83917bcadc18

--- source/TRANSFORMATIONS/RAW2PEAK/PeakPickerHiRes.C
+++ source/TRANSFORMATIONS/RAW2PEAK/PeakPickerHiRes.C
@@ -38,7 +38,7 @@ namespace OpenMS
 			ProgressLogger()
   {
 		// set default parameter values
-		defaults_.setValue("signal_to_noise", 1.0, "Minimal signal-to-noise ratio for a peak to be picked.");
+                defaults_.setValue("signal_to_noise", 1.0, "Minimal signal-to-noise ratio for a peak to be picked (0.0 disables SNT estimation!)");
 		defaults_.setMinFloat("signal_to_noise", 0.0);
 
 		defaults_.setValue("ms1_only", "false", "If true, peak picking is only applied to MS1 scans. Other scans are copied to the output without changes.");
