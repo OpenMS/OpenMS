dbb62590daacb377d648fbab4f9f1fd8f3de211e
Stephan Aiche <stephan.aiche@fu-berlin.de>
2011-11-21 10:05:59 +0000

    implemented IMSElement test #341
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@9199 6adb6e08-d915-0410-941f-83917bcadc18

--- source/TEST/IMSElement_test.C
+++ source/TEST/IMSElement_test.C
@@ -44,6 +44,14 @@ START_TEST(IMSElement, "$Id$")
 
 IMSElement* ptr = 0;
 IMSElement* null_ptr = 0;
+
+IMSIsotopeDistribution::peaks_container peaks;
+peaks.push_back(IMSIsotopeDistribution::peak_type(0.0078250319,.999885));
+peaks.push_back(IMSIsotopeDistribution::peak_type(0.01410178,.000115));
+peaks.push_back(IMSIsotopeDistribution::peak_type(0.01604927,.0));
+
+IMSIsotopeDistribution iso(peaks, 1);
+
 START_SECTION(IMSElement())
 {
 	ptr = new IMSElement();
@@ -57,117 +65,201 @@ START_SECTION(~IMSElement())
 }
 END_SECTION
 
-START_SECTION((IMSElement(const IMSElement &element)))
-{
-  // TODO
-}
-END_SECTION
+
+IMSElement * hydrogen = 0;
 
 START_SECTION((IMSElement(const name_type &name, const isotopes_type &isotopes)))
 {
-  IMSIsotopeDistribution::peaks_container peaks;
-  peaks.push_back(IMSIsotopeDistribution::peak_type(1.0078250319,.999885));
-  peaks.push_back(IMSIsotopeDistribution::peak_type(2.01410178,.000115));
-  peaks.push_back(IMSIsotopeDistribution::peak_type(3.01604927,.0));
-
-  IMSIsotopeDistribution iso(peaks, 1);
-
-  IMSElement * hydrogen = new IMSElement("H",iso);
+  hydrogen = new IMSElement("H",iso);
   TEST_NOT_EQUAL(hydrogen, null_ptr)
   TEST_STRING_EQUAL(hydrogen->getName(), "H")
   TEST_EQUAL(hydrogen->getIsotopeDistribution(), iso)
 }
 END_SECTION
 
+START_SECTION((IMSElement(const IMSElement &element)))
+{
+  IMSElement hydrogen_copy(*hydrogen);
+  TEST_EQUAL(hydrogen->getAverageMass(), hydrogen_copy.getAverageMass())
+  TEST_EQUAL(hydrogen->getIonMass(), hydrogen_copy.getIonMass())
+  TEST_EQUAL(hydrogen->getIsotopeDistribution(), hydrogen_copy.getIsotopeDistribution())
+  TEST_EQUAL(hydrogen->getMass(), hydrogen_copy.getMass())
+  TEST_EQUAL(hydrogen->getName(), hydrogen_copy.getName())
+  TEST_EQUAL(hydrogen->getNominalMass(), hydrogen_copy.getNominalMass())
+  TEST_EQUAL(hydrogen->getSequence(), hydrogen_copy.getSequence())
+}
+END_SECTION
+
 START_SECTION((IMSElement(const name_type &name, mass_type mass)))
 {
-  // TODO
+  DoubleReal oxygen_mass = 15.9994;
+  IMSElement element("O", oxygen_mass);
+  IMSIsotopeDistribution oxygen(oxygen_mass);
+
+  TEST_EQUAL(element.getName(), "O")
+  TEST_EQUAL(element.getNominalMass(), 0)
+  TEST_EQUAL(element.getMass(), oxygen_mass)
+  TEST_EQUAL(element.getIsotopeDistribution(), oxygen)
 }
 END_SECTION
 
 START_SECTION((IMSElement(const name_type &name, nominal_mass_type nominal_mass=0)))
 {
-  // TODO
+  IMSElement::nominal_mass_type nominal_mass = 16;
+  IMSElement element("O", nominal_mass);
+  IMSIsotopeDistribution oxygen(nominal_mass);
+
+  TEST_EQUAL(element.getName(), "O")
+  TEST_EQUAL(element.getNominalMass(), nominal_mass)
+  TEST_EQUAL(element.getIsotopeDistribution(), oxygen)
 }
 END_SECTION
 
 START_SECTION((const name_type& getName() const ))
 {
-  // TODO
+  TEST_STRING_SIMILAR(hydrogen->getName(), "H")
 }
 END_SECTION
 
 START_SECTION((void setName(const name_type &name)))
 {
-  // TODO
+  hydrogen->setName("D");
+  TEST_STRING_SIMILAR(hydrogen->getName(), "D")
+  hydrogen->setName("H");
+  TEST_STRING_SIMILAR(hydrogen->getName(), "H")
 }
 END_SECTION
 
 START_SECTION((const name_type& getSequence() const ))
 {
-  // TODO
+  TEST_STRING_SIMILAR(hydrogen->getSequence(), "H")
 }
 END_SECTION
 
 START_SECTION((void setSequence(const name_type &sequence)))
 {
-  // TODO
+  hydrogen->setSequence("H2");
+  TEST_STRING_SIMILAR(hydrogen->getSequence(), "H2")
+  hydrogen->setSequence("H");
+  TEST_STRING_SIMILAR(hydrogen->getSequence(), "H")
 }
 END_SECTION
 
 START_SECTION((nominal_mass_type getNominalMass() const ))
 {
-  // TODO
+  TEST_EQUAL(hydrogen->getNominalMass(), iso.getNominalMass())
 }
 END_SECTION
 
 START_SECTION((mass_type getMass(size_type index=0) const ))
 {
-  // TODO
+  TEST_EQUAL(hydrogen->getMass(), 1.0078250319)
+  TEST_EQUAL(hydrogen->getMass(0), 1.0078250319)
+  TEST_EQUAL(hydrogen->getMass(1), 2.01410178)
+  TEST_EQUAL(hydrogen->getMass(2), 3.01604927)
 }
 END_SECTION
 
 START_SECTION((mass_type getAverageMass() const ))
 {
-  // TODO
+  TEST_EQUAL(hydrogen->getAverageMass(), iso.getAverageMass())
 }
 END_SECTION
 
 START_SECTION((mass_type getIonMass(int electrons_number=1) const ))
 {
-  // TODO
+  DoubleReal expected_ion_mass = hydrogen->getMass() - IMSElement::ELECTRON_MASS_IN_U;
+  TEST_EQUAL(hydrogen->getIonMass(), expected_ion_mass)
+  TEST_EQUAL(hydrogen->getIonMass(1), expected_ion_mass)
+
+  expected_ion_mass = hydrogen->getMass() - 2 * IMSElement::ELECTRON_MASS_IN_U;
+  TEST_EQUAL(hydrogen->getIonMass(2), expected_ion_mass)
 }
 END_SECTION
 
 START_SECTION((const IMSIsotopeDistribution& getIsotopeDistribution() const ))
 {
-  // TODO
+  TEST_EQUAL(hydrogen->getIsotopeDistribution(), iso)
 }
 END_SECTION
 
 START_SECTION((void setIsotopeDistribution(const IMSIsotopeDistribution &isotopes)))
 {
-  // TODO
+  IMSIsotopeDistribution::peaks_container peaks_copy(peaks);
+  peaks_copy.push_back(IMSIsotopeDistribution::peak_type(0.03604927,.0));
+  IMSIsotopeDistribution modified_iso(peaks_copy, 1);
+  hydrogen->setIsotopeDistribution(modified_iso);
+  TEST_EQUAL(hydrogen->getIsotopeDistribution(), modified_iso)
+  hydrogen->setIsotopeDistribution(iso);
+  TEST_EQUAL(hydrogen->getIsotopeDistribution(), iso)
 }
 END_SECTION
 
 START_SECTION((IMSElement& operator=(const IMSElement &element)))
 {
-  // TODO
+  IMSElement hydrogen_copy;
+  hydrogen_copy = *hydrogen;
+
+  TEST_EQUAL(*hydrogen == hydrogen_copy, true)
+  TEST_EQUAL(hydrogen->getAverageMass(), hydrogen_copy.getAverageMass())
+  TEST_EQUAL(hydrogen->getIonMass(), hydrogen_copy.getIonMass())
+  TEST_EQUAL(hydrogen->getIsotopeDistribution(), hydrogen_copy.getIsotopeDistribution())
+  TEST_EQUAL(hydrogen->getMass(), hydrogen_copy.getMass())
+  TEST_EQUAL(hydrogen->getName(), hydrogen_copy.getName())
+  TEST_EQUAL(hydrogen->getNominalMass(), hydrogen_copy.getNominalMass())
+  TEST_EQUAL(hydrogen->getSequence(), hydrogen_copy.getSequence())
 }
 END_SECTION
 
 START_SECTION((bool operator==(const IMSElement &element) const ))
 {
-  // TODO
+  IMSElement also_hydrogen("H",iso);
+
+  TEST_EQUAL(*hydrogen == also_hydrogen, true)
+  also_hydrogen.setName("D");
+  TEST_EQUAL(*hydrogen == also_hydrogen, false)
+  also_hydrogen.setName("H");
+  also_hydrogen.setSequence("D");
+  TEST_EQUAL(*hydrogen == also_hydrogen, false)
+  also_hydrogen.setSequence("H");
+
+  IMSIsotopeDistribution::peaks_container peaks_copy(peaks);
+  peaks_copy.push_back(IMSIsotopeDistribution::peak_type(0.03604927,.0));
+  IMSIsotopeDistribution modified_iso(peaks_copy, 1);
+
+  also_hydrogen.setIsotopeDistribution(modified_iso);
+  TEST_EQUAL(*hydrogen == also_hydrogen, false)
+
+  IMSElement not_hydrogen;
+  TEST_EQUAL(*hydrogen == not_hydrogen, false)
 }
 END_SECTION
 
 START_SECTION((bool operator!=(const IMSElement &element) const ))
 {
-  // TODO
+  IMSElement also_hydrogen("H",iso);
+  TEST_EQUAL(*hydrogen != also_hydrogen, false)
+  also_hydrogen.setName("D");
+  TEST_EQUAL(*hydrogen != also_hydrogen, true)
+  also_hydrogen.setName("H");
+  also_hydrogen.setSequence("D");
+  TEST_EQUAL(*hydrogen != also_hydrogen, true)
+  also_hydrogen.setSequence("H");
+
+  IMSIsotopeDistribution::peaks_container peaks_copy(peaks);
+  peaks_copy.push_back(IMSIsotopeDistribution::peak_type(0.03604927,.0));
+  IMSIsotopeDistribution modified_iso(peaks_copy, 1);
+
+  also_hydrogen.setIsotopeDistribution(modified_iso);
+  TEST_EQUAL(*hydrogen != also_hydrogen, true)
+
+
+  IMSElement not_hydrogen;
+  TEST_EQUAL(*hydrogen != not_hydrogen, true)
 }
 END_SECTION
+
+delete hydrogen;
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
