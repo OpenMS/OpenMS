c958d2c1a99007ba4862b8b7e18bb0c916bcb734
Bastian Blank <waldi@users.sourceforge.net>
2011-12-06 14:19:50 +0000

    SILACClustering: Don't use ::abs (integer only) on floating point data
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/Release1.9@9267 6adb6e08-d915-0410-941f-83917bcadc18

--- source/COMPARISON/CLUSTERING/SILACClustering.C
+++ source/COMPARISON/CLUSTERING/SILACClustering.C
@@ -88,7 +88,7 @@ void SILACClustering::joinLarge_()
     // Generate new bucket if we got none yet
     if (sort_rt_check == sort_rt.end() ||
     // or if the new point is too far from the first one
-        abs(sort_it->first - sort_rt_check->first) > grid.cell_dimension[1])
+        (sort_it->first - sort_rt_check->first) > grid.cell_dimension[1])
     {
       sort_rt_check = sort_rt.insert(sort_rt_check, std::make_pair(sort_it->first, Sort()));
     }
