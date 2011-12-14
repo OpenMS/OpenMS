9292b9bcbdff47dcee0b64f82032764ddc07884e
Chris Bielow <chris.bielow@fu-berlin.de>
2011-12-14 08:45:22 +0000

    ** fixed ...
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/Release1.9@9330 6adb6e08-d915-0410-941f-83917bcadc18

--- source/APPLICATIONS/TOPP/PhosphoScoring.C
+++ source/APPLICATIONS/TOPP/PhosphoScoring.C
@@ -60,7 +60,7 @@ using namespace std;
 For details: Beausoleil et al.
 
 	<B>The command line parameters of this tool are:</B>
-	@verbinclude TOPP_PhosphoScoring.cli
+	@verbinclude UTILS_PhosphoScoring.cli
 */
 
 
