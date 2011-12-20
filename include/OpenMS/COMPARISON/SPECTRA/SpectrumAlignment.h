df230310b16a8b1d571a495ab23e38134fb2283b
Timo Sachsenberg <sachsenb@informatik.uni-tuebingen.de>
2011-12-20 13:00:57 +0000

    some maintainer fixes
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/Release1.9@9362 6adb6e08-d915-0410-941f-83917bcadc18

--- include/OpenMS/COMPARISON/SPECTRA/SpectrumAlignment.h
+++ include/OpenMS/COMPARISON/SPECTRA/SpectrumAlignment.h
@@ -21,8 +21,8 @@
 //  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
 // --------------------------------------------------------------------------
-// $Maintainer: Andreas Bertsch $
-// $Authors: $
+// $Maintainer: Timo Sachsenberg $
+// $Authors: Andreas Bertsch $
 // --------------------------------------------------------------------------
 //
 #ifndef OPENMS_COMPARISON_SPECTRA_SPECTRUMALIGNMENT_H
@@ -102,6 +102,7 @@ namespace OpenMS
 			// fill in the matrix
 			Size left_ptr(1);
 			Size last_i(0), last_j(0);
+
 			//Size off_band_counter(0);
 			for (Size i = 1; i <= s1.size(); ++i)
 			{
