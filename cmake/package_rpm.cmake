b9d050bf9ff72f4836855f33b8db4c948b9f1e6b
Johannes Veit <dr.kugelmehl@googlemail.com>
2012-01-31 21:24:45 +0000

    Added post-install script running ldconfig after user installs RPM package.
    (otherwise, OpenMS*.so are not found on clean
    Fedora installations)
    
    
    
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/Release1.9@9515 6adb6e08-d915-0410-941f-83917bcadc18

--- cmake/package_rpm.cmake
+++ cmake/package_rpm.cmake
@@ -6,6 +6,7 @@ if (OPENMS_64BIT_ARCHITECTURE)
 endif()
 set(CPACK_GENERATOR "RPM")
 set(CPACK_RPM_PACKAGE_REQUIRES "qt, qt-webkit")
+set(CPACK_RPM_POST_INSTALL_SCRIPT_FILE "${CMAKE_SOURCE_DIR}/cmake/run_ldconfig.sh")
 set(CPACK_COMPONENTS_ALL applications library share)
 
 ## should be the last include
