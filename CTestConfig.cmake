c275f2822e0689db7f847b6119b80811626264b6
Stephan Aiche <stephan.aiche@fu-berlin.de>
2012-04-30 21:19:17 +0000

    redirected cdash submissions to new cdash server cdash.openms.de
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@9680 6adb6e08-d915-0410-941f-83917bcadc18

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -3,12 +3,11 @@
 ## project to incorporate the testing dashboard.
 ## # The following are required to uses Dart and the Cdash dashboard
 ##   ENABLE_TESTING()
-##   INCLUDE(Dart)
+##   INCLUDE(CTest)
 set(CTEST_PROJECT_NAME "OpenMS")
 set(CTEST_NIGHTLY_START_TIME "23:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "www-bs2.informatik.uni-tuebingen.de")
-set(CTEST_DROP_LOCATION "/services/OpenMS/CDash/submit.php?project=OpenMS")
+set(CTEST_DROP_SITE "cdash.openms.de")
+set(CTEST_DROP_LOCATION "/submit.php?project=OpenMS")
 set(CTEST_DROP_SITE_CDASH TRUE)
-
