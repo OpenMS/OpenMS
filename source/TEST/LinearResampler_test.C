7c9fa0fd4767b33d388059c38a491eaef220719f
Hannes Roest <roest@imsb.biol.ethz.ch>
2012-06-19 13:41:12 +0000

    [TEST] add explanatory testcase to LinearResampler
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@9816 6adb6e08-d915-0410-941f-83917bcadc18

--- source/TEST/LinearResampler_test.C
+++ source/TEST/LinearResampler_test.C
@@ -55,6 +55,7 @@ START_SECTION((~LinearResampler()))
 END_SECTION
 
 START_SECTION((template<typename PeakType> void raster(MSSpectrum<PeakType>& spectrum)))
+{
   MSSpectrum< Peak1D > spec;
   spec.resize(5);
   spec[0].setMZ(0);
@@ -68,21 +69,61 @@ START_SECTION((template<typename PeakType> void raster(MSSpectrum<PeakType>& spe
   spec[4].setMZ(1.8);
   spec[4].setIntensity(1.0f);
 
-	LinearResampler lr;
-	Param param;
-	param.setValue("spacing",0.5);
+  LinearResampler lr;
+  Param param;
+  param.setValue("spacing",0.5);
   lr.setParameters(param);
   lr.raster(spec);
 
   DoubleReal sum = 0.0;
-	for (Size i=0; i<spec.size(); ++i)
+  for (Size i=0; i<spec.size(); ++i)
   {
     sum += spec[i].getIntensity();
   }
   TEST_REAL_SIMILAR(sum, 20);
+}
+END_SECTION
+
+START_SECTION((template<typename PeakType> void raster_spacing075(MSSpectrum<PeakType>& spectrum)))
+{
+  MSSpectrum< Peak1D > spec;
+  spec.resize(5);
+  spec[0].setMZ(0);
+  spec[0].setIntensity(3.0f);
+  spec[1].setMZ(0.5);
+  spec[1].setIntensity(6.0f);
+  spec[2].setMZ(1.);
+  spec[2].setIntensity(8.0f);
+  spec[3].setMZ(1.6);
+  spec[3].setIntensity(2.0f);
+  spec[4].setMZ(1.8);
+  spec[4].setIntensity(1.0f);
+
+  // A spacing of 0.75 will lead to a recalculation of intensities, each
+  // resampled point gets intensities from raw data points that are at most +/-
+  // spacing away.
+  LinearResampler lr;
+  Param param;
+  param.setValue("spacing",0.75);
+  lr.setParameters(param);
+  lr.raster(spec);
+
+  DoubleReal sum = 0.0;
+  for (Size i=0; i<spec.size(); ++i)
+  {
+    sum += spec[i].getIntensity();
+  }
+  TEST_REAL_SIMILAR(sum, 20);
+
+  TEST_REAL_SIMILAR(spec[0].getIntensity(), 3+2); 
+  TEST_REAL_SIMILAR(spec[1].getIntensity(), 4+2.0/3*8);
+  TEST_REAL_SIMILAR(spec[2].getIntensity(), 1.0/3*8+2+1.0/3);
+  TEST_REAL_SIMILAR(spec[3].getIntensity(), 2.0 / 3);
+}
 END_SECTION
 
 START_SECTION(( template <typename PeakType > void rasterExperiment(MSExperiment<PeakType>& exp)))
+{
   MSSpectrum< RichPeak1D > spec;
   spec.resize(5);
   spec[0].setMZ(0);
@@ -102,21 +143,22 @@ START_SECTION(( template <typename PeakType > void rasterExperiment(MSExperiment
 
   LinearResampler lr;
   Param param;
-	param.setValue("spacing",0.5);
+  param.setValue("spacing",0.5);
   lr.setParameters(param);
   lr.rasterExperiment(exp);
 
 
-	for (Size s=0; s<exp.size(); ++s)
+  for (Size s=0; s<exp.size(); ++s)
   {
-	  DoubleReal sum = 0.0;
-		for (Size i=0; i<exp[s].size(); ++i)
-	  {
-	    sum += exp[s][i].getIntensity();
-	  }
-	  TEST_REAL_SIMILAR(sum, 20);
-	}
+    DoubleReal sum = 0.0;
+    for (Size i=0; i<exp[s].size(); ++i)
+    {
+      sum += exp[s][i].getIntensity();
+    }
+    TEST_REAL_SIMILAR(sum, 20);
+  }
 
+}
 END_SECTION
 
 
