0a83a40878723dcb297d086f3732f6d19564721e
Chris Bielow <chris.bielow@fu-berlin.de>
2012-07-11 14:13:53 +0000

    [INTERNAL] fixed minor issues when finding boost
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@9858 6adb6e08-d915-0410-941f-83917bcadc18

--- cmake/OpenMSBuildSystem_externalLibs.cmake
+++ cmake/OpenMSBuildSystem_externalLibs.cmake
@@ -21,14 +21,14 @@ ENDIF()
 # and by the OpenMS macros (via CONTRIB_DIR).
 LIST(APPEND CMAKE_FIND_ROOT_PATH "${CONTRIB_DIR}")
 SET(TMP "")
-FOREACH(CUSTOM_PATH IN ITEMS ${CMAKE_FIND_ROOT_PATH})
+FOREACH(CUSTOM_PATH ${CMAKE_FIND_ROOT_PATH})
 	GET_FILENAME_COMPONENT(ABS_PATH ${CUSTOM_PATH} ABSOLUTE)
 	LIST(APPEND TMP ${ABS_PATH})
 ENDFOREACH()
 SET(CMAKE_FIND_ROOT_PATH "${TMP}")
 SET(CONTRIB_DIR "${CMAKE_FIND_ROOT_PATH}")
 
-MESSAGE(STATUS "CMake find root path: " ${CMAKE_FIND_ROOT_PATH})
+MESSAGE(STATUS "CMake find root path: ${CMAKE_FIND_ROOT_PATH}")
 
 set(CONTRIB_INCLUDE_DIR "" CACHE INTERNAL "contrib include dir")
 set(CONTRIB_LIB_DIR "" CACHE INTERNAL "contrib lib dir")
@@ -73,8 +73,12 @@ set(Boost_USE_STATIC_RUNTIME OFF)
 add_definitions(/DBOOST_ALL_NO_LIB) ## disable auto-linking of boost libs (boost tends to guess wrong lib names)
 set(Boost_COMPILER "")
 
-FIND_PACKAGE(Boost 1.42.0 REQUIRED iostreams date_time math_c99)
-if(!Boost_FOUND)
+# help boost finding it's packages
+set(Boost_ADDITIONAL_VERSIONS "1.47.0" "1.48.0" "1.49.0")
+
+# 1st attempt does not explicitly requires boost to enable second check (see below)
+FIND_PACKAGE(Boost 1.42.0 COMPONENTS iostreams date_time math_c99)
+if(NOT Boost_FOUND AND WIN32)
   # second attempt to find boost using plain "VC" as toolset, instead of vs110 or so.
   # this is required for cases where Boost was build without specifying a toolset explicitly
   set(Boost_COMPILER "-vc")
