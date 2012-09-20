bb7936501a7f748b9ffa257fa355db86fb729dd5
Chris Bielow <chris.bielow@fu-berlin.de>
2012-09-20 17:11:28 +0000

    [FIX, INTERNAL] * added infrastructure for optional SearchEngine tests  * added TOPP tests for OMSSAAdapter (all others are TODO)
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@10030 6adb6e08-d915-0410-941f-83917bcadc18

--- cmake/OpenMSBuildSystem_testConfig.cmake
+++ cmake/OpenMSBuildSystem_testConfig.cmake
@@ -17,3 +17,27 @@ endif()
 
 
 ## MS2 Search-Engines go here...
+## MACRO OPENMS_FINDBINARY:
+## fills ${varname} with the path to the binary given in ${binaryname}
+## @param varname      Name of the variable which will hold the result string (e.g. OMSSA_BINARY)
+## @param binaryname   List of binary names which are searched
+## @param name         Human readable version of binaryname for messages
+MACRO (OPENMS_FINDBINARY varname binaryname name)
+  find_program(${varname} ${binaryname} PATHS ENV PATH)
+  if (${${varname}} STREQUAL "${varname}-NOTFOUND")
+    message(STATUS "  - ${name} not found.")
+  else()
+    message(STATUS "  + ${name} binary found at ${binaryname}. Enabling corresponding tests.")
+  endif()  
+ENDMACRO (OPENMS_FINDBINARY)
+
+message(STATUS "Searching for MS2 search engines ...")
+
+## OMSSA
+OPENMS_FINDBINARY(OMSSA_BINARY "omssacl" "OMSSA")
+
+## X!Tandem
+OPENMS_FINDBINARY(XTANDEM_BINARY "tandem" "X!Tandem")
+
+## MyriMatch
+OPENMS_FINDBINARY(MYRIMATCH_BINARY "myrimatch" "Myrimatch")
