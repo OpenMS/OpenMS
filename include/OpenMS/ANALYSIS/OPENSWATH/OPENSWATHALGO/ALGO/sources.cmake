a2827062b9676dc916c604feb700342ec9c22cdc
Hannes Roest <roest@imsb.biol.ethz.ch>
2012-09-30 21:35:32 +0000

    [BUILD] OpenSWATH : cmake source.cmake include OPENSWATHALGO
    Author:    Witold <wewolski@gmail.com>
    
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@10177 6adb6e08-d915-0410-941f-83917bcadc18

---
+++ include/OpenMS/ANALYSIS/OPENSWATH/OPENSWATHALGO/ALGO/sources.cmake
@@ -0,0 +1,24 @@
+### the directory name
+set(directory include/OpenMS/ANALYSIS/OPENSWATH)
+
+### list all header files of the directory here
+set(sources_list_h
+corr.h
+DIAHelpers.h
+meanAndSd.h
+MRMScoring.h
+Scoring.h
+)
+
+### add path to the filenames
+set(sources_h)
+foreach(i ${sources_list_h})
+	list(APPEND sources_h ${directory}/${i})
+endforeach(i)
+
+### source group definition
+source_group("Header Files\\OpenMS\\ANALYSIS\\OPENSWATH" FILES ${sources_h})
+set_source_files_properties(${directory}/sources.cmake PROPERTIES HEADER_FILE_ONLY TRUE)
+
+set(OpenMS_sources_h ${OpenMS_sources_h} ${sources_h})
+
