7efefd751a3e8286eec2a7b3f7f080ed6feee9f0
Hannes Roest <roest@imsb.biol.ethz.ch>
2012-10-15 07:30:47 +0000

    [FIX] fix runtime error (SN parameters need to be larger than zero)
    * [BUILD] add new path for OpenSWATH
    
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@10305 6adb6e08-d915-0410-941f-83917bcadc18

--- source/ANALYSIS/OPENSWATH/DATAACCESS/MRMFeatureAccessOpenMS.C
+++ source/ANALYSIS/OPENSWATH/DATAACCESS/MRMFeatureAccessOpenMS.C
@@ -113,7 +113,7 @@ namespace OpenMS
 
   // default instances
   MSSpectrum<Peak1D> chromat;
-  SignalToNoiseOpenMS<Peak1D> default_signal_to_noise_openms(chromat, 0.0, 0.0);
+  SignalToNoiseOpenMS<Peak1D> default_signal_to_noise_openms(chromat, 1.0, 3);
 
   MRMTransitionGroup<MSSpectrum<Peak1D>, ReactionMonitoringTransition> trgroup;
   TransitionGroupOpenMS<MSSpectrum<Peak1D>, ReactionMonitoringTransition> default_transition_group_openms(trgroup);
