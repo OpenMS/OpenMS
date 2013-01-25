OpenMS/OpenMS

commit 320a8bc842909d1971c05bfb7cab7c776765243f

[FIX] TOPPAS could not create TOPPAS tmp dir when OpenMS tmp dir contains spaces
git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/Release1.10@10681 6adb6e08-d915-0410-941f-83917bcadc18

source/VISUAL/APPLICATIONS/TOPPASBase.C
@@ -264,9 +264,9 @@ namespace OpenMS
     current_path_ = param_.getValue("preferences:default_path");
 
     //set & create temporary path -- make sure its a new subdirectory, as TOPPASScene will delete it when its done
-    tmp_path_ =  File::getTempDirectory() + String(QDir::separator()) + File::getUniqueName();
-    QDir qd;
-    qd.mkpath(tmp_path_.toQString());
+    QDir qd(File::getTempDirectory().toQString());
+    qd.mkdir(File::getUniqueName().toQString());
+    tmp_path_ = qd.absolutePath();
 
     // online browser
     webview_ = new QWebView(parent);

commit 3d2a3a4ae4cfb8ed07cd5f1e17b97fe0ad829b4b

[FIX] bug introduced in r10681
git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/Release1.10@10682 6adb6e08-d915-0410-941f-83917bcadc18

source/VISUAL/APPLICATIONS/TOPPASBase.C
@@ -264,8 +264,10 @@ namespace OpenMS
     current_path_ = param_.getValue("preferences:default_path");
 
     //set & create temporary path -- make sure its a new subdirectory, as TOPPASScene will delete it when its done
+    QString new_tmp_dir = File::getUniqueName().toQString();
     QDir qd(File::getTempDirectory().toQString());
-    qd.mkdir(File::getUniqueName().toQString());
+    qd.mkdir(new_tmp_dir);
+    qd.cd(new_tmp_dir);
     tmp_path_ = qd.absolutePath();
 
     // online browser