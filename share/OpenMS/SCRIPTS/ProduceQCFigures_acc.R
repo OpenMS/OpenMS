7b3c45e0efc7624ff0ab13adb350210c8d81ae80
Mathias Walzer <walzer@informatik.uni-tuebingen.de>
2013-01-29 18:07:41 +0000

    [FIX] QC:the accuracy plot looks nicer that way
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/Release1.10@10693 6adb6e08-d915-0410-941f-83917bcadc18

--- share/OpenMS/SCRIPTS/ProduceQCFigures_acc.R
+++ share/OpenMS/SCRIPTS/ProduceQCFigures_acc.R
@@ -11,12 +11,12 @@ post<-commandArgs(TRUE)[2]
 a<-read.delim(file)
 png(post)
 if ('delta_ppm' %in% colnames(a)) {
-	hist(a$delta_ppm,xlim=c(-10,10),breaks=10000,xlab="ppm",main=paste("delta ppm"))
+	hist(a$delta_ppm,xlim=c(-10,10),breaks=seq(min(a$delta_ppm)-0.01, max(a$delta_ppm)+0.01, 0.01),xlab="ppm",main=paste("delta ppm"))
 	abline(v=median(a$delta_ppm),col="red", lwd=2)
 	mtext(paste("median(accuracy)=",round(median(a$delta_ppm),3)," ppm",sep=""))
 } else {
 	tmp<-(1-a$MZ/a$TheoreticalWeight)*1e6
-	hist(tmp,xlim=c(-10,10),breaks=10000,xlab="ppm",main=paste("delta ppm"))
+	hist(tmp,xlim=c(-10,10),breaks=seq(min(tmp)-0.01, max(tmp)+0.01, 0.01),xlab="ppm",main=paste("delta ppm"))
 	abline(v=median(tmp),col="red", lwd=2)
 	mtext(paste("median(accuracy)=",round(median(tmp),3)," ppm",sep=""))
 }
