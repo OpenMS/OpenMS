OpenMS/OpenMS

commit 7bfbe406ac49ce04e31ceaf0e978182b2179d9ba

fixed a few warnings (OSX) due to missing const cast
git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/Release1.10@10755 6adb6e08-d915-0410-941f-83917bcadc18

source/APPLICATIONS/UTILS/IDEvaluator.C
@@ -89,7 +89,10 @@ public:
     TOPPBase("IDEvaluator",
              "Computes a 'q-value vs. #PSM' plot which is saved as an image to visualize the number identifications for a certain q-value.", false)
   {
-    char * dummy = "dummy"; int argc = 1;  QApplication a(argc, &dummy);
+    int argc = 1;
+    const char* c = "IDEvaluator";
+    const char** argv = &c;
+    QApplication app(argc, const_cast<char**>(argv), false);
     out_formats_ = IDEvaluationBase().getSupportedImageFormats(); // can only be called if a QApplication is present...
   }
 
@@ -103,7 +106,10 @@ protected:
     Param p = FalseDiscoveryRate().getDefaults();
     p_my.insert("fdr:", p.copy("use_all_hits"));
 
-    char * dummy = "dummy"; int argc = 1;  QApplication a(argc, &dummy);
+    int argc = 1;
+    const char* c = "IDEvaluator";
+    const char** argv = &c;
+    QApplication app(argc, const_cast<char**>(argv), false);
     p_my.insert("image:", IDEvaluationBase().getParameters().copy("image:", true)); // can only be called if a QApplication is present...
     return p_my;
   }

commit 9470ddac696b4f6a46278cb1f078397859f8389f

[FIX] QApplication error introduced in r10755
git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/Release1.10@10773 6adb6e08-d915-0410-941f-83917bcadc18

source/APPLICATIONS/UTILS/IDEvaluator.C
@@ -92,7 +92,7 @@ public:
     int argc = 1;
     const char* c = "IDEvaluator";
     const char** argv = &c;
-    QApplication app(argc, const_cast<char**>(argv), false);
+    QApplication app(argc, const_cast<char**>(argv));
     out_formats_ = IDEvaluationBase().getSupportedImageFormats(); // can only be called if a QApplication is present...
   }
 
@@ -109,7 +109,7 @@ protected:
     int argc = 1;
     const char* c = "IDEvaluator";
     const char** argv = &c;
-    QApplication app(argc, const_cast<char**>(argv), false);
+    QApplication app(argc, const_cast<char**>(argv));
     p_my.insert("image:", IDEvaluationBase().getParameters().copy("image:", true)); // can only be called if a QApplication is present...
     return p_my;
   }