9f4ef11ec6043d85f809b65ee72fd3a5cb98858c
Hannes Roest <roest@imsb.biol.ethz.ch>
2013-05-14 23:06:23 +0000

    fixed build issue on linux
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@11062 6adb6e08-d915-0410-941f-83917bcadc18

--- pyOpenMS/run_memleaks.py
+++ pyOpenMS/run_memleaks.py
@@ -1,19 +1,3 @@
+#!/usr/bin/env python
 import nose
-import sys
-import os
-
-import env
-
-#os.environ["LD_LIBRARY_PATH"] = os.path.join(env.OPEN_MS_BUILD_DIR, "lib")
-#sys.path.insert(0, ".")
-
-import nose
-import sys
-import os
-
-import env
-
-#os.environ["LD_LIBRARY_PATH"] = os.path.join(env.OPEN_MS_BUILD_DIR, "lib")
-#sys.path.insert(0, ".")
-
 nose.run_exit(argv=["-v", "-w", "tests/memoryleaktests", "-s"])
--- pyOpenMS/run_nose.py
+++ pyOpenMS/run_nose.py
@@ -1,11 +1,3 @@
+#!/usr/bin/env python
 import nose
-import sys
-import os
-
-import env
-
-#os.environ["LD_LIBRARY_PATH"] = os.path.join(env.OPEN_MS_BUILD_DIR, "lib")
-#sys.path.insert(0, ".")
-
-#os.chdir("unittests")
 nose.run_exit(argv=["-v", "-w", "tests/unittests", "-s"])
