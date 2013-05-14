731686b90497a66ef2c18e16b915a6f133e19d7c
Hannes Roest <roest@imsb.biol.ethz.ch>
2013-05-14 23:33:37 +0000

    [FIX] also get the data back from IDRipper
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@11127 6adb6e08-d915-0410-941f-83917bcadc18

--- pyOpenMS/addons/IDRipper.pyx
+++ pyOpenMS/addons/IDRipper.pyx
@@ -47,10 +47,47 @@
  
         # call
         self.inst.get().rip(c_ripped, deref(c_protein_vec), deref(c_peptide_vec))
-        # TODO  get the data back from C
+
+        # get the data back from C++
+        replace = dict()
+        cdef libcpp_map[_String, libcpp_pair[ libcpp_vector[_ProteinIdentification], libcpp_vector[_PeptideIdentification]]].iterator it_ripped = c_ripped.begin()
+        cdef libcpp_pair[ libcpp_vector[_ProteinIdentification], libcpp_vector[_PeptideIdentification]] anotherPair
+        cdef libcpp_vector[_ProteinIdentification] another_c_protein_vec_inner 
+        cdef libcpp_vector[_PeptideIdentification] another_c_peptide_vec_inner 
+        cdef libcpp_vector[_ProteinIdentification].iterator it_prot
+        cdef libcpp_vector[_PeptideIdentification].iterator it_pep
+        cdef ProteinIdentification item_py_result_prot
+        cdef PeptideIdentification item_py_result_pep
+        while it_ripped != c_ripped.end():
+            # get the pair and the two vectors
+            anotherPair = deref(it_ripped).second
+            another_c_protein_vec_inner = anotherPair.first
+            another_c_peptide_vec_inner = anotherPair.second
+
+            replace_inner_protein = []
+            it_prot = another_c_protein_vec_inner.begin()
+            while it_prot != another_c_protein_vec_inner.end():
+                item_py_result_prot = ProteinIdentification.__new__(ProteinIdentification)   
+                item_py_result_prot.inst = shared_ptr[_ProteinIdentification](new _ProteinIdentification(deref(it_prot)))
+                replace_inner_protein.append(item_py_result_prot)
+                inc(it_prot)
+
+            replace_inner_peptide = []
+            it_pep = another_c_peptide_vec_inner.begin()
+            while it_pep != another_c_peptide_vec_inner.end():
+                item_py_result_pep = PeptideIdentification.__new__(PeptideIdentification)   
+                item_py_result_pep.inst = shared_ptr[_PeptideIdentification](new _PeptideIdentification(deref(it_pep)))
+                replace_inner_peptide.append(item_py_result_pep)
+                inc(it_pep)
+
+            replace[ <libcpp_string>deref(it_ripped).first ]  = [ replace_inner_protein, replace_inner_peptide] 
+            inc(it_ripped)
+        ripped.clear()
+        ripped.update(replace)
 
         del c_peptide_vec
         del c_protein_vec
         del c_protein_vec_inner
         del c_peptide_vec_inner
  
+
