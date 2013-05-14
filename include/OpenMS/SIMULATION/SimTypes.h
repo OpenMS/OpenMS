1e50c1ac288052f4b3f4f8ee3b67167c23964717
Hannes Roest <roest@imsb.biol.ethz.ch>
2013-05-14 23:49:24 +0000

    [FEATURE] initialize RNG directly
    OpenMSKernel changed
    
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@11178 6adb6e08-d915-0410-941f-83917bcadc18

--- include/OpenMS/SIMULATION/SimTypes.h
+++ include/OpenMS/SIMULATION/SimTypes.h
@@ -145,6 +145,30 @@ namespace OpenMS
       return *this;
     }
 
+    /// Initialize the RNGs
+    void initialize(bool random_)
+    {
+      biological_rng = gsl_rng_alloc(gsl_rng_mt19937);
+      if (random_)
+      {
+        gsl_rng_set(biological_rng, time(0));
+      }
+      else // use gsl default seed to get reproducible experiments
+      {
+        gsl_rng_set(biological_rng, 0);
+      }
+
+      technical_rng = gsl_rng_alloc(gsl_rng_mt19937);
+      if (random_)
+      {
+        gsl_rng_set(technical_rng, time(0));
+      }
+      else // use gsl default seed to get reproducible experiments
+      {
+        gsl_rng_set(technical_rng, 0);
+      }
+    }
+
   };
 
 }
