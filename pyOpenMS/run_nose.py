2199c6192acfe4eaba93eba9f70e3cebc06f341d
Hannes Roest <roest@imsb.biol.ethz.ch>
2013-05-14 23:03:41 +0000

    fixed run_nose.py
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@11053 6adb6e08-d915-0410-941f-83917bcadc18

--- pyOpenMS/run_nose.py
+++ pyOpenMS/run_nose.py
@@ -4,8 +4,8 @@ import os
 
 import env
 
-os.environ["LD_LIBRARY_PATH"] = os.path.join(env.OPEN_MS_BUILD_DIR, "lib")
-sys.path.insert(0, ".")
+#os.environ["LD_LIBRARY_PATH"] = os.path.join(env.OPEN_MS_BUILD_DIR, "lib")
+#sys.path.insert(0, ".")
 
-os.chdir("unittests")
-nose.run_exit(argv=["-v", "-w", "."])
+#os.chdir("unittests")
+nose.run_exit(argv=["-v", "-w", "tests/unittests", "-s"])
