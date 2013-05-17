519f73c2d659531f6697aa34c6d3e0227a618796
Hannes Roest <roest@imsb.biol.ethz.ch>
2013-05-17 16:23:06 +0000

    [FIX] pyOpenMS: fix SequestOutfile
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@11259 6adb6e08-d915-0410-941f-83917bcadc18

---
+++ pyOpenMS/addons/SequestOutfile.pyx
@@ -0,0 +1,78 @@
+
+
+
+
+    def getSequences(self, String database_filename , dict ac_position_map, list sequences, list found, dict not_found):
+        # void getSequences(String &database_filename,
+        # libcpp_map[ String, Size ] &ac_position_map,
+        # libcpp_vector[ String ] &sequences,
+        # libcpp_vector[ libcpp_pair[ String, Size ] ] &found,
+        # libcpp_map[ String, Size ] &not_found) nogil except +
+        assert isinstance(database_filename, String), 'arg database_filename wrong type'
+        assert isinstance(sequences, list) and all(isinstance(i, bytes) for i in sequences), 'arg sequences wrong type'
+    
+        cdef libcpp_vector[_String] * v1 = new libcpp_vector[_String]()
+        cdef bytes item1
+        for item1 in sequences:
+           v1.push_back(_String(<char *>item1))
+
+        cdef libcpp_map[_String, size_t ] c_dict_ac_position
+        for k,v in ac_position_map.iteritems():
+            c_dict_ac_position[ _String(<char *>k) ] = v
+
+        cdef libcpp_map[_String, size_t ] c_dict_not_found
+        for k,v in not_found.iteritems():
+            c_dict_not_found[ _String(<char *>k) ] = v
+
+        cdef libcpp_vector[ libcpp_pair[_String, size_t] ] * v4 = new libcpp_vector[ libcpp_pair[_String, size_t] ]()
+        cdef list item4
+        cdef libcpp_pair[_String, size_t] * aPair
+        for item4 in found:
+
+           aPair = new libcpp_pair[ _String, size_t] ( _String(<char *>item4[0]), item4[1])
+           v4.push_back( deref(aPair) )
+           del aPair
+ 
+        #
+        # Call C++
+        #
+        self.inst.get().getSequences(deref(database_filename.inst.get()), c_dict_ac_position, deref(v1), deref(v4), c_dict_not_found)
+        #
+        # Get arguments back from C++
+        #
+
+        replace_2 = dict()
+        cdef libcpp_map[_String, size_t].iterator it_dict2 = c_dict_ac_position.begin()
+        while it_dict2 != c_dict_ac_position.end():
+            replace_2[ <libcpp_string>deref(it_dict2).first ] = deref(it_dict2).second
+            inc(it_dict2)
+        ac_position_map.update(replace_2)
+
+        cdef replace = []
+        cdef libcpp_vector[_String].iterator it = v1.begin()
+        while it != v1.end():
+           replace.append(<char*>deref(it).c_str())
+           inc(it)
+        sequences[:] = replace
+
+        replace_4 = list()
+        cdef libcpp_vector[ libcpp_pair[_String, size_t] ].iterator it_list4 = deref(v4).begin()
+        cdef libcpp_pair[_String, size_t] anotherPair
+        while it_list4 != deref(v4).end():
+            # get the pair back
+            anotherPair = deref(it_list4)
+            replace_4.append( [<libcpp_string>anotherPair.first, anotherPair.second] )
+            inc(it_list4)
+        found[:] = replace_4
+
+        replace_5 = dict()
+        cdef libcpp_map[_String, size_t].iterator it_dict5 = c_dict_not_found.begin()
+        while it_dict5 != c_dict_not_found.end():
+            replace_2[ <libcpp_string>deref(it_dict5).first ] = deref(it_dict5).second
+            inc(it_dict5)
+        not_found.update(replace_2)
+
+
+        del v1
+        del v4
+
--- pyOpenMS/pxds/SequestOutfile.pxd
+++ pyOpenMS/pxds/SequestOutfile.pxd
@@ -15,8 +15,11 @@ cdef extern from "<OpenMS/FORMAT/SequestOutfile.h>" namespace "OpenMS":
         bool operator==(SequestOutfile &sequest_outfile) nogil except +
         void load(String &result_filename, libcpp_vector[ PeptideIdentification ] &peptide_identifications, ProteinIdentification &protein_identification, DoubleReal p_value_threshold, libcpp_vector[ double ] &pvalues, String &database, bool ignore_proteins_per_peptide) nogil except +
         bool getColumns(String &line, libcpp_vector[ String ] &substrings, Size number_of_columns, Size reference_column) nogil except +
-        # TODO STL map with wrapped key
-        # void getSequences(String &database_filename, libcpp_map[ String, Size ] &ac_position_map, libcpp_vector[ String ] &sequences, libcpp_vector[ libcpp_pair[ String, Size ] ] &found, libcpp_map[ String, Size ] &not_found) nogil except +
+        void getSequences(String &database_filename,
+                          libcpp_map[ String, size_t ] &ac_position_map,
+                          libcpp_vector[ String ] &sequences,
+                          libcpp_vector[ libcpp_pair[ String, size_t ] ] &found,
+                          libcpp_map[ String, size_t ] &not_found) nogil except + # wrap-ignore
         void getACAndACType(String line, String &accession, String &accession_type) nogil except +
         # TODO immutable types by reference
         # void readOutHeader(String &result_filename, DateTime &datetime, DoubleReal &precursor_mz_value, Int &charge, Size &precursor_mass_type, Size &ion_mass_type, Size &displayed_peptides, String &sequest, String &sequest_version, String &database_type, Int &number_column, Int &rank_sp_column, Int &id_column, Int &mh_column, Int &delta_cn_column, Int &xcorr_column, Int &sp_column, Int &sf_column, Int &ions_column, Int &reference_column, Int &peptide_column, Int &score_column, Size &number_of_columns) nogil except +
