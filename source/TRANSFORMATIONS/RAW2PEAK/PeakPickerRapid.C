62a05b5f8fb16f9d974e5782025497e1dc159bf1
Stephan Aiche <stephan.aiche@fu-berlin.de>
2013-06-24 15:27:09 +0000

    [FIX] switched to Constants::PI instead of M_PI to avoid redefinition warning
     * uncrustified PeackPickerRapid.(h|C) to ensure compliance with style guide
    
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@11411 6adb6e08-d915-0410-941f-83917bcadc18

--- source/TRANSFORMATIONS/RAW2PEAK/PeakPickerRapid.C
+++ source/TRANSFORMATIONS/RAW2PEAK/PeakPickerRapid.C
@@ -1,32 +1,32 @@
 // --------------------------------------------------------------------------
-//                   OpenMS -- Open-Source Mass Spectrometry               
+//                   OpenMS -- Open-Source Mass Spectrometry
 // --------------------------------------------------------------------------
 // Copyright The OpenMS Team -- Eberhard Karls University Tuebingen,
 // ETH Zurich, and Freie Universitaet Berlin 2002-2013.
-// 
+//
 // This software is released under a three-clause BSD license:
 //  * Redistributions of source code must retain the above copyright
 //    notice, this list of conditions and the following disclaimer.
 //  * Redistributions in binary form must reproduce the above copyright
 //    notice, this list of conditions and the following disclaimer in the
 //    documentation and/or other materials provided with the distribution.
-//  * Neither the name of any author or any participating institution 
-//    may be used to endorse or promote products derived from this software 
+//  * Neither the name of any author or any participating institution
+//    may be used to endorse or promote products derived from this software
 //    without specific prior written permission.
-// For a full list of authors, refer to the file AUTHORS. 
+// For a full list of authors, refer to the file AUTHORS.
 // --------------------------------------------------------------------------
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 // AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 // IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-// ARE DISCLAIMED. IN NO EVENT SHALL ANY OF THE AUTHORS OR THE CONTRIBUTING 
-// INSTITUTIONS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
-// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
-// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
-// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
-// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+// ARE DISCLAIMED. IN NO EVENT SHALL ANY OF THE AUTHORS OR THE CONTRIBUTING
+// INSTITUTIONS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 // ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// 
+//
 // --------------------------------------------------------------------------
 // $Maintainer: Erhan Kenar $
 // $Authors: Erhan Kenar $
@@ -40,10 +40,10 @@ using namespace std;
 
 namespace OpenMS
 {
-PeakPickerRapid::PeakPickerRapid()
-    : DefaultParamHandler("PeakPickerRapid"),
-      ProgressLogger()
-{
+  PeakPickerRapid::PeakPickerRapid() :
+    DefaultParamHandler("PeakPickerRapid"),
+    ProgressLogger()
+  {
     // set default parameter values
     // defaults_.setValue("signal_to_noise", 1.0, "Minimal signal-to-noise ratio for a peak to be picked (0.0 disables SNT estimation!)");
     // defaults_.setMinFloat("signal_to_noise", 0.0);
@@ -59,18 +59,17 @@ PeakPickerRapid::PeakPickerRapid()
     // write defaults into Param object param_
     defaultsToParam_();
 
-
     // initialize class members
     // signal_to_noise_ = param_.getValue("signal_to_noise");
-}
+  }
 
-PeakPickerRapid::~PeakPickerRapid()
-{
-}
+  PeakPickerRapid::~PeakPickerRapid()
+  {
+  }
 
-void PeakPickerRapid::updateMembers_()
-{
+  void PeakPickerRapid::updateMembers_()
+  {
     // signal_to_noise_ = param_.getValue("signal_to_noise");
-}
+  }
 
 } // namespace OpenMS
