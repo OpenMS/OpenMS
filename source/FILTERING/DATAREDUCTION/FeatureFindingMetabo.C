512f95984c16e40b1018b59008541c560ac01e6d
Hannes Roest <roest@imsb.biol.ethz.ch>
2013-07-15 12:09:33 +0000

    Revert "[FIX] Fixed bug in FeatureFinderMetabo that could cause negative feature intensities"
    This reverts commit r11549.
    
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/Release1.11@11557 6adb6e08-d915-0410-941f-83917bcadc18

--- source/FILTERING/DATAREDUCTION/FeatureFindingMetabo.C
+++ source/FILTERING/DATAREDUCTION/FeatureFindingMetabo.C
@@ -664,15 +664,13 @@ DoubleReal FeatureFindingMetabo::scoreRT_(const MassTrace& tr1, const MassTrace&
 
 
     DoubleReal proportion(overlap/max_length);
-    // std::cout << tr1.getLabel() << " & " << tr2.getLabel() << " prop:" << proportion << " overlap size" << overlap_rts.size() << std::endl;
+    // std::cout << tr1.getLabel() << " & " << tr2.getLabel() << " prop:" << proportion << std::endl;
 
     if (proportion < 0.7)
     {
         // std::cout << "reject";
         return 0.0;
     }
-
-    // std::cout << computeCosineSim_(x, y);
 
     return computeCosineSim_(x, y);
 }
@@ -802,7 +800,7 @@ void FeatureFindingMetabo::findLocalFeatures_(std::vector<MassTrace*>& candidate
 
                 fh_tmp.setScore(fh_tmp.getScore() + weighted_score);
                 fh_tmp.setCharge(charge);
-                // std::cout << "adding " << fh_tmp.getLabel() << << std::endl;
+                //std::cout << "adding " << fh_tmp.getLabel() << std::endl;
 
                 output_hypos.push_back(fh_tmp);
                 last_iso_idx = best_idx;
