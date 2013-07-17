cb67de6be50ddaed4f7e4f16dcdbb33a1913d35b
Hannes Roest <roest@imsb.biol.ethz.ch>
2013-07-17 13:49:29 +0000

    [BUILD] pyOpenMS: print Cython version number, no hex-chars in code
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/Release1.11@11574 6adb6e08-d915-0410-941f-83917bcadc18

--- pyOpenMS/pyopenms.cmake
+++ pyOpenMS/pyopenms.cmake
@@ -53,7 +53,14 @@ ENDIF()
 IF (CYTHON-MISSING)
 	MESSAGE(STATUS "Looking for cython - not found")
 ELSE()
-	MESSAGE(STATUS "Looking for cython - found")
+  # find out cython version info
+  execute_process(
+       COMMAND
+       ${PYTHON_EXECUTABLE} -c "import Cython; print Cython.__version__"
+       OUTPUT_VARIABLE CYTHON_VERSION
+       OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+  MESSAGE(STATUS "Looking for cython - found version ${CYTHON_VERSION}")
 ENDIF()
 
 # Check for autowrap Cython
