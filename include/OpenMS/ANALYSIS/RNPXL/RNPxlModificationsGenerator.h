7c49b90ea6d580e620ba0ebff9c8836577d5e417
Timo Sachsenberg <sachsenb@informatik.uni-tuebingen.de>
2013-09-17 14:25:37 +0000

    [NOP] minor refactoring of RNPxl.
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@11772 6adb6e08-d915-0410-941f-83917bcadc18

---
+++ include/OpenMS/ANALYSIS/RNPXL/RNPxlModificationsGenerator.h
@@ -0,0 +1,62 @@
+// --------------------------------------------------------------------------
+//                   OpenMS -- Open-Source Mass Spectrometry
+// --------------------------------------------------------------------------
+// Copyright The OpenMS Team -- Eberhard Karls University Tuebingen,
+// ETH Zurich, and Freie Universitaet Berlin 2002-2013.
+//
+// This software is released under a three-clause BSD license:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of any author or any participating institution
+//    may be used to endorse or promote products derived from this software
+//    without specific prior written permission.
+// For a full list of authors, refer to the file AUTHORS.
+// --------------------------------------------------------------------------
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL ANY OF THE AUTHORS OR THE CONTRIBUTING
+// INSTITUTIONS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// --------------------------------------------------------------------------
+// $Maintainer: Timo Sachsenberg $
+// $Authors: Timo Sachsenberg $
+// --------------------------------------------------------------------------
+
+#ifndef RNPXLMODIFICATIONSGENERATOR_H
+#define RNPXLMODIFICATIONSGENERATOR_H
+
+#include <vector>
+#include <map>
+#include <set>
+#include <OpenMS/KERNEL/StandardTypes.h>
+
+namespace OpenMS
+{  
+  struct RNPxlModificationMassesResult
+  {
+    std::map<String, DoubleReal> mod_masses; // empirical formula -> mass
+    std::map<String, std::set<String> > mod_combinations; // empirical formula -> nucleotide formula(s) (formulas if modifications lead to ambiguities)
+    std::map<Size, String> mod_formula_idx;
+  };
+
+  class RNPxlModificationsGenerator
+  {
+    public:
+      static RNPxlModificationMassesResult initModificationMassesRNA(StringList target_nucleotides, StringList mappings, StringList restrictions, StringList modifications, String sequence_restriction, bool cysteine_adduct, Int max_length = 4);
+    private:
+      static bool notInSeq(String res_seq, String query);
+      static void generateTargetSequences(const String& res_seq, Size pos, const std::map<char, std::vector<char> >& map_source2target, StringList& target_sequences);
+    };
+}
+
+#endif // RNPXLMODIFICATIONSGENERATOR_H
