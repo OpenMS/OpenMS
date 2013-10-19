057ceacb0d3e80bdf6216a28078e1fa5183b7d37
Stephan Aiche <stephan.aiche@fu-berlin.de>
2013-10-19 07:39:50 +0000

    [FIX,BUILD,NOP] fixed some cmake 2.8.12 warnings
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@11844 6adb6e08-d915-0410-941f-83917bcadc18

--- cmake/OpenMSBuildSystem_externalLibs.cmake
+++ cmake/OpenMSBuildSystem_externalLibs.cmake
@@ -75,7 +75,7 @@ set(Boost_USE_STATIC_RUNTIME OFF)
 add_definitions(/DBOOST_ALL_NO_LIB) ## disable auto-linking of boost libs (boost tends to guess wrong lib names)
 set(Boost_COMPILER "")
 # help boost finding it's packages
-set(Boost_ADDITIONAL_VERSIONS "1.47.0" "1.48.0" "1.49.0", "1.50.0", "1.51.0", "1.52.0")
+set(Boost_ADDITIONAL_VERSIONS "1.47.0" "1.48.0" "1.49.0" "1.50.0" "1.51.0" "1.52.0")
 # 1st attempt does not explicitly requires boost to enable second check (see below)
 FIND_PACKAGE(Boost 1.42.0 COMPONENTS iostreams date_time math_c99 regex)
 
