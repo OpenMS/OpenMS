6e397e4f02e23a2dd61f3586447c424e83a955b3
Stephan Aiche <stephan.aiche@fu-berlin.de>
2014-01-23 08:43:05 +0100

    [BUILD] adapted pyOpenMS to the new repo layout

--- pyOpenMS/tests/unittests/test_MRMFeatureFinderScoring.py
+++ pyOpenMS/tests/unittests/test_MRMFeatureFinderScoring.py
@@ -11,7 +11,7 @@ class TestMRMFeatureFinderScoring(unittest.TestCase):
 
     def setUp(self):
         self.dirname = os.path.dirname(os.path.abspath(__file__))
-        self.testdirname = os.path.join(env.OPEN_MS_SRC, "source/TEST/TOPP")
+        self.testdirname = os.path.join(env.OPEN_MS_SRC, "src/tests/topp")
         # set up files
         self.chromatograms = os.path.join(self.testdirname, "OpenSwathAnalyzer_1_input_chrom.mzML")
         self.tramlfile = os.path.join(self.testdirname, "OpenSwathAnalyzer_1_input.TraML")
