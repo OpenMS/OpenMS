24ec553065dfab2bab17f322cc6312281214717d
Stephan Aiche <stephan.aiche@fu-berlin.de>
2014-01-31 20:35:27 +0100

    [NOP] added support for travis-ci to OpenMS
     * for now we ignore AccurateMassSearch_test and GzipIfstream_test

---
+++ tools/lnx-cibuild.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# get some infos from git to embed it in the build name
+export SOURCE_DIRECTORY=`pwd`
+mkdir _build
+
+# define the build name
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  export BUILD_NAME=$TRAVIS_PULL_REQUEST
+elif [[ -n "$TRAVIS_COMMIT_RANGE" ]]; then
+  export BUILD_NAME=$TRAVIS_COMMIT_RANGE
+else
+  export BUILD_NAME=$TRAVIS_COMMIT
+fi
+
+ctest -V -S tools/lnx-cibuild.cmake
+
+# we indicate build failures if ctest experienced any errors
+if [ -f ${SOURCE_DIRECTORY}/failed ]; then
+	echo "Configure/Build failed"
+  exit -1
+fi
+
+FAILED_TEST=$(find _build -name "Test.xml" -type f | xargs grep "<Test Status=\"failed\">" -c)
+
+if [ "$FAILED_TEST" -gt "0" ]; then
+	echo "$FAILED_TEST tests failed"
+	exit -1
+fi
+
+# it seems like everything worked
+exit 0
+
