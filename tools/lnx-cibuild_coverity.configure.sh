5c35f13ca7193da6c1a11605ef56c897aa7960db
Stephan Aiche <stephan.aiche@fu-berlin.de>
2014-02-20 19:15:04 +0100

    [NOP] fixed path to contrib in coverity configure

--- tools/lnx-cibuild_coverity.configure.sh
+++ tools/lnx-cibuild_coverity.configure.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # simple script to configure a build environment for coverity
+SOURCE=`pwd`
+
 mkdir _coverity_build
 cd _coverity_build
 
-cmake -D CMAKE_FIND_ROOT_PATH="${PWD}/contrib/_build/;/usr;" -D BOOST_USE_STATIC=Off -D CMAKE_BUILD_TYPE=Release ..
+cmake -D CMAKE_FIND_ROOT_PATH="${SOURCE}/contrib/_build/;/usr;" -D BOOST_USE_STATIC=Off -D CMAKE_BUILD_TYPE=Release ..
