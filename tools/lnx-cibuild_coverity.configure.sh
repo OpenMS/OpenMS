22c39b9f905d373c1077d93246e3af53de6d589c
Stephan Aiche <stephan.aiche@fu-berlin.de>
2014-02-20 22:02:27 +0100

    [NOP] increase speed of build

--- tools/lnx-cibuild_coverity.configure.sh
+++ tools/lnx-cibuild_coverity.configure.sh
@@ -6,4 +6,8 @@ SOURCE=`pwd`
 mkdir _coverity_build
 cd _coverity_build
 
-cmake -D CMAKE_FIND_ROOT_PATH="${SOURCE}/contrib/_build/;/usr;" -D BOOST_USE_STATIC=Off -D CMAKE_BUILD_TYPE=Release ..
+cmake -D CMAKE_FIND_ROOT_PATH="${SOURCE}/contrib/_build/;/usr;" \
+	-D BOOST_USE_STATIC=Off \
+	-D CMAKE_BUILD_TYPE=Release \
+	-G "Unix Makefiles" \
+	..
