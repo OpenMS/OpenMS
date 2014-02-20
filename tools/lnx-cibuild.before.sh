8285c0c18bb5a671d005a570bd1f3f74b1dbb5f2
Stephan Aiche <stephan.aiche@fu-berlin.de>
2014-02-20 22:21:56 +0100

    [NOP] further build speedups and fix for path problem

--- tools/lnx-cibuild.before.sh
+++ tools/lnx-cibuild.before.sh
@@ -16,41 +16,35 @@ cmake .. -DBUILD_TYPE=GSL -DNUMBER_OF_JOBS=4
 sudo add-apt-repository --yes ppa:boost-latest/ppa
 sudo apt-get update
 
+# packages to install
+
 # install required packages
-sudo apt-get install -qq  libboost-date-time1.54-dev \
-                          libboost-iostreams1.54-dev \
-                          libboost-regex1.54-dev \
-                          libboost-math1.54-dev \
-                          libboost-random1.54-dev \
-                          libxerces-c3.1 \
-                          libxerces-c-dev \
-                          libicu-dev \
-                          qt4-dev-tools \
-                          libqt4-dev \
-                          libqt4-core \
-                          libqt4-gui \
-                          libsvm-dev \
-                          libsvm3 \
-                          glpk \
-                          subversion
-
-# install doxygen but not latex (saves some time)
-sudo apt-get install -qq \
-                     --no-install-recommends doxygen \
-                     graphviz
-
-# build custom cppcheck if we want to perform style tests
-if [ $ENABLE_STYLE_TESTING == "On" ]; then
-  sudo apt-get install -qq libpcre3-dev
-  git clone git://github.com/danmar/cppcheck.git
-  pushd cppcheck
-  git checkout 1.65
-  CXX=clang++ make SRCDIR=build CFGDIR=`pwd`/cfg HAVE_RULES=yes -j4
-  popd
+# we only need a subset for coverity, since we want to 
+# documentation there
+if [ "$COVERITY_SCAN_BRANCH" == "1" ]; then
+	sudo apt-get install -qq  boost1.54\
+	                          libxerces-c3.1\
+	                          libxerces-c-dev \
+	                          libicu-dev \
+	                          qt4-dev-tools \
+	                          libqt4-dev \
+	                          libqt4-core \
+	                          libqt4-gui \
+	                          libsvm-dev \
+	                          libsvm3 \
+	                          glpk 
 else
-  # regular builds .. get the search engine executables
-  svn checkout http://svn.code.sf.net/p/open-ms/code/THIRDPARTY/SEARCHENGINES/Linux/64bit/ _searchengines
-  # remove .svn otherwise we can't check out the other search engines into the same directory (TODO: maybe switch to wget)
-  rm _searchengines/.svn -R -f
-  svn checkout http://svn.code.sf.net/p/open-ms/code/THIRDPARTY/SEARCHENGINES/All/ _searchengines
+	sudo apt-get install -qq  boost1.54\
+	                          libxerces-c3.1\
+	                          libxerces-c-dev \
+	                          libicu-dev \
+	                          qt4-dev-tools \
+	                          libqt4-dev \
+	                          libqt4-core \
+	                          libqt4-gui \
+	                          libsvm-dev \
+	                          libsvm3 \
+	                          glpk \
+	                          doxygen \
+	                          graphviz
 fi
