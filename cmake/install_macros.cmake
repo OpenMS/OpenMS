4d9d26fff8f96ff6daa38990cd0778d90fbe8151
Stephan Aiche <stephan.aiche@fu-berlin.de>
2014-02-21 11:03:43 +0100

    [BUILD] made install directories customisable

--- cmake/install_macros.cmake
+++ cmake/install_macros.cmake
@@ -36,14 +36,6 @@
 # througout the OpenMS build system
 
 #------------------------------------------------------------------------------
-# customize output folders according to platform
-if (WIN32)
-	set(OPENMS_LIB_INSTALL_PATH "bin")
-else()	## Linux & MacOS
-  set(OPENMS_LIB_INSTALL_PATH "lib")
-endif()
-
-#------------------------------------------------------------------------------
 # Installs the library lib_target_name and all its headers set via
 # set_target_properties(lib_target_name PROPERTIES PUBLIC_HEADER ${headers})
 #
@@ -51,9 +43,9 @@ endif()
 macro(install_library lib_target_name)
   if ("${PACKAGE_TYPE}" STREQUAL "none")
     install(TARGETS ${lib_target_name}
-      LIBRARY DESTINATION ${OPENMS_LIB_INSTALL_PATH}
-      ARCHIVE DESTINATION ${OPENMS_LIB_INSTALL_PATH}
-    	RUNTIME DESTINATION ${OPENMS_LIB_INSTALL_PATH}
+      LIBRARY DESTINATION ${INSTALL_LIB_DIR}
+      ARCHIVE DESTINATION ${INSTALL_LIB_DIR}
+      RUNTIME DESTINATION ${INSTALL_LIB_DIR}
       COMPONENT library)
   endif()
 endmacro()
@@ -79,7 +71,7 @@ macro(install_headers header_list component)
     install(FILES ${_header}
             # note the missing slash, we need this for file directly located in
             # include/OpenMS (e.g., config.h)
-            DESTINATION include/OpenMS${_relative_header_path}
+            DESTINATION ${INSTALL_INCLUDE_DIR}/OpenMS${_relative_header_path}
             COMPONENT ${component}_headers)
   endforeach()
 endmacro()
@@ -90,8 +82,8 @@ endmacro()
 macro(install_tool tool_target_name)
   if ("${PACKAGE_TYPE}" STREQUAL "none")
     install(TARGETS ${tool_target_name}
-      RUNTIME DESTINATION bin
-      BUNDLE DESTINATION bin
+      RUNTIME DESTINATION ${INSTALL_BIN_DIR}
+      BUNDLE DESTINATION ${INSTALL_BIN_DIR}
       COMPONENT applications)
   endif()
 endmacro()
@@ -119,15 +111,5 @@ macro(install_file file destination component)
     install(FILES ${file}
       DESTINATION ${destination}
       COMPONENT ${component})
-  endif()
-endmacro()
-
-#------------------------------------------------------------------------------
-# Executes the given code on execution of install
-# @param directory The file to install
-macro(execute_on_install code)
-  if ("${PACKAGE_TYPE}" STREQUAL "none")
-    install(CODE "${code}"
-      COMPONENT 01) # we give this weird component name to ensure that it is executed before anything else
   endif()
 endmacro()
