a113c595b24c1adea980439eb0a5ec8441d82277
Hannes Roest <roest@imsb.biol.ethz.ch>
2014-04-11 12:23:59 +0200

    [NOP] migrate helper functions to OpenSwathHelper

--- src/topp/OpenSwathRTNormalizer.cpp
+++ src/topp/OpenSwathRTNormalizer.cpp
@@ -78,7 +78,7 @@ public:
 
   TOPPOpenSwathRTNormalizer() :
   TOPPBase("OpenSwathRTNormalizer",
-           "This tool will take a description of RT peptides and their normalized retention time to write out a transformation file on how to transoform the RT space into the normalized space.",
+           "This tool will take a description of RT peptides and their normalized retention time to write out a transformation file on how to transform the RT space into the normalized space.",
            true)
   {
   }
@@ -141,61 +141,6 @@ protected:
     return Param();
   }
 
-  void simple_find_best_feature(OpenMS::MRMFeatureFinderScoring::TransitionGroupMapType & transition_group_map, 
-      std::vector<std::pair<double, double> > & pairs, bool useQualCutoff = false, double qualCutoff = 0.0)
-  {
-    for (OpenMS::MRMFeatureFinderScoring::TransitionGroupMapType::iterator trgroup_it = transition_group_map.begin(); trgroup_it != transition_group_map.end(); trgroup_it++)
-    {
-      // we need at least one feature to find the best one
-      OpenMS::MRMFeatureFinderScoring::MRMTransitionGroupType * transition_group = &trgroup_it->second;
-      if (transition_group->getFeatures().size() == 0)
-      {
-        std::cout << "Did not find any features for group " + transition_group->getTransitionGroupID() << std::endl;
-        continue;
-      }
-
-      MRMFeature * bestf = 0;
-      // MRMFeature * secondbest = 0;
-
-      // Find the feature with the highest intensity
-      double highest_int = 0;
-      for (std::vector<MRMFeature>::iterator mrmfeature = transition_group->getFeaturesMuteable().begin();
-           mrmfeature != transition_group->getFeaturesMuteable().end(); mrmfeature++)
-      {
-        if (mrmfeature->getIntensity() > highest_int)
-        {
-          bestf = &(*mrmfeature);
-          highest_int = mrmfeature->getIntensity();
-        }
-      }
-
-      // Find the feature with the highest score
-      double highest_score = -1000;
-      //double second_highest_score = -1000;
-      for (std::vector<MRMFeature>::iterator mrmfeature = transition_group->getFeaturesMuteable().begin();
-           mrmfeature != transition_group->getFeaturesMuteable().end(); mrmfeature++)
-      {
-        if (mrmfeature->getOverallQuality() > highest_score)
-        {
-          //second_highest_score = highest_score;
-          bestf = &(*mrmfeature);
-          highest_score = mrmfeature->getOverallQuality();
-        }
-        // else if (mrmfeature->getOverallQuality() > second_highest_score)
-        // {
-        //   second_highest_score = mrmfeature->getOverallQuality();
-        //   secondbest = &(*mrmfeature);
-        // }
-      }
-
-      if (useQualCutoff && bestf->getOverallQuality() < qualCutoff) {continue;}
-
-      std::cout << " found best feature : " << bestf->getMetaValue("initialPeakQuality") << " and final " << bestf->getOverallQuality() << std::endl;
-      String pepref = trgroup_it->second.getTransitions()[0].getPeptideRef();
-      if (bestf) {pairs.push_back(std::make_pair(bestf->getRT(), PeptideRTMap[pepref]));}
-    }
-  }
-
   bool computeBinnedCoverage(const std::pair<double,double> & rtRange, 
       const std::vector<std::pair<double, double> > & pairs, int nrBins, 
       int minPeptidesPerBin, int minBinsFilled)
@@ -214,32 +159,18 @@ protected:
       }
       binCounter[ bin ]++;
     }
+
     int binsFilled = 0;
     for (Size i = 0; i < binCounter.size(); i++)
     {
       std::cout <<" In bin " << i << " out of " << binCounter.size() << " we have " << binCounter[i] << " peptides " << std::endl;
-      if (binCounter[i] >= minPeptidesPerBin) binsFilled++;
+      if (binCounter[i] >= minPeptidesPerBin) 
+      {
+        binsFilled++;
+      }
     }
-    if (binsFilled >= minBinsFilled) return true;
-    else return false;
-  }
 
-  std::pair<double,double> estimateRTRange(OpenSwath::LightTargetedExperiment & exp)
-  {
-    if (exp.getPeptides().empty()) 
-    {
-      throw Exception::IllegalArgument(__FILE__, __LINE__, __PRETTY_FUNCTION__,
-        "Input list of targets is empty.");
-    }
-    double max = exp.getPeptides()[0].rt;
-    double min = exp.getPeptides()[0].rt;
-    for (Size i = 0; i < exp.getPeptides().size(); i++)
-    {
-      if (exp.getPeptides()[i].rt < min) min = exp.getPeptides()[i].rt;
-      if (exp.getPeptides()[i].rt > max) max = exp.getPeptides()[i].rt;
-    }
-    return std::make_pair(min,max);
-
+    return (binsFilled >= minBinsFilled);
   }
 
   ExitCodes main_(int, const char **)
@@ -262,7 +193,7 @@ protected:
       TraMLFile().load(tr_file, transition_exp_);
       OpenSwathDataAccessHelper::convertTargetedExp(transition_exp_, targeted_exp);
     }
-    std::pair<double,double> RTRange = estimateRTRange(targeted_exp);
+    std::pair<double,double> RTRange = OpenSwathHelper::estimateRTRange(targeted_exp);
     std::cout << "Rt range from " << RTRange.first << " to " << RTRange.second << std::endl;
 
     Param pepEstimationParams = getParam_().copy("peptideEstimation:", true);
@@ -331,14 +262,20 @@ protected:
       }
 
       // find most likely correct feature for each group
+      std::map<std::string, double> res;
       if (estimateBestPeptides)
       {
-        simple_find_best_feature(transition_group_map, pairs, true, pepEstimationParams.getValue("OverallQualityCutoff"));
+        res = OpenSwathHelper::simpleFindBestFeature(transition_group_map, true, pepEstimationParams.getValue("OverallQualityCutoff"));
       }
       else
       {
-        simple_find_best_feature(transition_group_map, pairs);
+        res = OpenSwathHelper::simpleFindBestFeature(transition_group_map);
       }
+      for (std::map<std::string, double>::iterator it = res.begin(); it != res.end(); it++)
+      {
+        pairs.push_back(std::make_pair(it->second, PeptideRTMap[it->first])); // pair<exp_rt, theor_rt>
+      }
+
     }
 
     bool enoughPeptides = computeBinnedCoverage(RTRange, pairs, 10, 
@@ -354,8 +291,12 @@ protected:
     std::vector<std::pair<double, double> > pairs_corrected;
     if (outlierDetectionParams.getValue("useRANSAC") == "true")
     {
-      double max_rt_threshold = (RTRange.second - RTRange.first) / 30; // estimate of the maximum deviation from RT that is tollerated. Because 120 min gradient can have 4 min elution shift, we use this ratio to find upper RT threshold.
-      pairs_corrected = MRMRTNormalizer::rm_outliers_ransac(pairs, min_rsq, min_coverage, outlierDetectionParams.getValue("useRANSACMaxIterations"), max_rt_threshold);
+      // estimate of the maximum deviation from RT that is tolerated.
+      // Because 120 min gradient can have 4 min elution shift, we use this
+      // ratio to find upper RT threshold.
+      double max_rt_threshold = (RTRange.second - RTRange.first) / 30;
+      pairs_corrected = MRMRTNormalizer::rm_outliers_ransac(pairs, min_rsq, min_coverage,
+          outlierDetectionParams.getValue("useRANSACMaxIterations"), max_rt_threshold);
     }
     else
     {
@@ -384,7 +325,7 @@ protected:
     return EXECUTION_OK;
   }
 
-  std::map<OpenMS::String, double> PeptideRTMap;
+  std::map<std::string, double> PeptideRTMap;
 
 };
 
