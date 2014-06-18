a444c9a326e41bd2c0ccc2bfe9894c680b56b6a0
Stephan Aiche <stephan.aiche@fu-berlin.de>
2014-06-18 11:50:23 +0200

    [FIX-#861,DOC] fixed eigen 3.2.1 problems with GammaDistributionFitter
     * redefined gamma distribution to be 0.0 if parameters are <= 0.0, while this
       violated basic assumptions on statistical distributions it allows the usage of
       an unconstrained optimisation algorithm (like eigen's lm)
     * assigned myself as maintainer
     * updated documentation
     * fixed error in GAMMA_DISTRIBUTION_FITTER_VERBOSE code path

--- src/openms/source/MATH/STATISTICS/GammaDistributionFitter.cpp
+++ src/openms/source/MATH/STATISTICS/GammaDistributionFitter.cpp
@@ -28,7 +28,7 @@
 // ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 // --------------------------------------------------------------------------
-// $Maintainer: Andreas Bertsch $
+// $Maintainer: Stephan Aiche $
 // $Authors: Andreas Bertsch $
 // --------------------------------------------------------------------------
 //
@@ -51,8 +51,8 @@ namespace OpenMS
 {
   namespace Math
   {
-    GammaDistributionFitter::GammaDistributionFitter()
-    : init_param_(1.0, 5.0)
+    GammaDistributionFitter::GammaDistributionFitter() :
+      init_param_(1.0, 5.0)
     {
     }
 
@@ -60,7 +60,7 @@ namespace OpenMS
     {
     }
 
-    void GammaDistributionFitter::setInitialParameters(const GammaDistributionFitResult & param)
+    void GammaDistributionFitter::setInitialParameters(const GammaDistributionFitResult& param)
     {
       init_param_ = param;
     }
@@ -70,45 +70,71 @@ namespace OpenMS
       int inputs() const { return m_inputs; }
       int values() const { return m_values; }
 
-      GammaFunctor(unsigned dimensions, const std::vector<DPosition<2> >* data)
-      : m_inputs(dimensions), m_values(data->size()), m_data(data) {}
+      GammaFunctor(unsigned dimensions, const std::vector<DPosition<2> >* data) :
+        m_inputs(dimensions), m_values(data->size()), m_data(data) {}
 
-      int operator()(const Eigen::VectorXd &x, Eigen::VectorXd &fvec)
+      int operator()(const Eigen::VectorXd& x, Eigen::VectorXd& fvec)
       {
 
         double b = x(0);
         double p = x(1);
 
         UInt i = 0;
-        for (std::vector<DPosition<2> >::const_iterator it = m_data->begin(); it != m_data->end(); ++it, ++i)
+
+        // gamma distribution is only defined for positive parameter values
+        if (b > 0.0 && p > 0.0)
         {
-          double the_x = it->getX();
-          fvec(i) =  std::pow(b, p) / boost::math::tgamma(p) * std::pow(the_x, p - 1) * std::exp(-b * the_x) - it->getY();
+          for (std::vector<DPosition<2> >::const_iterator it = m_data->begin(); it != m_data->end(); ++it, ++i)
+          {
+            double the_x = it->getX();
+            fvec(i) =  std::pow(b, p) / boost::math::tgamma(p) * std::pow(the_x, p - 1) * std::exp(-b * the_x) - it->getY();
+          }
         }
+        else
+        {
+          for (std::vector<DPosition<2> >::const_iterator it = m_data->begin(); it != m_data->end(); ++it, ++i)
+          {
+            fvec(i) = -it->getY();
+          }
+        }
+
 
         return 0;
       }
+
       // compute Jacobian matrix for the different parameters
-      int df(const Eigen::VectorXd &x, Eigen::MatrixXd &J)
+      int df(const Eigen::VectorXd& x, Eigen::MatrixXd& J)
       {
 
         double b = x(0);
         double p = x(1);
 
         UInt i = 0;
-        for (std::vector<DPosition<2> >::const_iterator it = m_data->begin(); it != m_data->end(); ++it, ++i)
+        // gamma distribution is only defined for positive parameter values
+        if (b > 0.0 && p > 0.0)
         {
-          double the_x = it->getX();
+          for (std::vector<DPosition<2> >::const_iterator it = m_data->begin(); it != m_data->end(); ++it, ++i)
+          {
+            double the_x = it->getX();
 
-          // partial deviation regarding b
-          double part_dev_b = std::pow(the_x, p - 1) * std::exp(-the_x * b) / boost::math::tgamma(p) * (p * std::pow(b, p - 1) - the_x * std::pow(b, p));
-          J(i,0) = part_dev_b;
+            // partial deviation regarding b
+            double part_dev_b = std::pow(the_x, p - 1) * std::exp(-the_x * b) / boost::math::tgamma(p) * (p * std::pow(b, p - 1) - the_x * std::pow(b, p));
+            J(i, 0) = part_dev_b;
 
-          // partial deviation regarding p
-          double factor = std::exp(-b * the_x) * std::pow(the_x, p - 1) * std::pow(b, p) / std::pow(boost::math::tgamma(p), 2);
-          double argument = (std::log(b) + std::log(the_x)) * boost::math::tgamma(p) - boost::math::tgamma(p) * boost::math::digamma(p);
-          double part_dev_p = factor * argument;
-          J(i,1) = part_dev_p;
+            // partial deviation regarding p
+            double factor = std::exp(-b * the_x) * std::pow(the_x, p - 1) * std::pow(b, p) / std::pow(boost::math::tgamma(p), 2);
+            double argument = (std::log(b) + std::log(the_x)) * boost::math::tgamma(p) - boost::math::tgamma(p) * boost::math::digamma(p);
+            double part_dev_p = factor * argument;
+            J(i, 1) = part_dev_p;
+          }
+        }
+        else
+        {
+          for (std::vector<DPosition<2> >::const_iterator it = m_data->begin(); it != m_data->end(); ++it, ++i)
+          {
+            J(i, 0) = 0.0;
+            J(i, 1) = 0.0;
+          }
         }
         return 0;
       }
@@ -117,12 +143,12 @@ namespace OpenMS
       const std::vector<DPosition<2> >* m_data;
     };
 
-    GammaDistributionFitter::GammaDistributionFitResult GammaDistributionFitter::fit(const std::vector<DPosition<2> > & input)
+    GammaDistributionFitter::GammaDistributionFitResult GammaDistributionFitter::fit(const std::vector<DPosition<2> >& input)
     {
-      Eigen::VectorXd x_init (2);
-      x_init << init_param_.b, init_param_.p ;
-      GammaFunctor functor (2, &input);
-      Eigen::LevenbergMarquardt<GammaFunctor> lmSolver (functor);
+      Eigen::VectorXd x_init(2);
+      x_init << init_param_.b, init_param_.p;
+      GammaFunctor functor(2, &input);
+      Eigen::LevenbergMarquardt<GammaFunctor> lmSolver(functor);
       Eigen::LevenbergMarquardtSpace::Status status = lmSolver.minimize(x_init);
 
       //the states are poorly documented. after checking the source, we believe that
@@ -135,12 +161,12 @@ namespace OpenMS
 
 #ifdef GAMMA_DISTRIBUTION_FITTER_VERBOSE
       std::stringstream formula;
-      formula << "f(x)=" << "(" << result.b << " ** " << result.p << ") / gamma(" << result.p << ") * x ** (" << result.p << " - 1) * exp(- " << result.b << " * x)";
-      std::cout << formular.str() << std::endl;
+      formula << "f(x)=" << "(" << x_init(0) << " ** " << x_init(1) << ") / gamma(" << x_init(1) << ") * x ** (" << x_init(1) << " - 1) * exp(- " << x_init(0) << " * x)";
+      std::cout << formula.str() << std::endl;
 #endif
 
-      return GammaDistributionFitResult (x_init(0), x_init(1));
+      return GammaDistributionFitResult(x_init(0), x_init(1));
     }
 
-  }   //namespace Math
+  } //namespace Math
 } // namespace OpenMS
