8fef18dda7b904850981f303ec5964eb5826a5b2
Stephan Aiche <stephan.aiche@fu-berlin.de>
2014-07-29 16:47:47 +0200

    [FIX] fixed try/catch misuse for findNearest also in MassTraceDetection

--- src/openms/source/FILTERING/DATAREDUCTION/MassTraceDetection.cpp
+++ src/openms/source/FILTERING/DATAREDUCTION/MassTraceDetection.cpp
@@ -435,7 +435,7 @@ void MassTraceDetection::run(const MSExperiment<Peak1D> & input_exp, std::vector
 
             if (((trace_up_idx < work_exp.size() - 1) && toggle_up))
             {
-                try
+                if (!work_exp[trace_up_idx + 1].empty())
                 {
                     Size next_up_peak_idx = work_exp[trace_up_idx + 1].findNearest(centroid_mz);
                     double next_up_peak_mz = work_exp[trace_up_idx + 1][next_up_peak_idx].getMZ();
@@ -482,10 +482,6 @@ void MassTraceDetection::run(const MSExperiment<Peak1D> & input_exp, std::vector
                         ++conseq_missed_peak_up;
                     }
 
-                }
-                catch (...)
-                {
-                    //  std::cerr << "findNearest() ran into troubles..." << std::endl;
                 }
 
                 ++trace_up_idx;
