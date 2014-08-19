83f6cd7571f5b37ef84264720024d06e84272df1
liangoaix <liang.oaix@gmail.com>
2014-08-19 14:05:46 +0200

    [FIX] split up main function for each executable.

--- src/topp/ProteinProphetAdapter.cpp
+++ src/topp/ProteinProphetAdapter.cpp
@@ -40,8 +40,9 @@
 #include <OpenMS/DATASTRUCTURES/String.h>
 #include <OpenMS/DATASTRUCTURES/ListUtilsIO.h>
 #include <OpenMS/CONCEPT/ProgressLogger.h>
-#include <fstream>
+#include <OpenMS/SYSTEM/File.h>
 
+#include <fstream>
 #include <QtCore/QFile>
 #include <QtCore/QProcess>
 #include <QDir>
@@ -75,13 +76,13 @@ using namespace std;
   </table>
 </CENTER>
   @experimental This TOPP-tool is not well tested and not all features might be properly implemented and tested!
-  
+
   TPP PeptideProphet and ProteinProphet should be installed properly before running this tool.
-  
+
   Some information about the supported input types:
   @ref OpenMS::PepXMLFile "pepXML"
   @ref OpenMS::IdXMLFile "idXML"
-  
+
   <B>The command line parameters of this tool are:</B>
   @verbinclude TOPP_ProteinProphetAdapter.cli
   <B>INI file documentation of this tool:</B>
@@ -120,7 +121,7 @@ protected:
     registerInputFile_("tpp_executable", "<executable>", "/tpp/bin/xinteract",
                        "TPP bin directory e.g. '/usr/local/tpp/bin/xinteract'", true, false, ListUtils::create<String>("skipexists"));
     registerInputFile_("default_input_file", "<file>", "", "Default parameters input file, if not given default parameters are used", false);
-    
+
     registerIntOption_("num_extra_interation", "<num>", 20, "Number of extra PeptideProphet interations; default <num>=20", false);
     registerIntOption_("ignore_charge", "<num>", -1, "Ignore charge <num>+", false);
     registerStringOption_("decoy_prefix", "<tag>", "", "Use decoy hits to pin down the negative distribution; the decoy protein names must begin with <tag> (whitespace is not allowed). e.g. 'decoy_'. OMSSA results must contain decoy search with decoy prefix, otherwise TPP parser error.", false);
@@ -135,7 +136,7 @@ protected:
     setValidStrings_("fragment_type", ListUtils::create<String>("MONO,AVE"));
     registerStringOption_("sample_enzyme", "<enzyme>", "eT", "Specify sample enzyme: -eT = Trypsin, -eS = StrictTrypsin, -eC = Chymotrypsin, -eR = RalphTrypsin, -eA = AspN, -eD = Trypsin/CNBr, -eE = Elastase, -eN = Nonspecific or None, -eG, -eB, -eM, -e3, -eK, -eL, -eP, details see the TPP documentation.", false);
     setValidStrings_("sample_enzyme", ListUtils::create<String>("eT,eS,eC,eR,eA,eD,eE,eN,eG,eB,eM,e3,eK,eL,eP,"));
-    
+
     // (generaloptions) (-Oprophetoptions) (-Xxpressoptions) (-Aasapoptions)
     addEmptyLine_();
     registerStringOption_("iprophet_option", "<option>", "", "iProphet options, run iProphet on the PeptideProphet result. Starting with 'i', details see the TPP documentation. ", false);
@@ -147,106 +148,41 @@ protected:
     registerFlag_("refreshparser_off", "RefreshParser will be disabled (by -nR in TPP).");
   }
 
-  ExitCodes main_(int, const char**)
+  // Parsing parameters of mz_file and DB_file path on different platforms.
+  // TODO: using doubly escaped double quotes might not be necessary.
+  void parseBasenameDBname_(StringList& parameters, String& mz_file, String& base_name, String& db_name)
   {
-// path to the log file
-    String logfile(getStringOption_("log"));
-    StringList parameters;
-    String inputfile_name;
-    String outputfile_name;
-    inputfile_name = getStringOption_("in");
-    writeDebug_(String("Input file: ") + inputfile_name, 1);
-    if (inputfile_name == "")
+    if (!mz_file.empty())
     {
-      writeLog_("No input file specified. Aborting!");
-      printUsage_();
-      return ILLEGAL_PARAMETERS;
-    }
-    outputfile_name = getStringOption_("out");
-    writeDebug_(String("Output file: ") + outputfile_name, 1);
-    if (outputfile_name == "")
-    {
-      writeLog_("No output file specified. Aborting!");
-      printUsage_();
-      return ILLEGAL_PARAMETERS;
-    }
-    
-    String temp_directory = QDir::toNativeSeparators((File::getTempDirectory() + "/" + File::getUniqueName() + "/").toQString()); // body for the tmp files
-    {
-      QDir d;
-      d.mkpath(temp_directory.toQString());
-    }
-    String xinteract_output_filename(temp_directory + "xinteract_output_file.pep.xml");
-    String xinteract_input_filename;
-    // checking if input type is pepxml
-    if (FileHandler().getType(inputfile_name) == FileTypes::PEPXML)
-    {
-      xinteract_input_filename = inputfile_name;
-    }
-    else
-    {
-      xinteract_input_filename = temp_directory + "tpp_input_file.pep.xml";
-      // reading idXML input
-      vector<ProteinIdentification> protein_ids_in;
-      vector<PeptideIdentification> peptide_ids_in;
-      IdXMLFile().load(inputfile_name, protein_ids_in, peptide_ids_in);
-      String mz_file = getStringOption_("mz_file");
-      String mz_dir = "";
-      String base_name = "";
-      if (!mz_file.empty())
-      {
+      String mz_dir = File::path(mz_file);
+      base_name = File::basename(mz_file);
 #ifdef OPENMS_WINDOWSPLATFORM
-        StringList sp = ListUtils::create<String>(mz_file, '\\'); 
-        for (Size i = 0; i != sp.size() - 1; ++i)
-        {
-          mz_dir += sp[i] + "\\";
-        }
-        if (mz_dir.hasSubstring(" ")) // contain space
-        {
-          parameters << "-a" + String("\"\\\"") + String(mz_dir) + String("\\\"\"");
-        }
-        else
-        {
-          parameters << "-a" + mz_dir; // xinteract only needs the path to the mz file.
-        }
-        base_name = sp.back(); 
-#else
-        StringList sp = ListUtils::create<String>(mz_file, '/'); 
-        for (Size i = 0; i != sp.size() - 1; ++i)
-        {
-          mz_dir += sp[i] + "/";
-        }
-        if (mz_dir.hasSubstring(" "))
-        {
-          parameters << "-a" + String("\"") + String(mz_dir) + String("\"");
-        }
-        else
-        {
-          parameters << "-a" + mz_dir; // xinteract only needs the path to the mz file.
-        }
-        base_name = sp.back();
-#endif
+      if (mz_dir.hasSubstring(" ")) // contain space
+      {
+        parameters << "-a" + String("\"\\\"") + String(mz_dir) + String("\\\"\"");
       }
-      PepXMLFile().store(xinteract_input_filename, protein_ids_in, peptide_ids_in, mz_file, base_name, false);
+      else
+      {
+        parameters << "-a" + mz_dir; // xinteract only needs the path to the mz file.
+      }
+#else
+      if (mz_dir.hasSubstring(" "))
+      {
+        parameters << "-a" + String("\"") + String(mz_dir) + String("\"");
+      }
+      else
+      {
+        parameters << "-a" + mz_dir; // xinteract only needs the path to the mz file.
+      }
+#endif
     }
-    
-    //-------------------------------------------------------------
-    // parsing parameters
-    //-------------------------------------------------------------
-    parameters << "-N" + xinteract_output_filename; // store the output temp file
-    // checking database
-    String db_name = getStringOption_("database");
-    bool db_name_contains_space = false;
+
     if (!db_name.empty())
     {
       if (db_name.hasSubstring(" "))
       {
-        db_name_contains_space = true;
-      }
-      if (db_name_contains_space)
-      {
 #ifdef OPENMS_WINDOWSPLATFORM
-// Windows: use doubly escaped double quotes (and do a system call instead of QProcess later)
+// Windows: use doubly escaped double quotes
         parameters << "-D" + String("\"\\\"") + String(db_name) + String("\\\"\"");
 #else
         parameters << "-D" + String("\"") + String(db_name) + String("\"");
@@ -257,6 +193,126 @@ protected:
         parameters << "-D" + String(db_name);
       }
     }
+  }
+
+  String runXinteract_(String& exe_path, StringList& parameters, String& temp_directory, String& xinteract_input_filename)
+  {
+    String xinteract_executable(exe_path + "/" + "xinteract");
+    String xinteract_output_filename(temp_directory + "xinteract_output_file.pep.xml");
+    parameters << "-N" + xinteract_output_filename; // store the output temp file
+
+    QStringList qparam;
+    qparam << xinteract_input_filename.toQString();
+    for (Size i = 0; i < parameters.size(); ++i)
+    {
+      qparam << parameters[i].toQString();
+    }
+
+    int status = QProcess::execute(xinteract_executable.toQString(), qparam);
+    if (status != 0)
+    {
+      writeLog_("TPP problem. Aborting! Calling command was: '" + xinteract_executable + " \"" + xinteract_input_filename + "\"'.\nDoes the TPP executable exist?");
+      // clean temporary files
+      if (this->debug_level_ < 2)
+      {
+        File::removeDirRecursively(temp_directory);
+        LOG_WARN << "Set debug level to >=2 to keep the temporary files at '" << temp_directory << "'" << std::endl;
+      }
+      else
+      {
+        LOG_WARN << "Keeping the temporary files at '" << temp_directory << "'. Set debug level to <2 to remove them." << std::endl;
+      }
+      return EXTERNAL_PROGRAM_ERROR;
+    }
+    StringList directories;
+    directories << temp_directory;
+    return File::find("xinteract_output_file.pep.xml", directories); // will throw Exception:FileNotFound if not find output
+  }
+
+  String runProteinProphet_(String& exe_path, StringList& parameters, String& temp_directory, String& proteinprophet_input_filename)
+  {
+    String pp_executable(exe_path + "/" + "ProteinProphet");
+    String proteinprophet_output_filename(temp_directory + "proteinprophet_output_file.prot.xml");
+    QStringList qparam;
+    qparam << proteinprophet_input_filename.toQString();
+    qparam << proteinprophet_output_filename.toQString();
+    for (Size i = 0; i < parameters.size(); ++i)
+    {
+      qparam << parameters[i].toQString();
+    }
+    int status = QProcess::execute(pp_executable.toQString(), qparam);
+    if (status != 0)
+    {
+      writeLog_("ProteinProphet problem. Aborting! Calling command was: '" + pp_executable + " \"" + proteinprophet_input_filename + "\"'.\nIs ProteinProphet executable in the same folder as xinteract.");
+      if (this->debug_level_ < 2)
+      {
+        File::removeDirRecursively(temp_directory);
+        LOG_WARN << "Set debug level to >=2 to keep the temporary files at '" << temp_directory << "'" << std::endl;
+      }
+      else
+      {
+        LOG_WARN << "Keeping the temporary files at '" << temp_directory << "'. Set debug level to <2 to remove them." << std::endl;
+      }
+      return EXTERNAL_PROGRAM_ERROR;
+    }
+    StringList directories;
+    directories << temp_directory;
+    return File::find("proteinprophet_output_file.prot.xml", directories); // will throw Exception:FileNotFound if not find output
+  }
+
+  ExitCodes main_(int, const char**)
+  {
+    // path to the log file
+    String logfile(getStringOption_("log"));
+    StringList parameters;
+    String inputfile_name = getStringOption_("in");
+    writeDebug_(String("Input file: ") + inputfile_name, 1);
+    if (inputfile_name == "")
+    {
+      writeLog_("No input file specified. Aborting!");
+      printUsage_();
+      return ILLEGAL_PARAMETERS;
+    }
+    String outputfile_name = getStringOption_("out");
+    writeDebug_(String("Output file: ") + outputfile_name, 1);
+    if (outputfile_name == "")
+    {
+      writeLog_("No output file specified. Aborting!");
+      printUsage_();
+      return ILLEGAL_PARAMETERS;
+    }
+
+    //-------------------------------------------------------------
+    // parsing parameters
+    //-------------------------------------------------------------
+    String db_name = getStringOption_("database");
+    String mz_file = getStringOption_("mz_file");
+    String base_name = "";
+    // Parsing parameters of mz_file and DB file path, considering different platform cases,
+    parseBasenameDBname_(parameters, mz_file, base_name, db_name);
+
+    String temp_directory = QDir::toNativeSeparators((File::getTempDirectory() + "/" + File::getUniqueName() + "/").toQString()); // body for the tmp files
+    {
+      QDir d;
+      d.mkpath(temp_directory.toQString());
+    }
+
+    String xinteract_input_filename;
+    // checking if input type is pepxml
+    if (FileHandler().getType(inputfile_name) == FileTypes::PEPXML)
+    {
+      xinteract_input_filename = inputfile_name;
+    }
+    else
+    {
+      xinteract_input_filename = temp_directory + "tpp_input_file.pep.xml";
+      // reading idXML input and converting into pepXML format as xinteract input
+      vector<ProteinIdentification> protein_ids_in;
+      vector<PeptideIdentification> peptide_ids_in;
+      IdXMLFile().load(inputfile_name, protein_ids_in, peptide_ids_in);
+      PepXMLFile().store(xinteract_input_filename, protein_ids_in, peptide_ids_in, mz_file, base_name, false);
+    }
+
     parameters << "-T" + getStringOption_("database_type");
     parameters << "-x" + String(getIntOption_("num_extra_interation"));
     if (getIntOption_("ignore_charge") >= 0)
@@ -317,108 +373,36 @@ protected:
     ProgressLogger pl;
     pl.setLogType(ProgressLogger::CMD);
     pl.startProgress(0, 1, "running xinteract...");
-    
-    String xinteract_executable(getStringOption_("tpp_executable"));
-    QStringList qparam;
-    qparam << xinteract_input_filename.toQString();
-    for (Size ip = 0; ip < parameters.size(); ++ip)
+    String exe_path;
+    if (File::isDirectory(getStringOption_("tpp_executable")))
     {
-      qparam << parameters[ip].toQString();
-    }
-    Int status = 0;
-#ifdef OPENMS_WINDOWSPLATFORM
-    if (db_name_contains_space)
-    {
-      String call_string = xinteract_executable + " " + ListUtils::concatenate(parameters, " ");
-      writeDebug_(call_string, 5);
-      status = system(call_string.c_str());
+      exe_path = getStringOption_("tpp_executable");
     }
     else
     {
-      writeDebug_(xinteract_executable + " " + ListUtils::concatenate(parameters, " "), 5);
-      status = QProcess::execute(xinteract_executable.toQString(), qparam);
+      exe_path = File::path(getStringOption_("tpp_executable"));
     }
-#else
-    writeDebug_(xinteract_executable + " " + ListUtils::concatenate(parameters, " "), 5);
-    status = QProcess::execute(xinteract_executable.toQString(), qparam);
-#endif
-    if (status != 0)
-    {
-      writeLog_("TPP problem. Aborting! Calling command was: '" + xinteract_executable + " \"" + xinteract_input_filename + "\"'.\nDoes the TPP executable exist?");
-// clean temporary files
-      if (this->debug_level_ < 2)
-      {
-        File::removeDirRecursively(temp_directory);
-        LOG_WARN << "Set debug level to >=2 to keep the temporary files at '" << temp_directory << "'" << std::endl;
-      }
-      else
-      {
-        LOG_WARN << "Keeping the temporary files at '" << temp_directory << "'. Set debug level to <2 to remove them." << std::endl;
-      }
-      return EXTERNAL_PROGRAM_ERROR;
-    }
-    StringList files;
-    File::fileList(temp_directory, "xinteract_output_file*.pep.xml", files);
-    if (files.size() != 1)
-    {
-      throw Exception::FileNotFound(__FILE__, __LINE__, __PRETTY_FUNCTION__, xinteract_output_filename);
-    }
+    String xinteract_output_filename = runXinteract_(exe_path, parameters, temp_directory, xinteract_input_filename);
     pl.endProgress();
+
     //-------------------------------------------------------------
     // run ProteinProphet afterwards
     //-------------------------------------------------------------
     // "usage:\tProteinProphet <interact_pepxml_file1> [<interact_pepxml_file2>[....]] <output_protxml_file> (ICAT) (GLYC) (XPRESS) (ASAP_PROPHET) (FPKM) (NONSP) (ACCURACY) (ASAP) (PROTLEN) (NOPROTLEN) (IPROPHET) (NORMPROTLEN) (GROUPWTS) (INSTANCES) (REFRESH) (DELUDE) (NOOCCAM) (NOPLOT) (PROTMW)
     if (!getFlag_("proteinprophet_off"))
     {
-      pl.startProgress(0, 1, "Running ProteinProphet...");
-#ifdef OPENMS_WINDOWSPLATFORM
-      StringList exe_path = ListUtils::create<String>(xinteract_executable, '\\'); // TODO: different platform
-      String pp_executable;
-      for (Size i = 0; i != exe_path.size() - 1; ++i)
-      {
-        pp_executable += exe_path[i] + '\\';
-      }
-#else
-      StringList exe_path = ListUtils::create<String>(xinteract_executable, '/'); // TODO: different platform
-      String pp_executable;
-      for (Size i = 0; i != exe_path.size() - 1; ++i)
-      {
-        pp_executable += exe_path[i] + '/';
-      }
-#endif
-      pp_executable += "ProteinProphet";
-      String proteinprophet_input_filename(xinteract_output_filename); // TODO: more than one interact_pepxml files as input.
-      String proteinprophet_output_filename(temp_directory + "proteinprophet_output_file.prot.xml");
-      QStringList qparam_pp;
-      qparam_pp << proteinprophet_input_filename.toQString();
-      qparam_pp << proteinprophet_output_filename.toQString();
+      StringList parameters_pp;
+      parameters_pp << "MINPROB" + String(getDoubleOption_("filter_result"));
+      parameters_pp << "NOPLOT";
       if (!getStringOption_("proteinprophet_option").empty())
       {
-        qparam_pp << String(getStringOption_("proteinprophet_option")).toQString();
+        parameters_pp << String(getStringOption_("proteinprophet_option"));
       }
-      Int new_status = 0;
-      writeDebug_(pp_executable + " ", 5);
-      status = QProcess::execute(pp_executable.toQString(), qparam_pp);
-      if (new_status != 0)
-      {
-        writeLog_("ProteinProphet problem. Aborting! Calling command was: '" + pp_executable + " \"" + proteinprophet_input_filename + "\"'.\nIs ProteinProphet executable in the same folder as xinteract.");
-        if (this->debug_level_ < 2)
-        {
-          File::removeDirRecursively(temp_directory);
-          LOG_WARN << "Set debug level to >=2 to keep the temporary files at '" << temp_directory << "'" << std::endl;
-        }
-        else
-        {
-          LOG_WARN << "Keeping the temporary files at '" << temp_directory << "'. Set debug level to <2 to remove them." << std::endl;
-        }
-        return EXTERNAL_PROGRAM_ERROR;
-      }
-      StringList files_pp;
-      File::fileList(temp_directory, "proteinprophet_output_file*.prot.xml", files_pp);
-      if (files_pp.size() != 1)
-      {
-        throw Exception::FileNotFound(__FILE__, __LINE__, __PRETTY_FUNCTION__, proteinprophet_output_filename);
-      }
+      pl.startProgress(0, 1, "Running ProteinProphet...");
+      String proteinprophet_output_filename = runProteinProphet_(exe_path, parameters_pp, temp_directory, xinteract_output_filename);
+      //-------------------------------------------------------------
+      // writing output
+      //-------------------------------------------------------------
       if (getStringOption_("out_type") == "idXML")
       {
         vector<ProteinIdentification> protein_ids;
@@ -433,6 +417,7 @@ protected:
       {
         QFile::copy(proteinprophet_output_filename.toQString(), outputfile_name.toQString());
       }
+      // Deletion of temporary files
       if (this->debug_level_ < 2)
       {
         File::removeDirRecursively(temp_directory);
@@ -444,11 +429,8 @@ protected:
       }
       pl.endProgress();
     }
-    else
+    else // if not running ProteinProphet, then only storing PeptideProphet results.
     {
-      //-------------------------------------------------------------
-      // only storing PeptideProphet result.
-      //-------------------------------------------------------------
       if (getStringOption_("out_type") == "idXML")
       {
         vector<ProteinIdentification> protein_ids;
