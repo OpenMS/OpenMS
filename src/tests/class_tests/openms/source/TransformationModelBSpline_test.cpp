9c4472f0b31c8f11dc9a0b4a5fd9681cb436f7f9
Hendrik Weisser <hw5@sanger.ac.uk>
2014-10-15 16:52:23 +0100

    [COMMENT] added maintainers

--- src/tests/class_tests/openms/source/TransformationModelBSpline_test.cpp
+++ src/tests/class_tests/openms/source/TransformationModelBSpline_test.cpp
@@ -28,7 +28,7 @@
 // ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 // --------------------------------------------------------------------------
-// $Maintainer: $
+// $Maintainer: Hendrik Weisser $
 // $Authors: Hendrik Weisser, Stephan Aiche $
 // --------------------------------------------------------------------------
 
--- src/tests/class_tests/openms/source/TransformationModelLinear_test.cpp
+++ src/tests/class_tests/openms/source/TransformationModelLinear_test.cpp
@@ -28,7 +28,7 @@
 // ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 // --------------------------------------------------------------------------
-// $Maintainer: $
+// $Maintainer: Stephan Aiche $
 // $Authors: Hendrik Weisser, Stephan Aiche $
 // --------------------------------------------------------------------------
 
