c6ff9b5f47261e706ae229a6920c6e262bc88325
mwalzer <walzer@informatik.uni-tuebingen.de>
2015-01-20 13:34:56 +0100

    [FIX] fix for the write out of mzid from old id formats with insufficient data provision - nan value for spectrumreferenceid now equal on all platforms, added warning if such is necessary

--- src/openms/source/FORMAT/HANDLERS/MzIdentMLHandler.cpp
+++ src/openms/source/FORMAT/HANDLERS/MzIdentMLHandler.cpp
@@ -614,6 +614,16 @@ namespace OpenMS
           sid = String(it->getMetaValue("spectrum_id"));
           if (sid.empty())
           {
+            if (emz.empty())
+            {
+              emz = "nan";
+              LOG_WARN << "Found no spectrum reference and no mz position of identified spectrum! You are probabliy converting from an old format with insufficient data provision. Setting 'nan' - downstream applications might fail unless you set the references right." << std::endl;
+            }
+            if (ert.empty())
+            {
+              ert = "nan";
+              LOG_WARN << "Found no spectrum reference and no RT position of identified spectrum! You are probabliy converting from an old format with insufficient data provision. Setting 'nan' - downstream applications might fail unless you set the references right." << std::endl;
+            }
             sid = String("MZ:") + emz + String("@RT:") + ert;
           }
         }
