50ddb032397af9e98b023aa36f349374dcd3bf59
Hendrik Weisser <hw5@sanger.ac.uk>
2015-03-12 15:01:35 +0000

    [TEST] updated and extended ConsensusID class test

--- src/tests/class_tests/openms/source/ConsensusID_test.cpp
+++ src/tests/class_tests/openms/source/ConsensusID_test.cpp
@@ -60,277 +60,219 @@ START_SECTION(~ConsensusID())
   delete(ptr);
 END_SECTION
 
-// PeptideIdentification with 3 id runs is created
-vector<PeptideIdentification> ids(3);
+// 3 ID runs are created:
+PeptideIdentification temp;
+temp.setScoreType("Posterior Error Probability");
+temp.setHigherScoreBetter(false);
+vector<PeptideIdentification> ids(3, temp);
 vector<PeptideHit> hits;
-cout<<"HELLO"<<endl;
 // the first ID has 5 hits
 hits.resize(5);
-hits[0].setRank(1);
 hits[0].setSequence(AASequence::fromString("A"));
-hits[0].setScore(31);
-hits[1].setRank(2);
+hits[0].setScore(0.1);
 hits[1].setSequence(AASequence::fromString("B"));
-hits[1].setScore(28);
-hits[2].setRank(3);
+hits[1].setScore(0.2);
 hits[2].setSequence(AASequence::fromString("C"));
-hits[2].setScore(17);
-hits[3].setRank(4);
+hits[2].setScore(0.3);
 hits[3].setSequence(AASequence::fromString("D"));
-hits[3].setScore(7);
-hits[4].setRank(5);
+hits[3].setScore(0.4);
 hits[4].setSequence(AASequence::fromString("E"));
-hits[4].setScore(3);
+hits[4].setScore(0.5);
 ids[0].setHits(hits);
 // the second ID has 3 hits
 hits.resize(3);
-hits[0].setRank(1);
 hits[0].setSequence(AASequence::fromString("C"));
-hits[0].setScore(32);
-hits[1].setRank(2);
+hits[0].setScore(0.2);
 hits[1].setSequence(AASequence::fromString("A"));
-hits[1].setScore(30);
-hits[2].setRank(3);
+hits[1].setScore(0.4);
 hits[2].setSequence(AASequence::fromString("B"));
-hits[2].setScore(29);
+hits[2].setScore(0.6);
 ids[1].setHits(hits);
 // the third ID has 10 hits
 hits.resize(10);
-hits[0].setRank(1);
 hits[0].setSequence(AASequence::fromString("F"));
-hits[0].setScore(81);
-hits[1].setRank(2);
+hits[0].setScore(0.0);
 hits[1].setSequence(AASequence::fromString("C"));
-hits[1].setScore(60);
-hits[2].setRank(3);
+hits[1].setScore(0.1);
 hits[2].setSequence(AASequence::fromString("G"));
-hits[2].setScore(50);
-hits[3].setRank(4);
+hits[2].setScore(0.2);
 hits[3].setSequence(AASequence::fromString("D"));
-hits[3].setScore(40);
-hits[4].setRank(5);
+hits[3].setScore(0.3);
 hits[4].setSequence(AASequence::fromString("B"));
-hits[4].setScore(25);
-hits[5].setRank(6);
+hits[4].setScore(0.4);
 hits[5].setSequence(AASequence::fromString("E"));
-hits[5].setScore(5);
-hits[6].setRank(7);
+hits[5].setScore(0.5);
 hits[6].setSequence(AASequence::fromString("H"));
-hits[6].setScore(4);
-hits[7].setRank(8);
+hits[6].setScore(0.6);
 hits[7].setSequence(AASequence::fromString("I"));
-hits[7].setScore(3);
-hits[8].setRank(9);
+hits[7].setScore(0.7);
 hits[8].setSequence(AASequence::fromString("J"));
-hits[8].setScore(2);
-hits[9].setRank(10);
+hits[8].setScore(0.8);
 hits[9].setSequence(AASequence::fromString("K"));
-hits[9].setScore(1);
+hits[9].setScore(0.9);
 ids[2].setHits(hits);
 
 START_SECTION(void apply(std::vector<PeptideIdentification>& ids))
+{
   TOLERANCE_ABSOLUTE(0.01)
 
-  // ***** Ranked ********
+  // ***** Best ********
 
   ConsensusID consensus;
   //define parameters
   Param param;
-  param.setValue("algorithm","ranked");
-  param.setValue("considered_hits",5);
+  param.setValue("algorithm", "best");
+  param.setValue("considered_hits", 0);
   consensus.setParameters(param);
   //apply
   vector<PeptideIdentification> f = ids;
   consensus.apply(f);
 
-  TEST_EQUAL(f.size(),1);
+  TEST_EQUAL(f.size(), 1);
   hits = f[0].getHits();
-  TEST_EQUAL(hits.size(),7);
+  TEST_EQUAL(hits.size(), 11);
 
-  TEST_EQUAL(hits[0].getRank(),1);
-  TEST_EQUAL(hits[0].getSequence(),AASequence::fromString("C"));
-  TEST_REAL_SIMILAR(hits[0].getScore(),80.0f);
+  TEST_EQUAL(hits[0].getRank(), 1);
+  TEST_EQUAL(hits[0].getSequence(), AASequence::fromString("F"));
+  TEST_REAL_SIMILAR(hits[0].getScore(), 0.0);
 
-  TEST_EQUAL(hits[1].getRank(),2);
-  TEST_EQUAL(hits[1].getSequence(),AASequence::fromString("A"));
-  TEST_REAL_SIMILAR(hits[1].getScore(),60.0f);
+  // hits with the same score get assigned the same rank:
+  TEST_EQUAL(hits[1].getRank(), 2);
+  TEST_EQUAL(hits[1].getSequence(), AASequence::fromString("A"));
+  TEST_REAL_SIMILAR(hits[1].getScore(), 0.1);
 
-  TEST_EQUAL(hits[2].getRank(),3);
-  TEST_EQUAL(hits[2].getSequence(),AASequence::fromString("B"));
-  TEST_REAL_SIMILAR(hits[2].getScore(),53.33f);
+  TEST_EQUAL(hits[2].getRank(), 2);
+  TEST_EQUAL(hits[2].getSequence(), AASequence::fromString("C"));
+  TEST_REAL_SIMILAR(hits[2].getScore(), 0.1);
 
-  TEST_EQUAL(hits[3].getRank(),4);
-  TEST_EQUAL(hits[3].getSequence(),AASequence::fromString("F"));
-  TEST_REAL_SIMILAR(hits[3].getScore(),33.333f);
+  TEST_EQUAL(hits[3].getRank(), 3);
+  TEST_EQUAL(hits[3].getSequence(), AASequence::fromString("B"));
+  TEST_REAL_SIMILAR(hits[3].getScore(), 0.2);
 
-  TEST_EQUAL(hits[4].getRank(),5);
-  TEST_EQUAL(hits[4].getSequence(),AASequence::fromString("D"));
-  TEST_REAL_SIMILAR(hits[4].getScore(),26.666f);
+  TEST_EQUAL(hits[4].getRank(), 3);
+  TEST_EQUAL(hits[4].getSequence(), AASequence::fromString("G"));
+  TEST_REAL_SIMILAR(hits[4].getScore(), 0.2);
 
-  TEST_EQUAL(hits[5].getRank(),6);
-  TEST_EQUAL(hits[5].getSequence(),AASequence::fromString("G"));
-  TEST_REAL_SIMILAR(hits[5].getScore(),20.0f);
+  TEST_EQUAL(hits[5].getRank(), 4);
+  TEST_EQUAL(hits[5].getSequence(), AASequence::fromString("D"));
+  TEST_REAL_SIMILAR(hits[5].getScore(), 0.3);
 
-  TEST_EQUAL(hits[6].getRank(),7);
-  TEST_EQUAL(hits[6].getSequence(),AASequence::fromString("E"));
-  TEST_REAL_SIMILAR(hits[6].getScore(),6.666f);
+  TEST_EQUAL(hits[6].getRank(), 5);
+  TEST_EQUAL(hits[6].getSequence(), AASequence::fromString("E"));
+  TEST_REAL_SIMILAR(hits[6].getScore(), 0.5);
 
-  //~ // ***** Merge ********
+  TEST_EQUAL(hits[7].getRank(), 6);
+  TEST_EQUAL(hits[7].getSequence(), AASequence::fromString("H"));
+  TEST_REAL_SIMILAR(hits[7].getScore(), 0.6);
 
-  //~ //define parameters
-  //~ param.clear();
-  //~ param.setValue("algorithm","merge");
-  //~ param.setValue("considered_hits",6);
-  //~ consensus.setParameters(param);
-  //~ //apply
-  //~ f = ids;
-  //~ consensus.apply(f);
+  TEST_EQUAL(hits[8].getRank(), 7);
+  TEST_EQUAL(hits[8].getSequence(), AASequence::fromString("I"));
+  TEST_REAL_SIMILAR(hits[8].getScore(), 0.7);
 
-  //~ TEST_EQUAL(f.size(),1);
-  //~ hits = f[0].getHits();
-  //~ TEST_EQUAL(hits.size(),7);
+  TEST_EQUAL(hits[9].getRank(), 8);
+  TEST_EQUAL(hits[9].getSequence(), AASequence::fromString("J"));
+  TEST_REAL_SIMILAR(hits[9].getScore(), 0.8);
 
-  //~ TEST_EQUAL(hits[0].getRank(),1);
-  //~ TEST_EQUAL(hits[0].getSequence(),"F");
-  //~ TEST_REAL_SIMILAR(hits[0].getScore(),81.0f);
+  TEST_EQUAL(hits[10].getRank(), 9);
+  TEST_EQUAL(hits[10].getSequence(), AASequence::fromString("K"));
+  TEST_REAL_SIMILAR(hits[10].getScore(), 0.9);
 
-  //~ TEST_EQUAL(hits[1].getRank(),2);
-  //~ TEST_EQUAL(hits[1].getSequence(),"C");
-  //~ TEST_REAL_SIMILAR(hits[1].getScore(),60.0f);
+  // ***** Ranked ********
 
-  //~ TEST_EQUAL(hits[2].getRank(),3);
-  //~ TEST_EQUAL(hits[2].getSequence(),"G");
-  //~ TEST_REAL_SIMILAR(hits[2].getScore(),50.0f);
+  //define parameters
+  param.clear();
+  param.setValue("algorithm", "rank");
+  param.setValue("considered_hits", 5);
+  param.setValue("rank:number_of_runs", 3);
+  consensus.setParameters(param);
 
-  //~ TEST_EQUAL(hits[3].getRank(),4);
-  //~ TEST_EQUAL(hits[3].getSequence(),"D");
-  //~ TEST_REAL_SIMILAR(hits[3].getScore(),40.0f);
+  //apply
+  f = ids;
+  consensus.apply(f);
 
-  //~ TEST_EQUAL(hits[4].getRank(),5);
-  //~ TEST_EQUAL(hits[4].getSequence(),"A");
-  //~ TEST_REAL_SIMILAR(hits[4].getScore(),31.0f);
+  TEST_EQUAL(f.size(), 1);
+  hits = f[0].getHits();
+  TEST_EQUAL(hits.size(), 7);
 
-  //~ TEST_EQUAL(hits[5].getRank(),6);
-  //~ TEST_EQUAL(hits[5].getSequence(),"B");
-  //~ TEST_REAL_SIMILAR(hits[5].getScore(),29.0f);
+  TEST_EQUAL(hits[0].getRank(), 1);
+  TEST_EQUAL(hits[0].getSequence(), AASequence::fromString("C"));
+  TEST_REAL_SIMILAR(hits[0].getScore(), 0.8);
 
-  //~ TEST_EQUAL(hits[6].getRank(),7);
-  //~ TEST_EQUAL(hits[6].getSequence(),"E");
-  //~ TEST_REAL_SIMILAR(hits[6].getScore(),5.0f);
+  TEST_EQUAL(hits[1].getRank(), 2);
+  TEST_EQUAL(hits[1].getSequence(), AASequence::fromString("A"));
+  TEST_REAL_SIMILAR(hits[1].getScore(), 0.6);
+
+  TEST_EQUAL(hits[2].getRank(), 3);
+  TEST_EQUAL(hits[2].getSequence(), AASequence::fromString("B"));
+  TEST_REAL_SIMILAR(hits[2].getScore(), 0.5333);
+
+  TEST_EQUAL(hits[3].getRank(), 4);
+  TEST_EQUAL(hits[3].getSequence(), AASequence::fromString("F"));
+  TEST_REAL_SIMILAR(hits[3].getScore(), 0.33333);
+
+  TEST_EQUAL(hits[4].getRank(), 5);
+  TEST_EQUAL(hits[4].getSequence(), AASequence::fromString("D"));
+  TEST_REAL_SIMILAR(hits[4].getScore(), 0.26666);
+
+  TEST_EQUAL(hits[5].getRank(), 6);
+  TEST_EQUAL(hits[5].getSequence(), AASequence::fromString("G"));
+  TEST_REAL_SIMILAR(hits[5].getScore(), 0.2);
+
+  TEST_EQUAL(hits[6].getRank(), 7);
+  TEST_EQUAL(hits[6].getSequence(), AASequence::fromString("E"));
+  TEST_REAL_SIMILAR(hits[6].getScore(), 0.06666);
 
   // ***** Average ********
 
   //define parameters
   param.clear();
-  param.setValue("algorithm","average");
-  param.setValue("considered_hits",4);
+  param.setValue("algorithm", "average");
+  param.setValue("considered_hits", 5);
   consensus.setParameters(param);
   //apply
   f = ids;
   consensus.apply(f);
 
-  TEST_EQUAL(f.size(),1);
+  TEST_EQUAL(f.size(), 1);
   hits = f[0].getHits();
-  TEST_EQUAL(hits.size(),6);
+  TEST_EQUAL(hits.size(), 7);
 
-  TEST_EQUAL(hits[0].getRank(),1);
-  TEST_EQUAL(hits[0].getSequence(),AASequence::fromString("C"));
-  TEST_REAL_SIMILAR(hits[0].getScore(),36.333f);
+  TEST_EQUAL(hits[0].getRank(), 1);
+  TEST_EQUAL(hits[0].getSequence(), AASequence::fromString("F"));
+  TEST_REAL_SIMILAR(hits[0].getScore(), 0.0);
 
-  TEST_EQUAL(hits[1].getRank(),2);
-  TEST_EQUAL(hits[1].getSequence(),AASequence::fromString("F"));
-  TEST_REAL_SIMILAR(hits[1].getScore(),27.0f);
+  // the two "0.2" scores are not equal (due to floating-point number effects),
+  // therefore the ranks of the hits differ:
+  TEST_EQUAL(hits[1].getScore() < hits[2].getScore(), true);
 
-  TEST_EQUAL(hits[2].getRank(),3);
-  TEST_EQUAL(hits[2].getSequence(),AASequence::fromString("A"));
-  TEST_REAL_SIMILAR(hits[2].getScore(),20.333f);
+  TEST_EQUAL(hits[1].getRank(), 2);
+  TEST_EQUAL(hits[1].getSequence(), AASequence::fromString("C"));
+  TEST_REAL_SIMILAR(hits[1].getScore(), 0.2);
 
-  TEST_EQUAL(hits[3].getRank(),4);
-  TEST_EQUAL(hits[3].getSequence(),AASequence::fromString("B"));
-  TEST_REAL_SIMILAR(hits[3].getScore(),19.0f);
+  TEST_EQUAL(hits[2].getRank(), 3);
+  TEST_EQUAL(hits[2].getSequence(), AASequence::fromString("G"));
+  TEST_REAL_SIMILAR(hits[2].getScore(), 0.2);
+  
+  TEST_EQUAL(hits[3].getRank(), 4);
+  TEST_EQUAL(hits[3].getSequence(), AASequence::fromString("A"));
+  TEST_REAL_SIMILAR(hits[3].getScore(), 0.25);
 
-  TEST_EQUAL(hits[4].getRank(),5);
-  TEST_EQUAL(hits[4].getSequence(),AASequence::fromString("G"));
-  TEST_REAL_SIMILAR(hits[4].getScore(),16.666f);
+  TEST_EQUAL(hits[4].getRank(), 5);
+  TEST_EQUAL(hits[4].getSequence(), AASequence::fromString("D"));
+  TEST_REAL_SIMILAR(hits[4].getScore(), 0.35);
 
-  TEST_EQUAL(hits[5].getRank(),6);
-  TEST_EQUAL(hits[5].getSequence(),AASequence::fromString("D"));
-  TEST_REAL_SIMILAR(hits[5].getScore(),15.666f);
+  TEST_EQUAL(hits[5].getRank(), 6);
+  TEST_EQUAL(hits[5].getSequence(), AASequence::fromString("B"));
+  TEST_REAL_SIMILAR(hits[5].getScore(), 0.4);
 
-  // ***** Average, Inverse Order ********
+  TEST_EQUAL(hits[6].getRank(), 7);
+  TEST_EQUAL(hits[6].getSequence(), AASequence::fromString("E"));
+  TEST_REAL_SIMILAR(hits[6].getScore(), 0.5);
 
-  //define parameters
-  param.clear();
-  param.setValue("algorithm","average");
-  param.setValue("considered_hits",1);
-  consensus.setParameters(param);
-  f = ids;
-  for (Size i = 0; i < f.size(); ++i)
-  {
-    f[i].setHigherScoreBetter(false);
-  }
-  //apply
-  consensus.apply(f);
-
-  TEST_EQUAL(f.size(),1);
-  hits = f[0].getHits();
-  TEST_EQUAL(hits.size(),3);
-
-  TEST_EQUAL(hits[0].getRank(),1);
-  TEST_EQUAL(hits[0].getSequence(),AASequence::fromString("K"));
-  TEST_REAL_SIMILAR(hits[0].getScore(),0.333f);
-
-  TEST_EQUAL(hits[1].getRank(),2);
-  TEST_EQUAL(hits[1].getSequence(),AASequence::fromString("E"));
-  TEST_REAL_SIMILAR(hits[1].getScore(),1.0f);
-
-  TEST_EQUAL(hits[2].getRank(),3);
-  TEST_EQUAL(hits[2].getSequence(),AASequence::fromString("B"));
-  TEST_REAL_SIMILAR(hits[2].getScore(),9.666f);
-
-  // ***** probability ********
-
-/*	//define parameters
-  param.clear();
-  param.setValue("algorithm","probability");
-  param.setValue("considered_hits",5);
-  consensus.setParameters(param);
-  //apply
-  f = ids;
-  consensus.apply(f);
-
-  TEST_EQUAL(f.size(),1);
-  hits = f[0].getHits();
-  TEST_EQUAL(hits.size(),7);
-
-  TEST_EQUAL(hits[0].getRank(),1);
-  TEST_EQUAL(hits[0].getSequence(),"C");
-  TEST_REAL_SIMILAR(hits[0].getScore(),32640.0f);
-
-  TEST_EQUAL(hits[1].getRank(),2);
-  TEST_EQUAL(hits[1].getSequence(),"B");
-  TEST_REAL_SIMILAR(hits[1].getScore(),20300.0f);
-
-  TEST_EQUAL(hits[2].getRank(),3);
-  TEST_EQUAL(hits[2].getSequence(),"A");
-  TEST_REAL_SIMILAR(hits[2].getScore(),930.0f);
-
-  TEST_EQUAL(hits[3].getRank(),4);
-  TEST_EQUAL(hits[3].getSequence(),"D");
-  TEST_REAL_SIMILAR(hits[3].getScore(),280.0f);
-
-  TEST_EQUAL(hits[4].getRank(),5);
-  TEST_EQUAL(hits[4].getSequence(),"F");
-  TEST_REAL_SIMILAR(hits[4].getScore(),81.0f);
-
-  TEST_EQUAL(hits[5].getRank(),6);
-  TEST_EQUAL(hits[5].getSequence(),"G");
-  TEST_REAL_SIMILAR(hits[5].getScore(),50.0f);
-
-*/
   // ***** Exception ********
   param.setValue("algorithm","Bla4711");
-  TEST_EXCEPTION(Exception::InvalidParameter,consensus.setParameters(param));
+  TEST_EXCEPTION(Exception::InvalidParameter, consensus.setParameters(param));
+}
 END_SECTION
 
 /////////////////////////////////////////////////////////////
