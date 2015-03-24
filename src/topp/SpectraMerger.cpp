b6beb41eb134fb4e34ae1ca165f20a0509e8f836
lars20070 <l.nilse@dunelm.org.uk>
2015-03-24 09:16:05 +0100

    [FEATURE] spectrum_type option fixed

--- src/topp/SpectraMerger.cpp
+++ src/topp/SpectraMerger.cpp
@@ -105,11 +105,11 @@ protected:
     registerSubsection_("algorithm", "Algorithm section for merging spectra");
     
     registerTOPPSubsection_("algorithm:average_gaussian", "Averaging spectra using a Gaussian RT profile");
-    registerStringOption_("algorithm:average_gaussian:spectrum_type", "<spectrum_type>", "", "Spectrum type of the MS level to be averaged", true);
+    registerStringOption_("algorithm:average_gaussian:spectrum_type", "<spectrum_type>", "automatic", "Spectrum type of the MS level to be averaged", false, true);
     setValidStrings_("algorithm:average_gaussian:spectrum_type", ListUtils::create<String>("profile,centroid,automatic"));
     
     registerTOPPSubsection_("algorithm:average_tophat", "Averaging spectra using a Top-Hat RT profile");
-    registerStringOption_("algorithm:average_tophat:spectrum_type", "<spectrum_type>", "", "Spectrum type of the MS level to be averaged", true);
+    registerStringOption_("algorithm:average_tophat:spectrum_type", "<spectrum_type>", "automatic", "Spectrum type of the MS level to be averaged", false, true);
     setValidStrings_("algorithm:average_tophat:spectrum_type", ListUtils::create<String>("profile,centroid,automatic"));
   }
 
