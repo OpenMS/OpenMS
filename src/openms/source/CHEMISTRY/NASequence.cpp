7c4855c4c7ef867e4c6222556ef4225e690db677
poshul <poshul@gmail.com>
2015-04-30 14:42:14 +0200

    [Fix] Fixed NASequence calculation of monomass and formula. Added tests for these functions.

--- src/openms/source/CHEMISTRY/NASequence.cpp
+++ src/openms/source/CHEMISTRY/NASequence.cpp
@@ -75,24 +75,25 @@ EmpiricalFormula NASequence::getFormula(Residue::ResidueType type, Int charge) c
     static const EmpiricalFormula OH_weight = EmpiricalFormula("OH");
     static const EmpiricalFormula NH_weight = EmpiricalFormula("NH");
     static const EmpiricalFormula internal_to_full = EmpiricalFormula("H2O");
-    static const EmpiricalFormula fivePrime_to_full = EmpiricalFormula("PO3");
+    static const EmpiricalFormula fivePrime_to_full = EmpiricalFormula("HPO3");
     static const EmpiricalFormula threePrime_to_full = EmpiricalFormula("");
     static const EmpiricalFormula b_ion_to_full = EmpiricalFormula("PO3");
-    static const EmpiricalFormula a_ion_to_full = EmpiricalFormula("PO4");
+    static const EmpiricalFormula a_ion_to_full = EmpiricalFormula("HPO4");
     static const EmpiricalFormula c_ion_to_full = EmpiricalFormula("O"); // OH or just O?
     static const EmpiricalFormula d_ion_to_full = EmpiricalFormula("H2O"); //H2O falls off here
     static const EmpiricalFormula x_ion_to_full = EmpiricalFormula("O");
-    static const EmpiricalFormula y_ion_to_full = EmpiricalFormula("PO3");
-    static const EmpiricalFormula z_ion_to_full = EmpiricalFormula("PO4");
+    static const EmpiricalFormula y_ion_to_full = EmpiricalFormula("HPO3");
+    static const EmpiricalFormula z_ion_to_full = EmpiricalFormula("HPO4");
     static const EmpiricalFormula w_ion_to_full = EmpiricalFormula("");
+    static const EmpiricalFormula abasicform = EmpiricalFormula("C5H7O6P");
     //generate monophosphate mass list TODO make this static
-    std::map<char, EmpiricalFormula> monophosphate_to_formula;
+    std::map<char, EmpiricalFormula> base_to_formula;
     //remove H2O since it gets added in internal_to_full
-    monophosphate_to_formula['A']=EmpiricalFormula("C10H12N5O6P");
-    monophosphate_to_formula['C']=EmpiricalFormula("C9H12N3O7P");
-    monophosphate_to_formula['G']=EmpiricalFormula("C10H12N5O7P");
-    monophosphate_to_formula['U']=EmpiricalFormula("C9H11N2O8P");
-
+    base_to_formula['A']=EmpiricalFormula("C5H5N5"); //("C10H12N5O6P");
+    base_to_formula['C']=EmpiricalFormula("C4H5N3O");//("C9H12N3O7P");
+    base_to_formula['G']=EmpiricalFormula("C5H5N5O");//("C10H12N5O7P");
+    base_to_formula['U']=EmpiricalFormula("C4H4N2O2"); //("C9H11N2O8P");
+    //C5H7O6P= PO4
     EmpiricalFormula mono_formula;
 
     // double mono_weight(Constants::PROTON_MASS_U * charge*-1); //the original assumed positive mode
@@ -101,13 +102,13 @@ EmpiricalFormula NASequence::getFormula(Residue::ResidueType type, Int charge) c
     {
         if (s_.size() == 1)
         {
-            mono_formula+=monophosphate_to_formula[s_[0]];
+            mono_formula+=base_to_formula[s_[0]]+abasicform;
             return mono_formula-(H_weight*charge);
         }
         else
         {
             for (size_t i=0;i<s_.size();i++){
-                mono_formula+=monophosphate_to_formula[s_[i]];
+                mono_formula+=base_to_formula[s_[i]]+abasicform;
             }
             //            for (ConstIterator it = s_->begin(); it != s_->end(); ++it)
             //            {
@@ -121,14 +122,14 @@ EmpiricalFormula NASequence::getFormula(Residue::ResidueType type, Int charge) c
             case Residue::Full:
                 return mono_formula + internal_to_full - fivePrime_to_full-(H_weight*charge);
 
-            case Residue::Internal:
-                return mono_formula-(H_weight*charge) /* + add_protons*/;
+//            case Residue::Internal:
+//                return EmpiricalFormula("");//mono_formula-(H_weight*charge) /* THIS IS NOT CORRECT AND SHOULDNT BE USED FIXME*/;
 
             case Residue::NTerminal:
                 return mono_formula + internal_to_full - fivePrime_to_full-(H_weight*charge);
 
-            case Residue::CTerminal:
-                return mono_formula + internal_to_full - threePrime_to_full-(H_weight*charge);
+//            case Residue::CTerminal:
+//                return EmpiricalFormula("");//mono_formula + internal_to_full - threePrime_to_full-(H_weight*charge); //NEED TO CHECK WHAT IS CORRECT FIXME
 
             case Residue::BIon:
                 return mono_formula + internal_to_full - b_ion_to_full - H_weight-(H_weight*charge);
@@ -137,22 +138,25 @@ EmpiricalFormula NASequence::getFormula(Residue::ResidueType type, Int charge) c
                 return mono_formula + internal_to_full - a_ion_to_full - H_weight * 2 -(H_weight*charge);
 
             case Residue::CIon:
-                return mono_formula + internal_to_full - c_ion_to_full -(H_weight*charge);
+                return mono_formula + internal_to_full - c_ion_to_full - (H_weight*charge);
 
             case Residue::DIon:
-                return mono_formula + internal_to_full - d_ion_to_full -(H_weight*charge);
+                return mono_formula + internal_to_full - d_ion_to_full - (H_weight*charge);
 
             case Residue::XIon:
-                return mono_formula + internal_to_full - x_ion_to_full -(H_weight*charge);
+                return mono_formula + internal_to_full - x_ion_to_full - (H_weight*charge);
 
             case Residue::WIon:
-                return mono_formula + internal_to_full - w_ion_to_full -(H_weight*charge);
+                return mono_formula + internal_to_full - w_ion_to_full - (H_weight*charge);
 
             case Residue::YIon:
-                return mono_formula + internal_to_full - y_ion_to_full -(H_weight*charge);
+                return mono_formula + internal_to_full - y_ion_to_full - (H_weight*charge);
 
             case Residue::ZIon:
-                return mono_formula + internal_to_full - z_ion_to_full -(H_weight*charge);
+                return mono_formula + internal_to_full - z_ion_to_full - (H_weight*charge);
+
+            case Residue::AminusB:
+                return mono_formula + internal_to_full - a_ion_to_full - H_weight * 2 - (H_weight*charge) - base_to_formula[s_[s_.size()-1]];
 
             default:
                 LOG_ERROR << "AASequence::getMonoWeight: unknown ResidueType" << std::endl;
@@ -190,8 +194,8 @@ double NASequence::getMonoWeight(Residue::ResidueType type, Int charge) const
 
 
 
-    double mono_weight(Constants::PROTON_MASS_U * charge*-1); //the original assumed positive mode
-    return mono_weight+getFormula(type,charge).getMonoWeight();
+    double mono_weight(getFormula(type, charge).getMonoWeight()); //the original assumed positive mode
+    return mono_weight/(double)charge;//+getFormula(type,charge).getMonoWeight();
 
 
     //    if (s_.size() > 0)
