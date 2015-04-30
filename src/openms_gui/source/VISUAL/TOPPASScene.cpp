13baf03630f5489ce25a99abbad3447dc6c31d6e
Chris Bielow <chris.bielow@mdc-berlin.de>
2015-04-30 14:49:43 +0200

    forward error messages to log window [fixes #1321]

--- src/openms_gui/source/VISUAL/TOPPASScene.cpp
+++ src/openms_gui/source/VISUAL/TOPPASScene.cpp
@@ -1272,8 +1272,9 @@ namespace OpenMS
     emit entirePipelineFinished();
   }
 
-  void TOPPASScene::pipelineErrorSlot(const QString /*msg*/)
+  void TOPPASScene::pipelineErrorSlot(const QString msg)
   {
+    logTOPPOutput(msg); // print to log window or console
     error_occured_ = true;
     setPipelineRunning(false);
     abortPipeline();
