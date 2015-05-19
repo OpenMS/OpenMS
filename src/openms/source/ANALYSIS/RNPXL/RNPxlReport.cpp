9963a55c92292684c4ef6941cbf94c3c94788e38
timosachsenberg <sachsenb@abinode05.Informatik.Uni-Tuebingen.De>
2015-05-19 17:40:02 +0200

    [FIX, TEST] added more spectra for validation and fixed mass calculation bug in RNPxlReport resulting from partial losses

--- src/openms/source/ANALYSIS/RNPXL/RNPxlReport.cpp
+++ src/openms/source/ANALYSIS/RNPXL/RNPxlReport.cpp
@@ -85,4 +85,136 @@ namespace OpenMS
 
     return ListUtils::concatenate(sl, separator);
   }
+
+
+  std::vector<RNPxlReportRow> RNPxlReport::annotate(const PeakMap& spectra, std::vector<PeptideIdentification>& peptide_ids, double marker_ions_tolerance)
+  {
+    std::map<Size, Size> map_spectra_to_id;
+    for (Size i = 0; i != peptide_ids.size(); ++i)
+    {
+      OPENMS_PRECONDITION(!peptide_ids[i].getHits().empty(), "Error: no empty peptide ids allowed.");
+      Size scan_index = (unsigned int)peptide_ids[i].getMetaValue("scan_index");
+      map_spectra_to_id[scan_index] = i;
+    }
+
+    std::vector<RNPxlReportRow> csv_rows;
+
+    for (PeakMap::ConstIterator s_it = spectra.begin(); s_it != spectra.end(); ++s_it)
+    {
+      int scan_index = s_it - spectra.begin();
+     std::vector<Precursor> precursor = s_it->getPrecursors();
+ 
+      // there should only one precursor and MS2 should contain at least a few peaks to be considered (e.g. at least for every AA in the peptide)
+      if (s_it->getMSLevel() == 2 && precursor.size() == 1)
+      {
+        Size charge = precursor[0].getCharge();
+        double mz = precursor[0].getMZ();
+        RNPxlMarkerIonExtractor::MarkerIonsType marker_ions = RNPxlMarkerIonExtractor::extractMarkerIons(*s_it, marker_ions_tolerance);
+
+        double rt = s_it->getRT();
+
+        RNPxlReportRow row;
+
+        // case 1: no peptide identification: store rt, mz, charge and marker ion intensities
+        if (map_spectra_to_id.find(scan_index) == map_spectra_to_id.end())
+        {
+          row.no_id = true;
+          row.rt = rt;
+          row.original_mz = mz;
+          row.charge = charge;
+          row.marker_ions = marker_ions;
+          csv_rows.push_back(row);
+          continue;
+        }
+
+        PeptideIdentification& pi = peptide_ids[map_spectra_to_id[scan_index]];
+        std::vector<PeptideHit>& phs = pi.getHits();
+
+        // case 2: identification data present for spectrum
+        PeptideHit& ph = phs[0];
+       
+        // total weight = precursor RNA weight + peptide weight
+        // this hack ensures that sequences with additional reported partial loss match the total weight 
+        // Note that the partial loss is only relevent on the MS2 and would otherwise be added to the totalweight
+        String sequence_string = ph.getSequence().toString();
+        sequence_string.substitute("(RNA:U_prime-H2O)", "");
+        sequence_string.substitute("(RNA:U_prime)", "");
+        sequence_string.substitute("(RNA:U-H3PO4)", "");
+        sequence_string.substitute("(RNA:U-H2O)", "");
+        sequence_string.substitute("(RNA:U)", "");
+        sequence_string.substitute("(RNA:C3O)", "");
+
+        const AASequence sequence = AASequence::fromString(sequence_string); 
+
+        double peptide_weight = sequence.getMonoWeight();
+        String rna_name = ph.getMetaValue("RNPxl:RNA");
+        double rna_weight = ph.getMetaValue("RNPxl:RNA_MASS_z0");
+
+        // crosslink weight for different charge states
+        double weight_z1 = (peptide_weight + rna_weight + 1.0 * Constants::PROTON_MASS_U);
+        double weight_z2 = (peptide_weight + rna_weight + 2.0 * Constants::PROTON_MASS_U) / 2.0;
+        double weight_z3 = (peptide_weight + rna_weight + 3.0 * Constants::PROTON_MASS_U) / 3.0;
+        double weight_z4 = (peptide_weight + rna_weight + 4.0 * Constants::PROTON_MASS_U) / 4.0;
+
+        double xl_weight = peptide_weight + rna_weight;
+        double theo_mz = (xl_weight + static_cast<double>(charge) * Constants::PROTON_MASS_U) / (double)charge;
+        double absolute_difference = theo_mz - mz;
+        double ppm_difference =  absolute_difference / theo_mz * 1e6;
+
+        String protein_accessions;
+        std::set<String> accs = ph.extractProteinAccessions();
+
+        // concatenate set into String
+        for (std::set<String>::const_iterator a_it = accs.begin(); a_it != accs.end(); ++a_it)
+        {
+          if (a_it != accs.begin())
+          {
+            protein_accessions += ",";
+          }
+          protein_accessions += *a_it;
+        }
+
+        row.no_id = false;
+        row.rt = rt;
+        row.original_mz = mz;
+        row.accessions = protein_accessions;
+        row.RNA = rna_name;
+        row.peptide = sequence.toString();
+        row.charge = charge;
+        row.score = ph.getScore();
+        row.peptide_weight = peptide_weight;
+        row.RNA_weight = rna_weight;
+        row.xl_weight = peptide_weight + rna_weight;
+
+        ph.setMetaValue("RNPxl:peptide_mass_z0", DataValue(peptide_weight));
+        ph.setMetaValue("RNPxl:xl_mass_z0", xl_weight);
+
+        for (RNPxlMarkerIonExtractor::MarkerIonsType::const_iterator it = marker_ions.begin(); it != marker_ions.end(); ++it)
+        {
+          for (Size i = 0; i != it->second.size(); ++i)
+          {
+            ph.setMetaValue(it->first + "_" + it->second[i].first, static_cast<double>(it->second[i].second * 100.0));
+          }
+        }
+
+        row.marker_ions = marker_ions;
+        row.abs_prec_error = absolute_difference;
+        row.rel_prec_error = ppm_difference;
+        row.m_H = weight_z1;
+        row.m_2H = weight_z2;
+        row.m_3H = weight_z3;
+        row.m_4H = weight_z4;
+
+        ph.setMetaValue("RNPxl:Da difference", (double)absolute_difference);
+        ph.setMetaValue("RNPxl:ppm difference", (double)ppm_difference);
+        ph.setMetaValue("RNPxl:z1 mass", (double)weight_z1);
+        ph.setMetaValue("RNPxl:z2 mass", (double)weight_z2);
+        ph.setMetaValue("RNPxl:z3 mass", (double)weight_z3);
+        ph.setMetaValue("RNPxl:z4 mass", (double)weight_z4);
+
+        csv_rows.push_back(row);
+    }
+  }
+  return csv_rows;
+}
 }
