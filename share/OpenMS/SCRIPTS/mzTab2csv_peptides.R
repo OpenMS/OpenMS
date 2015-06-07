153a36c8c3e447f6439c97ed45cbf53285237934
lars20070 <l.nilse@dunelm.org.uk>
2015-06-07 17:04:22 +0200

    [INTERNAL] functionality to R script added

--- share/OpenMS/SCRIPTS/mzTab2csv_peptides.R
+++ share/OpenMS/SCRIPTS/mzTab2csv_peptides.R
@@ -1,27 +1,46 @@
-## This is an R script to produce the figures that are attached to the qcML format
+## This is an R script reformats mzTab files (*.tsv) to excel files (*.csv)
 
-#options
-options(digits=10)
+#source("http://bioconductor.org/biocLite.R")
+#biocLite("MSnbase")
+library(MSnbase)
 
-file<-commandArgs(TRUE)[1]
-post<-commandArgs(TRUE)[2]
-######
-###TIC
-######
-a<-read.csv(file=file,head=TRUE,sep="\t")
-######################################
-#png(post)
-#res = barplot(t(a$TIC), xlab="RT (min)",ylab="Intensity")
-#time_seq = seq(min(a$RT),max(a$RT),1200)
-#time_seq = round(time_seq)/60
-#t<-which(time_seq %% 1==0 & duplicated(time_seq)==F)
-#tmp = seq(min(res),max(res),max(res)/(length(time_seq)))
-#axis(1,at=tmp[t],labels=time_seq[t])
-######################################
-#dev.off()
-#
-#
-#
-#
-#
-write.table(a, post, sep=",")
+input.file<-commandArgs(TRUE)[1]
+output.file<-commandArgs(TRUE)[2]
+
+# MSnSet
+x <- readMzTabData(input.file, what = "PEP")
+
+# data frame
+peptide.data <- fData(x)
+peptide.data[] <- lapply(peptide.data, as.character)
+
+# column names
+columns <- colnames(peptide.data)
+
+unique.stripped.sequences <- unique(peptide.data$sequence)
+unique.modified.sequences <- unique(peptide.data$opt_global_modified_sequence)
+
+# find best (lowest PEP score) modified sequences
+n <- length(unique.modified.sequences)
+indices <- c()
+for (i in 1:n) {
+	j <- which(peptide.data$"opt_global_modified_sequence" == unique.modified.sequences[i])
+	PEPs <- peptide.data[j,]$"opt_psm_Posterior Error Probability_score"
+	k <- j[which(PEPs == min(PEPs))]
+	k <- k[1]
+	
+	indices <- c(indices, k)
+}
+
+# simplify accession
+list <- strsplit(peptide.data$accession,"[|]")
+peptide.data$accession <- unlist(lapply(list, '[[', 2))
+
+# add fold change
+peptide.data$fold_change <- log(as.numeric(peptide.data$"peptide_abundance_study_variable[2]")/as.numeric(peptide.data$"peptide_abundance_study_variable[1]"), base =2)
+
+# prepare output
+output.columns <- c(1,24,2,3,28,25,12,10,13,15,18,29)
+output <- peptide.data[indices,output.columns]
+colnames(output) <- c("sequence_stripped","sequence_modified","accession","proteotypic","target_decoy","posterior_error_probability","charge","retention_time","mass_to_charge","abundance_light","abundance_heavy","fold_change")
+write.table(output, output.file, sep=",", row.names=FALSE, col.names=TRUE, quote=FALSE)
