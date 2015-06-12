a83ef055f996837a183d1af39bd1f8735f362b6c
lars20070 <l.nilse@dunelm.org.uk>
2015-06-12 10:23:24 +0200

    [INTERNAL] quotes disabled

--- share/OpenMS/SCRIPTS/mzTab2tsv_peptides.R
+++ share/OpenMS/SCRIPTS/mzTab2tsv_peptides.R
@@ -7,8 +7,8 @@ input.file <- commandArgs(TRUE)[1]
 output.file <- commandArgs(TRUE)[2]
 
 # read entire mzTab
-no.col <- max(count.fields(input.file, sep = "\t"))
-data <- read.table(input.file,sep="\t",fill=TRUE,col.names=1:no.col)
+no.col <- max(count.fields(input.file, sep = "\t", quote=""))
+data <- read.table(input.file, sep="\t", fill=TRUE, quote="", col.names=1:no.col)
 
 # extract peptide data
 peptide.data <- data[which(data[,1]=="PEP"),]
