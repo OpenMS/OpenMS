f1b8b3e523fc126e66d83efcf4ad098d571942cf
Hendrik Weisser <hw5@sanger.ac.uk>
2015-06-25 16:12:35 +0100

    [DOC] improved wording of note regarding mzid support

--- src/utils/RTAnnotator.cpp
+++ src/utils/RTAnnotator.cpp
@@ -77,7 +77,8 @@ using namespace std;
     missing RT. This tool performs the annotation of the identification given the
     source file to the identification process.
 
-    @note For mzid in-/out- put, due to legacy reason issues you are temporarily asked to use IDFileConverter as a wrapper.
+    @note Currently mzIdentML (mzid) is not directly supported as an input/output format of this tool. Convert mzid files to/from idXML using @ref TOPP_IDFileConverter if necessary.
+
     <B>The command line parameters of this tool are:</B>
     @verbinclude UTILS_RTAnnotator.cli
     <B>INI file documentation of this tool:</B>
