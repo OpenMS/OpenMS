c83d9b4b776ed1903ad55714b26d80ea1913837d
Chris Bielow <chris.bielow@fu-berlin.de>
2015-08-16 18:29:03 +0200

    [DOC] better docs for StablePairFinder and FeatureDistance

--- src/openms/include/OpenMS/ANALYSIS/MAPMATCHING/FeatureDistance.h
+++ src/openms/include/OpenMS/ANALYSIS/MAPMATCHING/FeatureDistance.h
@@ -57,17 +57,31 @@ namespace OpenMS
 
      @f$ RT_i @f$, @f$ MZ_i @f$, and @f$ int_i @f$ are the RT, m/z, and intensity values of the respective feature.
 
-     @f$ {\Delta RT_{max}} @f$ and @f$ {\Delta MZ_{max}} @f$ are the maximum allowed differences in RT and m/z, respectively. They are specified by the parameters @p distance_RT:max_difference and @p distance_MZ:max_difference, and are used for normalization. If an absolute difference exceeds the specified maximum, the behavior depends on the value used for @p check_constraints in the constructor: If "false", the distance will be computed normally, but may become greater than 1; if "true", the fixed value @ref infinity is returned.
+     Constraints are: @f$ {\Delta RT_{max}}, {\Delta MZ_{max}}, int_{max}  @f$ and (optionally) @f$ charge @f$.
+     If an absolute difference exceeds the specified maximum, the behavior depends on the value used for @p check_constraints in the constructor: 
+     If "false" (i.e., no constraints), the distance in that dimension may become greater than 1; if "true", @ref infinity is returned as overall distance.
 
-     @f$ int_{max} @f$ is the maximum intensity that can occur for features compared by this distance function. It is not a parameter specified by the user, but depends on the data at hand and is thus set in the constructor (via parameter @p max_intensity).
+     @f$ {\Delta RT_{max}} @f$ and @f$ {\Delta MZ_{max}} @f$ are the maximum allowed differences in RT and m/z, respectively. 
+     They are specified by the parameters @p distance_RT:max_difference and @p distance_MZ:max_difference, and are used for normalization,
+     i.e., the observed RT or m/z differences of the feature pair are scaled relative to this value.
+     
+     @f$ int_{max} @f$ is the intensity which yields a normalized intensity of 1. This parameter is not settable via user params,
+     but is set in the constructor (via parameter @p max_intensity), since it depends on the data at hand.
 
-     @f$ w_X @f$ is the weight of distance component X, specified by the parameter @p distance_X:weight. The weights can be used to increase or decrease the contribution of RT, m/z, or intensity in the distance function. (Note that the default weight for the intensity component is zero, i.e. intensity is not considered by default.)
+     @f$ p_X @f$ is the exponent for the distance in dimension X, specified by the parameter @p distance_X:exponent. 
+     Normalized differences (between (0, 1) unless unconstrained) are taken to this power. This makes it possible to compare values using linear, quadratic, etc. distance.
 
-     @f$ p_X @f$ is the exponent for distance component X, specified by the parameter @p distance_X:exponent. Normalized differences are taken to this power. This makes it possible to compare values using linear, quadratic, etc. distance.
+     By default, two features are paired only if they have the same charge state (or at least one unknown charge '0') - otherwise, @ref infinity is returned. 
+     This behavior can be changed by the @p ignore_charge parameter.
 
-     By default, two features are only compared if they have the same charge state (or charge state 0 for "undefined") - otherwise, @ref infinity is returned. This behavior can be changed by the @p ignore_charge parameter.
+     @f$ w_X @f$ is the weight of final distance in dimension X, specified by the parameter @p distance_X:weight. The weights can be used to increase or decrease 
+     the contribution of RT, m/z, or intensity in the distance function. 
+     (The default weight for the intensity dimension is zero, i.e. intensity is not considered by default. However, @f$ int_{max} @f$ is still a constraint and
+      should be set sensibly in the c'tor.)
 
-     @note Peptide identifications annotated to features are not taken into account here, because they are stored in a format that is not suitable for rapid comparison.
+
+     @note Peptide identifications annotated to features are not taken into account here, 
+           because they are stored in a format that is not suitable for rapid comparison.
 
    @htmlinclude OpenMS_FeatureDistance.parameters
 
