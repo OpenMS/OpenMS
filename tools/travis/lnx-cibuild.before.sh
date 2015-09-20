717caf08dc5a0921755c402fce22d45521b74c6a
Timo Sachsenberg <timosachsenberg@users.noreply.github.com>
2015-09-20 18:06:28 +0200

    Update .travis.yml
    Update lnx-cibuild.before.sh
    
    Update .travis.yml
    
    Update .travis.yml
    
    Update .travis.yml
    
    Update .travis.yml
    
    Update .travis.yml
    
    Update .travis.yml
    
    Update .travis.yml
    
    Update .travis.yml
    
    Update .travis.yml
    
    Update .travis.yml
    
    Update lnx-cibuild.cmake

--- tools/travis/lnx-cibuild.before.sh
+++ tools/travis/lnx-cibuild.before.sh
@@ -35,32 +35,8 @@ popd
 sudo add-apt-repository --yes ppa:boost-latest/ppa
 sudo apt-get update
 
-# install required packages
-sudo apt-get install -qq  libboost-date-time1.54-dev \
-                          libboost-iostreams1.54-dev \
-                          libboost-regex1.54-dev \
-                          libboost-math1.54-dev \
-                          libboost-random1.54-dev \
-                          libxerces-c3.1 \
-                          libxerces-c-dev \
-                          libicu-dev \
-                          qt4-dev-tools \
-                          libqt4-dev \
-                          libqt4-core \
-                          libqt4-gui \
-                          libsvm-dev \
-                          libsvm3 \
-                          glpk \
-                          subversion
-
-# install doxygen but not latex (saves some time)
-sudo apt-get install -qq \
-                     --no-install-recommends doxygen \
-                     graphviz
-
 # build custom cppcheck if we want to perform style tests
 if [ $ENABLE_STYLE_TESTING == "On" ]; then
-  sudo apt-get install -qq libpcre3-dev
   git clone git://github.com/danmar/cppcheck.git
   pushd cppcheck
   git checkout 1.65
