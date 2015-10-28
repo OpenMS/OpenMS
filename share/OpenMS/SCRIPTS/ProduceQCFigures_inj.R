a8fdfdd8d1d13aec34507662b38249ce508b15c1
mwalzer <walzer@informatik.uni-tuebingen.de>
2015-10-28 17:35:35 +0100

    [FEATURE] added more R scripts for a QC workflow in TOPPAS, updated others to play nicely with the GenericWrapper(s)

--- share/OpenMS/SCRIPTS/ProduceQCFigures_inj.R
+++ share/OpenMS/SCRIPTS/ProduceQCFigures_inj.R
@@ -4,16 +4,24 @@
 library("ggplot2")
 library(scales)
 options(warn=-1) #suppress warnings
+
+file<-commandArgs(TRUE)[1]
+post<-commandArgs(TRUE)[2]
+
+#file<-"/tmp/2015-10-28_171407_Cetirizin_2990_1/TOPPAS_tmp/QCWorkflow_fr/019_QCExtractor/out_csv/old1.dta"
+knime.in<-read.csv(file=file,head=TRUE,sep="\t")
+names(knime.in)<-c("rt","it")
+knime.in$rt <- as.POSIXct(as.character(0),format="%S")+knime.in$rt
+png(post)
 ##########################
 ###Injection time
 ##########################
-knime.in<-read.csv(file=file,head=TRUE,sep="\t")
-knime.in$rt <- as.POSIXct(as.character(0),format="%S")+knime.in$"MS:1000894_[sec]"
-knime.in$it <- knime.in$"MS:1000927"
 ggplot(data=knime.in, aes(x=rt, y=it)) + 
- geom_point(shape=2) + 
- geom_smooth() +
- scale_x_datetime( breaks="10 mins",  minor_breaks="1 mins", labels=date_format("%H:%M")) + 
- xlab("RT (HH:MM)") +
- ylab("Injection time (ms)")
-##########################
+  geom_point(shape=2) + 
+  geom_line(y=0, colour="blue") + 
+  stat_smooth(colour="red", method=loess) +
+  #scale_x_datetime( breaks="10 mins",  minor_breaks="1 mins", labels=date_format("%H:%M")) + 
+  xlab("RT (HH:MM)") +
+  ylab("Injection time (ms)")
+######################################
+garbage<-dev.off()
