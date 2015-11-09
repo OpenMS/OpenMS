86c3dcec7fb94b3bebc785e8b142d52e3250feaa
timosachsenberg <sachsenb@abinode05.Informatik.Uni-Tuebingen.De>
2015-11-09 14:01:48 +0100

    [FEATURE] pair generation

--- src/utils/xQuest.cpp
+++ src/utils/xQuest.cpp
@@ -33,15 +33,29 @@
 // --------------------------------------------------------------------------
 
 #include <OpenMS/KERNEL/StandardTypes.h>
+#include <OpenMS/FORMAT/FASTAFile.h>
 #include <OpenMS/FORMAT/MzMLFile.h>
 #include <OpenMS/DATASTRUCTURES/ListUtilsIO.h>
 #include <OpenMS/APPLICATIONS/TOPPBase.h>
 #include <OpenMS/FORMAT/IdXMLFile.h>
+#include <OpenMS/CHEMISTRY/EnzymaticDigestion.h>
+#include <OpenMS/CHEMISTRY/EnzymesDB.h>
+#include <OpenMS/CHEMISTRY/ModificationsDB.h>
+#include <OpenMS/ANALYSIS/RNPXL/ModifiedPeptideGenerator.h>
 
 #include <iostream>
 
 using namespace std;
 using namespace OpenMS;
+
+
+#ifdef _OPENMP
+#include <omp.h>
+#define NUMBER_OF_THREADS (omp_get_num_threads())
+#else
+#define NUMBER_OF_THREADS (1)
+#endif
+
 
 /**
     @page UTILS_xQuest xQuest
@@ -79,6 +93,37 @@ public:
   {
   }
 
+  // Slimmer structure to store a string representation
+  struct IndexedString
+  {
+    String::const_iterator begin;
+    String::const_iterator end; // one after last character in substring
+  
+    bool operator<(const IndexedString& other) const
+    {
+      if (end - begin < other.end - other.begin) return true;
+
+      if (end - begin > other.end - other.begin) return false;
+  
+      // same size
+      String::const_iterator b = begin;
+      String::const_iterator bo = other.begin;
+  
+      for (; b != end; ++b, ++bo)
+      {
+        if (*b < *bo) return true;
+        if (*b > *bo) return false;
+      }
+  
+      return false;
+    }
+
+   inline String getString() const
+   {
+     return String(begin, end);
+   }
+  };
+
 protected:
   void registerOptionsAndFlags_()
   {
@@ -89,16 +134,126 @@ protected:
     registerInputFile_("database", "<file>", "", "Input file containing the protein database.");
     setValidFormats_("database", ListUtils::create<String>("fasta"));
 
+    registerTOPPSubsection_("precursor", "Precursor (Parent Ion) Options");
+    registerDoubleOption_("precursor:mass_tolerance", "<tolerance>", 10.0, "Width of precursor mass tolerance window", false);
+
+    StringList precursor_mass_tolerance_unit_valid_strings;
+    precursor_mass_tolerance_unit_valid_strings.push_back("ppm");
+    precursor_mass_tolerance_unit_valid_strings.push_back("Da");
+
+    registerStringOption_("precursor:mass_tolerance_unit", "<unit>", "ppm", "Unit of precursor mass tolerance.", false, false);
+    setValidStrings_("precursor:mass_tolerance_unit", precursor_mass_tolerance_unit_valid_strings);
+
+    registerIntOption_("precursor:min_charge", "<num>", 2, "Minimum precursor charge to be considered.", false, true);
+    registerIntOption_("precursor:max_charge", "<num>", 5, "Maximum precursor charge to be considered.", false, true);
+
+    registerTOPPSubsection_("fragment", "Fragments (Product Ion) Options");
+    registerDoubleOption_("fragment:mass_tolerance", "<tolerance>", 10.0, "Fragment mass tolerance", false);
+
+    StringList fragment_mass_tolerance_unit_valid_strings;
+    fragment_mass_tolerance_unit_valid_strings.push_back("ppm");
+    fragment_mass_tolerance_unit_valid_strings.push_back("Da");
+
+    registerStringOption_("fragment:mass_tolerance_unit", "<unit>", "ppm", "Unit of fragment m", false, false);
+    setValidStrings_("fragment:mass_tolerance_unit", fragment_mass_tolerance_unit_valid_strings);
+
+    registerTOPPSubsection_("modifications", "Modifications Options");
+    vector<String> all_mods;
+    ModificationsDB::getInstance()->getAllSearchModifications(all_mods);
+    registerStringList_("modifications:fixed", "<mods>", ListUtils::create<String>(""), "Fixed modifications, specified using UniMod (www.unimod.org) terms, e.g. 'Carbamidomethyl (C)'", false);
+    setValidStrings_("modifications:fixed", all_mods);
+    registerStringList_("modifications:variable", "<mods>", ListUtils::create<String>(""), "Variable modifications, specified using UniMod (www.unimod.org) terms, e.g. 'Oxidation (M)'", false);
+    setValidStrings_("modifications:variable", all_mods);
+    registerIntOption_("modifications:variable_max_per_peptide", "<num>", 2, "Maximum number of residues carrying a variable modification per candidate peptide", false, false);
+
+    vector<String> all_enzymes;
+    EnzymesDB::getInstance()->getAllNames(all_enzymes);
+    registerStringOption_("enzyme", "<cleavage site>", "Trypsin", "The enzyme used for peptide digestion.", false);
+    setValidStrings_("enzyme", all_enzymes);
+
+    registerTOPPSubsection_("peptide", "Peptide Options");
+    registerIntOption_("peptide:min_size", "<num>", 7, "Minimum size a peptide must have after digestion to be considered in the search.", false, true);
+    registerIntOption_("peptide:missed_cleavages", "<num>", 1, "Number of missed cleavages.", false, false);
+
     // output file
     registerOutputFile_("out", "<file>", "", "Result file\n");
     setValidFormats_("out", ListUtils::create<String>("idXML"));
   }
 
+  vector<ResidueModification> getModifications_(StringList modNames)
+  {
+    vector<ResidueModification> modifications;
+
+    // iterate over modification names and add to vector
+    for (StringList::iterator mod_it = modNames.begin(); mod_it != modNames.end(); ++mod_it)
+    {
+      String modification(*mod_it);
+      modifications.push_back(ModificationsDB::getInstance()->getModification(modification));
+    }
+
+    return modifications;
+  }
+
+  // check if for minimum size
+  class HasInvalidPeptideLengthPredicate
+  {
+      public:
+        explicit HasInvalidPeptideLengthPredicate(Size min_size)
+          :min_size_(min_size)
+        {
+        }
+
+        bool operator()(const AASequence& aas)
+        {
+          return (aas.size() < min_size_);
+        }
+    private:
+        Size min_size_;
+  };
+
+
   ExitCodes main_(int, const char**)
   {
+    ProgressLogger progresslogger;
+    progresslogger.setLogType(log_type_);
+
     const string in_mzml(getStringOption_("in"));
     const string in_fasta(getStringOption_("database"));
     const string out_idxml(getStringOption_("out"));
+
+    Int min_precursor_charge = getIntOption_("precursor:min_charge");
+    Int max_precursor_charge = getIntOption_("precursor:max_charge");
+    double precursor_mass_tolerance = getDoubleOption_("precursor:mass_tolerance");
+    bool precursor_mass_tolerance_unit_ppm = (getStringOption_("precursor:mass_tolerance_unit") == "ppm");
+
+    double fragment_mass_tolerance = getDoubleOption_("fragment:mass_tolerance");
+    bool fragment_mass_tolerance_unit_ppm = (getStringOption_("fragment:mass_tolerance_unit") == "ppm");
+
+    double cross_link_mass_light = getDoubleOption_("cross_linker:mass_light");
+    double cross_link_mass_heavy = getDoubleOption_("cross_linker:mass_heavy");
+
+    StringList fixedModNames = getStringList_("modifications:fixed");
+    set<String> fixed_unique(fixedModNames.begin(), fixedModNames.end());
+
+    Size peptide_min_size = getIntOption_("peptide:min_size");
+
+    if (fixed_unique.size() != fixedModNames.size())
+    {
+      cout << "duplicate fixed modification provided." << endl;
+      return ILLEGAL_PARAMETERS;
+    }
+
+    StringList varModNames = getStringList_("modifications:variable");
+    set<String> var_unique(varModNames.begin(), varModNames.end());
+    if (var_unique.size() != varModNames.size())
+    {
+      cout << "duplicate variable modification provided." << endl;
+      return ILLEGAL_PARAMETERS;
+    }
+
+    vector<ResidueModification> fixed_modifications = getModifications_(fixedModNames);
+    vector<ResidueModification> variable_modifications = getModifications_(varModNames);
+    Size max_variable_mods_per_peptide = getIntOption_("modifications:variable_max_per_peptide");
     
     // load MS2 map
     PeakMap spectra;
@@ -111,8 +266,176 @@ protected:
     f.getOptions() = options;
     f.load(in_mzml, spectra);
     spectra.sortSpectra(true);
+
+    // load fasta database    
+    progresslogger.startProgress(0, 1, "Load database from FASTA file...");
+    FASTAFile fastaFile;
+    vector<FASTAFile::FASTAEntry> fasta_db;
+    fastaFile.load(in_fasta, fasta_db);
+    progresslogger.endProgress();
     
-     
+    const Size missed_cleavages = getIntOption_("peptide:missed_cleavages");
+    EnzymaticDigestion digestor;
+    digestor.setEnzyme(getStringOption_("peptide:enzyme"));
+    digestor.setMissedCleavages(missed_cleavages);
+
+    progresslogger.startProgress(0, (Size)(fasta_db.end() - fasta_db.begin()), "Scoring peptide models against spectra...");
+
+    // lookup for processed peptides. must be defined outside of omp section and synchronized
+    multimap<IndexedString, AASequence> processed_peptides;
+    
+    // set minimum size of peptide after digestion
+    Size min_peptide_length = getIntOption_("peptide:min_size");
+
+    // build multimap of precursor mass to scan index
+    multimap<double, Size> multimap_mass_2_scan_index;
+
+    for (PeakMap::ConstIterator s_it = spectra.begin(); s_it != spectra.end(); ++s_it)
+    {
+      int scan_index = s_it - spectra.begin();
+      vector<Precursor> precursor = s_it->getPrecursors();
+    
+      // there should only one precursor and MS2 should contain at least a few peaks to be considered (e.g. at least for every AA in the peptide)
+      if (precursor.size() == 1 && s_it->size() >= peptide_min_size)
+      {
+        int precursor_charge = precursor[0].getCharge();
+        if (precursor_charge < min_precursor_charge || precursor_charge > max_precursor_charge)
+        {
+          continue;
+        }
+    
+        double precursor_mz = precursor[0].getMZ();
+        double precursor_mass = (double) precursor_charge * precursor_mz - (double) precursor_charge * Constants::PROTON_MASS_U;
+    
+        multimap_mass_2_scan_index.insert(make_pair(precursor_mass, scan_index));
+      }
+    }
+    
+    Size count_proteins = 0;
+    Size count_peptides = 0;
+    
+#ifdef _OPENMP
+#pragma omp parallel for
+#endif
+    for (SignedSize fasta_index = 0; fasta_index < (SignedSize)fasta_db.size(); ++fasta_index)
+    {
+#ifdef _OPENMP
+#pragma omp atomic
+#endif
+      ++count_proteins;
+
+      IF_MASTERTHREAD
+      {
+        progresslogger.setProgress((SignedSize)fasta_index * NUMBER_OF_THREADS);
+      }
+
+      // store vector of substrings pointing in fasta database (bounded by pairs of begin, end iterators)    
+      vector<pair<String::const_iterator, String::const_iterator> > current_digest;
+      digestor.digestUnmodifiedString(fasta_db[fasta_index].sequence, current_digest, min_peptide_length);
+
+      for (vector<pair<String::const_iterator, String::const_iterator> >::iterator cit = current_digest.begin(); cit != current_digest.end(); ++cit)
+      {
+        bool already_processed = false;
+        IndexedString string_idx;
+        string_idx.begin = cit->first;
+        string_idx.end = cit->second;
+#ifdef _OPENMP
+#pragma omp critical (processed_peptides_access)
+#endif
+        {
+          if (processed_peptides.find(string_idx) != processed_peptides.end())
+          {
+            // peptide (and all modified variants) already processed so skip it
+            already_processed = true;
+          }
+        }
+
+        if (already_processed)
+        {
+          continue;
+        }
+            
+
+#ifdef _OPENMP
+#pragma omp atomic
+#endif
+        ++count_peptides;
+
+
+        vector<AASequence> all_modified_peptides;
+
+        // generate all modified variants of a peptide
+        // Note: no critial section is needed despite ResidueDB not beeing thread sage.
+        //       It is only written to on introduction of novel modified residues. These residues have been already added above (single thread context).
+        {
+          AASequence aas = AASequence::fromString(String(cit->first, cit->second));
+          ModifiedPeptideGenerator::applyFixedModifications(fixed_modifications.begin(), fixed_modifications.end(), aas);
+          ModifiedPeptideGenerator::applyVariableModifications(variable_modifications.begin(), variable_modifications.end(), aas, max_variable_mods_per_peptide, all_modified_peptides);
+        }
+        
+        for (SignedSize mod_pep_idx = 0; mod_pep_idx < (SignedSize)all_modified_peptides.size(); ++mod_pep_idx)
+        {
+          const AASequence& candidate = all_modified_peptides[mod_pep_idx];
+
+#ifdef _OPENMP
+#pragma omp critical (processed_peptides_access)
+#endif
+          {
+            processed_peptides.insert(pair<IndexedString, AASequence>(string_idx, candidate));
+          }
+        }
+      }
+    }
+
+    // calculate mass pairs
+    for (map<IndexedString, AASequence>::const_iterator a = processed_peptides.begin(); a != processed_peptides.end(); ++a)
+    {
+      for (map<IndexedString, AASequence>::const_iterator b = a; b != processed_peptides.end(); ++b)
+      {
+        const double light_heavy_shift = cross_link_mass_heavy - cross_link_mass_light;
+        double cross_link_mass = a->second.getMonoWeight() + b->second.getMonoWeight() + light_heavy_shift;
+
+        // determine MS2 precursors that match to the current peptide mass
+        multimap<double, Size>::const_iterator low_it;
+        multimap<double, Size>::const_iterator up_it;
+
+        if (precursor_mass_tolerance_unit_ppm) // ppm
+        {
+          low_it = multimap_mass_2_scan_index.lower_bound(cross_link_mass - cross_link_mass * precursor_mass_tolerance * 1e-6);
+          up_it = multimap_mass_2_scan_index.upper_bound(cross_link_mass + cross_link_mass * precursor_mass_tolerance * 1e-6);
+        }
+        else // Dalton
+        {
+          low_it = multimap_mass_2_scan_index.lower_bound(cross_link_mass - precursor_mass_tolerance);
+          up_it = multimap_mass_2_scan_index.upper_bound(cross_link_mass + precursor_mass_tolerance);
+        }
+
+        if (low_it == up_it) continue; // no matching precursor in data
+
+        for (; low_it != up_it; ++low_it)
+        {
+          const Size& scan_index = low_it->second;
+          const PeakSpectrum& exp_spectrum = spectra[scan_index];
+
+//          HyperScore::IndexScorePair best_score = HyperScore::compute(fragment_mass_tolerance, fragment_mass_tolerance_unit_ppm, exp_spectrum, theoretical_spectra);
+
+          // no good hit
+              //               if (best_score.second < 1.0)
+              //                             {
+              //                                             continue;
+              //                                                           }
+              //
+              //                                                                         // add peptide hit
+              //                                                                                       AnnotatedHit ah;
+              //                                                                                                     ah.sequence.begin = cit->first;
+              //                                                                                                                   ah.sequence.end = cit->second;
+              //                                                                                                                                 ah.peptide_mod_index = mod_pep_idx;
+              //                                                                                                                                               ah.score = best_score.second;
+              //                                                                                                                                                             ah.rna_mod_index = rna_mod_index;
+          cout << "Pair: " << a->second << ", " << b->second << " matched" << endl;
+        }  
+      }     
+    }
  
     return EXECUTION_OK;
   }
