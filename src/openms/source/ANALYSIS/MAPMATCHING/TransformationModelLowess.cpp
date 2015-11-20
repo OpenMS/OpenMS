2be27e4bbdd7536e92d76fb92d26a048b52f7550
Hannes Roest <roest@imsb.biol.ethz.ch>
2015-11-20 15:26:44 -0800

    [FEATURE] add TransformationModelLowess

---
+++ src/openms/source/ANALYSIS/MAPMATCHING/TransformationModelLowess.cpp
@@ -0,0 +1,146 @@
+// --------------------------------------------------------------------------
+//                   OpenMS -- Open-Source Mass Spectrometry
+// --------------------------------------------------------------------------
+// Copyright The OpenMS Team -- Eberhard Karls University Tuebingen,
+// ETH Zurich, and Freie Universitaet Berlin 2002-2015.
+//
+// This software is released under a three-clause BSD license:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of any author or any participating institution
+//    may be used to endorse or promote products derived from this software
+//    without specific prior written permission.
+// For a full list of authors, refer to the file AUTHORS.
+// --------------------------------------------------------------------------
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL ANY OF THE AUTHORS OR THE CONTRIBUTING
+// INSTITUTIONS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// --------------------------------------------------------------------------
+// $Maintainer: Hannes Roest $
+// $Authors: Hannes Roest $
+// --------------------------------------------------------------------------
+
+#include <OpenMS/ANALYSIS/MAPMATCHING/TransformationModelLowess.h>
+#include <OpenMS/ANALYSIS/MAPMATCHING/TransformationModelLinear.h>
+#include <OpenMS/FILTERING/SMOOTHING/FastLowessSmoothing.h>
+
+#include <OpenMS/ANALYSIS/MAPMATCHING/TransformationModelInterpolated.h>
+
+#include <iostream>
+
+using namespace std;
+
+namespace OpenMS
+{
+  bool cmpFirstDimension(const TransformationModel::DataPoint& x, const TransformationModel::DataPoint& y)
+  {
+    return (x.first < y.first);
+  }
+
+  TransformationModelLowess::TransformationModelLowess(
+      const TransformationModel::DataPoints& data_,
+      const Param& params) : model_(0)
+  {
+    // parameter handling/checking:
+    params_ = params;
+    Param defaults;
+    getDefaultParameters(defaults);
+    params_.setDefaults(defaults);
+
+    if (data_.size() < 2)
+    {
+      throw Exception::IllegalArgument(__FILE__, __LINE__, __PRETTY_FUNCTION__,
+                                       "'lowess' model requires more data");
+    }
+
+    // TODO copy ... 
+    TransformationModel::DataPoints data(data_);
+
+    // sort data
+    std::sort(data.begin(), data.end(), cmpFirstDimension);
+
+    vector<double> x(data.size()), y(data.size()), result(data.size());
+    double xmin_ = data[0].first;
+    double xmax_ = xmin_;
+    for (Size i = 0; i < data.size(); ++i)
+    {
+      x[i] = data[i].first;
+      y[i] = data[i].second;
+      if (x[i] < xmin_) 
+      {
+        xmin_ = x[i];
+      }
+      else if (x[i] > xmax_)
+      {
+        xmax_ = x[i];
+      }
+    }
+
+    double span = params.getValue("span");
+    int nsteps = params.getValue("num_iterations");
+    double delta = params.getValue("delta");
+    
+    if (delta < 0.0)
+    {
+      delta = (xmax_ - xmin_) * 0.01; // automatically determine delta
+    }
+
+    FastLowessSmoothing::lowess(x, y, span, nsteps, delta, result);
+
+    TransformationModel::DataPoints data_out;
+    for (Size i = 0; i < result.size(); ++i)
+    {
+      data_out.push_back( std::make_pair(x[i], result[i]) );
+    }
+
+    // TODO thin out data here ? we may not need that many points here to interpolate ...  it is enough if we store a few datapoints
+
+    Param p;
+    TransformationModelInterpolated::getDefaultParameters(p);
+    /// p.setValue("interpolation_type", "cspline"); // linear interpolation between lowess pts
+    /// p.setValue("extrapolation_type", "four-point-linear");
+    p.setValue("interpolation_type", params.getValue("interpolation_type"));
+    p.setValue("extrapolation_type", params.getValue("extrapolation_type"));
+
+    // create new interpolation model based on the lowess data
+    model_ = new TransformationModelInterpolated(data_out, p);
+  }
+
+  TransformationModelLowess::~TransformationModelLowess()
+  {
+    if (model_) delete model_;
+  }
+
+  void TransformationModelLowess::getDefaultParameters(Param& params)
+  {
+    params.clear();
+    params.setValue("span", 2/3.0, "Fraction of datapoints (f) to use for each local regression (determines the amount of smoothing). Choosing this parameter in the range .2 to .8 usually results in a good fit.");
+    params.setMinFloat("span", 0.0);
+    params.setMaxFloat("span", 1.0);
+
+    params.setValue("num_iterations", 3, "Number of rubstifying iterations for lowess fitting.");
+    params.setMinInt("num_iterations", 0);
+
+    params.setValue("delta", -1.0, "Nonnegative parameter which may be used to save computations (recommended value is 0.01 of the range of the input, e.g. for data ranging from 1000 seconds to 2000 seconds, it could be set to 10). Setting a negative value will automatically do this.");
+
+    params.setValue("interpolation_type", "cspline", "Method to use for interpolation between datapoints computed by lowess. 'linear': Linear interpolation. 'cspline': Use the cubic spline for interpolation. 'akima': Use an akima spline for interpolation");
+    params.setValidStrings("interpolation_type", ListUtils::create<String>("linear,cspline,akima"));
+
+    params.setValue("extrapolation_type", "four-point-linear", "Method to use for extrapolation outside the data range. 'two-point-linear': Uses a line through the first and last point to extrapolate. 'four-point-linear': Uses a line through the first and second point to extrapolate in front and and a line through the last and second-to-last point in the end. 'global-linear': Uses a linear regression to fit a line through all data points and use it for interpolation.");
+    StringList etypes = ListUtils::create<String>("two-point-linear,four-point-linear,global-linear");
+    params.setValidStrings("extrapolation_type", etypes);
+  }
+
+}
---
+++ src/tests/class_tests/openms/source/TransformationModelLowess_test.cpp
@@ -0,0 +1,172 @@
+// --------------------------------------------------------------------------
+//                   OpenMS -- Open-Source Mass Spectrometry
+// --------------------------------------------------------------------------
+// Copyright The OpenMS Team -- Eberhard Karls University Tuebingen,
+// ETH Zurich, and Freie Universitaet Berlin 2002-2015.
+//
+// This software is released under a three-clause BSD license:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of any author or any participating institution
+//    may be used to endorse or promote products derived from this software
+//    without specific prior written permission.
+// For a full list of authors, refer to the file AUTHORS.
+// --------------------------------------------------------------------------
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL ANY OF THE AUTHORS OR THE CONTRIBUTING
+// INSTITUTIONS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// --------------------------------------------------------------------------
+// $Maintainer: Hannes Roest $
+// $Authors: Hannes Roest $
+// --------------------------------------------------------------------------
+
+#include <OpenMS/CONCEPT/ClassTest.h>
+#include <OpenMS/test_config.h>
+
+///////////////////////////
+
+#include <OpenMS/ANALYSIS/MAPMATCHING/TransformationModel.h>
+#include <OpenMS/ANALYSIS/MAPMATCHING/TransformationModelLowess.h>
+
+///////////////////////////
+
+START_TEST(TransformationModelLowess, "$Id$")
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+using namespace OpenMS;
+using namespace std;
+
+TransformationModelLowess* ptr = 0;
+
+TransformationModel::DataPoints data, empty;
+data.push_back(make_pair(1.2, 5.2));
+data.push_back(make_pair(3.2, 7.3));
+data.push_back(make_pair(2.2, 6.25));
+data.push_back(make_pair(2.2, 3.1));
+data.push_back(make_pair(2.2, 7.25));
+data.push_back(make_pair(3.0, 8.5));
+data.push_back(make_pair(3.1, 4.7));
+data.push_back(make_pair(1.7, 6.0));
+data.push_back(make_pair(2.9, 4.7));
+data.push_back(make_pair(4.2, 5.0));
+data.push_back(make_pair(3.7, -2.4));
+
+Param params_default;
+TransformationModelLowess::getDefaultParameters(params_default);
+
+START_SECTION((TransformationModelLowess(const DataPoints&, const Param&)))
+{
+  TEST_EXCEPTION(Exception::IllegalArgument, TransformationModelLowess tm(empty, params_default)); // need data
+  ptr = new TransformationModelLowess(data, params_default);
+  TEST_NOT_EQUAL(ptr, 0)
+}
+END_SECTION
+
+START_SECTION((~TransformationModelLowess()))
+{
+  delete ptr;
+}
+END_SECTION
+
+START_SECTION((virtual double evaluate(double value) const))
+{
+  // test data: sine function with added noise
+  double x[] = {-0.547062107104045, -2.14564213748743, -3.07082880304281, 0.470273389368586, 1.79367651606654, 0.595846950617167, 1.58738829599701, -3.11534942614546, -2.55761408378404, -0.996199010293142, -0.553164304142189, 3.11858532047631, 0.74970539948485, 0.276411185223925, 1.85962696821902, 0.960234253336655, -1.62536120645258, -2.72457034250236, 1.67812366716942, -0.838775352531627, -0.654629712755158, 1.8220799029759, -1.8653140724926, -0.235789436296459, -0.29890807257244, 0.405216494893513, 0.233453956340058, -2.82471832316488, -3.08393846252989, -1.41524590344969, -0.199886448130033};
+  double y[] = {-0.584809756448807, -0.866407723341462, -0.0471640435125096, 0.435337754412529, 0.861949333280581, 0.616243288851563, 1.1228424073836, -0.0483419751019981, -0.532873307735754, -0.917205998701872, -0.301045308942404, 0.0120964875551685, 0.758584328691163, 0.405241179450931, 1.00118722437611, 0.765459021914008, -1.03191739643009, -0.477999500942485, 0.872168291767237, -0.770691257861706, -0.496027498267174, 0.743777383059081, -0.982264617804229, -0.398462173815226, -0.40498973770553, 0.348305878579121, 0.0755855659375029, -0.457381746018402, 0.245483195014945, -1.07618910469392, -0.0880708165561682};
+  // results validated by visual inspection:
+  // double pred_spl[] = {0.846137, 0.689856, 0.5094,   0.31183, 0.10421,    -0.106399, -0.312921, -0.508271, -0.685362, -0.837111, -0.95643, -1.03623, -1.06944, -1.05016, -0.981868, -0.872412, -0.729666, -0.561505, -0.375803, -0.180434, 0.016728, 0.20827, 0.38867, 0.55289, 0.695895, 0.812645, 0.898104, 0.947234, 0.955013, 0.919484, 0.845545, 0.739022, 0.60574, 0.451526, 0.282206, 0.103606, -0.0784482, -0.258084, -0.429374, -0.586387, -0.723191};
+  double pred_low[] = {0.81549, 0.62377, 0.432051, 0.240331, 0.0486111, -0.142406, -0.326079, -0.505898, -0.656341, -0.774182, -0.879421, -0.948104, -0.973173, -0.977378, -0.919574, -0.829349, -0.716313, -0.546735, -0.398708, -0.236083, -0.0312524, 0.171936, 0.376142, 0.553262, 0.711474, 0.825752, 0.868956, 0.873851, 0.881359, 0.905248, 0.888206, 0.814595, 0.694699, 0.538594, 0.356359, 0.158072, -0.0192127, -0.157386, -0.295559, -0.433733, -0.571906};
+
+  /* R code:
+   *
+   * Note that compared to the spline, the lowess contains a linear model and
+   * is thus less suited for highly non-linear data such as a sine. However, it
+   * performs quite well on the data.
+   *
+
+    x = c(-0.547062107104045, -2.14564213748743, -3.07082880304281, 0.470273389368586, 1.79367651606654, 0.595846950617167, 1.58738829599701, -3.11534942614546, -2.55761408378404, -0.996199010293142, -0.553164304142189, 3.11858532047631, 0.74970539948485, 0.276411185223925, 1.85962696821902, 0.960234253336655, -1.62536120645258, -2.72457034250236, 1.67812366716942, -0.838775352531627, -0.654629712755158, 1.8220799029759, -1.8653140724926, -0.235789436296459, -0.29890807257244, 0.405216494893513, 0.233453956340058, -2.82471832316488, -3.08393846252989, -1.41524590344969, -0.199886448130033)
+    y = c(-0.584809756448807, -0.866407723341462, -0.0471640435125096, 0.435337754412529, 0.861949333280581, 0.616243288851563, 1.1228424073836, -0.0483419751019981, -0.532873307735754, -0.917205998701872, -0.301045308942404, 0.0120964875551685, 0.758584328691163, 0.405241179450931, 1.00118722437611, 0.765459021914008, -1.03191739643009, -0.477999500942485, 0.872168291767237, -0.770691257861706, -0.496027498267174, 0.743777383059081, -0.982264617804229, -0.398462173815226, -0.40498973770553, 0.348305878579121, 0.0755855659375029, -0.457381746018402, 0.245483195014945, -1.07618910469392, -0.0880708165561682)
+
+    pred_spl = c(0.846137, 0.689856, 0.5094,   0.31183, 0.10421,    -0.106399, -0.312921, -0.508271, -0.685362, -0.837111, -0.95643, -1.03623, -1.06944, -1.05016, -0.981868, -0.872412, -0.729666, -0.561505, -0.375803, -0.180434, 0.016728, 0.20827, 0.38867, 0.55289, 0.695895, 0.812645, 0.898104, 0.947234, 0.955013, 0.919484, 0.845545, 0.739022, 0.60574, 0.451526, 0.282206, 0.103606, -0.0784482, -0.258084, -0.429374, -0.586387, -0.723191)
+    pred_low = c(0.81549, 0.62377, 0.432051, 0.240331, 0.0486111, -0.142406, -0.326079, -0.505898, -0.656341, -0.774182, -0.879421, -0.948104, -0.973173, -0.977378, -0.919574, -0.829349, -0.716313, -0.546735, -0.398708, -0.236083, -0.0312524, 0.171936, 0.376142, 0.553262, 0.711474, 0.825752, 0.868956, 0.873851, 0.881359, 0.905248, 0.888206, 0.814595, 0.694699, 0.538594, 0.356359, 0.158072, -0.0192127, -0.157386, -0.295559, -0.433733, -0.571906)
+
+
+    plot(x, y, xlim=c(-4,4))
+    lines( seq(-4, 4.1, 0.2), pred_spl, col="blue")
+    lines( seq(-4, 4.1, 0.2), pred_low, col="red")
+
+  */
+
+  data.resize(31);
+  for (Size i = 0; i < 31; ++i)
+  {
+    data[i] = make_pair(x[i], y[i]);
+  }
+
+  Param params;
+  params.setValue("span", 0.3); // use a very low span value for non-linear data (only use 30 % of all points at a time ...)
+  params.setValue("num_iterations", 3);
+  params.setValue("delta", -1.0);
+  params.setValue("interpolation_type", "cspline");
+  params.setValue("extrapolation_type", "four-point-linear");
+  TransformationModelLowess tm(data, params);
+  
+  vector<double> results;
+  Size index = 0;
+  for (double v = -4; v < 4.1; v += 0.2, index++)
+  {
+    TEST_REAL_SIMILAR(tm.evaluate(v), pred_low[index]);
+  }
+
+  // test extrapolation:
+  params.setValue("extrapolation_type", "four-point-linear");
+  TransformationModelLowess tm_lin(data, params);
+  TEST_REAL_SIMILAR(tm_lin.evaluate(-4.0), 0.815490292172986);
+  TEST_REAL_SIMILAR(tm_lin.evaluate(4.0), -0.571905836956494);
+
+  params.setValue("extrapolation_type", "two-point-linear");
+  TransformationModelLowess tm_const(data, params);
+  TEST_REAL_SIMILAR(tm_const.evaluate(-4.0), -0.04240732863);
+  TEST_REAL_SIMILAR(tm_const.evaluate(4.0), 0.046870277);
+
+  params.setValue("extrapolation_type", "global-linear");
+  TransformationModelLowess tm_global(data, params);
+  TEST_REAL_SIMILAR(tm_global.evaluate(-4.0), -0.9501004);
+  TEST_REAL_SIMILAR(tm_global.evaluate(4.0), 1.08486397);
+}
+END_SECTION
+
+START_SECTION((void getParameters(Param& params) const))
+{
+  Param p_in;
+
+  p_in.setValue("span", 0.3);
+  p_in.setValue("num_iterations", 8);
+  p_in.setValue("delta", 1.0);
+  p_in.setValue("extrapolate", "b_spline");
+  p_in.setValue("interpolation_type", "cspline");
+  p_in.setValue("extrapolation_type", "four-point-linear");
+
+  TransformationModelLowess tm(data, p_in);
+  TEST_EQUAL(tm.getParameters().getValue("num_iterations"),
+             p_in.getValue("num_iterations"));
+}
+END_SECTION
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+END_TEST
