c846073d068ec11b72efcffdc22af217ee181e31
timosachsenberg <sachsenb@abinode06.Informatik.Uni-Tuebingen.De>
2015-12-09 09:19:35 +0100

    [FIX] fixed hashindex problem with index=0

--- src/utils/xQuest.cpp
+++ src/utils/xQuest.cpp
@@ -393,21 +393,17 @@ protected:
         cout << "Common peaks: " << matched_fragments_without_shift.size() << " different peaks: " << spectrum_light.size() - matched_fragments_without_shift.size() << ", " << spectrum_heavy.size() - matched_fragments_without_shift.size() << endl;
         cout << "Matched shifted peaks: " << matched_fragments_with_shift.size() << " unexplained peaks: " << spectrum_light_different.size() - matched_fragments_with_shift.size() << ", " << spectrum_heavy_to_light.size() - matched_fragments_with_shift.size() << endl;
 #endif
-        // generate a merged spectrum with artifically removed cross-linker
-        PeakSpectrum spectrum_cross_linker_removed;
+        // generate common peaks spectrum TODO: check if only light / only heavy or e.g. mean of both peaks are used here
+        PeakSpectrum common_peaks;
         for (Size i = 0; i != matched_fragments_without_shift.size(); ++i)
         {
-          spectrum_cross_linker_removed.push_back(spectrum_light[matched_fragments_without_shift[i].first]);
+          common_peaks.push_back(spectrum_light[matched_fragments_without_shift[i].first]);
         }
-        for (Size i = 0; i != matched_fragments_with_shift.size(); ++i)
-        {
-          spectrum_cross_linker_removed.push_back(spectrum_light_no_linker[matched_fragments_with_shift[i].first]);
-        }
-        spectrum_cross_linker_removed.sortByPosition();
+        common_peaks.sortByPosition();
 #ifdef DEBUG_XQUEST
-        cout << "Peaks to match: " << spectrum_cross_linker_removed.size() << endl;
+        cout << "Peaks to match: " << common_peaks.size() << endl;
 #endif
-        std::swap(ps.spectra_cross_linker_removed[scan_index], spectrum_cross_linker_removed);
+        std::swap(ps.spectra_common_peaks[scan_index], common_peaks);
       }
     }
     return ps;
@@ -470,7 +466,7 @@ protected:
       h_.resize(n_buckets);
     }
 
-    void insert(double position, AASequence* v)
+    void insert(double position, AASequence*& v)
     {
       if (position < min_) position = min_;
       if (position > max_) position = max_;
@@ -480,7 +476,7 @@ protected:
       if (bucket_index - (Size)bucket_index <= 0.5)
       {
         h_[bucket_index].push_back(v);
-        if (bucket_index >= 0) h_[bucket_index - 1].push_back(v);
+        if (bucket_index > 0) h_[bucket_index - 1].push_back(v);
       } 
       else
       {
@@ -777,52 +773,41 @@ protected:
       }
     }
 
+    // create spectrum generator
+    TheoreticalSpectrumGenerator spectrum_generator;
+
     cout << "Peptide " << processed_peptides.size() << " candidates." << endl;
 
     multimap<double, pair<AASequence, AASequence> > enumerated_cross_link_masses;
 
-    if (!ion_index_mode)
-    {
-      enumerated_cross_link_masses = enumerateCrossLinksAndMasses_(processed_peptides, cross_link_mass_light, cross_link_mass_loss_type2);
-    }
-
-    cout << "Enumerated cross-links: " << enumerated_cross_link_masses.size() << endl;
-
-    Size counter(0);
     //TODO remove, adapt to ppm
     HashGrid1D hg(0.0, 5000.0, fragment_mass_tolerance);
 
-    // create spectrum generator
-    TheoreticalSpectrumGenerator spectrum_generator;
-
-    Size has_aligned_peaks(0);
-    Size no_aligned_peaks(0);
-
-    // filtering peptide candidates
-    for (map<StringView, AASequence>::iterator a = processed_peptides.begin(); a != processed_peptides.end(); ++a)
+    if (!ion_index_mode)
     {
-      String s = a->second.toString();
-      cout << s << endl;
+      enumerated_cross_link_masses = enumerateCrossLinksAndMasses_(processed_peptides, cross_link_mass_light, cross_link_mass_loss_type2);
+      cout << "Enumerated cross-links: " << enumerated_cross_link_masses.size() << endl;
     }
- 
-    // filtering peptide candidates
-    for (map<StringView, AASequence>::const_iterator a = processed_peptides.begin(); a != processed_peptides.end(); ++a)
+    else
     {
-      //create theoretical spectrum
-      MSSpectrum<RichPeak1D> theo_spectrum = MSSpectrum<RichPeak1D>();
-
-      const AASequence& seq = a->second;
-      //add peaks for b and y ions with charge 1
-      //cout << a->first.getString() << ":" << a->second.toString() << endl;
-      spectrum_generator.getSpectrum(theo_spectrum, seq, 1);
-      
-      //sort by mz
-      theo_spectrum.sortByPosition();
-
-      for (Size i = 0; i != theo_spectrum.size(); ++i)
+      for (map<StringView, AASequence>::iterator a = processed_peptides.begin(); a != processed_peptides.end(); ++a)
       {
-        hg.insert(theo_spectrum[i].getMZ(), &(a->second)); // TODO add real index here
+        //create theoretical spectrum
+        MSSpectrum<RichPeak1D> theo_spectrum = MSSpectrum<RichPeak1D>();
+
+        AASequence * seq = &(a->second);
+        spectrum_generator.getSpectrum(theo_spectrum, *seq, 1);
+      
+        //sort by mz
+        theo_spectrum.sortByPosition();
+
+        for (Size i = 0; i != theo_spectrum.size(); ++i)
+        {
+          hg.insert(theo_spectrum[i].getMZ(), seq);
+        }
       }
+
+      cout << "finished adding peaks to hash map."  << endl;
     }
 
     // TODO test variable, can be removed
