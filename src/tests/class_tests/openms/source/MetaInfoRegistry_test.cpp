8eeda5d54b17c30cce08a8a5b294c4ed23a5cc00
Hendrik Weisser <hw5@sanger.ac.uk>
2016-01-04 15:49:29 +0000

    [TEST] updated test to changes in MetaInfoRegistry; simplified code

--- src/tests/class_tests/openms/source/MetaInfoRegistry_test.cpp
+++ src/tests/class_tests/openms/source/MetaInfoRegistry_test.cpp
@@ -63,150 +63,115 @@ END_SECTION
 
 MetaInfoRegistry mir;
 
-START_SECTION((UInt registerName(const String& name, const String& description, const String& unit="") const))
-	UInt testname = mir.registerName("testname","this is just a test");
-	TEST_EQUAL(1024,testname)
-	UInt retention_time = mir.registerName("retention time","this is just another test","sec");
-	TEST_EQUAL(1025,retention_time)
-	UInt another_testname = mir.registerName("another testname", "i will be set later", "so am i");
-	TEST_EQUAL(1026,another_testname);
+START_SECTION((UInt registerName(const String& name, const String& description = "", const String& unit = "")))
+{
+	UInt testname = mir.registerName("testname", "this is just a test");
+  TEST_EQUAL(testname, 1024);
+	UInt retention_time = mir.registerName("retention time", "this is just another test", "sec");
+  TEST_EQUAL(retention_time, 1025);
+	UInt another_testname = mir.registerName("another testname", "i will be set later", "me too");
+  TEST_EQUAL(another_testname, 1026);
+}
 END_SECTION
 
-START_SECTION((void setDescription(UInt index, const String& description) ))
+START_SECTION((void setDescription(UInt index, const String& description)))
 	mir.setDescription(1026, "foo");
-	TEST_EQUAL(mir.getDescription(1026), "foo")
+	TEST_STRING_EQUAL(mir.getDescription(1026), "foo")
 END_SECTION
 
-START_SECTION((void setDescription(const String& name, const String& description) ))
+START_SECTION((void setDescription(const String& name, const String& description)))
 	mir.setDescription("another testname", "bar");
-	TEST_EQUAL(mir.getDescription(1026), "bar")
+	TEST_STRING_EQUAL(mir.getDescription(1026), "bar")
 END_SECTION
 
-START_SECTION((void setUnit(UInt index, const String& unit) ))
+START_SECTION((void setUnit(UInt index, const String& unit)))
 	mir.setUnit(1026, "foo");
-	TEST_EQUAL(mir.getUnit(1026), "foo")
+	TEST_STRING_EQUAL(mir.getUnit(1026), "foo")
 END_SECTION
 
-START_SECTION((void setUnit(const String& name, const String& unit) ))
+START_SECTION((void setUnit(const String& name, const String& unit)))
 	mir.setUnit("another testname", "bar");
-	TEST_EQUAL(mir.getUnit(1026), "bar")
+	TEST_STRING_EQUAL(mir.getUnit(1026), "bar")
 END_SECTION
 
 START_SECTION((UInt getIndex(const String& name) const))
-	UInt tmp;
-	tmp = mir.getIndex ("testname");
-	TEST_EQUAL(1024,tmp)
-	tmp = mir.getIndex ("retention time");
-	TEST_EQUAL(1025,tmp)
-	tmp = mir.getIndex ("isotopic_range");
-	TEST_EQUAL(1,tmp)
-	tmp = mir.getIndex ("cluster_id");
-	TEST_EQUAL(2,tmp)
-	tmp = mir.getIndex ("unregistered name");
-	TEST_EQUAL(1027,tmp)
-	tmp = mir.getIndex ("another unregistered name");
-	TEST_EQUAL(1028,tmp)
-	tmp = mir.getIndex ("unregistered name");
-	TEST_EQUAL(1027,tmp)
+	TEST_EQUAL(mir.getIndex("testname"), 1024)
+	TEST_EQUAL(mir.getIndex("retention time"), 1025)
+	TEST_EQUAL(mir.getIndex("isotopic_range"), 1)
+	TEST_EQUAL(mir.getIndex("cluster_id"), 2)
+  TEST_EQUAL(mir.getIndex("unregistered name"), UInt(-1))
 END_SECTION
 
-START_SECTION((String getName(UInt index) const ))
-	string tmp;
-	tmp = mir.getName (1);
-	TEST_EQUAL(tmp,"isotopic_range")
-	tmp = mir.getName (2);
-	TEST_EQUAL(tmp,"cluster_id")
-	tmp = mir.getName (3);
-	TEST_EQUAL(tmp,"label")
-	tmp = mir.getName (4);
-	TEST_EQUAL(tmp,"icon")
-	tmp = mir.getName (1024);
-	TEST_EQUAL(tmp,"testname")
-	tmp = mir.getName (1025);
-	TEST_EQUAL(tmp,"retention time")
+START_SECTION((String getName(UInt index) const))
+	TEST_STRING_EQUAL(mir.getName(1), "isotopic_range")
+	TEST_STRING_EQUAL(mir.getName(2), "cluster_id")
+	TEST_STRING_EQUAL(mir.getName(3), "label")
+	TEST_STRING_EQUAL(mir.getName(4), "icon")
+	TEST_STRING_EQUAL(mir.getName(1024), "testname")
+	TEST_STRING_EQUAL(mir.getName(1025), "retention time")
 END_SECTION
 
-START_SECTION((String getDescription(UInt index) const ))
-	string tmp;
-	tmp = mir.getDescription(1024);
-	TEST_EQUAL(tmp,string("this is just a test"))
-	tmp = mir.getDescription(1025);
-	TEST_EQUAL(tmp,string("this is just another test"))
-	tmp = mir.getDescription(1);
-	TEST_EQUAL(tmp,string("consecutive numbering of the peaks in an isotope pattern. 0 is the monoisotopic peak"))
-	tmp = mir.getDescription(2);
-	TEST_EQUAL(tmp,string("consecutive numbering of isotope clusters in a spectrum"))
+START_SECTION((String getDescription(UInt index) const))
+	TEST_STRING_EQUAL(mir.getDescription(1024), "this is just a test")
+	TEST_STRING_EQUAL(mir.getDescription(1025), "this is just another test")
+	TEST_STRING_EQUAL(mir.getDescription(1), "consecutive numbering of the peaks in an isotope pattern. 0 is the monoisotopic peak")
+	TEST_STRING_EQUAL(mir.getDescription(2), "consecutive numbering of isotope clusters in a spectrum")
 END_SECTION
 
-START_SECTION((String getDescription(const String& name) const ))
-	string tmp;
-	tmp = mir.getDescription("testname");
-	TEST_EQUAL(tmp,string("this is just a test"))
-	tmp = mir.getDescription("retention time");
-	TEST_EQUAL(tmp,string("this is just another test"))
-	tmp = mir.getDescription("isotopic_range");
-	TEST_EQUAL(tmp,string("consecutive numbering of the peaks in an isotope pattern. 0 is the monoisotopic peak"))
-	tmp = mir.getDescription("cluster_id");
-	TEST_EQUAL(tmp,string("consecutive numbering of isotope clusters in a spectrum"))
+START_SECTION((String getDescription(const String& name) const))
+	TEST_STRING_EQUAL(mir.getDescription("testname"), "this is just a test")
+	TEST_STRING_EQUAL(mir.getDescription("retention time"), "this is just another test")
+	TEST_STRING_EQUAL(mir.getDescription("isotopic_range"), "consecutive numbering of the peaks in an isotope pattern. 0 is the monoisotopic peak")
+	TEST_STRING_EQUAL(mir.getDescription("cluster_id"), "consecutive numbering of isotope clusters in a spectrum")
 END_SECTION
 
-START_SECTION((String getUnit(UInt index) const ))
-	string tmp;
-	tmp = mir.getUnit(1024);
-	TEST_EQUAL(tmp,string(""))
-	tmp = mir.getUnit(1025);
-	TEST_EQUAL(tmp,string("sec"))
-	tmp = mir.getUnit(1);
-	TEST_EQUAL(tmp,string(""))
-	tmp = mir.getUnit(2);
-	TEST_EQUAL(tmp,string(""))
+START_SECTION((String getUnit(UInt index) const))
+	TEST_STRING_EQUAL(mir.getUnit(1024), "")
+	TEST_STRING_EQUAL(mir.getUnit(1025), "sec")
+	TEST_STRING_EQUAL(mir.getUnit(1), "")
+	TEST_STRING_EQUAL(mir.getUnit(2), "")
 END_SECTION
 
-START_SECTION((String getUnit(const String& name) const ))
-	string tmp;
-	tmp = mir.getUnit("testname");
-	TEST_EQUAL(tmp,string(""))
-	tmp = mir.getUnit("retention time");
-	TEST_EQUAL(tmp,string("sec"))
-	tmp = mir.getUnit("isotopic_range");
-	TEST_EQUAL(tmp,string(""))
-	tmp = mir.getUnit("cluster_id");
-	TEST_EQUAL(tmp,string(""))
+START_SECTION((String getUnit(const String& name) const))
+	TEST_STRING_EQUAL(mir.getUnit("testname"), "")
+  TEST_STRING_EQUAL(mir.getUnit("retention time"), "sec")
+	TEST_STRING_EQUAL(mir.getUnit("isotopic_range"), "")
+	TEST_STRING_EQUAL(mir.getUnit("cluster_id"), "")
 END_SECTION
 
 START_SECTION((MetaInfoRegistry(const MetaInfoRegistry& rhs)))
 	MetaInfoRegistry mir2(mir);
-	TEST_EQUAL(1024,mir2.getIndex ("testname"))
-	TEST_EQUAL(1025,mir2.getIndex ("retention time"))
-	TEST_EQUAL(mir2.getName(1),"isotopic_range")
-	TEST_EQUAL(mir2.getName(1024),"testname")
-	TEST_EQUAL(mir2.getName(1025),"retention time")
-	TEST_EQUAL(mir2.getDescription(1024),string("this is just a test"))
-	TEST_EQUAL(mir2.getDescription(1025),string("this is just another test"))
-	TEST_EQUAL(mir2.getDescription("testname"),string("this is just a test"))
-	TEST_EQUAL(mir2.getDescription("retention time"),string("this is just another test"))
-	TEST_EQUAL(mir2.getUnit(1024),string(""))
-	TEST_EQUAL(mir2.getUnit(1025),string("sec"))
-	TEST_EQUAL(mir2.getUnit("testname"),string(""))
-	TEST_EQUAL(mir2.getUnit("retention time"),string("sec"))
+  TEST_EQUAL(mir2.getIndex("testname"), 1024)
+  TEST_EQUAL(mir2.getIndex("retention time"), 1025)
+	TEST_STRING_EQUAL(mir2.getName(1), "isotopic_range")
+	TEST_STRING_EQUAL(mir2.getName(1024), "testname")
+	TEST_STRING_EQUAL(mir2.getName(1025), "retention time")
+	TEST_STRING_EQUAL(mir2.getDescription(1024), "this is just a test")
+	TEST_STRING_EQUAL(mir2.getDescription(1025), "this is just another test")
+	TEST_STRING_EQUAL(mir2.getDescription("testname"), "this is just a test")
+	TEST_STRING_EQUAL(mir2.getDescription("retention time"), "this is just another test")
+	TEST_STRING_EQUAL(mir2.getUnit(1024), "")
+	TEST_STRING_EQUAL(mir2.getUnit(1025), "sec")
+	TEST_STRING_EQUAL(mir2.getUnit("testname"), "")
+	TEST_STRING_EQUAL(mir2.getUnit("retention time"), "sec")
 END_SECTION
 
-START_SECTION((MetaInfoRegistry& operator = (const MetaInfoRegistry& rhs)))
+START_SECTION((MetaInfoRegistry& operator=(const MetaInfoRegistry& rhs)))
 	MetaInfoRegistry mir2;
 	mir2 = mir;
-	TEST_EQUAL(1024,mir2.getIndex ("testname"));
-	TEST_EQUAL(1025,mir2.getIndex ("retention time"));
-	TEST_EQUAL(mir2.getName(1),"isotopic_range");
-	TEST_EQUAL(mir2.getName(1024),"testname");
-	TEST_EQUAL(mir2.getName(1025),"retention time");
-	TEST_EQUAL(mir2.getDescription(1024),string("this is just a test"))
-	TEST_EQUAL(mir2.getDescription(1025),string("this is just another test"))
-	TEST_EQUAL(mir2.getDescription("testname"),string("this is just a test"))
-	TEST_EQUAL(mir2.getDescription("retention time"),string("this is just another test"))
-	TEST_EQUAL(mir2.getUnit(1024),string(""))
-	TEST_EQUAL(mir2.getUnit(1025),string("sec"))
-	TEST_EQUAL(mir2.getUnit("testname"),string(""))
-	TEST_EQUAL(mir2.getUnit("retention time"),string("sec"))
+  TEST_EQUAL(mir2.getIndex("testname"), 1024)
+  TEST_EQUAL(mir2.getIndex("retention time"), 1025)
+	TEST_STRING_EQUAL(mir2.getName(1), "isotopic_range")
+	TEST_STRING_EQUAL(mir2.getName(1024), "testname")
+	TEST_STRING_EQUAL(mir2.getName(1025), "retention time")
+	TEST_STRING_EQUAL(mir2.getDescription(1024), "this is just a test")
+	TEST_STRING_EQUAL(mir2.getDescription(1025), "this is just another test")
+	TEST_STRING_EQUAL(mir2.getDescription("testname"), "this is just a test")
+	TEST_STRING_EQUAL(mir2.getDescription("retention time"), "this is just another test")
+	TEST_STRING_EQUAL(mir2.getUnit(1024), "")
+	TEST_STRING_EQUAL(mir2.getUnit(1025), "sec")
+	TEST_STRING_EQUAL(mir2.getUnit("testname"), "")
+	TEST_STRING_EQUAL(mir2.getUnit("retention time"), "sec")
 END_SECTION
 
 /////////////////////////////////////////////////////////////
