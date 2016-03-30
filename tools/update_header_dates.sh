7f47b1ee18891313417322eff2675883448f7e0a
timosachsenberg <sachsenb@abinode05.Informatik.Uni-Tuebingen.De>
2016-03-30 16:11:12 +0200

    [NOP] update header dates and script

--- tools/update_header_dates.sh
+++ tools/update_header_dates.sh
@@ -1,1 +1,1 @@
-find . -name "*.h" -o -name "*.doxygen" -o -name "*.txt" -o -name "*.cpp" | xargs sed -i 's/and Freie Universitaet Berlin 2002-2015/and Freie Universitaet Berlin 2002-2016/g'
+find . -name "*.h" -o -name "*.doxygen" -o -name "*.txt" -o -name "*.cpp" | xargs sed -i 's/and Freie Universitaet Berlin 2002-20../and Freie Universitaet Berlin 2002-2016/g'
