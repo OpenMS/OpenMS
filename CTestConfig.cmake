e5714274e4ea6329036d66d88018bef601d03d58
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2016-04-04 11:16:31 +0200

    Update CDash to new Tuebingen based server

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,13 +1,15 @@
 ## This file should be placed in the root directory of your project.
 ## Then modify the CMakeLists.txt file in the root directory of your
 ## project to incorporate the testing dashboard.
-## # The following are required to uses Dart and the Cdash dashboard
+##
+## # The following are required to submit to the CDash dashboard:
 ##   ENABLE_TESTING()
 ##   INCLUDE(CTest)
+
 set(CTEST_PROJECT_NAME "OpenMS")
 set(CTEST_NIGHTLY_START_TIME "23:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "cdash.openms.de")
+set(CTEST_DROP_SITE "build-cdash.informatik.uni-tuebingen.de")
 set(CTEST_DROP_LOCATION "/submit.php?project=OpenMS")
 set(CTEST_DROP_SITE_CDASH TRUE)
