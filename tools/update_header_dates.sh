07996f6cefff36557d3a908f6c9b8a43afefbfad
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2016-04-15 12:24:04 +0200

    [NOP] Updated years of non-source copyrights. sed automatically fixed some newlines at end of files.

--- tools/update_header_dates.sh
+++ tools/update_header_dates.sh
@@ -1,1 +1,1 @@
-find . -name "*.h" -o -name "*.doxygen" -o -name "*.txt" -o -name "*.cpp" | xargs sed -i 's/and Freie Universitaet Berlin 2002-20../and Freie Universitaet Berlin 2002-2016/g'
+find . -type f | xargs sed -i 's/and Freie Universitaet Berlin 2002-2016/and Freie Universitaet Berlin 2002-2016/g'
