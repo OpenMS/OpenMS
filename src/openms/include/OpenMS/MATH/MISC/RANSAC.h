a8f72ccefb363b2685e5439c94b0d1fc769da1ac
Chris Bielow <chris.bielow@fu-berlin.de>
2016-05-07 12:21:31 +0200

    [DOC] some hints on RANSAC usage

--- src/openms/include/OpenMS/MATH/MISC/RANSAC.h
+++ src/openms/include/OpenMS/MATH/MISC/RANSAC.h
@@ -60,14 +60,14 @@ namespace OpenMS
     {
       /// Default constructor
       RANSACParam()
-        : n(0), k(0), t(0), d(0), relative(false), rng(NULL)
+        : n(0), k(0), t(0), d(0), relative_d(false), rng(NULL)
         {
         }
       /// Full constructor
-      RANSACParam(size_t p_n, size_t p_k, double p_t, size_t p_d, bool p_relative = false, int (*p_rng)(int) = NULL)
-        : n(p_n), k(p_k), t(p_t), d(p_d), relative(p_relative), rng(p_rng)
+      RANSACParam(size_t p_n, size_t p_k, double p_t, size_t p_d, bool p_relative_d = false, int (*p_rng)(int) = NULL)
+        : n(p_n), k(p_k), t(p_t), d(p_d), relative_d(p_relative_d), rng(p_rng)
       {
-        if (relative)
+        if (relative_d)
         {
           if (d >= 100) throw Exception::Precondition(__FILE__, __LINE__, __PRETTY_FUNCTION__, String("RANSAC: Relative 'd' >= 100% given. Use a lower value; the more outliers you expect, the lower it should be."));
         }
@@ -84,7 +84,7 @@ namespace OpenMS
       size_t k; //< iterations: The maximum number of iterations allowed in the algorithm 
       double t; //< Threshold value: for determining when a data point fits a model. Corresponds to the maximal squared deviation in units of the _second_ dimension (dim2).
       size_t d; //< The number of close data values (according to 't') required to assert that a model fits well to data
-      bool relative; //< Should 'n' and 'd' be interpreted as percentages (0-100) of data input size.
+      bool relative_d; //< Should 'd' be interpreted as percentages (0-100) of data input size.
       int (*rng)(int); //< Optional RNG function (useful for testing with fixed seeds)
     };
 
@@ -103,13 +103,26 @@ public:
         const std::vector<std::pair<double, double> >& pairs, 
         const RANSACParam& p)
       {
-        return ransac(pairs, p.n, p.k, p.t, p.d, p.relative, p.rng);
+        return ransac(pairs, p.n, p.k, p.t, p.d, p.relative_d, p.rng);
       }
 
       /**
         @brief This function provides a generic implementation of the RANSAC
          outlier detection algorithm. Is implemented and tested after the
          SciPy reference: http://wiki.scipy.org/Cookbook/RANSAC
+
+        If possible, restrict 'n' to the minimal number of points which the model requires to make a
+        fit, i.e. n=2 for linear, n=3 for quadratic. Any higher number will result in increasing the chance
+        of including an outlier, hence a lost iteration.
+
+        While iterating, this RANSAC implementation will consider any model which explains more data points
+        than the currently best model as even better. If the data points are equal, RSS (residual sum of squared error)
+        will be used.
+
+        Making 'd' a relative measure (1-99%) is useful if you cannot predict how many points RANSAC will actually receive
+        at runtime, but you have a rough idea how many percent will be outliers. E.g. if you expect 20% outliers,
+        then setting d=60, relative_d=true (i.e. 60% inliers with some margin for error) is a good bet for a larger input set.
+        (Consider that 2-3 data points will be used for the initial model already -- they cannot possibly become inliers).
 
         @param pairs Input data (paired data of type <dim1, dim2>)
         @param n The minimum number of data points required to fit the model
@@ -196,11 +209,10 @@ public:
             betterrsq = model.rm_rsq(betterdata);
     #endif
 
-            // If the current model explains more points, we assume its better; if the number of points is equal, we trust rss.
-            // E.g. imagine gaining a zillion more points (which pass the threshold!), at the cost of slightly worse rss.
-            // Alternative: use only rss (or similar), but the problem with rss is that
-            //              it will not accept a model with more points (usually at either end of the x-axis), if the fit gets even slightly worse
-            //
+            // If the current model explains more points, we assume its better (these points pass the error threshold 't', so they should be ok);
+            // If the number of points is equal, we trust rss.
+            // E.g. imagine gaining a zillion more points (which pass the threshold!) -- then rss will automatically be worse, no matter how good
+            //      these points fit, since its a simple absolute SUM() of residual error over all points.
             if (betterdata.size() > bestdata.size() || (betterdata.size() == bestdata.size() && (bettererror < besterror)))
             {
               besterror = bettererror;
