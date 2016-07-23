8cc96d91206910e82d5eb775c6124d36c161c953
Hannes Roest <roest@imsb.biol.ethz.ch>
2016-07-23 16:32:40 -0700

    [FIX] change assumptions about charge

--- src/openms/source/ANALYSIS/OPENSWATH/DIAScoring.cpp
+++ src/openms/source/ANALYSIS/OPENSWATH/DIAScoring.cpp
@@ -208,7 +208,7 @@ namespace OpenMS
   {
     bseries_score = 0;
     yseries_score = 0;
-    OPENMS_PRECONDITION(charge > 0, "Charge is a positive integer");
+    OPENMS_PRECONDITION(charge > 0, "Charge is a positive integer"); // for peptides, charge should be positive
 
     double mz, intensity, left, right;
     std::vector<double> yseries, bseries;
@@ -346,7 +346,7 @@ namespace OpenMS
                                           const std::vector<double>& isotopes_int, int putative_fragment_charge,
                                           std::string sum_formula)
   {
-    OPENMS_PRECONDITION(putative_fragment_charge > 0, "Charge is a positive integer");
+    OPENMS_PRECONDITION(putative_fragment_charge != 0, "Charge needs to be set"); // charge can be positive and negative
 
     typedef OpenMS::FeatureFinderAlgorithmPickedHelperStructs::TheoreticalIsotopePattern TheoreticalIsotopePattern;
 
@@ -362,7 +362,7 @@ namespace OpenMS
     {
       // create the theoretical distribution from the peptide weight
       isotope_dist.setMaxIsotope(dia_nr_isotopes_ + 1);
-      isotope_dist.estimateFromPeptideWeight(product_mz * putative_fragment_charge);
+      isotope_dist.estimateFromPeptideWeight(std::fabs(product_mz * putative_fragment_charge));
     }
 
 
