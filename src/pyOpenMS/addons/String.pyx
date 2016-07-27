e77a31ec6e8cbfc8059afc0d04ce4a7299671138
Hannes Roest <roest@imsb.biol.ethz.ch>
2016-07-27 12:23:07 -0700

    [FEATURE] use single function for conversion
    - returning directly a shared_ptr has the least overhead and does not
      create a new Python object along the way - it also solves the memory
      management issue.

--- src/pyOpenMS/addons/String.pyx
+++ src/pyOpenMS/addons/String.pyx
@@ -1,83 +1,48 @@
 from String cimport String as _String
-cdef class StringPtrWrapper:
-    cdef shared_ptr[_String] wrapped
-    def __dealloc__(self): self.wrapped.reset()
-cdef convString(argument_var):
-    cdef StringPtrWrapper wrapper = StringPtrWrapper()
-    #cdef _String * cs_argument_var
+cdef shared_ptr[_String] convString(argument_var):
+    # Generic function to convert Python strings to OpenMS::String
+    # 
+    # This allows us to either extract the already existing shared_ptr from a
+    # Python-type OpenMS::String holder or create a new shared_ptr from a
+    # Python string.
+    # In case the user only provides a str, unicode or bytes argument, we will
+    # have to create a new OpenMS::String object, the shared_ptr will
+    # automatically delete it after it has been used for a function call
+    # (presumably the user does not want to have the value returned by
+    # reference).
+    #
+    # Note: even though Python 3.x does not know the unicode keyword,
+    # Cython does and uses the PyUnicode_Check call. 
+    #
     cdef char* c_string_argument_var
-    # cdef shared_ptr[_String] res
+    cdef shared_ptr[_String] res
     if isinstance(argument_var, String):
-        wrapper.wrapped = (<String>argument_var).inst
-        return wrapper
+        res = (<String>argument_var).inst
+        return res
     elif isinstance(argument_var, bytes):
-        wrapper.wrapped = shared_ptr[_String](new _String(<char*>argument_var))
-        return wrapper
-    elif isinstance(argument_var, str):
+        # Simple, convert directly to char*
+        res = shared_ptr[_String](new _String(<char*>argument_var))
+        return res
+    elif isinstance(argument_var, str) or isinstance(argument_var, unicode):
+        # First encode with UTF8 (note that toString below decodes with UTF8),
+        # then convert the result to char*
         py_byte_string = argument_var.encode('UTF-8')
         c_string_argument_var = py_byte_string
-        # cs_argument_var = new _String((<char *>c_string_argument_var))
-        wrapper.wrapped = shared_ptr[_String](new _String(<char*>c_string_argument_var))
-        return wrapper
-    elif isinstance(argument_var, unicode):
-        py_byte_string = argument_var.encode('UTF-8')
-        c_string_argument_var = py_byte_string
-        #cs_argument_var = new _String((<char *>c_string_argument_var))
-        wrapper.wrapped = shared_ptr[_String](new _String(<char*>c_string_argument_var))
-        return wrapper
+        res = shared_ptr[_String](new _String(<char*>c_string_argument_var))
+        return res
     else:
         raise Exception("Can only convert the following types to String: pyopenms.String, bytes, str, unicode")
 
 
-
+    # This goes into the class
     def _init_0(self):
         self.inst = shared_ptr[_String](new _String())
     
-    def _init_1(self, String in_0 ):
-        assert isinstance(in_0, String), 'arg in_0 wrong type'
-    
-        self.inst = shared_ptr[_String](new _String(deref(in_0.inst.get())))
-
-    def _init_2(self, bytes in_0 ):
-        assert isinstance(in_0, bytes), 'arg in_0 wrong type'
-    
-        self.inst = shared_ptr[_String](new _String((<char *>in_0)))
-
-    def _init_3(self, str in_0 ):
-        assert isinstance(in_0, str), 'arg in_0 wrong type'
-    
-        py_byte_string = in_0.encode('UTF-8')
-        cdef char* c_string = py_byte_string
-        self.inst = shared_ptr[_String](new _String((<char *>c_string)))
-
-    def _init_4(self, unicode in_0 ):
-        assert isinstance(in_0, unicode), 'arg in_0 wrong type'
-    
-        py_byte_string = in_0.encode('UTF-8')
-        cdef char* c_string = py_byte_string
-        self.inst = shared_ptr[_String](new _String((<char *>c_string)))
-    
     def __init__(self, *args):
-
-        #cdef shared_ptr[_String] mystr = (<shared_ptr[_String]>convString(*args))
-        cdef StringPtrWrapper mystr = (<StringPtrWrapper>convString(args[0]))
-        cdef shared_ptr[_String] mystr2 = (<StringPtrWrapper>convString(args[0])).wrapped
-
-
-        # Note: even though Python 3.x does not know the unicode keyword,
-        # Cython does and uses the PyUnicode_Check call. We just need to check
-        # for unicode last, this will ensure all Python 3.x call go through the
-        # "str" branch below:
         if not args:
              self._init_0(*args)
-        elif (len(args)==1) and (isinstance(args[0], String)):
-             self._init_1(*args)
-        elif (len(args)==1) and (isinstance(args[0], bytes)):
-             self._init_2(*args)
-        elif (len(args)==1) and (isinstance(args[0], str)):
-             self._init_3(*args)
-        elif (len(args)==1) and (isinstance(args[0], unicode)):
-             self._init_4(*args)
+        elif (len(args)==1):
+            self.inst = convString(args[0])
         else:
              raise Exception('can not handle type of %s' % (args,)) 
 
