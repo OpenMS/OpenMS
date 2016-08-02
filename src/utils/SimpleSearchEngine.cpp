3ca0b98a642071b4c6a617692af3c963dcbf0797
timosachsenberg <sachsenb@abinode05.Informatik.Uni-Tuebingen.De>
2016-08-02 12:10:07 +0200

    [FIX] extendend SimpleSearchEngine output

--- src/utils/SimpleSearchEngine.cpp
+++ src/utils/SimpleSearchEngine.cpp
@@ -443,6 +443,22 @@ class SimpleSearchEngine :
       protein_ids[0].setDateTime(DateTime::now());
       protein_ids[0].setSearchEngine("SimpleSearchEngine");
       protein_ids[0].setSearchEngineVersion(VersionInfo::getVersion());
+
+      ProteinIdentification::SearchParameters search_parameters;
+      search_parameters.db = getStringOption_("database");
+      search_parameters.charges = "+" + String(getIntOption_("precursor:min_charge")) + "-+" + String(getIntOption_("precursor:max_charge"));
+
+      ProteinIdentification::PeakMassType mass_type = ProteinIdentification::MONOISOTOPIC;
+      search_parameters.mass_type = mass_type;
+      search_parameters.fixed_modifications = getStringList_("modifications:fixed");
+      search_parameters.variable_modifications = getStringList_("modifications:variable");
+      search_parameters.missed_cleavages = getIntOption_("peptide:missed_cleavages");
+      search_parameters.fragment_mass_tolerance = getDoubleOption_("fragment:mass_tolerance");
+      search_parameters.precursor_mass_tolerance = getDoubleOption_("precursor:mass_tolerance");
+      search_parameters.precursor_mass_tolerance_ppm = getStringOption_("precursor:mass_tolerance_unit") == "ppm" ? true : false;
+      search_parameters.fragment_mass_tolerance_ppm = getStringOption_("fragment:mass_tolerance_unit") == "ppm" ? true : false;
+      search_parameters.digestion_enzyme = *EnzymesDB::getInstance()->getEnzyme(getStringOption_("enzyme"));
+      protein_ids[0].setSearchParameters(search_parameters);
     }
 
     ExitCodes main_(int, const char**)
@@ -676,6 +692,7 @@ class SimpleSearchEngine :
       progresslogger.endProgress();
 
       protein_ids[0].setPrimaryMSRunPath(spectra.getPrimaryMSRunPath());
+
       // write ProteinIdentifications and PeptideIdentifications to IdXML
       IdXMLFile().store(out_idxml, protein_ids, peptide_ids);
 
