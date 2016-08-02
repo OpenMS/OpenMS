c3e11bed9fbd6329f40a5a47c939389ad87aa553
Hendrik Weisser <hw5@sanger.ac.uk>
2016-08-02 16:16:02 +0100

    [DOC] ModificationsDB: improved documentation (especially which exceptions may be thrown)

--- src/openms/include/OpenMS/CHEMISTRY/ModificationsDB.h
+++ src/openms/include/OpenMS/CHEMISTRY/ModificationsDB.h
@@ -70,9 +70,9 @@ namespace OpenMS
   {
 public:
 
-    inline static ModificationsDB * getInstance()
+    inline static ModificationsDB* getInstance()
     {
-      static ModificationsDB * db_ = 0;
+      static ModificationsDB* db_ = 0;
       if (db_ == 0)
       {
         db_ = new ModificationsDB;
@@ -83,45 +83,75 @@ public:
     /// returns the number of modifications read from the unimod.xml file
     Size getNumberOfModifications() const;
 
-    /// returns the modification with the given index
-    const ResidueModification & getModification(Size index) const;
+    /**
+       @brief returns the modification with the given index
 
-    /// returns all modifications which have the given name as synonym
-    void searchTerminalModifications(std::set<const ResidueModification *> & mods, const String & name, ResidueModification::Term_Specificity term_spec) const;
-
-    /// returns all modification which have the given name as synonym and the given origin
-    void searchModifications(std::set<const ResidueModification *> & mods, const String & orgin, const String & mod_name, ResidueModification::Term_Specificity term_spec) const;
-
-    /// returns all modification which have the given name as synonym
-    void searchModifications(std::set<const ResidueModification *> & mods, const String & mod_name, ResidueModification::Term_Specificity term_spec) const;
-
-    /** @brief returns the modifications of the given name
-
-            This can either be the PSI-MOD identifier or every other unique
-            identifier which can be found in the PSI-MOD definitions file.
-
-            To search for more than one modification searchModifications() can be used!
-
-            @exception ElementNotFound is thrown if no or more than one element is found
+       @throw Exception::IndexOverflow if the index is too large
     */
-    const ResidueModification & getTerminalModification(const String & name, ResidueModification::Term_Specificity term_spec) const;
+    const ResidueModification& getModification(Size index) const;
 
-    /// returns the modification with the given name and given residue
-    const ResidueModification & getModification(const String & residue_name, const String & mod_name, ResidueModification::Term_Specificity term_spec) const;
+    /**
+       @brief returns all modifications which have the given name as synonym
 
-    const ResidueModification & getModification(const String & modification) const;
+       @throw Exception::ElementNotFound if no modification named @p name exists
+    */
+    void searchTerminalModifications(std::set<const ResidueModification*>& mods, const String& name, ResidueModification::Term_Specificity term_spec) const;
 
-    /// returns the index of the modification in the mods_ vector; a unique name must be given
-    Size findModificationIndex(const String & mod_name) const;
+    /**
+       @brief returns all modification which have the given name as synonym and the given origin
+
+       @throw Exception::ElementNotFound if no modification named @p name exists
+    */
+    void searchModifications(std::set<const ResidueModification*>& mods, const String& orgin, const String& mod_name, ResidueModification::Term_Specificity term_spec) const;
+
+    /**
+       @brief returns all modification which have the given name as synonym
+
+       @throw Exception::ElementNotFound if no modification named @p name exists
+    */
+    void searchModifications(std::set<const ResidueModification*>& mods, const String& mod_name, ResidueModification::Term_Specificity term_spec) const;
+
+    /**
+       @brief returns the modifications of the given name
+
+       This can either be the PSI-MOD identifier or every other unique identifier which can be found in the PSI-MOD definitions file.
+       To search for more than one modification searchModifications() can be used!
+
+       @throw Exception::ElementNotFound is thrown if no or more than one element is found
+    */
+    const ResidueModification& getTerminalModification(const String& name, ResidueModification::Term_Specificity term_spec) const;
+
+    /**
+       @brief returns the modification with the given name and given residue
+
+       @throw Exception::InvalidValue if no matching residue or modification were found
+    */
+    const ResidueModification& getModification(const String& residue_name, const String& mod_name, ResidueModification::Term_Specificity term_spec) const;
+
+    /**
+       @brief returns the modification with the given name
+
+       If more than one matching modification are found, the first one is returned with a warning.
+
+       @throw Exception::ElementNotFound if no matching modification was found
+    */
+    const ResidueModification& getModification(const String& modification) const;
+
+    /**
+       @brief returns the index of the modification in the mods_ vector; a unique name must be given
+
+       @throw Exception::ElementNotFound if not exactly one matching modification was found
+    */
+    Size findModificationIndex(const String& mod_name) const;
 
     /// query the modifications DB to get the terminal modifications with mass
-    void getTerminalModificationsByDiffMonoMass(std::vector<String> & mods, double mass, double error, ResidueModification::Term_Specificity term_spec);
+    void getTerminalModificationsByDiffMonoMass(std::vector<String>& mods, double mass, double error, ResidueModification::Term_Specificity term_spec);
 
     /// query the modifications DB to get the modifications with mass, without any specific origin
-    void getModificationsByDiffMonoMass(std::vector<String> & mods, double mass, double error = 0.0);
+    void getModificationsByDiffMonoMass(std::vector<String>& mods, double mass, double error = 0.0);
 
     /// query the modifications DB to get modifications with the given delta mass at the given residue
-    void getModificationsByDiffMonoMass(std::vector<String> & mods, const String & residue, double mass, double error = 0.0);
+    void getModificationsByDiffMonoMass(std::vector<String>& mods, const String & residue, double mass, double error = 0.0);
 
     /** @brief returns the best matching modification for the given mass and residue
 
@@ -142,7 +172,7 @@ public:
         @return A pointer to the best matching modification (or NULL if none was found)
 
     */
-    const ResidueModification * getBestModificationsByMonoMass(const String & residue, double mass, double max_error = 0.0);
+    const ResidueModification* getBestModificationsByMonoMass(const String& residue, double mass, double max_error = 0.0);
 
     /** @brief returns the best matching modification for the given delta mass and residue
 
@@ -163,27 +193,31 @@ public:
         @return A pointer to the best matching modification (or NULL if none was found)
 
     */
-    const ResidueModification * getBestModificationsByDiffMonoMass(const String & residue, double mass, double max_error = 0.0);
+    const ResidueModification* getBestModificationsByDiffMonoMass(const String& residue, double mass, double max_error = 0.0);
 
-    /// adds modifications from a given file in OBO format
-    void readFromOBOFile(const String & filename);
+    /**
+       @brief adds modifications from a given file in OBO format
+
+       @throw Exception::ParseError if the file cannot be parsed correctly
+    */
+    void readFromOBOFile(const String& filename);
 
     /// adds modifications from a given file in Unimod XML format
-    void readFromUnimodXMLFile(const String & filename);
+    void readFromUnimodXMLFile(const String& filename);
 
     /// get all modifications that can be used for identification searches
-    void getAllSearchModifications(std::vector<String> & modifications);
-    
+    void getAllSearchModifications(std::vector<String>& modifications);
+
     /// returns true if the modification exists
     bool has(String modification) const;
 
 protected:
 
     /// stores the modifications
-    std::vector<ResidueModification *> mods_;
+    std::vector<ResidueModification*> mods_;
 
     /// stores the mappings of (unique) names to the modifications
-    Map<String, std::set<const ResidueModification *> > modification_names_;
+    Map<String, std::set<const ResidueModification*> > modification_names_;
 
 
 private:
@@ -195,7 +229,7 @@ private:
     ModificationsDB();
 
     ///copy constructor
-    ModificationsDB(const ModificationsDB & residue_db);
+    ModificationsDB(const ModificationsDB& residue_db);
 
     /// destructor
     virtual ~ModificationsDB();
@@ -205,7 +239,7 @@ private:
     */
     //@{
     /// assignment operator
-    ModificationsDB & operator=(const ModificationsDB & aa);
+    ModificationsDB & operator=(const ModificationsDB& aa);
     //@}
   };
 }
