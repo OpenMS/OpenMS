18b556fed14d477cce8daf5cc7f5bbb234907e10
timosachsenberg <sachsenb@abinode06.Informatik.Uni-Tuebingen.De>
2016-08-16 21:13:42 +0200

    [TEST] more CDASH inactive maintainer fixes

--- src/tests/class_tests/openms/source/FeatureFindingMetabo_test.cpp
+++ src/tests/class_tests/openms/source/FeatureFindingMetabo_test.cpp
@@ -28,7 +28,7 @@
 // ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 // 
 // --------------------------------------------------------------------------
-// $Maintainer: Erhan Kenar$
+// $Maintainer: Timo Sachsenberg$
 // $Authors: Erhan Kenar$
 // --------------------------------------------------------------------------
 
