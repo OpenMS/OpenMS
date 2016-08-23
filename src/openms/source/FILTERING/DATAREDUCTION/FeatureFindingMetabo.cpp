4e11067933dfeee5029ff4a1e44fe7a9bb8a97fa
Chris Bielow <chris.bielow@fu-berlin.de>
2016-08-23 09:12:40 +0200

    [FIX] move quantification parameter from FF-Metabo into MTD (where it belongs); As a side effect, TOPP-MTD also gets the option to quantify via either AREA (default) or MEDIAN (for direct injection); Quantification values of TOPP_MTD are now consistent with TOPP_FF-Metabo (since both use trace.getIntensity() now).

--- src/openms/source/FILTERING/DATAREDUCTION/FeatureFindingMetabo.cpp
+++ src/openms/source/FILTERING/DATAREDUCTION/FeatureFindingMetabo.cpp
@@ -243,8 +243,6 @@ namespace OpenMS
   FeatureFindingMetabo::FeatureFindingMetabo() :
     DefaultParamHandler("FeatureFindingMetabo"), ProgressLogger()
   {
-    defaults_.setValue("quant_method", String(MassTrace::names_of_quantmethod[0]), "Method of quantification for mass traces. For LC data 'area' is recommended, 'median' for direct injection data.");
-    defaults_.setValidStrings("quant_method", std::vector<String>(MassTrace::names_of_quantmethod, MassTrace::names_of_quantmethod +(int)MassTrace::SIZE_OF_MT_QUANTMETHOD));
     defaults_.setValue("local_rt_range", 10.0, "RT range where to look for coeluting mass traces", ListUtils::create<String>("advanced")); // 5.0
     defaults_.setValue("local_mz_range", 6.5, "MZ range where to look for isotopic mass traces", ListUtils::create<String>("advanced")); // 6.5
     defaults_.setValue("charge_lower_bound", 1, "Lowest charge state to consider"); // 1
@@ -723,18 +721,7 @@ namespace OpenMS
     this->startProgress(0, input_mtraces.size(), "assembling mass traces to features");
 
     // *********************************************************** //
-    // Step 1 configure quantification method
-    // *********************************************************** //
-    MassTrace::MT_QUANTMETHOD method = MassTrace::getQuantMethod((String)param_.getValue("quant_method"));
-    for (std::vector<MassTrace>::iterator it = input_mtraces.begin();
-      it != input_mtraces.end();
-      ++it)
-    {
-      it->setQuantMethod(method);
-    }
-
-    // *********************************************************** //
-    // Step 2 initialize SVM model for isotope ratio filtering
+    // Step 1 initialize SVM model for isotope ratio filtering
     // *********************************************************** //
     if (isotope_filtering_model_ == "metabolites (2% RMS)")
     {
@@ -754,7 +741,7 @@ namespace OpenMS
     }
 
     // *********************************************************** //
-    // Step 3 Iterate through all mass traces to find likely matches 
+    // Step 2 Iterate through all mass traces to find likely matches 
     // and generate isotopic / charge hypotheses
     // *********************************************************** //
     std::vector<FeatureHypothesis> feat_hypos;
@@ -808,7 +795,7 @@ namespace OpenMS
 #endif
 
     // *********************************************************** //
-    // Step 4 Iterate through all hypotheses, starting with the highest 
+    // Step 3 Iterate through all hypotheses, starting with the highest 
     // scoring one. Accept them if they do not contain traces that have 
     // already been used by a higher scoring hypothesis.
     // *********************************************************** //
