31e66a60416712b9f5ca0caf2c4eeabee3ac6e72
Chris Bielow <chris.bielow@fu-berlin.de>
2016-10-24 19:22:28 +0200

    [FIX] InternalCalibration: much better error descriptions if Rscript executable is not found or broken.

--- src/openms/include/OpenMS/SYSTEM/RWrapper.h
+++ src/openms/include/OpenMS/SYSTEM/RWrapper.h
@@ -86,8 +86,8 @@ public:
       @brief Run an R script with certain arguments on the command line
 
       The following checks are done before running the script:
-         1) The script_file is searched in 'OpenMS/share/SCRIPTS' (see findScript()).
-         2) 'Rscript' executable is searched (see findR())
+         1) [optional] 'Rscript' executable is searched (see findR() -- set @p find_R to true)
+         2) The script_file is searched in 'OpenMS/share/SCRIPTS' (see findScript()).
          3) The script is run as $ Rscript <path/to/script> <arg1> <arg2> ...
       
       If any of the above steps fail, an error message is printed and false is returned.
@@ -97,10 +97,12 @@ public:
 
       @param script_file Filename of the R script
       @param cmd_args Command line arguments to the script
+      @param find_R Run findR()? May be skipped if runScript() is run repeatedly
+      @param verbose Print status information; also passed internally to findR() and findScript().
       @return Success status
 
     */
-    static bool runScript(const String& script_file, const QStringList& cmd_args);
+    static bool runScript(const String& script_file, const QStringList& cmd_args, bool find_R = false, bool verbose = true);
 
   };
 
