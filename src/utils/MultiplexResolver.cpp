c5f04ba46a0f0e5e227dbb95bb0e5f397a0ecfad
lars20070 <l.nilse@dunelm.org.uk>
2016-12-28 11:40:03 +0100

    [INTERNAL] correct for-loop syntax

--- src/utils/MultiplexResolver.cpp
+++ src/utils/MultiplexResolver.cpp
@@ -406,7 +406,7 @@ private:
     std::vector<bool>::const_iterator it_delta_mass_matched;
     ConsensusFeature::HandleSetType::const_iterator it_feature;
     for (it_mass_shift = pattern.begin(), it_delta_mass_matched = delta_mass_matched.begin(), it_feature = consensus.getFeatures().begin();
-         it_mass_shift != pattern.end(), it_delta_mass_matched != delta_mass_matched.end();
+         it_mass_shift != pattern.end() && it_delta_mass_matched != delta_mass_matched.end();
          ++it_mass_shift, ++it_delta_mass_matched)
     {
       
