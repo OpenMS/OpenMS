f5c5adb94a5925ade05272ee5ea11fc89a2ef932
Hannes Roest <hroest@stanford.edu>
2017-03-15 13:35:02 -0700

    [FIX] bound check operator []

--- src/pyOpenMS/addons/StringDataArray.pyx
+++ src/pyOpenMS/addons/StringDataArray.pyx
@@ -6,6 +6,9 @@
         assert isinstance(in_0, (int, long)), 'arg in_0 wrong type'
     
         cdef long _idx = (<int>in_0)
+        if _idx >= self.inst.get().size():
+            raise IndexError("invalid index %d" % _idx)
+
         cdef _String _r = deref(self.inst.get())[(<int>in_0)]
         py_result = _cast_const_away(<char*>_r.c_str())
         return py_result
@@ -15,6 +18,9 @@
         assert (isinstance(value, str) or isinstance(value, unicode) or isinstance(value, bytes) or isinstance(value, String)), 'arg value wrong type'
 
         cdef long _idx = (<int>key)
+        if _idx >= self.inst.get().size():
+            raise IndexError("invalid index %d" % _idx)
+
         cdef shared_ptr[_String] _s = convString(value)
         deref(self.inst.get())[(<int>key)] = deref(_s.get())
 
