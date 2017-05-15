f592d520f75d1e78980f7d6532c01713cea97b2c
enetz <eugen.netz@gmail.com>
2017-05-15 16:55:12 +0200

    [DOC] improved documentation of TSG for XL-MS

--- src/openms/include/OpenMS/CHEMISTRY/TheoreticalSpectrumGeneratorXLMS.h
+++ src/openms/include/OpenMS/CHEMISTRY/TheoreticalSpectrumGeneratorXLMS.h
@@ -47,7 +47,7 @@ namespace OpenMS
   class AASequence;
 
   /**
-      @brief Generates theoretical spectra for cross-linked peptides with various options
+      @brief Generates theoretical spectra for cross-linked peptides
 
   @htmlinclude OpenMS_TheoreticalSpectrumGeneratorXLMS.parameters
 
@@ -56,58 +56,125 @@ namespace OpenMS
   class OPENMS_DLLAPI TheoreticalSpectrumGeneratorXLMS :
     public DefaultParamHandler
   {
-public:
+    public:
 
-    /** @name Constructors and Destructors
-    */
-    //@{
-    /// default constructor
-    TheoreticalSpectrumGeneratorXLMS();
+      /** @name Constructors and Destructors
+      */
+      //@{
+      /// default constructor
+      TheoreticalSpectrumGeneratorXLMS();
 
-    /// copy constructor
-    TheoreticalSpectrumGeneratorXLMS(const TheoreticalSpectrumGeneratorXLMS & source);
+      /// copy constructor
+      TheoreticalSpectrumGeneratorXLMS(const TheoreticalSpectrumGeneratorXLMS & source);
 
-    /// destructor
-    virtual ~TheoreticalSpectrumGeneratorXLMS();
-    //@}
+      /// destructor
+      virtual ~TheoreticalSpectrumGeneratorXLMS();
+      //@}
 
-    /// assignment operator
-    TheoreticalSpectrumGeneratorXLMS & operator=(const TheoreticalSpectrumGeneratorXLMS & tsg);
+      /// assignment operator
+      TheoreticalSpectrumGeneratorXLMS & operator=(const TheoreticalSpectrumGeneratorXLMS & tsg);
 
-    /** @name Acessors
-     */
-    //@{
-    /// returns a spectrum with b and y peaks
-    /// TODO crashes, if called without generating metainfo
-    virtual void getCommonIonSpectrum(PeakSpectrum & spectrum, AASequence peptide, Size link_pos, bool frag_alpha, int charge = 1, Size link_pos_2 = 0) const;
+      /**
+       * @brief Generates fragment ions not containing the cross-linker for one peptide.
 
-    /// adds peaks to a spectrum of the given ion-type, peptide, charge, and intensity
-    virtual void addCommonPeaks(PeakSpectrum & spectrum, DataArrays::IntegerDataArray & integer_array, DataArrays::StringDataArray & string_array, AASequence peptide, Size link_pos, bool frag_alpha, Residue::ResidueType res_type, int charge = 1, Size link_pos_2 = 0) const;
+        B-ions are generated from the beginning of the peptide up to the first linked position,
+        y-ions are generated from the second linked position up the end of the peptide.
+        If link_pos_2 is 0, a mono-link or cross-link is assumed and the second position is the same as the first position.
+        For a loop-link two different positions can be set and link_pos_2 must be larger than link_pos.
+        The generated ion types and other additional settings are determined by the tool parameters.
 
-    virtual void getXLinkIonSpectrum(PeakSpectrum & spectrum, AASequence peptide, Size link_pos, double precursor_mass, bool frag_alpha, int mincharge, int maxcharge, Size link_pos_2 = 0) const;
+       * @param spectrum The spectrum to which the new peaks are added. Does not have to be empty, the generated peaks will be pushed onto it.
+       * @param peptide The peptide to fragment
+       * @param link_pos The position of the cross-linker on the given peptide
+       * @param frag_alpha True, if the fragmented peptide is the Alpha peptide. Used for ion-name annotation.
+       * @param charge The maximal charge of the ions
+       * @param link_pos_2 A second position for the linker, in case it is a loop link
+       */
+      virtual void getCommonIonSpectrum(PeakSpectrum & spectrum, AASequence peptide, Size link_pos, bool frag_alpha, int charge = 1, Size link_pos_2 = 0) const;
 
-    // TODO
-    virtual void addXLinkIonPeaks(PeakSpectrum& spectrum, DataArrays::IntegerDataArray & integer_array, DataArrays::StringDataArray & string_array, AASequence peptide, Size link_pos, double precursor_mass, bool frag_alpha, Residue::ResidueType res_type, int charge, Size link_pos_2 = 0) const;
+      /**
+       * @brief Generates fragment ions containing the cross-linker for one peptide.
+       *
+          B-ions are generated from the first linked position up to the end of the peptide,
+          y-ions are generated from the beginning of the peptide up to the second linked position.
+          If link_pos_2 is 0, a mono-link or cross-link is assumed and the second position is the same as the first position.
+          For a loop-link two different positions can be set and link_pos_2 must be larger than link_pos.
+          Since in the case of a cross-link a whole second peptide is attached to the other side of the cross-link,
+          a precursor mass for the two peptides and the linker is needed.
+          In the case of a loop link the precursor mass is the mass of the only peptide and the linker.
+          The generated ion types and other additional settings are determined by the tool parameters.
 
-    /// overwrite
-    void updateMembers_();
+       * @param spectrum The spectrum to which the new peaks are added. Does not have to be empty, the generated peaks will be pushed onto it.
+       * @param peptide The peptide to fragment
+       * @param link_pos The position of the cross-linker on the given peptide
+       * @param precursor_mass The mass of the whole cross-link candidate or the precursor mass of the experimental MS2 spectrum.
+       * @param frag_alpha True, if the fragmented peptide is the Alpha peptide. Used for ion-name annotation.
+       * @param mincharge The minimal charge of the ions
+       * @param maxcharge The maximal charge of the ions
+       * @param link_pos_2 A second position for the linker, in case it is a loop link
+       */
+      virtual void getXLinkIonSpectrum(PeakSpectrum & spectrum, AASequence peptide, Size link_pos, double precursor_mass, bool frag_alpha, int mincharge, int maxcharge, Size link_pos_2 = 0) const;
 
-    //@}
+      /// overwrite
+      void updateMembers_();
 
     protected:
-//      /// helper to add an isotope cluster to a spectrum
-//      void addIsotopeCluster_(PeakSpectrum & spectrum, const AASequence ion, const AASequence other_peptide, double cross_linker_mass, Residue::ResidueType res_type, Int charge, double intensity, String ion_type) const;
 
-      /// helper to add a single peak to a spectrum
-      void addPeak_(PeakSpectrum & spectrum, DataArrays::IntegerDataArray & integer_array, DataArrays::StringDataArray & string_array, double pos, double intensity, Residue::ResidueType res_type, Size ion_index, int charge, String ion_type) const;
+      /**
+       * @brief Adds cross-link-less ions of a specific ion type and charge to a spectrum and adds ion name and charge annotations to the DataArrays
+       * @param spectrum The spectrum to which the new peaks are added. Does not have to be empty, the generated peaks will be pushed onto it.
+       * @param charges A DataArray collecting the charges of the added peaks
+       * @param ion_names A DataArray collecting the ion names of the added peaks
+       * @param peptide The peptide to fragment
+       * @param link_pos The position of the cross-linker on the given peptide
+       * @param frag_alpha True, if the fragmented peptide is the Alpha peptide. Used for ion-name annotation.
+       * @param res_type The ion type of the added peaks
+       * @param charge The charge of the added peaks
+       * @param link_pos_2 A second position for the linker, in case it is a loop link
+       */
+      virtual void addCommonPeaks_(PeakSpectrum & spectrum, DataArrays::IntegerDataArray & charges, DataArrays::StringDataArray & ion_names, AASequence peptide, Size link_pos, bool frag_alpha, Residue::ResidueType res_type, int charge = 1, Size link_pos_2 = 0) const;
+
+      /**
+       * @brief Adds cross-linked ions of a specific ion type and charge to a spectrum and adds ion name and charge annotations to the DataArrays
+       * @param spectrum The spectrum to which the new peaks are added. Does not have to be empty, the generated peaks will be pushed onto it.
+       * @param charges A DataArray collecting the charges of the added peaks
+       * @param ion_names A DataArray collecting the ion names of the added peaks
+       * @param peptide The peptide to fragment
+       * @param link_pos The position of the cross-linker on the given peptide
+       * @param precursor_mass The mass of the whole cross-link candidate or the precursor mass of the experimental MS2 spectrum.
+       * @param frag_alpha True, if the fragmented peptide is the Alpha peptide. Used for ion-name annotation.
+       * @param res_type The ion type of the added peaks
+       * @param charge The charge of the added peaks
+       * @param link_pos_2 A second position for the linker, in case it is a loop link
+       */
+      virtual void addXLinkIonPeaks_(PeakSpectrum& spectrum, DataArrays::IntegerDataArray & charges, DataArrays::StringDataArray & ion_names, AASequence peptide, Size link_pos, double precursor_mass, bool frag_alpha, Residue::ResidueType res_type, int charge, Size link_pos_2 = 0) const;
+
+
+      /**
+       * @brief Adds a single peak to a spectrum and its charge and ion name to the given DataArrays
+
+          The ion_type is a string in this form: "alpha|xi",
+          the first word can be either "alpha" or "beta" and indicates the fragmented peptide,
+          the two letters at the end are either "ci" or "xi" for common ion or cross-linked ion.
+
+       * @param spectrum The spectrum to which the new peak is added
+       * @param charges A DataArray collecting the charges of the added peaks
+       * @param ion_names A DataArray collecting the ion names of the added peaks
+       * @param pos
+       * @param intensity
+       * @param res_type The ion type of the added peak
+       * @param ion_index The index of the ion (fragmentation position)
+       * @param charge The charge of the ion
+       * @param ion_type Another cross-linking specific ion-type
+       */
+      void addPeak_(PeakSpectrum & spectrum, DataArrays::IntegerDataArray & charges, DataArrays::StringDataArray & ion_names, double pos, double intensity, Residue::ResidueType res_type, Size ion_index, int charge, String ion_type) const;
 
       // TODO copied from normal TSG, but it is protected over there. Move it to Residue class maybe?
       /// helper for mapping residue type to letter
       char residueTypeToIonLetter_(Residue::ResidueType res_type) const;
 
-//      /// helper to add full neutral loss ladders
+//      /// TODO helper to add full neutral loss ladders
 //      //void addLosses_(RichPeakSpectrum & spectrum, const AASequence & ion, double intensity, Residue::ResidueType res_type, int charge, bool fragment_alpha_chain) const;
-//      void addXLinkLosses_(PeakSpectrum & spectrum, const AASequence & ion, const AASequence & second_peptide, double cross_linker_mass, Residue::ResidueType res_type, int charge, double intensity, String ion_type) const;
 
       bool add_b_ions_;
       bool add_y_ions_;
