3412b579b740795c894f075a3ef41feb48547ab1
enetz <eugen.netz@gmail.com>
2017-05-17 17:42:46 +0200

    [FIX] updated OPXL name in ToolHandler

--- src/openms/source/APPLICATIONS/ToolHandler.cpp
+++ src/openms/source/APPLICATIONS/ToolHandler.cpp
@@ -207,8 +207,8 @@ namespace OpenMS
     util_map["MSSimulator"] = Internal::ToolDescription("MSSimulator", util_category);
     util_map["MultiplexResolver"] = Internal::ToolDescription("MultiplexResolver", util_category);
     util_map["MzMLSplitter"] = Internal::ToolDescription("MzMLSplitter", util_category);
-    util_map["OpenProXL"] = Internal::ToolDescription("OpenProXL", util_category);
-    util_map["OpenProXLLF"] = Internal::ToolDescription("OpenProXLLF", util_category);
+    util_map["OpenPepXL"] = Internal::ToolDescription("OpenPepXL", util_category);
+    util_map["OpenPepXLLF"] = Internal::ToolDescription("OpenPepXLLF", util_category);
     util_map["OpenSwathWorkflow"] = Internal::ToolDescription("OpenSwathWorkflow", util_category);
     util_map["OpenSwathRewriteToFeatureXML"] = Internal::ToolDescription("OpenSwathRewriteToFeatureXML", "Targeted Experiments");
     util_map["OpenSwathFileSplitter"] = Internal::ToolDescription("OpenSwathFileSplitter", "Targeted Experiments");
