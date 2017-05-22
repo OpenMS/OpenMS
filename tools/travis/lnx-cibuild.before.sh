ba73c6305d75ce40f7aa2d43a702aae9592f96eb
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2017-05-22 15:10:04 +0200

    next try to get a new cmake

--- tools/travis/lnx-cibuild.before.sh
+++ tools/travis/lnx-cibuild.before.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+mkdir cmake_tmp
+cd cmake_tmp
+wget http://www.cmake.org/files/v3.2/cmake-3.2.2-Linux-x86_64.tar.gz
+tar xf cmake-3.2.2-Linux-x86_64.tar.gz
+sudo cp -r cmake-3.2.2-Linux-x86_64/* /usr
+
 function build_contrib {
   cmake . -DBUILD_TYPE=$1
 
