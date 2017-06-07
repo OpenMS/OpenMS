ce8d62c80a04ac26cfd0d6b7cedcc848a148e813
timosachsenberg <sachsenb@abinode05.Informatik.Uni-Tuebingen.De>
2017-06-07 13:27:06 +0200

    [FIX] sort by identifications by map index after idmapping

--- src/openms/source/KERNEL/ConsensusMap.cpp
+++ src/openms/source/KERNEL/ConsensusMap.cpp
@@ -311,6 +311,36 @@ namespace OpenMS
     std::stable_sort(Base::begin(), Base::end(), ConsensusFeature::MapsLess());
   }
 
+  void ConsensusMap::sortPeptideIdentificationsByMapIndex()
+  {
+    // lambda predicate
+    auto mapIndexLess = [] (const PeptideIdentification & a, const PeptideIdentification & b) -> bool
+    {
+      bool has_a = a.metaValueExists("map_index");
+      bool has_b = b.metaValueExists("map_index");
+
+      // moves IDs without meta value to end
+      if (has_a && !has_b) return true;
+      if (!has_a && has_b) return false;
+
+      // both have map index annotated
+      if (has_a && has_b)
+      {
+        return a.getMetaValue("map_index") < b.getMetaValue("map_index");
+      }
+
+      // no map index annotated in both
+      return false;
+    };
+
+    std::transform(begin(), end(), begin(),
+      [mapIndexLess](ConsensusFeature c) 
+      { 
+        vector<PeptideIdentification> & pids = c.getPeptideIdentifications();
+        return sort(pids.begin(), pids.end(), mapIndexLess); 
+      });
+  }
+
   void ConsensusMap::swap(ConsensusMap& from)
   {
     ConsensusMap tmp;
