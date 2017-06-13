9fb370a7e47630be28dbde494681e1398d8a1d1d
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2017-06-13 15:34:44 +0200

    Fixing lnx package type scripts

--- cmake/package_deb.cmake
+++ cmake/package_deb.cmake
@@ -1,10 +1,4 @@
-set(INSTALL_FORCE_DOC TRUE) ## force documentation to be present
-include(cmake/install_common.cmake)
-
 set(CPACK_DEBIAN_PACKAGE_MAINTAINER "bielow@mi.fu-berlin.de")
 set(CPACK_GENERATOR "DEB")
 set(CPACK_COMPONENTS_ALL applications library share)
 set(CPACK_DEBIAN_PACKAGE_DEPENDS "libqt4-core|libqt4core, libqt4-gui|libqt4gui, libqt4-network, libqt4-opengl, libqt4-sql, libqt4-svg, libqt4-webkit, libqt4-xmlpatterns")
-
-## should be the last include
-include(CPack)
