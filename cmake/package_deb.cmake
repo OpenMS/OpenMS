3d91ece9d802675d7c7c5773f00b13edd3af3ad0
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2017-06-16 18:30:41 +0200

    Try autodep. finding for deb. Rename folder on dmg.

--- cmake/package_deb.cmake
+++ cmake/package_deb.cmake
@@ -1,5 +1,6 @@
 set(CPACK_DEBIAN_PACKAGE_MAINTAINER "OpenMS developers <open-ms-general@lists.sourceforge.net>")
 set(CPACK_GENERATOR "DEB")
+
 ## CPack issues when building the package.
 ## https://bugs.launchpad.net/ubuntu/+source/cmake/+bug/972419
 ## https://ubuntuforums.org/showthread.php?t=2316865
@@ -7,9 +8,19 @@ set(CPACK_GENERATOR "DEB")
 ## Following needs CMake 3.7+ just install from cmake.org
 set(CPACK_DEBIAN_ARCHIVE_TYPE "gnutar")
 
+## Not sure if we should ship them. Could mess up a system slighlty, when installed system wide?
+#INCLUDE(InstallRequiredSystemLibraries)
+
+## Try autogeneration of dependencies
+set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
+
 ## TODO also install headers? make a dev package configuration?
 set(CPACK_COMPONENTS_ALL applications library share ${THIRDPARTY_COMPONENT_GROUP})
-set(CPACK_DEBIAN_PACKAGE_DEPENDS "libsqlite3-dev, libxerces-c-dev (>= 3.1.1), libeigen3-dev, libwildmagic-dev, libboost-dev (>= 1.54.0), libboost-iostreams-dev (>= 1.54.0), libboost-date-time-dev (>= 1.54.0), libboost-math-dev (>= 1.54.0), seqan-dev (>= 1.4.1), libsvm-dev (>= 3.12), libglpk-dev (>= 4.52.1), zlib1g-dev (>= 1.2.7), libbz2-dev (>= 1.0.6), libqt4-dev (>= 4.8.2), libqt4-opengl-dev (>= 4.8.2), libqtwebkit-dev (>= 2.2.1), coinor-libcoinutils-dev (>= 2.6.4)")
+
+## TODO we only need to put dependencies on shared libs. But depends on what is found on build machine.
+## We should probably use a full system-shared-libs-only machine for building.
+#set(CPACK_DEBIAN_PACKAGE_DEPENDS "libsqlite3-dev, libxerces-c-dev (>= 3.1.1), libeigen3-dev, libwildmagic-dev, libboost-dev (>= 1.54.0), libboost-iostreams-dev (>= 1.54.0), libboost-date-time-dev (>= 1.54.0), libboost-math-dev (>= 1.54.0), libsvm-dev (>= 3.12), libglpk-dev (>= 4.52.1), zlib1g-dev (>= 1.2.7), libbz2-dev (>= 1.0.6), libqt4-dev (>= 4.8.2), libqt4-opengl-dev (>= 4.8.2), libqtwebkit-dev (>= 2.2.1), coinor-libcoinutils-dev (>= 2.6.4)")
+
 SET(CPACK_DEBIAN_PACKAGE_PRIORITY "optional")
 SET(CPACK_DEBIAN_PACKAGE_SECTION "science")
 SET(CPACK_PACKAGE_DESCRIPTION_SUMMARY "package for LC/MS data management and analysis")
@@ -25,5 +36,7 @@ SET(CPACK_PACKAGE_DESCRIPTION "
  for a specific problem."
  )
 
-# For source packages
-#set(CPACK_DEBIAN_PACKAGE_BUILDS_DEPENDS "debhelper (>= 9), dpkg-dev (>= 1.16.1~), cmake (>= 2.6.3), imagemagick, doxygen (>= 1.8.1.2), graphviz, texlive-extra-utils, texlive-latex-extra, latex-xcolor, texlive-font-utils, ghostscript, texlive-fonts-recommended"
+## TODO make postinstall script that sets OPENMS_DATA_PATH
+
+# For source packages add build dependencies. Not used and not tested.
+#set(CPACK_DEBIAN_PACKAGE_BUILDS_DEPENDS "debhelper (>= 9), dpkg-dev (>= 1.16.1~), cmake (>= 2.6.3), imagemagick, doxygen (>= 1.8.1.2), graphviz, seqan-dev (>= 1.4.1), texlive-extra-utils, texlive-latex-extra, latex-xcolor, texlive-font-utils, ghostscript, texlive-fonts-recommended"
