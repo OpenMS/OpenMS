822c7bfe47580f3643dc6da9a7120131bae78b8e
Timo Sachsenberg <timosachsenberg@users.noreply.github.com>
2017-07-05 17:05:26 +0200

    Update OpenSwathWorkflow.cpp
    just a small test to trigger codacy

--- src/utils/OpenSwathWorkflow.cpp
+++ src/utils/OpenSwathWorkflow.cpp
@@ -843,8 +843,10 @@ protected:
 
     for (Size i = 0; i < swath_maps.size(); i++)
     {
-      LOG_DEBUG << "Found swath map " << i << " with lower " << swath_maps[i].lower
-        << " and upper " << swath_maps[i].upper << " and " << swath_maps[i].sptr->getNrSpectra()
+      LOG_DEBUG << "Found swath map " << i 
+        << " with lower " << swath_maps[i].lower
+        << " and upper " << swath_maps[i].upper 
+        << " and " << swath_maps[i].sptr->getNrSpectra()
         << " spectra." << std::endl;
     }
 
