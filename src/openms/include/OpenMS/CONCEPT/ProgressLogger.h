adc33636f15dbabdba4ea181547c55c721b0d05f
Timo Sachsenberg <timosachsenberg@users.noreply.github.com>
2017-07-07 08:40:05 +0200

    Update ProgressLogger.h

--- src/openms/include/OpenMS/CONCEPT/ProgressLogger.h
+++ src/openms/include/OpenMS/CONCEPT/ProgressLogger.h
@@ -81,7 +81,7 @@ public:
     class OPENMS_DLLAPI ProgressLoggerImpl
     {
 public:
-      virtual void startProgress(const SignedSize begin, const SignedSize end, const String& label, const int current_recursion_depth) const = 0;
+      virtual void startProgress(const SignedSize begin, const SignedSize end, const String label, const int current_recursion_depth) const = 0;
       virtual void setProgress(const SignedSize value, const int current_recursion_depth) const = 0;
       virtual void endProgress(const int current_recursion_depth) const = 0;
 
