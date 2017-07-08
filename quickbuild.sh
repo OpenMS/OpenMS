66706d74f8596ce1edc0ad6af2cc85457c670746
Hannes Roest <hroest@stanford.edu>
2017-07-08 14:45:21 -0700

    [BUILD] fix quickbuild

--- quickbuild.sh
+++ quickbuild.sh
@@ -6,24 +6,26 @@ set -e
 if [ ! -d "contrib" ] ; then
   git clone https://github.com/OpenMS/contrib.git
 fi
-mkdir -p contrib-build
-cd contrib-build
 
-CONTRIB_PATH=`pwd`
-cmake -DBUILD_TYPE=ALL ../contrib
+# set contrib absolute path for configure
+CONTRIB_PATH=`pwd`/contrib-build
+
+if [ ! -d "contrib-build" ] ; then
+  mkdir -p contrib-build
+  cd contrib-build
+  cmake -DBUILD_TYPE=ALL ../contrib
+  cd ..
+fi
 
 ###################################
 # 2. build OpenMS
 #  - default builds all but first argument is passed to make
-cd ..
 mkdir -p openms-build
 cd openms-build
 
-# needs absolute path!
-cmake -DOPENMS_CONTRIB_LIBS="$CONTRIB_PATH" ../
-if [ -z "$1" ]; then
-  make $1
-else
-  make
-fi
+# contrib path needs to be an absolute path!
+cmake -DOPENMS_CONTRIB_LIBS="$CONTRIB_PATH" -DBOOST_USE_STATIC=On ../
+make $@
+
+cd ..
 
