ed7952ac9bd86c49d04ae2849646653e75fa24f0
Hannes Roest <hroest@stanford.edu>
2017-07-08 12:43:56 -0700

    config option for write meta

--- src/openms/source/FORMAT/SqMassFile.cpp
+++ src/openms/source/FORMAT/SqMassFile.cpp
@@ -46,14 +46,14 @@ namespace OpenMS
   void SqMassFile::load(const String& filename, MapType& map)
   {
     OpenMS::Internal::MzMLSqliteHandler sql_mass(filename);
-    sql_mass.setConfig(config_.use_lossy_numpress, config_.linear_fp_mass_acc);
+    sql_mass.setConfig(config_.write_full_meta, config_.use_lossy_numpress, config_.linear_fp_mass_acc);
     sql_mass.readExperiment(map);
   }
 
   void SqMassFile::store(const String& filename, MapType& map)
   {
     OpenMS::Internal::MzMLSqliteHandler sql_mass(filename);
-    sql_mass.setConfig(config_.use_lossy_numpress, config_.linear_fp_mass_acc);
+    sql_mass.setConfig(config_.write_full_meta, config_.use_lossy_numpress, config_.linear_fp_mass_acc);
     sql_mass.createTables();
     sql_mass.writeExperiment(map);
   }
