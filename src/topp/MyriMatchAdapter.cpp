dc82a7b13ee76aca7a4e9e827ad6d87a561642ee
timosachsenberg <sachsenb@abinode05.Informatik.Uni-Tuebingen.De>
2017-07-26 16:24:42 +0200

    [FIX, PARAM] fixed comets and OMSSAs default tolerance.

--- src/topp/MyriMatchAdapter.cpp
+++ src/topp/MyriMatchAdapter.cpp
@@ -223,9 +223,9 @@ protected:
     setValidFormats_("in", ListUtils::create<String>("mzML"));
     registerOutputFile_("out", "<file>", "", "Output file");
     setValidFormats_("out", ListUtils::create<String>("idXML"));
-    registerDoubleOption_("precursor_mass_tolerance", "<tolerance>", 1.5, "Precursor mono mass tolerance.", false);
+    registerDoubleOption_("precursor_mass_tolerance", "<tolerance>", 10.0, "Precursor mono mass tolerance.", false);
 
-    registerStringOption_("precursor_mass_tolerance_unit", "<unit>", "Da", "Unit to be used for precursor mass tolerance.", false);
+    registerStringOption_("precursor_mass_tolerance_unit", "<unit>", "ppm", "Unit to be used for precursor mass tolerance.", false);
     setValidStrings_("precursor_mass_tolerance_unit", ListUtils::create<String>("Da,ppm"));
 
     registerFlag_("precursor_mass_tolerance_avg", "If this flag is set, the average mass is used in the precursor mass tolerance.");
