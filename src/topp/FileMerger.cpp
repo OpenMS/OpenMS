6800ad987350044903fdd2ba7c44b885b4c5863f
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2017-07-27 20:33:04 +0200

    [FIX] Also annotate first file when merging cXML

--- src/topp/FileMerger.cpp
+++ src/topp/FileMerger.cpp
@@ -245,8 +245,18 @@ protected:
     {
       ConsensusMap out;
       ConsensusXMLFile fh;
+      // load the metadata from the first file
       fh.load(file_list[0], out);
-      // skip first file
+      // but annotate the origins
+      if (annotate_file_origin)
+      {
+        for (ConsensusMap::iterator it = out.begin(); it != out.end(); ++it)
+        {
+          it->setMetaValue("file_origin", DataValue(file_list[0]));
+        }
+      }
+
+      // skip first file for adding
       for (Size i = 1; i < file_list.size(); ++i)
       {
         ConsensusMap map;
