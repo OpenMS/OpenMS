1fde5054cbf5c4d345d312b1b480c26998116ba9
Hannes Roest <hroest@stanford.edu>
2017-08-01 17:41:44 -0700

    [TEST] cleanup, remove (for now)

--- src/pyOpenMS/tests/unittests/test_Isobaric_Quantitation.py
+++ src/pyOpenMS/tests/unittests/test_Isobaric_Quantitation.py
@@ -54,24 +54,24 @@ class TestIsobaricChannelExtractor(unittest.TestCase):
             # Will not work with empty map
             # inst.extractChannels(exp, map1)
 
-class TestIsobaricNormalizer(unittest.TestCase):
-
-    def setUp(self):
-        general_setup(self)
-
-    def testInit(self):
-        assert pyopenms.IsobaricNormalizer(self.eightplex) is not None
-        assert pyopenms.IsobaricNormalizer(self.fourplex) is not None
-        assert pyopenms.IsobaricNormalizer(self.tmt) is not None
-
-    def testFunction(self):
-
-        for method in [self.eightplex, self.fourplex, self.tmt]:
-            inst = pyopenms.IsobaricNormalizer(method)
-            map1 = pyopenms.ConsensusMap()
-            assert inst.normalize is not None
-            inst.normalize(map1)
-
+# class TestIsobaricNormalizer(unittest.TestCase):
+# 
+#     def setUp(self):
+#         general_setup(self)
+# 
+#     def testInit(self):
+#         assert pyopenms.IsobaricNormalizer(self.eightplex) is not None
+#         assert pyopenms.IsobaricNormalizer(self.fourplex) is not None
+#         assert pyopenms.IsobaricNormalizer(self.tmt) is not None
+# 
+#     def testFunction(self):
+# 
+#         for method in [self.eightplex, self.fourplex, self.tmt]:
+#             inst = pyopenms.IsobaricNormalizer(method)
+#             map1 = pyopenms.ConsensusMap()
+#             assert inst.normalize is not None
+#             inst.normalize(map1)
+# 
 #class TestIsobaricIsotopeCorrector(unittest.TestCase):
 
 #    def setUp(self):
