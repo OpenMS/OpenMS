3ea5d4d530ceaef0afc89e2c23be1e9af9ee3f50
Hannes Roest <hroest@stanford.edu>
2017-08-03 12:17:35 -0700

    fix patch

--- tools/travis/lnx-cibuild.before.sh
+++ tools/travis/lnx-cibuild.before.sh
@@ -22,8 +22,8 @@ if [ "${PYOPENMS}" = "ON" ]; then
 
   # small patch to accelerate build
   pwd
-  ls OpenMS/OpenMS/src/pyOpenMS/create_cpp_extension.py
-  sed -i 's/import time/import time\nPY_NUM_THREADS=4/g' OpenMS/OpenMS/src/pyOpenMS/create_cpp_extension.py
+  ls src/pyOpenMS/create_cpp_extension.py
+  sed -i 's/import time/import time\nPY_NUM_THREADS=4/g' src/pyOpenMS/create_cpp_extension.py
 
   pip install -U setuptools
   pip install -U pip
