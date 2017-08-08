2efafb63a9bccf9ac2597d512262bcb0c9f7b382
Nikos Patikas <nik.patik@gmail.com>
2017-08-08 13:11:59 +0300

    [FIX] fix for boost 1.64

--- src/openms/include/OpenMS/MATH/STATISTICS/StatisticFunctions.h
+++ src/openms/include/OpenMS/MATH/STATISTICS/StatisticFunctions.h
@@ -38,6 +38,9 @@
 #include <OpenMS/CONCEPT/Exception.h>
 #include <OpenMS/CONCEPT/Types.h>
 
+#if OPENMS_BOOST_VERSION_MINOR >= 64
+#include <boost/serialization/array_wrapper.hpp>
+#endif
 #include <boost/accumulators/accumulators.hpp>
 #include <boost/accumulators/statistics/covariance.hpp>
 #include <boost/accumulators/statistics/mean.hpp>
