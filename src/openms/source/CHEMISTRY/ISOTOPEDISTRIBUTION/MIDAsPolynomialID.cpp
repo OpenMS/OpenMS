e84b72201be978735ad8c40df6c725ae823b5ac4
Nikos Patikas <nik.patik@gmail.com>
2017-08-23 00:40:05 +0300

    [INTERNAL] cleanup MIDAsPolynomialID

--- src/openms/source/CHEMISTRY/ISOTOPEDISTRIBUTION/MIDAsPolynomialID.cpp
+++ src/openms/source/CHEMISTRY/ISOTOPEDISTRIBUTION/MIDAsPolynomialID.cpp
@@ -10,10 +10,24 @@
 
 #include <vector>
 #include <utility>
+
+#define DEBUG
+
 using namespace std;
 namespace OpenMS
 {
-  /* Start of the midas polynomial method */
+
+  void addCounter(CounterSet& c, const double& abundance, const UInt& size, const UInt& N)
+  {
+    double expectation = size * abundance;
+    double var = size * abundance *(1 - abundance);
+    UInt U = expectation + (N * sqrt(1 + var));
+    UInt B = expectation > (N * sqrt(1 + var)) ? ceil(expectation - (N * sqrt(1 + var))) : 0;
+#ifdef DEBUG
+    LOG_INFO << "Added counter with values " << B << " " << U <<endl;
+#endif
+    c.addCounter(B, U);
+  }
 
 
   MIDAsPolynomialID::MIDAsPolynomialID(EmpiricalFormula& formula, double resolution):
@@ -44,7 +58,9 @@ namespace OpenMS
     for(EmpiricalFormula::ConstIterator element = formula_.begin(); element != formula_.end(); ++element)
     {
       el_dist.push_back(generatePolynomial(*(element->first), element->second));
+#ifdef DEBUG
       LOG_INFO << element->first->getName() <<" has " << el_dist.back().size() << " data points " << endl;
+#endif
     }
     Polynomial& T = *(el_dist.begin());
 
@@ -53,62 +69,41 @@ namespace OpenMS
       multiplyPolynomials(T, *pol);
     }
 
-    LOG_INFO << "T after multiplication has " << T.size() <<" elements" << endl;
+#ifdef DEBUG
+    LOG_INFO << "Distributian after multiplication has " << T.size() <<" elements" << endl;
+#endif
 
-    //BZ2_bzWrite ( NULL, NULL, (void*)NULL, NULL );
-
-    LOG_INFO << "RESULTS---------------" << endl;
     double probability = 0;
     for(Polynomial::const_iterator it = T.begin(); it != T.end(); ++it)
     {
-      //LOG_INFO << it->power*mw_resolution << " " << it->probability << endl;
       probability += it->getIntensity();
     }
-    LOG_INFO << "probability sum " << probability <<endl;
 
     sort(T.begin(), T.end(), by_power);
     for(auto& pmember : T)
     {
       pmember.setMZ(pmember.getMZ() * mw_resolution);
     }
-    ContainerType tmp;
-    move(T.begin(), T.end(),back_inserter(tmp));
+    distribution_.clear();
+    move(T.begin(), T.end(), back_inserter(distribution_));
 
-    merge(tmp, 0.0001);
+    merge(distribution_, 0.0001);
 
     LOG_INFO << "Lightest theoretical element " << lighter_isotope << endl;
 
     trimRight(0.0001);
     trimLeft(0.0001);
-    LOG_INFO << "Final distribution has " << distribution_.size() <<endl;
-    for(ContainerType::const_iterator it = distribution_.begin(); it != distribution_.end(); ++it)
-    {
 
-    }
-
-
+#ifdef DEBUG
     LOG_INFO << "Isotope Distribution of " << formula_.toString() << " successfully computed " << endl;
     LOG_INFO << "Isotope Distribution has " << T.size() << " data points " << endl;
-
+#endif
   }
-
-  void addCounter(CounterSet& c, const double& abundance, const UInt& size, const UInt& N)
-  {
-    double expectation = size * abundance;
-    double var = size * abundance *(1 - abundance);
-    UInt U = expectation + (N * sqrt(1 + var));
-    UInt B = expectation > (N * sqrt(1 + var)) ? ceil(expectation - (N * sqrt(1 + var))) : 0;
-    //LOG_INFO << "Added counter with values " << B << " " << U <<endl;
-    c.addCounter(B, U);
-  }
-
-
-
 
   MIDAsPolynomialID::Polynomial MIDAsPolynomialID::generatePolynomial(const Element& p, const SignedSize size)
   {
-    std::vector<unsigned long> base_power;
-    std::vector<double> log_prob;
+    vector<unsigned long> base_power;
+    vector<double> log_prob;
     const IsotopeDistribution::ContainerType& isotope = p.getIsotopeDistribution().getContainer();
     CounterSet c(size);
     Polynomial pol;
@@ -127,7 +122,6 @@ namespace OpenMS
     for(const CounterSet::ContainerType& counters = c.getCounters(); c.hasNext(); ++c)
     {
       Peak1D member;
-      // UInt s = 0;
       member.setMZ(0);
       member.setIntensity(fact_ln(size));
       UInt index = 0;
@@ -146,10 +140,8 @@ namespace OpenMS
       index = 0;
       for(CounterSet::ContainerType::const_iterator iso_count = counters.begin(); iso_count != counters.end(); ++iso_count, ++index)
       {
-        //LOG_INFO << *iso_count <<" "<<base_power[index]<<endl;
         member.setMZ( member.getMZ() + (*iso_count)*base_power[index]);
       }
-      //      LOG_INFO << member.power <<" "<<member.probability<<endl;
 
       pol.push_back(member);
 
@@ -161,11 +153,10 @@ namespace OpenMS
   void MIDAsPolynomialID::multiplyPolynomials(Polynomial& f, Polynomial& g)
   {
 
-    LOG_INFO << "Sorting polynomial" << f.size() << " and " << g.size() <<endl;
+    // sort polynomials to accelerate multiplication loop 
     sort(f.begin(), f.end(), desc_prob);
     sort(g.begin(), g.end(), desc_prob);
 
-    LOG_INFO << "Multiplying polynomial" << f.size() << " and " << g.size() <<endl;
     double min_mass = min_element(g.begin(), g.end(), by_power)->getMZ()
                       + min_element(f.begin(), f.end(), by_power)->getMZ();
     double max_mass = max_element(g.begin(), g.end(), by_power)->getMZ()
