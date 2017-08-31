07e03e19623aac5093c2c0783988c9799f4c5867
Hannes Roest <hroest@stanford.edu>
2017-08-31 10:22:22 -0400

    [FIX] fix pyOpenMS

--- src/pyOpenMS/tests/unittests/test_StandardTypes.py
+++ src/pyOpenMS/tests/unittests/test_StandardTypes.py
@@ -1,6 +1,5 @@
 def test_if_available():
     import pyopenms
     assert pyopenms.PeakMap == pyopenms.MSExperiment
-#   assert pyopenms.Chromatogram == pyopenms.MSChromatogram
     assert pyopenms.PeakSpectrum == pyopenms.MSSpectrum
 
