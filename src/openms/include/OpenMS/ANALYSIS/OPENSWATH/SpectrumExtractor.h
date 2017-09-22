71bd214815044aa8c2fb6d8bac99634347adceec
Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
2017-09-22 13:08:30 +0200

    feat: Add cpp, h, test files and a constructor

---
+++ src/openms/include/OpenMS/ANALYSIS/OPENSWATH/SpectrumExtractor.h
@@ -0,0 +1,66 @@
+// --------------------------------------------------------------------------
+//                   OpenMS -- Open-Source Mass Spectrometry
+// --------------------------------------------------------------------------
+// Copyright The OpenMS Team -- Eberhard Karls University Tuebingen,
+// ETH Zurich, and Freie Universitaet Berlin 2002-2017.
+//
+// This software is released under a three-clause BSD license:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of any author or any participating institution
+//    may be used to endorse or promote products derived from this software
+//    without specific prior written permission.
+// For a full list of authors, refer to the file AUTHORS.
+// --------------------------------------------------------------------------
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL ANY OF THE AUTHORS OR THE CONTRIBUTING
+// INSTITUTIONS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// --------------------------------------------------------------------------
+// $Maintainer: TODO UPDATE HERE $
+// $Authors: TODO UPDATE HERE $
+// --------------------------------------------------------------------------
+
+#ifndef OPENMS_ANALYSIS_OPENSWATH_SPECTRUMEXTRACTOR_H
+#define OPENMS_ANALYSIS_OPENSWATH_SPECTRUMEXTRACTOR_H
+
+#include <OpenMS/config.h> // OPENMS_DLLAPI
+
+namespace OpenMS
+{
+  /**
+  @brief TODO BRIEF EXPLANATION HERE
+  
+  TODO MULTILINE
+  EXPLANATION
+  HERE
+
+  */
+  class OPENMS_DLLAPI SpectrumExtractor
+  {
+
+public:
+    SpectrumExtractor();
+
+private:
+    double rt_window;
+    double min_score;
+    double min_match;
+    double mz_tolerance;
+    double mz_tolerance_units;
+  };
+}
+
+#endif // OPENMS_ANALYSIS_OPENSWATH_SPECTRUMEXTRACTOR_H
+
