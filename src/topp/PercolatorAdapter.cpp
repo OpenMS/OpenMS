e70889a7b58691e7a72fb46873afaa8f488feef3
George Rosenberger <gr2578@cumc.columbia.edu>
2017-10-11 12:35:43 -0400

    [FIX] Remove unavailable parameter

--- src/topp/PercolatorAdapter.cpp
+++ src/topp/PercolatorAdapter.cpp
@@ -257,7 +257,6 @@ protected:
     registerFlag_("post-processing-tdc", "Use target-decoy competition to assign q-values and PEPs.", is_advanced_option);
 
     //OSW/IPF parameters
-    registerIntOption_("ipf_max_peakgroup_rank", "<value>", 1, "OSW/IPF: Assess transitions only for candidate peak groups until maximum peak group rank.", !is_required, is_advanced_option);
     registerDoubleOption_("ipf_max_peakgroup_pep", "<value>", 0.3, "OSW/IPF: Assess transitions only for candidate peak groups until maximum posterior error probability.", !is_required, is_advanced_option);
     registerDoubleOption_("ipf_max_transition_isotope_overlap", "<value>", 0.5, "OSW/IPF: Maximum isotope overlap to consider transitions in IPF.", !is_required, is_advanced_option);
     registerDoubleOption_("ipf_min_transition_sn", "<value>", 0, "OSW/IPF: Minimum log signal-to-noise level to consider transitions in IPF. Set -1 to disable this filter.", !is_required, is_advanced_option);
@@ -656,7 +655,7 @@ protected:
     return EXECUTION_OK;
   }
 
-  ExitCodes readOSWFile_(std::string in_osw, std::string osw_level, std::stringstream& pin_output, int ipf_max_peakgroup_rank, double ipf_max_peakgroup_pep, double ipf_max_transition_isotope_overlap, double ipf_min_transition_sn) {
+  ExitCodes readOSWFile_(std::string in_osw, std::string osw_level, std::stringstream& pin_output, double ipf_max_peakgroup_pep, double ipf_max_transition_isotope_overlap, double ipf_min_transition_sn) {
 
     sqlite3 *db;
     sqlite3_stmt * stmt;
@@ -909,7 +908,6 @@ protected:
     bool peptide_level_fdrs = getFlag_("peptide-level-fdrs");
     bool protein_level_fdrs = getFlag_("protein-level-fdrs");  
 
-    int ipf_max_peakgroup_rank = getIntOption_("ipf_max_peakgroup_rank");
     double ipf_max_peakgroup_pep = getDoubleOption_("ipf_max_peakgroup_pep");
     double ipf_max_transition_isotope_overlap = getDoubleOption_("ipf_max_transition_isotope_overlap");
     double ipf_min_transition_sn = getDoubleOption_("ipf_min_transition_sn");
@@ -1054,7 +1052,7 @@ protected:
       LOG_DEBUG << "Writing percolator input file." << endl;
       TextFile txt;  
       std::stringstream pin_output;
-      readOSWFile_(in_osw, osw_level, pin_output, ipf_max_peakgroup_rank, ipf_max_peakgroup_pep, ipf_max_transition_isotope_overlap, ipf_min_transition_sn);
+      readOSWFile_(in_osw, osw_level, pin_output, ipf_max_peakgroup_pep, ipf_max_transition_isotope_overlap, ipf_min_transition_sn);
       txt << pin_output.str();
       txt.store(pin_file);
     }
