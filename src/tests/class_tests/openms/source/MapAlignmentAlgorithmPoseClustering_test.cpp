da1bedffbe611d49453b833d0e39b4e2b85a1afa
Douglas McCloskey <dmccloskey@users.noreply.github.com>
2017-11-08 09:49:00 +0100

    Feature/tml dataweights (#2746)
    * [FEATURE] added methods for data weighting to TransformationModel and TransformationModelLinear
    
    * [FIX] updated file names
    
    * [FIX] fixed bug in TransformationModelBSpline caused by new TransformationModel getParameters method
    
    * [FIX] fixed bug in ElutionModelFit caused by new TransformationModel getParameters method
    
    * [INTERNAL] added x and y weight parameters to call to getParameters for TransformationModelLinear in file MapAlignmentAlgorithmPoseClustering_test.cpp
    
    * [API] fix to TransformationModel.pxd call to new weightData functions
    
    * [API] fix to TransformationModel.pxd call to new weightData functions
    
    * [TEST] add x and y weight for tests with outputs that use TRAFOXML
    
    * [TEST] add x and y weight for tests with outputs that use TRAFOXML
    
    * [FEATURE] added checkDatumRange method
    
    * [FEATURE] added support for user defined upper and lower x and y data ranges
    
    * [FIX] updated transformationModelLinear call to weightData, return of default parameters, and tests
    
    * [FIX] updated TOPP test output files that call transformationModelLinear
    
    * [FIX] updated TransformationModelBSpline call to getParameters
    
    * [FIX] updated ElutionModelFitter call to getParameters
    
    * [FIX] updated ElutionModelFitter call to getParameters
    
    * [FIX] updated MapAlignmentPoseClustiner?test call to getParameters
    
    * [FIX] added additional tests for evaluate and invert/evaluate
    
    * [FIX] added std::swap
    
    * [FIX] updated tests expecting less default parameters
    
    * [FIX] typo
    
    * [FIX] typo
    
    * [FIX] fixed tests
    
    * [FIX] fixed tests
    
    * [FIX] changed automatic addition of default params for backwards compatibility
    
    * [FIX] changed automatic addition of default params for backwards compatibility
    
    * [FIX] changed automatic addition of default params for backwards compatibility
    
    * [FIX] changed automatic addition of default params for backwards compatibility
    
    * [FIX] changed automatic addition of default params for backwards compatibility
    
    * [FIX] updated tests
    
    * [FIX] updated tests
    
    * [FIX] updated tests
    
    * [FIX] updated tests
    
    * [FIX] updated tests
    
    * [FIX] updated tests
    
    * [COMMENT] improved new function documentation
    
    * [DOC] improved function documentation
    
    * [FIX] bug in pyopenms
    
    * [FIX] bug in pyopenms
    
    * [FIX] bug in pyopenms
    
    * [MERGE] develop
    
    * [FIX] switched from std::str to String
    
    * [FIX] added conditional statement to default weighting in evaluate
    
    * [FIX] switched from std::str to String
    
    * [FIX] switched from std::str to String
    
    * [FIX] switched from std::str to String
    
    * [FIX] breaking change to DataPoint
    
    * [FIX] breaking change to DataPoint
    
    * [FIX] breaking change to DataPoint
    
    * [TESTS] updates to String
    
    * [TESTS] updates to String
    
    * [TESTS] updates to String
    
    * [TESTS] updates to String
    
    * [TESTS] bug fixes
    
    * [TESTS] bug fixes
    
    * [FIX] cleanup, simplify
    
    * [FIX] remove duplicated code with parent class
    
    * [TEST] adopt tests
    
    * [FIX] removed .vscode tags
    
    * [FIX]
    
    * [FIX]
    
    * [PYOPENMS] fix pyopenms
    
    * [TEST] fix tests
    
    * [TEST] fix tests
    
    * [TESTS] updated tests
    
    * [TESTS] updated tests
    
    * [TESTS] updated tests
    
    * [TESTS] updated tests
    
    * [TESTS] updated tests

--- src/tests/class_tests/openms/source/MapAlignmentAlgorithmPoseClustering_test.cpp
+++ src/tests/class_tests/openms/source/MapAlignmentAlgorithmPoseClustering_test.cpp
@@ -89,7 +89,9 @@ START_SECTION((void align(const PeakMap& map, TransformationDescription& trafo))
   TransformationModelLinear lm(trafo.getDataPoints(),
                                trafo.getModelParameters());
   double slope, intercept;
-  lm.getParameters(slope, intercept);
+  String x_weight, y_weight;
+  double x_datum_min, x_datum_max, y_datum_min, y_datum_max;
+  lm.getParameters(slope, intercept, x_weight, y_weight, x_datum_min, x_datum_max, y_datum_min, y_datum_max);
   TEST_REAL_SIMILAR(slope, 1.01164);
   TEST_REAL_SIMILAR(intercept, -32.0912);
 }
