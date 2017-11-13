344bcd76cac986c00f8eb78d384319019095e535
Hannes Roest <hroest@stanford.edu>
2017-11-13 18:21:32 -0500

    [FIX] unused variable cleanup

--- src/openms/source/TRANSFORMATIONS/RAW2PEAK/TwoDOptimization.cpp
+++ src/openms/source/TRANSFORMATIONS/RAW2PEAK/TwoDOptimization.cpp
@@ -221,7 +221,7 @@ namespace OpenMS
                         + signal2D[2 * current_scan].second + current_point)->getIntensity() << std::endl;
 #endif
 
-        size_t current_peak = 0;
+        // size_t current_peak = 0;
         peak_iter = iso_map_iter->second.peaks.begin();
         while (peak_iter != iso_map_iter->second.peaks.end() && peak_iter->first != curr_scan_idx)
           ++peak_iter;
@@ -265,7 +265,7 @@ namespace OpenMS
 #endif
             computed_signal += p_height / pow(cosh(p_width * (current_position - p_position)), 2);
           }
-          ++current_peak;
+          // ++current_peak;
           ++peak_iter;
 
         }                        // end while
@@ -415,7 +415,7 @@ namespace OpenMS
 
 #endif
 
-        size_t current_peak = 0;
+        // size_t current_peak = 0;
         peak_iter = iso_map_iter->second.peaks.begin();
         while (peak_iter != iso_map_iter->second.peaks.end() && peak_iter->first != curr_scan_idx)
           ++peak_iter;
@@ -498,7 +498,7 @@ namespace OpenMS
             J(counter_posf, total_nr_peaks + 3 * map_idx  + 1) = ddl_left * weight + ddl_left_old;
             J(counter_posf, total_nr_peaks + 3 * map_idx  + 2) = ddl_right * weight + ddl_right_old;
           }
-          ++current_peak;
+          // ++current_peak;
           ++peak_iter;
 
         }                        // end while
