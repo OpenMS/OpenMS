f5a002a15056ccd78c3a59b0f49627808eda33cb
Douglas McCloskey <dmccloskey87@gmail.com>
2017-11-29 22:10:59 +0100

    [FEATURE] added uniqueJoin

--- src/openms/source/ANALYSIS/OPENSWATH/MRMFeatureFilter.cpp
+++ src/openms/source/ANALYSIS/OPENSWATH/MRMFeatureFilter.cpp
@@ -436,7 +436,7 @@ namespace OpenMS
     //remove trailing delimm
     if (str_cat != "")
     {
-      str_cat = str_cat.substr(0, sizeof(str_cat)-1);
+      str_cat = str_cat.substr(0, sizeof(str_cat));
     }
     std::cout << str_cat << std::endl; //debugging
     return str_cat;
