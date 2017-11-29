ea2bfb64d8632a44174c1dcfea9a32c4a2cab808
Douglas McCloskey <dmccloskey87@gmail.com>
2017-11-29 23:57:36 +0100

    [FEATURE] added uniqueJoin

--- src/tests/class_tests/openms/source/MRMFeatureFilter_test.cpp
+++ src/tests/class_tests/openms/source/MRMFeatureFilter_test.cpp
@@ -89,7 +89,7 @@ START_SECTION(String uniqueJoin(std::vector<String>& str_vec, const String& deli
   // tests
   String delim = ";";
   TEST_EQUAL(mrmff.uniqueJoin(str_vec, delim), "bye;hello");
-  delim = "||");
+  delim = "||";
   TEST_EQUAL(mrmff.uniqueJoin(str_vec, delim), "bye;hello");
 
 }
