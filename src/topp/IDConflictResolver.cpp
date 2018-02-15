fb6c993b0410b480ec8dc2ebd4641feade42fe08
timosachsenberg <sachsenb@abinode05.Informatik.Uni-Tuebingen.De>
2018-02-15 15:08:47 +0100

    [FEATURE] IDConflictResolver unmapps conflicting IDs instead of removing them entirely

--- src/topp/IDConflictResolver.cpp
+++ src/topp/IDConflictResolver.cpp
@@ -75,6 +75,9 @@ using namespace std;
     with a single hit (with the best score) is associated to each feature. (If
     two IDs have the same best score, either one of them may be selected.)
 
+    The the filtered identifications are added to the vector of unassigned peptides
+    and also reduced to a single best hit.
+
     This step may be useful before applying @ref TOPP_ProteinQuantifier
     "ProteinQuantifier", because features with ambiguous annotation are not
     considered for the quantification.
@@ -117,15 +120,23 @@ protected:
     return false;
   }
 
-  void resolveConflict_(vector<PeptideIdentification> & peptides)
+  void resolveConflict_(vector<PeptideIdentification> & peptides, vector<PeptideIdentification> & removed)
   {
-    if (peptides.empty()) return;
+    if (peptides.empty()) { return; }
 
-    for (vector<PeptideIdentification>::iterator pep_id = peptides.begin();
-         pep_id != peptides.end(); ++pep_id)
+    for (PeptideIdentification & pep : peptides)
     {
-      pep_id->sort();
+      // sort hits
+      pep.sort();
+
+      // remove all but the best hit
+      if (!pep.getHits().empty())
+      {
+        vector<PeptideHit> best_hit(1, pep.getHits()[0]);
+        pep.setHits(best_hit);
+      }
     }
+
     vector<PeptideIdentification>::iterator pos;
     if (peptides[0].isHigherScoreBetter())     // find highest-scoring ID
     {
@@ -135,15 +146,23 @@ protected:
     {
       pos = min_element(peptides.begin(), peptides.end(), compareIDsSmallerScores_);
     }
+
+    // copy conflicting ones left to best one
+    for (auto it = peptides.begin(); it != pos; ++it)
+    {
+      removed.push_back(*it);
+    }
+     
+    // copy conflicting ones right of best one
+    vector<PeptideIdentification>::iterator pos1p = pos + 1;
+    for (auto it = pos1p; it != peptides.end(); ++it)
+    {
+      removed.push_back(*it);
+    }
+
+    // set best one to first position and shrink vector
     peptides[0] = *pos;
     peptides.resize(1);
-
-    if (!peptides[0].getHits().empty())
-    {
-      // remove all but the best hit:
-      vector<PeptideHit> best_hit(1, peptides[0].getHits()[0]);
-      peptides[0].setHits(best_hit);
-    }
   }
 
   void registerOptionsAndFlags_() override
@@ -162,10 +181,10 @@ protected:
     {
       FeatureMap features;
       FeatureXMLFile().load(in, features);
-      for (FeatureMap::Iterator feat_it = features.begin();
-           feat_it != features.end(); ++feat_it)
+      for (Feature & f : features)
       {
-        resolveConflict_(feat_it->getPeptideIdentifications());
+        resolveConflict_(f.getPeptideIdentifications(), 
+          features.getUnassignedPeptideIdentifications());
       }
       addDataProcessing_(features,
                          getProcessingInfo_(DataProcessing::FILTERING));
@@ -175,10 +194,10 @@ protected:
     {
       ConsensusMap consensus;
       ConsensusXMLFile().load(in, consensus);
-      for (ConsensusMap::Iterator cons_it = consensus.begin();
-           cons_it != consensus.end(); ++cons_it)
+      for (ConsensusFeature & c : consensus)
       {
-        resolveConflict_(cons_it->getPeptideIdentifications());
+        resolveConflict_(c.getPeptideIdentifications(), 
+          consensus.getUnassignedPeptideIdentifications());
       }
       addDataProcessing_(consensus,
                          getProcessingInfo_(DataProcessing::FILTERING));
