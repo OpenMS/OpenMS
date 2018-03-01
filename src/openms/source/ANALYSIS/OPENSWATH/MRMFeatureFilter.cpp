12b7a790e32191b4fe50588966200e69be65d3f3
Douglas McCloskey <dmccloskey87@gmail.com>
2018-03-01 14:53:20 +0100

    fix: changed if else loop structure

--- src/openms/source/ANALYSIS/OPENSWATH/MRMFeatureFilter.cpp
+++ src/openms/source/ANALYSIS/OPENSWATH/MRMFeatureFilter.cpp
@@ -368,35 +368,42 @@ namespace OpenMS
   
   double MRMFeatureFilter::calculateIonRatio(const Feature & component_1, const Feature & component_2, const String & feature_name)
   {
-    
     double ratio = 0.0;
-    if (feature_name == "intensity" && component_1.metaValueExists("native_id") && component_2.metaValueExists("native_id"))
+    // member feature_name access
+    if (feature_name == "intensity")
     {
-      const double feature_1 = component_1.getIntensity();
-      const double feature_2 = component_2.getIntensity();
-      ratio = feature_1 / feature_2;
+      if (component_1.metaValueExists("native_id") && component_2.metaValueExists("native_id"))
+      {
+        const double feature_1 = component_1.getIntensity();
+        const double feature_2 = component_2.getIntensity();
+        ratio = feature_1 / feature_2;
+      }
+      else if (component_1.metaValueExists("native_id"))
+      {
+        LOG_DEBUG << "Warning: no IS found for component " << component_1.getMetaValue("native_id") << ".";
+        const double feature_1 = component_1.getIntensity();
+        ratio = feature_1;
+      }
     }
-    else if (feature_name == "intensity" && component_1.metaValueExists("native_id"))
-    {
-      LOG_DEBUG << "Warning: no IS found for component " << component_1.getMetaValue("native_id") << ".";
-      const double feature_1 = component_1.getIntensity();
-      ratio = feature_1;
-    }
-    else if (component_1.metaValueExists(feature_name) && component_2.metaValueExists(feature_name))
-    {
-      double feature_1 = component_1.getMetaValue(feature_name);
-      double feature_2 = component_2.getMetaValue(feature_name);
-      ratio = feature_1/feature_2;
-    }
-    else if (component_1.metaValueExists(feature_name))
-    {
-      LOG_DEBUG << "Warning: no IS found for component " << component_1.getMetaValue("native_id") << ".";
-      double feature_1 = component_1.getMetaValue(feature_name);
-      ratio = feature_1;
-    }
+    // metaValue feature_name access
     else
     {
-      LOG_DEBUG << "Feature metaValue " << feature_name << " not found for components " << component_1.getMetaValue("native_id") << " and " << component_2.getMetaValue("native_id") << ".";
+      else if (component_1.metaValueExists(feature_name) && component_2.metaValueExists(feature_name))
+      {
+        const double feature_1 = component_1.getMetaValue(feature_name);
+        const double feature_2 = component_2.getMetaValue(feature_name);
+        ratio = feature_1/feature_2;
+      }
+      else if (component_1.metaValueExists(feature_name))
+      {
+        LOG_DEBUG << "Warning: no IS found for component " << component_1.getMetaValue("native_id") << ".";
+        const double feature_1 = component_1.getMetaValue(feature_name);
+        ratio = feature_1;
+      }
+      else
+      {
+        LOG_DEBUG << "Feature metaValue " << feature_name << " not found for components " << component_1.getMetaValue("native_id") << " and " << component_2.getMetaValue("native_id") << ".";
+      }
     }
 
     return ratio;
