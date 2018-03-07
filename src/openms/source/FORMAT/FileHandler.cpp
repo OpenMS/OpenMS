eb259211ecea6441e9201380f3aa1d9e5aadfa40
Eugen Netz <eugen.netz@gmail.com>
2018-03-07 16:04:38 +0100

    [FEATURE] complete xQuest XML format integration, XFDR (#3285)
    * [FEATURE] added xQuest XML to FileTypes and FileHandler
    
    * [FEATURE] started reimplementation of the xQuest XML format
    
    * [PARAM] OPXL: added possible file types for output files
    
    * [FEATURE] OPXL: use new xquest.xml file writer
    
    * [FEATURE] OPXL: writing xquest.xml, implemented correct looping for spectra and hits
    
    loops still have to be filled with content
    
    * [FIX] OPXL: corrected precursor mass calculation and added some MetaValues for output
    
    * [FEATURE] OPXL: using new xQuest XML writing fuction in OPXLLF
    
    * [FEATURE] OPXL: finished implementation of new xQuest XML writing function
    
    The new xQuest XML writing function takes a file path,
    ProteinIdentifications and PeptideIdentifications as input,
    like most other OpenMS ID file writers.
    A reader to recreate these ProtIDs and PepIDs from an xQuest XML file will be the next step.
    
    * [TEST] OPXL: updated Test files with some additional MetaValues and to make the xQuest XML file compatible with the new writer
    
    * [TEST, COMMENT, NOP] OPXL: removed old xQuest XML writer function and temporarily disabled test
    
    also some style and comment changes, and renamed variables
    
    * [FIX] XFDR: use decoy info from ProteinID instead of constant
    
    * [FEATURE] improvements in xQuest XML parsing
    
    * [FIX] XFDR: updated possible input types
    
    * [FIX] OPXL: corrected search_param for charge
    
    * [FIX] XFDR: reverted class determination change
    
    * [FEATURE] improvements in xQuest XML parsing
    
    * [FEATURE] further xQuest XML reading improvements
    
    * [FEATURE] further xQuest XML reading improvements
    
    * [FEATURE] added xQuest XML to IDFileConverter input types
    
    * [FEATURE] XFDR: more consistent handling of different input types
    
    * [PARAM, FIX] OPXL: changed output type ending
    
    * [FIX] OPXL: corrected writing out searched precursor charges
    
    * [TEST] OPXL: adapted test files with additional output values
    
    * [FEATURE, PARAM] XFDR: improvements in consistent treatment of input and output file formats
    
    * [FIX] XQuestResultXML: removed some unused variables
    
    * [TEST] XFDR tests renewed
    
    * [TEST] fixed XFDR mzid test files
    
    * [TEST] added more XFDR tests
    
    * [TEST] fixed OpenPepXL test files after bad merge
    
    * [COMMENT, NOP] XFDR, XQuestXML: deleted dead code
    
    * [FIX] XQuest XML: removed redundant functions
    
    * [FIX] XFDR: corrected intra- / interprotein class assignment
    
    * [TEST] XFDR: updated test files
    
    * [FIX] XQuest XML: removed terminal output for debugging
    
    * [FIX] XFDR: removed terminal output for debugging
    
    * [FIX] XFDR: removed some useless terminal output
    
    * [COMMENT] IDFileConverter: removed dead code
    
    * [TEST] removed old unused XFDR test files
    
    * [TEST] XQuestResultXMLFile: added test file
    
    * [FIX] XQuestResultXMLFile: removed redundant outputs and made parsing some values more robust
    
    * [TEST] XQuestResultXMLFile: redid and reactivated load and store test
    
    * [TEST] XFDR: fixed test files
    
    * [COMMENT, NOP] XFDR: removed dead code, style corrections
    
    * [TEST, COMMENT] removed dead code
    
    * [COMMENT] OPXL: removed dead code
    
    * [FIX, COMMENT] OPXL: removed unused and dead code
    
    * [FIX] XQuest XML: redid map initialization and removed boost dependency
    
    * [COMMENT] XFDR: removed dead code
    
    * [PARAM] IDFileConverter: added dots to file extensions in input param description
    
    * [FIX, PYOPENMS] fixed python bindings for XQuestResultXMLFile

--- src/openms/source/FORMAT/FileHandler.cpp
+++ src/openms/source/FORMAT/FileHandler.cpp
@@ -83,6 +83,9 @@ namespace OpenMS
     if (basename.hasSuffix(".prot.xml"))
       return FileTypes::PROTXML;
 
+    if (basename.hasSuffix(".xquest.xml"))
+      return FileTypes::XQUESTXML;
+
     try
     {
       tmp = basename.suffix('.');
@@ -525,7 +528,7 @@ if (first_line.hasSubstring("File	First Scan	Last Scan	Num of Scans	Charge	Monoi
 
   bool FileHandler::loadExperiment(const String& filename, PeakMap& exp, FileTypes::Type force_type, ProgressLogger::LogType log, const bool rewrite_source_file, const bool compute_hash)
   {
-    // setting the flag for hash recomputation only works if source file entries are rewritten 
+    // setting the flag for hash recomputation only works if source file entries are rewritten
     OPENMS_PRECONDITION(rewrite_source_file || !compute_hash, "Can't compute hash if no SourceFile written");
 
     //determine file type
@@ -631,8 +634,8 @@ if (first_line.hasSubstring("File	First Scan	Last Scan	Num of Scans	Charge	Monoi
       SourceFile src_file;
       src_file.setNameOfFile(File::basename(filename));
       String path_to_file = File::path(File::absolutePath(filename)); //convert to absolute path and strip file name
-      
-      // make sure we end up with at most 3 forward slashes       
+
+      // make sure we end up with at most 3 forward slashes
       String uri = path_to_file.hasPrefix("/") ? String("file://") + path_to_file : String("file:///") + path_to_file;
       src_file.setPathToFile(uri);
       // this is more complicated since the data formats allowed by mzML are very verbose.
