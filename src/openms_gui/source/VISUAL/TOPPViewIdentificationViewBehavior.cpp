68c2107256a2027e87589df7a4aed28a4b09b825
hendrikweisser <hendrikweisser@users.noreply.github.com>
2018-03-23 15:29:33 +0000

    [FIX] fixed erroneous deletion in copyright notice

--- src/openms_gui/source/VISUAL/TOPPViewIdentificationViewBehavior.cpp
+++ src/openms_gui/source/VISUAL/TOPPViewIdentificationViewBehavior.cpp
@@ -12,7 +12,7 @@
 //    documentation and/or other materials provided with the distribution.
 //  * Neither the name of any author or any participating institution
 //    may be used to endorse or promote products derived from this software
-//    without specific prior written permissi
+//    without specific prior written permission.
 // For a full list of authors, refer to the file AUTHORS.
 // --------------------------------------------------------------------------
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
