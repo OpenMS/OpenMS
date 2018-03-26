871b461ddcd7d300e4d26a286e24360d3f223d2a
oliveralka <oliver.alka@gmx.de>
2018-03-26 12:49:20 +0200

    [feature] First version AssayGeneratorMetabo

---
+++ src/utils/AssayGeneratorMetabo.cpp
@@ -0,0 +1,118 @@
+// --------------------------------------------------------------------------
+//                   OpenMS -- Open-Source Mass Spectrometry
+// --------------------------------------------------------------------------
+// Copyright The OpenMS Team -- Eberhard Karls University Tuebingen,
+// ETH Zurich, and Freie Universitaet Berlin 2002-2017.
+//
+// This software is released under a three-clause BSD license:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of any author or any participating institution
+//    may be used to endorse or promote products derived from this software
+//    without specific prior written permission.
+// For a full list of authors, refer to the file AUTHORS.
+// --------------------------------------------------------------------------
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL ANY OF THE AUTHORS OR THE CONTRIBUTING
+// INSTITUTIONS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// --------------------------------------------------------------------------
+// $Maintainer: Oliver Alka $
+// $Authors: Oliver Alka $
+// --------------------------------------------------------------------------
+
+#include <OpenMS/APPLICATIONS/TOPPBase.h>
+#include <OpenMS/FORMAT/MzMLFile.h>
+#include <OpenMS/KERNEL/MSExperiment.h>
+#include <OpenMS/FORMAT/FeatureXMLFile.h>
+
+using namespace OpenMS;
+using namespace std;
+
+
+//-------------------------------------------------------------
+//Doxygen docu
+//----------------------------------------------------------
+/**
+  @page UTILS_AssayGeneratorMetabo AssayGeneratorMetabo
+
+  @brief Assay Library generator using DDA Metabolomics Data
+
+  mzml blablabla
+
+  featureXML blabalbla
+
+  <B>The command line parameters of this tool are:</B>
+  @verbinclude UTILS_SiriusAdapter.cli
+  <B>INI file documentation of this tool:</B>
+  @htmlinclude UTILS_SiriusAdapter.html
+ */
+
+/// @cond TOPPCLASSES
+
+class TOPPAssayGeneratorMetabo :
+  public TOPPBase
+{
+public:
+  TOPPAssayGeneratorMetabo() :
+    TOPPBase("AssayGeneratorMetabo", "Tool for assay library generation from metabolomics DDA data", false)
+    {}
+
+protected:
+
+  void registerOptionsAndFlags_() override
+  {
+
+    registerInputFile_("in", "<file>", "", "MzML Input file");
+    setValidFormats_("in", ListUtils::create<String>("mzml"));
+
+    registerInputFile_("in_id", "<file>", "", "FeatureXML Input with id information (accurate mass search)", false);
+    setValidFormats_("in_id", ListUtils::create<String>("featurexml"));
+
+    registerOutputFile_("out", "<file>", "", "Assay library output file");
+    setValidFormats_("out", ListUtils::create<String>("tsv"));
+  }
+
+  ExitCodes main_(int, const char **) override
+  {
+    //-------------------------------------------------------------
+    // Parsing parameters
+    //-------------------------------------------------------------
+
+    String in = getStringOption_("in");
+    String id = getStringOption_("in_id");
+    String out = getStringOption_("out");
+
+
+    
+
+    //-------------------------------------------------------------
+    // Calculations
+    //-------------------------------------------------------------
+
+
+    //-------------------------------------------------------------
+    // writing output
+    //-------------------------------------------------------------
+
+  }
+};
+
+int main(int argc, const char ** argv)
+{
+  TOPPAssayGeneratorMetabo tool;
+  return tool.main(argc, argv);
+}
+
+/// @endcond
