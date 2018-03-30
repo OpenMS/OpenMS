0f3dff7e22d3b3d2a328fb4f3463a5ec52f93d46
lukaszimmermann <lukas.zimmermann@informatik.uni-tuebingen.de>
2018-03-30 15:35:35 +0200

    [FIX] Header guard in experimental design

--- src/openms/include/OpenMS/FORMAT/ExperimentalDesignFile.h
+++ src/openms/include/OpenMS/FORMAT/ExperimentalDesignFile.h
@@ -32,8 +32,8 @@
 // $Authors: Timo Sachsenberg, Lukas Zimmermann $
 // --------------------------------------------------------------------------
 
-#ifndef OPENMS_KERNEL_EXPERIMENTALDESIGNIO_H
-#define OPENMS_KERNEL_EXPERIMENTALDESIGNIO_H
+#ifndef OPENMS_KERNEL_EXPERIMENTALDESIGNFILE_H
+#define OPENMS_KERNEL_EXPERIMENTALDESIGNFILE_H
 
 #include <OpenMS/DATASTRUCTURES/String.h>
 
