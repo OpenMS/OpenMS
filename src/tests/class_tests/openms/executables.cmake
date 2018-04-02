6fa96f16ddffaa3fe8795642df9f44340c2eaa20
Chris Bielow <chris.bielow@fu-berlin.de>
2018-04-02 15:11:11 -0700

    removed ComposeFunctionAdapters

--- src/tests/class_tests/openms/executables.cmake
+++ src/tests/class_tests/openms/executables.cmake
@@ -1,5 +1,4 @@
 set(concept_executables_list
-  BinaryComposeFunctionAdapter_test
   ClassTest_test
   Exception_Base_test
   FactoryBase_test
@@ -11,7 +10,6 @@ set(concept_executables_list
   VersionInfo_test
   LogConfigHandler_test
   LogStream_test
-  UnaryComposeFunctionAdapter_test
   UniqueIdGenerator_test
   UniqueIdIndexer_test
   UniqueIdInterface_test
