e22adda302e89198db1e235ba5d22de0b7036c3f
FabianAicheler <aicheler@informatik.uni-tuebingen.de>
2018-04-05 09:43:33 +0200

    extend FeatureDistance test for adduct comparison

--- src/tests/class_tests/openms/source/FeatureDistance_test.cpp
+++ src/tests/class_tests/openms/source/FeatureDistance_test.cpp
@@ -1,32 +1,32 @@
 // --------------------------------------------------------------------------
-//                   OpenMS -- Open-Source Mass Spectrometry               
+//                   OpenMS -- Open-Source Mass Spectrometry
 // --------------------------------------------------------------------------
 // Copyright The OpenMS Team -- Eberhard Karls University Tuebingen,
 // ETH Zurich, and Freie Universitaet Berlin 2002-2017.
-// 
+//
 // This software is released under a three-clause BSD license:
 //  * Redistributions of source code must retain the above copyright
 //    notice, this list of conditions and the following disclaimer.
 //  * Redistributions in binary form must reproduce the above copyright
 //    notice, this list of conditions and the following disclaimer in the
 //    documentation and/or other materials provided with the distribution.
-//  * Neither the name of any author or any participating institution 
-//    may be used to endorse or promote products derived from this software 
+//  * Neither the name of any author or any participating institution
+//    may be used to endorse or promote products derived from this software
 //    without specific prior written permission.
-// For a full list of authors, refer to the file AUTHORS. 
+// For a full list of authors, refer to the file AUTHORS.
 // --------------------------------------------------------------------------
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 // AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 // IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-// ARE DISCLAIMED. IN NO EVENT SHALL ANY OF THE AUTHORS OR THE CONTRIBUTING 
-// INSTITUTIONS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
-// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
-// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
-// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
-// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+// ARE DISCLAIMED. IN NO EVENT SHALL ANY OF THE AUTHORS OR THE CONTRIBUTING
+// INSTITUTIONS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 // ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// 
+//
 // --------------------------------------------------------------------------
 // $Maintainer: Hendrik Weisser
 // $Authors: Clemens Groepl, Hendrik Weisser, Chris Bielow $
@@ -38,6 +38,7 @@
 ///////////////////////////
 
 #include <OpenMS/ANALYSIS/MAPMATCHING/FeatureDistance.h>
+#include <OpenMS/CHEMISTRY/EmpiricalFormula.h>
 
 ///////////////////////////
 
@@ -153,6 +154,35 @@ START_SECTION((std::pair<bool, double> operator()(const BaseFeature& left, const
   result = dist(left, right);
   TEST_EQUAL(result.first, true); // --> valid
   TEST_REAL_SIMILAR(result.second, 0.5);
+
+	// adduct
+	param.setValue("ignore_adduct", "false");
+  dist.setParameters(param);
+  right.setMZ(100.0 + 100.0/1e6 * 5); // 5ppm off --> valid in m/z
+  // only one adduct set
+  right.setMetaValue("dc_charge_adducts", "NH4");
+	result = dist(left, right);
+  TEST_EQUAL(result.first, true); // --> valid
+  TEST_REAL_SIMILAR(result.second, 0.5);
+  //both set with adducts having same element composition (even if string different ordered)
+	left.setMetaValue("dc_charge_adducts", "H4N");
+	result = dist(left, right);
+  TEST_EQUAL(result.first, true); // --> valid
+  TEST_REAL_SIMILAR(result.second, 0.5);
+	//one different adduct
+	left.setMetaValue("dc_charge_adducts", "H5N");
+	result = dist(left, right);
+  TEST_EQUAL(result.first, false); // --> invalid
+  TEST_REAL_SIMILAR(result.second, FeatureDistance::infinity);
+  // ignore adduct
+  param.setValue("ignore_adduct", "true");
+  dist.setParameters(param);
+  // adducts differ, but we don't care
+  result = dist(left, right);
+  TEST_EQUAL(result.first, true); // --> valid
+  TEST_REAL_SIMILAR(result.second, 0.5);
+
+
 
 }
 END_SECTION
