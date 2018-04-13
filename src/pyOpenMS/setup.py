74b73a71d379b36bbfefe2beafafba7ed4e7fd42
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2018-04-13 11:06:15 +0000

    Removed xerces dependency for pyoms. Resolve softlinks of deps.

--- src/pyOpenMS/setup.py
+++ src/pyOpenMS/setup.py
@@ -102,7 +102,7 @@ if iswin:
     else:
         libraries = ["OpenMS", "OpenSwathAlgo", "SuperHirn", "Qt5Core", "Qt5Network"]
 elif sys.platform.startswith("linux"):
-    libraries = ["OpenMS", "OpenSwathAlgo", "SuperHirn", "xerces-c", "Qt5Core", "Qt5Network"]
+    libraries = ["OpenMS", "OpenSwathAlgo", "SuperHirn", "Qt5Core", "Qt5Network"]
 elif sys.platform == "darwin":
     libraries = ["OpenMS", "OpenSwathAlgo", "SuperHirn"]
 else:
