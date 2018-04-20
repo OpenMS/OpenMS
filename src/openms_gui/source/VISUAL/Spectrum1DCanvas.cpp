7dfe8c19bf2a4324959de7dc744ce38d13c474eb
Hannes Roest <hroest@stanford.edu>
2018-04-19 23:38:25 -0400

    [FIX] fix ion mobility display

--- src/openms_gui/source/VISUAL/Spectrum1DCanvas.cpp
+++ src/openms_gui/source/VISUAL/Spectrum1DCanvas.cpp
@@ -1426,8 +1426,7 @@ namespace OpenMS
         context_menu->addAction("Switch to 3D view");
       }
 
-      if (TOPPViewBase::containsIMData(getCurrentLayer().getPeakData()->getSpectrum( 
-              getCurrentLayer().getCurrentSpectrumIndex() ) ))
+      if (TOPPViewBase::containsIMData(getCurrentLayer().getCurrentSpectrum()))
       {
         context_menu->addAction("Switch to ion mobility view");
       }
