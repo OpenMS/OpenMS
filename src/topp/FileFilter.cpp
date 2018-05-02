dff1f6220956669c31c2b37442c4ecd1b8a23ab4
Moritz Freidank <freidankm@gmail.com>
2018-05-02 14:26:04 +0200

    error handling for 'isWhiteListed'
    Calling 'isWhiteListed' with an unsupported 'string_comparison_method'
    now throws an `Exception::InvalidParameter`.

--- src/topp/FileFilter.cpp
+++ src/topp/FileFilter.cpp
@@ -152,7 +152,12 @@ private:
             || peptide_hit_sequence.toUnmodifiedString() == *seq_it) {
                 return true;
             }
-        }
+       } else {
+           throw Exception::InvalidParameter(
+                __FILE__, __LINE__, OPENMS_PRETTY_FUNCTION, 
+                "Invalid sequence comparison method given: '" + sequence_comparison_method + "'"
+            );
+       }
       }
       return false;
   }
@@ -378,8 +383,7 @@ protected:
     registerStringOption_("f_and_c:charge", "[min]:[max]", ":", "Charge range to extract", false);
     registerStringOption_("f_and_c:size", "[min]:[max]", ":", "Size range to extract", false);
     registerStringList_("f_and_c:remove_meta", "<name> 'lt|eq|gt' <value>", StringList(), "Expects a 3-tuple (=3 entries in the list), i.e. <name> 'lt|eq|gt' <value>; the first is the name of meta value, followed by the comparison operator (equal, less or greater) and the value to compare to. All comparisons are done after converting the given value to the corresponding data value type of the meta value (for lists, this simply compares length, not content!)!", false);
-
-    addEmptyLine_();
+    // XXX: Change description
     registerTOPPSubsection_("id", "ID options. The Priority of the id-flags is: remove_annotated_features / remove_unannotated_features -> remove_clashes -> keep_best_score_id -> sequences_whitelist  / accessions_whitelist");
     registerFlag_("id:remove_clashes", "Remove features with id clashes (different sequences mapped to one feature)", true);
     registerFlag_("id:keep_best_score_id", "in case of multiple peptide identifications, keep only the id with best score");
