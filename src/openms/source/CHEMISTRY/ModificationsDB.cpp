61308c27074e817c7a54dbef6381a83af328d594
Hannes Roest <hroest@stanford.edu>
2018-05-05 23:12:01 -0400

    [FIX] small

--- src/openms/source/CHEMISTRY/ModificationsDB.cpp
+++ src/openms/source/CHEMISTRY/ModificationsDB.cpp
@@ -98,7 +98,7 @@ namespace OpenMS
     {
       throw Exception::IndexOverflow(__FILE__, __LINE__, OPENMS_PRETTY_FUNCTION, index, mods_.size());
     }
-    return *mods_[index];
+    return *mods_.at(index);
   }
 
 
