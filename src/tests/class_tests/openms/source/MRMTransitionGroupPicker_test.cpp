dd28aa147cf5991baa8c7dbe6cae3d3ce75f1e09
Douglas McCloskey <dmccloskey87@gmail.com>
2018-05-18 20:49:23 +0200

    tests:  control no consensus test

--- src/tests/class_tests/openms/source/MRMTransitionGroupPicker_test.cpp
+++ src/tests/class_tests/openms/source/MRMTransitionGroupPicker_test.cpp
@@ -333,7 +333,38 @@ START_SECTION((template < typename SpectrumT, typename TransitionT > void pickTr
     53900 - resampling_loss);
   }
 
-  { // transition group 2
+  { // transition group 2 (Consensus)
+    MRMTransitionGroupType transition_group;
+    setup_transition_group2(transition_group);
+
+    MRMTransitionGroupPicker trgroup_picker;
+    Param picker_param = trgroup_picker.getDefaults();
+    picker_param.setValue("resample_boundary", 1.0);
+    picker_param.setValue("PeakPickerMRM:gauss_width", 10.0);
+    picker_param.setValue("PeakPickerMRM:peak_width", -1.0);
+    picker_param.setValue("PeakPickerMRM:signal_to_noise", 1.0);
+    picker_param.setValue("PeakPickerMRM:method", "corrected");
+    picker_param.setValue("use_consensus", "true");
+    trgroup_picker.setParameters(picker_param);
+    trgroup_picker.pickTransitionGroup(transition_group);
+
+    TEST_EQUAL(transition_group.getFeatures().size(), 2)
+    MRMFeature mrmfeature = transition_group.getFeatures()[0];
+    TEST_REAL_SIMILAR(mrmfeature.getRT(), 14);
+    TEST_REAL_SIMILAR(mrmfeature.getMetaValue("leftWidth"), 7);
+    TEST_REAL_SIMILAR(mrmfeature.getMetaValue("rightWidth"), 21);
+    TEST_REAL_SIMILAR(mrmfeature.getFeature("1").getIntensity(), 140);
+    TEST_REAL_SIMILAR(mrmfeature.getFeature("2").getIntensity(), 117);
+    TEST_REAL_SIMILAR(mrmfeature.getFeature("3").getIntensity(), 45);
+    TEST_REAL_SIMILAR(mrmfeature.getFeature("1").getMetaValue("peak_apex_int"), 16);
+    TEST_REAL_SIMILAR(mrmfeature.getFeature("2").getMetaValue("peak_apex_int"), 11);
+    TEST_REAL_SIMILAR(mrmfeature.getFeature("3").getMetaValue("peak_apex_int"), 3);
+    TEST_REAL_SIMILAR(mrmfeature.getFeature("1").getMetaValue("peak_apex_position"), 14);
+    TEST_REAL_SIMILAR(mrmfeature.getFeature("2").getMetaValue("peak_apex_position"), 7);
+    TEST_REAL_SIMILAR(mrmfeature.getFeature("3").getMetaValue("peak_apex_position"), 7);
+  }
+
+  { // transition group 2 (no consensus)
     MRMTransitionGroupType transition_group;
     setup_transition_group2(transition_group);
 
