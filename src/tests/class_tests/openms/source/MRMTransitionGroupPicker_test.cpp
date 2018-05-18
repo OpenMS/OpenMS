8f99cb22bf875acaa691a578b13bbf10a06c2578
Douglas McCloskey <dmccloskey87@gmail.com>
2018-05-18 11:38:20 +0200

    fix: debugging no_consensus

--- src/tests/class_tests/openms/source/MRMTransitionGroupPicker_test.cpp
+++ src/tests/class_tests/openms/source/MRMTransitionGroupPicker_test.cpp
@@ -342,7 +342,7 @@ START_SECTION((template < typename SpectrumT, typename TransitionT > void pickTr
     picker_param.setValue("resample_boundary", 0.5);
     picker_param.setValue("PeakPickerMRM:gauss_width", 0.1);
     picker_param.setValue("PeakPickerMRM:peak_width", -1.0);
-    picker_param.setValue("PeakPickerMRM:signal_to_noise", 0.1);
+    picker_param.setValue("PeakPickerMRM:signal_to_noise", 0.01);
     picker_param.setValue("PeakPickerMRM:method", "corrected");
     picker_param.setValue("use_consensus", "false");
     trgroup_picker.setParameters(picker_param);
