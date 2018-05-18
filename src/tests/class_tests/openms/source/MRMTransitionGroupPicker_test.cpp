3b46dd9d04f6ee96b562eb28beb86137eec3a2cd
Douglas McCloskey <dmccloskey87@gmail.com>
2018-05-18 12:08:00 +0200

    fix: debugging no_consensus

--- src/tests/class_tests/openms/source/MRMTransitionGroupPicker_test.cpp
+++ src/tests/class_tests/openms/source/MRMTransitionGroupPicker_test.cpp
@@ -371,8 +371,8 @@ START_SECTION((template < typename SpectrumT, typename TransitionT > void pickTr
     MRMTransitionGroupPicker trgroup_picker;
     Param picker_param = trgroup_picker.getDefaults();
     picker_param.setValue("resample_boundary", 1.0);
-    picker_param.setValue("PeakPickerMRM:gauss_width", 1.0);
-    picker_param.setValue("PeakPickerMRM:peak_width", 0.1);
+    picker_param.setValue("PeakPickerMRM:gauss_width", 10.0);
+    picker_param.setValue("PeakPickerMRM:peak_width", -1.0);
     picker_param.setValue("PeakPickerMRM:signal_to_noise", 0.1);
     picker_param.setValue("PeakPickerMRM:method", "corrected");
     picker_param.setValue("use_consensus", "false");
@@ -388,7 +388,7 @@ START_SECTION((template < typename SpectrumT, typename TransitionT > void pickTr
     Param picker_param = trgroup_picker.getDefaults();
     picker_param.setValue("resample_boundary", 1.0);
     picker_param.setValue("PeakPickerMRM:gauss_width", 1.0);
-    picker_param.setValue("PeakPickerMRM:peak_width", 0.01);
+    picker_param.setValue("PeakPickerMRM:peak_width", -1.0);
     picker_param.setValue("PeakPickerMRM:signal_to_noise", 0.1);
     picker_param.setValue("PeakPickerMRM:method", "corrected");
     picker_param.setValue("use_consensus", "false");
@@ -403,8 +403,8 @@ START_SECTION((template < typename SpectrumT, typename TransitionT > void pickTr
     MRMTransitionGroupPicker trgroup_picker;
     Param picker_param = trgroup_picker.getDefaults();
     picker_param.setValue("resample_boundary", 1.0);
-    picker_param.setValue("PeakPickerMRM:gauss_width", 1.0);
-    picker_param.setValue("PeakPickerMRM:peak_width", 1.0);
+    picker_param.setValue("PeakPickerMRM:gauss_width", 0.1);
+    picker_param.setValue("PeakPickerMRM:peak_width", -1.0);
     picker_param.setValue("PeakPickerMRM:signal_to_noise", 0.1);
     picker_param.setValue("PeakPickerMRM:method", "corrected");
     picker_param.setValue("use_consensus", "false");
@@ -419,8 +419,8 @@ START_SECTION((template < typename SpectrumT, typename TransitionT > void pickTr
     MRMTransitionGroupPicker trgroup_picker;
     Param picker_param = trgroup_picker.getDefaults();
     picker_param.setValue("resample_boundary", 1.0);
-    picker_param.setValue("PeakPickerMRM:gauss_width", 1.0);
-    picker_param.setValue("PeakPickerMRM:peak_width", 10.0);
+    picker_param.setValue("PeakPickerMRM:gauss_width", 0.01);
+    picker_param.setValue("PeakPickerMRM:peak_width", -1.0);
     picker_param.setValue("PeakPickerMRM:signal_to_noise", 0.1);
     picker_param.setValue("PeakPickerMRM:method", "corrected");
     picker_param.setValue("use_consensus", "false");
