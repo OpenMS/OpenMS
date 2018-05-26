6afd514fa7eeff520cebc431342c5bc5a2d0618d
Zethson <lukas.heumos@gmail.com>
2018-05-27 01:20:46 +0200

    [REFACTORING]

--- src/openms/include/OpenMS/ANALYSIS/ID/PeptideIndexing.h
+++ src/openms/include/OpenMS/ANALYSIS/ID/PeptideIndexing.h
@@ -793,7 +793,7 @@ public:
        }
 
        // DEBUG ONLY: print counts of found decoys
-       for (auto &a : decoy_count) LOG_WARN << a.first << "\t" << a.second.first << "\t" << a.second.second << std::endl;
+       for (auto &a : decoy_count) LOG_DEBUG << a.first << "\t" << a.second.first << "\t" << a.second.second << std::endl;
 
        // less than 40% of proteins are decoys -> won't be able to determine a decoy string and its position
        // return default values
