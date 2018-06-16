a245a80dd910e7659166d9ad62e7c09b49fa3ff6
George Rosenberger <gr2578@cumc.columbia.edu>
2018-06-16 12:12:44 -0400

    [FIX] TransitionPQP/TSVReader: Support for conversion of identification transitions

--- src/openms/source/ANALYSIS/OPENSWATH/TransitionTSVFile.cpp
+++ src/openms/source/ANALYSIS/OPENSWATH/TransitionTSVFile.cpp
@@ -160,7 +160,8 @@ namespace OpenMS
     "Decoy",
     "DetectingTransition",
     "IdentifyingTransition",
-    "QuantifyingTransition"
+    "QuantifyingTransition",
+    "Peptidoforms"
   };
 
 
@@ -357,13 +358,16 @@ namespace OpenMS
       !extractName(mytransition.FullPeptideName, "ModifiedPeptideSequence", tmp_line, header_dict);
 
       //// IPF
+      String peptidoforms;
       !extractName<bool>(mytransition.detecting_transition, "detecting_transition", tmp_line, header_dict) &&
       !extractName<bool>(mytransition.detecting_transition, "DetectingTransition", tmp_line, header_dict);
       !extractName<bool>(mytransition.identifying_transition, "identifying_transition", tmp_line, header_dict) &&
       !extractName<bool>(mytransition.identifying_transition, "IdentifyingTransition", tmp_line, header_dict);
       !extractName<bool>(mytransition.quantifying_transition, "quantifying_transition", tmp_line, header_dict) &&
       !extractName<bool>(mytransition.quantifying_transition, "QuantifyingTransition", tmp_line, header_dict) &&
-      !extractName<bool>(mytransition.quantifying_transition, "Quantitative", tmp_line, header_dict); // Skyline
+      !extractName<bool>(mytransition.quantifying_transition, "Quantitative", tmp_line, header_dict) && // Skyline
+      !extractName(peptidoforms, "Peptidoforms", tmp_line, header_dict);
+      peptidoforms.split('|', mytransition.peptidoforms);
 
       //// Targeted Metabolomics
       !extractName(mytransition.CompoundName, "CompoundName", tmp_line, header_dict) &&
@@ -935,6 +939,11 @@ namespace OpenMS
     rm_trans.setDetectingTransition(tr_it->detecting_transition);
     rm_trans.setIdentifyingTransition(tr_it->identifying_transition);
     rm_trans.setQuantifyingTransition(tr_it->quantifying_transition);
+
+    if (!tr_it->peptidoforms.empty())
+    {
+      rm_trans.setMetaValue("Peptidoforms", ListUtils::concatenate(tr_it->peptidoforms, "|"));
+    }
   }
 
   void TransitionTSVFile::createProtein_(std::vector<TSVTransition>::iterator& tr_it, OpenMS::TargetedExperiment::Protein& protein)
@@ -1345,7 +1354,7 @@ namespace OpenMS
     }
     if (it->metaValueExists("Peptidoforms"))
     {
-      it->getMetaValue("Peptidoforms").toString().split('|', mytransition.peptidoforms);
+      String(it->getMetaValue("Peptidoforms")).split('|', mytransition.peptidoforms);
     }
     mytransition.detecting_transition = it->isDetectingTransition();
     mytransition.identifying_transition = it->isIdentifyingTransition();
@@ -1411,7 +1420,8 @@ namespace OpenMS
         + (String)it->decoy                    + "\t"
         + (String)it->detecting_transition     + "\t"
         + (String)it->identifying_transition   + "\t"
-        + (String)it->quantifying_transition;
+        + (String)it->quantifying_transition   + "\t"
+        + ListUtils::concatenate(it->peptidoforms, "|");
 
       os << line << std::endl;
 
