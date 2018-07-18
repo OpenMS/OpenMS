d206c0ac0297ed5ab14fa8318b94d90d23015bc9
Timo Sachsenberg <sachsenb@informatik.uni-tuebingen.de>
2018-07-18 09:16:45 +0200

    mend

--- src/openms/source/TRANSFORMATIONS/FEATUREFINDER/FeatureFinderMultiplexAlgorithm.cpp
+++ src/openms/source/TRANSFORMATIONS/FEATUREFINDER/FeatureFinderMultiplexAlgorithm.cpp
@@ -972,11 +972,11 @@ namespace OpenMS
     }
 
     bool centroided;
-    if (param_.getValue("algorithm:spectrum_type")=="automatic")
+    if (param_.getValue("algorithm:spectrum_type") == "automatic")
     {
-      centroided = spectrum_type == SpectrumSettings::CENTROID;
+      centroided = (spectrum_type == SpectrumSettings::CENTROID);
     }
-    else if (param_.getValue("algorithm:spectrum_type")=="centroid")
+    else if (param_.getValue("algorithm:spectrum_type") == "centroid")
     {
       centroided = true;
     }
