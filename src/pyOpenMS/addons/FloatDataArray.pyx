091cfdcd8341b8693f537df6dd9656985eaa9b80
Hannes Roest <hroest@stanford.edu>
2018-07-29 14:50:33 -0400

    [PYOPEMS] efficient access to extra data arrays
    - efficient access to float and integer data arrays

--- src/pyOpenMS/addons/FloatDataArray.pyx
+++ src/pyOpenMS/addons/FloatDataArray.pyx
@@ -23,3 +23,51 @@
         cdef float _v = (<float>value)
         deref( self.inst.get() )[(<int>key)] = _v
 
+    def get_data(self):
+        """
+        Gets the raw data for the float data array
+
+        Example usage: 
+
+          fd = pyopenms.FloatDataArray()
+          data = fd.get_data()
+
+        """
+
+        cdef _FloatDataArray * fda_ = self.inst.get()
+        cdef libcpp_vector[float].iterator it = fda_.begin()
+        cdef unsigned int n = fda_.size()
+        cdef np.ndarray[np.float32_t, ndim=1] data
+        data = np.zeros( (n,), dtype=np.float32)
+
+        cdef int i = 0
+        while it != fda_.end():
+            data[i] = deref(it)
+            inc(it)
+            i += 1
+
+        return data
+
+    def set_data(self, np.ndarray[float, ndim=1, mode="c"] data not None):
+        """
+        Sets the raw data for the float data array
+
+        Example usage: 
+
+          fd = pyopenms.FloatDataArray()
+          data = numpy.array( [1, 2, 3, 5 ,6] ).astype(numpy.float32)
+          fd.set_data(data)
+
+        """
+        cdef _FloatDataArray * fda_ = self.inst.get()
+        fda_.clear()
+
+        # cdef float * array_start = <float*>data.data
+        cdef int N
+        N = len(data)
+        # fda_.assign(array_start, array_start + N)
+        fda_.reserve(N) # allocate space for incoming data
+        for i in range(N):
+            fda_.push_back(data[i])
+
+
