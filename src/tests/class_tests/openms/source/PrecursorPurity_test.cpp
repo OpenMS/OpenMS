f807aa8cc8574039b95189dc4a7be7587f76bb52
enetz <eugen.netz@gmail.com>
2018-09-04 17:32:57 +0200

    [TEST] adapted PrecursorPurity tests to recent fixes

--- src/tests/class_tests/openms/source/PrecursorPurity_test.cpp
+++ src/tests/class_tests/openms/source/PrecursorPurity_test.cpp
@@ -60,17 +60,10 @@ f.load(OPENMS_GET_TEST_DATA_PATH("PrecursorPurity_input.mzML"), spectra);
 START_SECTION(static PurityScores computePrecursorPurity(const PeakSpectrum& ms1, const Precursor& pre, const double precursor_mass_tolerance, const bool precursor_mass_tolerance_unit_ppm))
 
   TEST_EQUAL(spectra.size(), 7)
+  // the MS1 spectra are soectra[0] and spectra[6]
 
   Precursor pre = spectra[2].getPrecursors()[0];
-  PrecursorPurity::PurityScores score = PrecursorPurity::computePrecursorPurity(spectra[6], pre, 0.2, false);
-  TEST_REAL_SIMILAR(score.total_intensity, 11557777.1875)
-  TEST_REAL_SIMILAR(score.target_intensity, 10320935.25)
-  TEST_REAL_SIMILAR(score.signal_proportion, 0.89298)
-  TEST_EQUAL(score.target_peak_count, 2)
-  TEST_EQUAL(score.residual_peak_count, 2)
-
-  // testing a narrower tolerance for deisotoping
-  score = PrecursorPurity::computePrecursorPurity(spectra[6], pre, 10, true);
+  PrecursorPurity::PurityScores score = PrecursorPurity::computePrecursorPurity(spectra[6], pre, 10, true);
   TEST_REAL_SIMILAR(score.total_intensity, 11557777.1875)
   TEST_REAL_SIMILAR(score.target_intensity, 8923915)
   TEST_REAL_SIMILAR(score.signal_proportion, 0.77211)
@@ -79,18 +72,17 @@ START_SECTION(static PurityScores computePrecursorPurity(const PeakSpectrum& ms1
 
   pre = spectra[3].getPrecursors()[0];
   score = PrecursorPurity::computePrecursorPurity(spectra[0], pre, 0.2, false);
-
   TEST_REAL_SIMILAR(score.total_intensity, 9098343.89062)
-  TEST_REAL_SIMILAR(score.target_intensity, 8266450.1875)
-  TEST_REAL_SIMILAR(score.signal_proportion, 0.90856)
-  TEST_EQUAL(score.target_peak_count, 3)
-  TEST_EQUAL(score.residual_peak_count, 2)
+  TEST_REAL_SIMILAR(score.target_intensity, 7057944)
+  TEST_REAL_SIMILAR(score.signal_proportion, 0.77573)
+  TEST_EQUAL(score.target_peak_count, 1)
+  TEST_EQUAL(score.residual_peak_count, 4)
 
 END_SECTION
 
 START_SECTION(static std::vector<PurityScores> computePrecursorPurities(const PeakMap& spectra, double precursor_mass_tolerance, bool precursor_mass_tolerance_unit_ppm))
 
-  vector<PrecursorPurity::PurityScores> purityscores = PrecursorPurity::computePrecursorPurities(spectra, 0.2, false);
+  vector<PrecursorPurity::PurityScores> purityscores = PrecursorPurity::computePrecursorPurities(spectra, 0.1, false);
 
   TEST_REAL_SIMILAR(purityscores[0].total_intensity, 9849578.5)
   TEST_REAL_SIMILAR(purityscores[0].target_intensity, 9849578.5)
@@ -99,22 +91,22 @@ START_SECTION(static std::vector<PurityScores> computePrecursorPurities(const Pe
   TEST_EQUAL(purityscores[0].residual_peak_count, 0)
 
   TEST_REAL_SIMILAR(purityscores[1].total_intensity, 22845744.8125)
-  TEST_REAL_SIMILAR(purityscores[1].target_intensity, 19139751)
-  TEST_REAL_SIMILAR(purityscores[1].signal_proportion, 0.83778)
-  TEST_EQUAL(purityscores[1].target_peak_count, 4)
-  TEST_EQUAL(purityscores[1].residual_peak_count, 4)
+  TEST_REAL_SIMILAR(purityscores[1].target_intensity, 16314393.5)
+  TEST_REAL_SIMILAR(purityscores[1].signal_proportion, 0.71411)
+  TEST_EQUAL(purityscores[1].target_peak_count, 2)
+  TEST_EQUAL(purityscores[1].residual_peak_count, 6)
 
   TEST_REAL_SIMILAR(purityscores[2].total_intensity, 19751783.375)
-  TEST_REAL_SIMILAR(purityscores[2].target_intensity, 18752920.5)
-  TEST_REAL_SIMILAR(purityscores[2].signal_proportion, 0.94942)
-  TEST_EQUAL(purityscores[2].target_peak_count, 6)
-  TEST_EQUAL(purityscores[2].residual_peak_count, 3)
+  TEST_REAL_SIMILAR(purityscores[2].target_intensity, 16388424)
+  TEST_REAL_SIMILAR(purityscores[2].signal_proportion, 0.82971)
+  TEST_EQUAL(purityscores[2].target_peak_count, 2)
+  TEST_EQUAL(purityscores[2].residual_peak_count, 7)
 
   TEST_REAL_SIMILAR(purityscores[3].total_intensity, 23979143.35156)
-  TEST_REAL_SIMILAR(purityscores[3].target_intensity, 18107037.9375)
-  TEST_REAL_SIMILAR(purityscores[3].signal_proportion, 0.75511)
-  TEST_EQUAL(purityscores[3].target_peak_count, 4)
-  TEST_EQUAL(purityscores[3].residual_peak_count, 7)
+  TEST_REAL_SIMILAR(purityscores[3].target_intensity, 17510631.5)
+  TEST_REAL_SIMILAR(purityscores[3].signal_proportion, 0.73024)
+  TEST_EQUAL(purityscores[3].target_peak_count, 2)
+  TEST_EQUAL(purityscores[3].residual_peak_count, 9)
 
   TEST_REAL_SIMILAR(purityscores[4].total_intensity, 11964238)
   TEST_REAL_SIMILAR(purityscores[4].target_intensity, 11964238)
