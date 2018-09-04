c9ebfe447799f8cd43494c89ad230b4075894cce
enetz <eugen.netz@gmail.com>
2018-09-04 17:30:32 +0200

    [FIX] more robust usage of PrecursorPurity in OpenPepXLLF

--- src/openms/source/ANALYSIS/XLMS/OpenPepXLLFAlgorithm.cpp
+++ src/openms/source/ANALYSIS/XLMS/OpenPepXLLFAlgorithm.cpp
@@ -46,7 +46,6 @@
 #include <OpenMS/ANALYSIS/ID/IDMapper.h>
 #include <OpenMS/ANALYSIS/ID/PeptideIndexing.h>
 #include <OpenMS/MATH/STATISTICS/StatisticFunctions.h>
-
 #include <OpenMS/ANALYSIS/ID/PrecursorPurity.h>
 
 #include <OpenMS/CHEMISTRY/TheoreticalSpectrumGeneratorXLMS.h>
@@ -219,8 +218,21 @@ using namespace OpenMS;
 
     // preprocess spectra (filter out 0 values, sort by position)
     progresslogger.startProgress(0, 1, "Filtering spectra...");
-    spectra = OPXLSpectrumProcessingAlgorithms::preprocessSpectra(unprocessed_spectra, fragment_mass_tolerance_xlinks_, fragment_mass_tolerance_unit_ppm_, peptide_min_size_, min_precursor_charge_, max_precursor_charge_, deisotope, false);
+    vector<Size> discarded_spectra;
+    spectra = OPXLSpectrumProcessingAlgorithms::preprocessSpectra(unprocessed_spectra, fragment_mass_tolerance_xlinks_, fragment_mass_tolerance_unit_ppm_, peptide_min_size_, min_precursor_charge_, max_precursor_charge_, discarded_spectra, deisotope, false);
     progresslogger.endProgress();
+
+    // discard the precursor purities of discarded spectra
+    if (precursor_purities.size() > 0)
+    {
+      // cout << "Discarded spectra: " << discarded_spectra.size() << " | ";
+      for (Size discarded_index : discarded_spectra)
+      {
+        // cout << discarded_index << " | ";
+        precursor_purities.erase(precursor_purities.begin()+discarded_index);
+      }
+      // cout << endl;
+    }
 
     ProteaseDigestion digestor;
     digestor.setEnzyme(enzyme_name_);
@@ -701,20 +713,16 @@ using namespace OpenMS;
         csm.scan_index_light = scan_index;
         csm.scan_index_heavy = -1;
 
-        // default values to avoid too many "else {var = 0}" in the code below
-        csm.num_iso_peaks_mean = 0;
-        csm.num_iso_peaks_mean_linear_alpha = 0;
-        csm.num_iso_peaks_mean_linear_beta = 0;
-        csm.num_iso_peaks_mean_xlinks_alpha = 0;
-        csm.num_iso_peaks_mean_xlinks_beta = 0;
-
         csm.precursor_correction = cross_link_candidate.precursor_correction;
 
-        csm.precursor_total_intensity = precursor_purities[scan_index].total_intensity;
-        csm.precursor_target_intensity = precursor_purities[scan_index].target_intensity;
-        csm.precursor_signal_proportion = precursor_purities[scan_index].signal_proportion;
-        csm.precursor_target_peak_count = precursor_purities[scan_index].target_peak_count;
-        csm.precursor_residual_peak_count = precursor_purities[scan_index].residual_peak_count;
+        if (precursor_purities.size() > scan_index)
+        {
+          csm.precursor_total_intensity = precursor_purities[scan_index].total_intensity;
+          csm.precursor_target_intensity = precursor_purities[scan_index].target_intensity;
+          csm.precursor_signal_proportion = precursor_purities[scan_index].signal_proportion;
+          csm.precursor_target_peak_count = precursor_purities[scan_index].target_peak_count;
+          csm.precursor_residual_peak_count = precursor_purities[scan_index].residual_peak_count;
+        }
 
         // num_iso_peaks array from deisotoping
         if (deisotope)
