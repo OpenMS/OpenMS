4898b1ebd84dca500908f4d924ce55a1f78419d4
Chris Bielow <chris.bielow@fu-berlin.de>
2018-10-02 11:35:27 +0200

    [FEATURE] OpenSwathWorkflow: better checking of swath_window_file ranges before overwriting the data ranges (only narrowing is allowed; window range constraint: lower < upper) unless -force flag

--- src/openms/include/OpenMS/ANALYSIS/OPENSWATH/SwathWindowLoader.h
+++ src/openms/include/OpenMS/ANALYSIS/OPENSWATH/SwathWindowLoader.h
@@ -60,41 +60,46 @@ namespace OpenMS
   public:
 
     /**
-     * @brief Annotate a Swath map using a Swath window file specifying the individual windows
-     *
-     * @note It is assumed that the files in the swath_maps vector are in the
-     * same order as the windows in the provided file (usually from lowest to
-     * highest).
-     *
-     * @param filename The filename of the tab delimited file
-     * @param swath_maps The list of SWATH maps (assumed to be in the same order as in the file)
-     * @param doSort Sort the windows after reading in ascending order
-     * @throw Exception::IllegalArgument if the maps in the file and the provided input maps to not match
-     *
-     */
-    static void annotateSwathMapsFromFile(const std::string & filename,
-                                          std::vector< OpenSwath::SwathMap >& swath_maps,
-                                          bool doSort);
+       @brief Annotate a Swath map using a Swath window file specifying the individual windows
+     
+       @note It is assumed that the files in the swath_maps vector are in the
+       same order as the windows in the provided file (usually from lowest to
+       highest).
+
+       @param filename The filename of the tab delimited file
+       @param swath_maps The list of SWATH maps (assumed to be in the same order as in the file)
+       @param do_sort Sort the windows after reading in ascending order
+       @param force Force overriding the window boundaries, even if the new boundaries are wider than the data boundaries
+       @throw Exception::IllegalArgument if the number of maps in the file and the provided input maps to not match
+              , or if the new boundaries are outside of the data boundaries (unless force==true)
+    */
+    static void annotateSwathMapsFromFile(const std::string& filename,
+                                          std::vector<OpenSwath::SwathMap>& swath_maps,
+                                          bool do_sort,
+                                          bool force);
 
     /**
-     * @brief Reading a tab delimited file specifying the SWATH windows
-     *
-     * The file must of be tab delimited and of the following format:
-     *    window_lower window_upper
-     *    400 425
-     *    425 450
-     *    ...
-     *
-     * Note that the first line is a header and will be skipped.
-     *
-     * @param filename The filename of the tab delimited file
-     * @param swath_prec_lower_ The output vector for the window start
-     * @param swath_prec_upper_ The output vector for the window end
+      @brief Reading a tab delimited file specifying the SWATH windows
+     
+      The file must of be tab delimited and of the following format:
+         window_lower window_upper
+         400 425
+         425 450
+         ...
+     
+      Note that the first line is a header and will be skipped.
+     
+      @param filename The filename of the tab delimited file
+      @param swath_prec_lower_ The output vector for the window start
+      @param swath_prec_upper_ The output vector for the window end
+
+      @throw Exception::InvalidValue if window's start >= end
+
      *
      */
-    static void readSwathWindows(const std::string & filename,
-                                 std::vector<double> & swath_prec_lower_,
-                                 std::vector<double> & swath_prec_upper_ );
+    static void readSwathWindows(const std::string& filename,
+                                 std::vector<double>& swath_prec_lower,
+                                 std::vector<double>& swath_prec_upper);
   };
 }
 
--- src/pyOpenMS/pxds/SwathWindowLoader.pxd
+++ src/pyOpenMS/pxds/SwathWindowLoader.pxd
@@ -6,12 +6,12 @@ cdef extern from "<OpenMS/ANALYSIS/OPENSWATH/SwathWindowLoader.h>" namespace "Op
     cdef cppclass SwathWindowLoader:
 
         SwathWindowLoader() nogil except +
-        SwathWindowLoader(SwathWindowLoader) nogil except + 
+        SwathWindowLoader(SwathWindowLoader) nogil except +
 
         void annotateSwathMapsFromFile(libcpp_string filename,
-                                       libcpp_vector[ SwathMap ] & swath_maps, bool doSort) nogil except +
+                                       libcpp_vector[ SwathMap ]& swath_maps, bool do_sort, bool force) nogil except +
 
-        void readSwathWindows(libcpp_string filename, 
-                              libcpp_vector[double] & swath_prec_lower_,
-                              libcpp_vector[double] & swath_prec_upper_ ) nogil except +
+        void readSwathWindows(libcpp_string filename,
+                              libcpp_vector[double]& swath_prec_lower,
+                              libcpp_vector[double]& swath_prec_upper) nogil except +
 
