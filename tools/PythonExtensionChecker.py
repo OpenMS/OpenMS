97264774c59a322988b9641f24dd0f6b5b32c390
Hannes Roest <hannes.rost@utoronto.ca>
2018-10-10 16:48:54 -0400

    [FIX] fix pxd generation script

--- tools/PythonExtensionChecker.py
+++ tools/PythonExtensionChecker.py
@@ -482,7 +482,7 @@ class DoxygenXMLFile(object):
         res  = DoxygenCppFunction.generate_imports(imports_needed) # add default cimport
         res += includes
         res += cldef
-        if default_ctor:
+        if not default_ctor:
             res += "        %s() nogil except +\n" % comp_name.split("::")[-1]
         if not copy_ctor:
             res += "        %s(%s) nogil except + #wrap-ignore\n" % (comp_name.split("::")[-1], comp_name.split("::")[-1])
