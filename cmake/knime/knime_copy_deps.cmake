84d5a9702dca6a41f636a8efd491873aa96f558d
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2018-10-15 13:39:22 +0000

    Fixes for KNIME packaging. Shipping every non-system lib now.

--- cmake/knime/knime_copy_deps.cmake
+++ cmake/knime/knime_copy_deps.cmake
@@ -1,9 +1,9 @@
 INCLUDE(GetPrerequisites)
-message ("WTF")
-get_prerequisites(${DEPS} DEPENDENCIES 0 1 "" "${LOOKUP_DIRS}")
 
 get_filename_component(BIN_DIR ${DEPS} DIRECTORY)
 set(LOOKUP_DIRS "${BIN_DIR};${LOOKUP_DIRS}")
+
+get_prerequisites(${DEPS} DEPENDENCIES 0 1 "${BIN_DIR}" "${LOOKUP_DIRS}" "\$ORIGIN/../lib")
 
 foreach(DEPENDENCY_FILE ${DEPENDENCIES})
   ## Skip things like glibc, gomp, stdcpp
@@ -11,7 +11,12 @@ foreach(DEPENDENCY_FILE ${DEPENDENCIES})
   gp_resolve_item(${DEPS} "${DEPENDENCY_FILE}" "" "${LOOKUP_DIRS}" resolved_file)
   
   get_filename_component(resolved_file_nosymlink ${resolved_file} REALPATH)
+  get_filename_component(resolved_file_nosymlink_name ${resolved_file_nosymlink} NAME)
+  get_filename_component(resolved_filename ${resolved_file} NAME)
   message("realpath of resolved dependency " ${resolved_file} " is " ${resolved_file_nosymlink})
   file(COPY ${resolved_file_nosymlink} DESTINATION ${TARGET})
+  file(RENAME ${TARGET}/${resolved_file_nosymlink_name} ${TARGET}/${resolved_filename})
+  else()
+  message("Skipped system dependency " ${DEPENDENCY_FILE})
   endif()
 endforeach()
