b74cac1bdfdecc74e61d6f8bee5b59862851b965
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2018-10-18 15:20:58 +0200

    [NOP] indenting

--- src/tests/class_tests/openms/source/Multithreading_test.cpp
+++ src/tests/class_tests/openms/source/Multithreading_test.cpp
@@ -44,7 +44,7 @@
 
 // OpenMP support
 #ifdef _OPENMP
-	#include <omp.h>
+    #include <omp.h>
 #endif
 #include <limits>
 
@@ -61,42 +61,45 @@ START_TEST(Multithreading, "$Id$")
 /////////////////////////////////////////////////////////////
 
 START_SECTION((OpenMP test))
-{	
-	int wanted_threads = 1;
-	int threads = 1;
-	#ifdef _OPENMP
-	wanted_threads = 2;
-	omp_set_dynamic(0); // Explicitly disable dynamic teams
-	omp_set_num_threads(wanted_threads); // Use 2 threads for all consecutive parallel regions
-	threads = omp_get_max_threads();
-	#endif
+{
+  int wanted_threads = 1;
+  int threads = 1;
+  #ifdef _OPENMP
+  wanted_threads = 2;
+  omp_set_dynamic(0);  // Explicitly disable dynamic teams
+  // Use 2 threads for all consecutive parallel regions
+  omp_set_num_threads(wanted_threads);
+  threads = omp_get_max_threads();
+  #endif
 
-	int max = INT_MIN;
-	int i = 0;
-	#pragma omp parallel private(i)
-	{
-		int maxi = INT_MIN;
-		#pragma omp for
-		for (i = 0; i < 10; i++)
-		{
-		  #ifdef _OPENMP
-			int threadnum = omp_get_thread_num() + 1;
-		  #else
-			int threadnum = 1;
-		  #endif
+  int max = INT_MIN;
+  int i = 0;
+  #pragma omp parallel private(i)
+  {
+    int maxi = INT_MIN;
 
-			if (threadnum > maxi)
-				maxi = threadnum;
-		}
-		#pragma omp critical
-		{
-		if (maxi > max)
-			max = maxi;
-		}
-	}
+    #pragma omp for
+    for (i = 0; i < 10; i++)
+    {
+      #ifdef _OPENMP
+      int threadnum = omp_get_thread_num() + 1;
+      #else
+      int threadnum = 1;
+      #endif
 
-	TEST_EQUAL(threads, wanted_threads)
-	TEST_EQUAL(max, wanted_threads)
+      if (threadnum > maxi)
+        maxi = threadnum;
+    }
+
+    #pragma omp critical
+    {
+      if (maxi > max)
+        max = maxi;
+    }
+  }
+
+  TEST_EQUAL(threads, wanted_threads)
+  TEST_EQUAL(max, wanted_threads)
 }
 END_SECTION
 
