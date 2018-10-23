2af7d9ba673cca8dd81b8f8238a078b5bb36063c
JeeK <jeehyeong91@gmail.com>
2018-10-23 15:35:34 +0200

    generate interval - in progress - detect apexes : done - sortng with RT : in progress

--- src/utils/TopDownFeatureDeconv.cpp
+++ src/utils/TopDownFeatureDeconv.cpp
@@ -20,14 +20,17 @@
 #include <OpenMS/CONCEPT/Types.h>
 
 #include <OpenMS/FILTERING/DATAREDUCTION/MassTraceDetection.h>
+#include <OpenMS/FILTERING/DATAREDUCTION/ElutionPeakDetection.h>
 
-
+#ifdef _OPENMP
+#include <omp.h>
+#endif
 
 using namespace OpenMS;
 using namespace std;
 
 class TDfeatureDeconvolution :
-    public TOPPBase
+    public TOPPBase, public ProgressLogger
 {
 public:
     TDfeatureDeconvolution():
@@ -66,23 +69,17 @@ protected:
         cout << "Loaded consensus maps" << endl;
 
         //-------------------------------------------------------------
-        // calculations
-        //-------------------------------------------------------------
-        // mass trace part
-        cout << map.size() << endl;
-
-        vector<MassTrace> m_traces;
-
-        //-------------------------------------------------------------
         // set parameters
         //-------------------------------------------------------------
-
+        vector<MassTrace> m_traces;
         Param common_param = getParam_().copy("algorithm:common:", true);
         writeDebug_("Common parameters passed to sub-algorithms (mtd and ffm)", common_param, 3);
 
         Param mtd_param = getParam_().copy("algorithm:mtd:", true);
         writeDebug_("Parameters passed to MassTraceDetection", mtd_param, 3);
 
+        Param epd_param = getParam_().copy("algorithm:epd:", true);
+        writeDebug_("Parameters passed to ElutionPeakDetection", epd_param, 3);
 
         //-------------------------------------------------------------
         // configure and run mass trace detection
@@ -98,9 +95,54 @@ protected:
         mtdet.run(map, m_traces);  // m_traces : output of this function
 
         //-------------------------------------------------------------
+        // configure and run elution peak detection
+        //-------------------------------------------------------------
+
+//        std::vector<MassTrace> m_traces_final;
+
+        std::vector<MassTrace> splitted_mtraces;
+        epd_param.remove("enabled"); // artificially added above
+        epd_param.insert("", common_param);
+        ElutionPeakDetection epdet;
+        epdet.setParameters(epd_param);
+        // fill mass traces with smoothed data as well --> code exists in FeatureFinderMetabo.cpp (not using here)
+        // some filtering should work here, but removed for now. (22.10.18)
+
+        vector<pair<MassTrace, vector<MassTrace>>> mt_submt;
+        detectPeaks(epdet, m_traces, mt_submt);
+
+        //
+        cout << "mass traces" << endl;
+        cout << m_traces.size() << endl;
+        cout << m_traces[0][m_traces[0].findMaxByIntPeak()] << endl;
+        int cnt = 0;
+        for (auto &m_trace : m_traces) {
+            cnt += m_trace.getSize();
+        }
+        cout << cnt << endl;
+
+        cout << "elution detection" << endl;
+        cout << mt_submt.size() << endl;\
+        cout << mt_submt[0].first[mt_submt[0].first.findMaxByIntPeak()] << endl;
+        cout << mt_submt[0].second.size() << endl;
+
+        cnt = 0;
+        vector<int> maxINmt;
+        for (auto &i : mt_submt) {
+            maxINmt.push_back(i.second.size());
+            for (auto &j : i.second) {
+                cnt += j.getSize();
+            }
+        }
+        cout << (1.0 * accumulate(maxINmt.begin(), maxINmt.end(), 0LL) / maxINmt.size()) << endl;
+        cout << cnt << endl;
+
+
+
+        //-------------------------------------------------------------
         // calculate interval
         //-------------------------------------------------------------
-        generateInterval(m_traces);
+//        generateInterval(m_traces);
 
 
         //m_traces[0][0].getPosition()
@@ -130,6 +172,8 @@ protected:
         mfile << "};" << endl;
         mfile.close();
         */
+
+        return EXECUTION_OK;
     }
 
     void generateInterval(std::vector<MassTrace>& m_traces)
@@ -140,11 +184,11 @@ protected:
         float rw_wndw_size = 0.01; // delatRT
         int N = 5;
 
-        cout << m_traces[0].findMaxByIntPeak() << endl;
-        cout << m_traces[0][m_traces[0].findMaxByIntPeak()] << endl;
-        for(int j = 0; j<m_traces[0].getSize(); j++){
-            cout << m_traces[0][j].getRT() << "," << m_traces[0][j].getMZ() << "," << m_traces[0][j].getIntensity() << endl;
-        }
+//        cout << m_traces[0].findMaxByIntPeak() << endl;
+//        cout << m_traces[0][m_traces[0].findMaxByIntPeak()] << endl;
+//        for(int j = 0; j<m_traces[0].getSize(); j++){
+//            cout << m_traces[0][j].getRT() << "," << m_traces[0][j].getMZ() << "," << m_traces[0][j].getIntensity() << endl;
+//        }
 
         // find apexes
 
@@ -154,6 +198,39 @@ protected:
 //    }
 
     }
+
+
+    // this is from 'elutionPeakDetection'
+    void detectPeaks(ElutionPeakDetection& epdet, vector<MassTrace>& mt_vec, vector<pair<MassTrace, vector<MassTrace>>>& out_mtraces)
+    {
+        out_mtraces.clear();
+        vector<MassTrace> single_mtraces;
+
+        this->startProgress(0, mt_vec.size(), "elution peak detection");
+        Size progress(0);
+    #ifdef _OPENMP
+    #pragma omp parallel for
+    #endif
+
+        SignedSize mt_size = mt_vec.size();
+        for (SignedSize i = 0; i < mt_size; ++i)
+        {
+            IF_MASTERTHREAD this->setProgress(progress);
+
+    #ifdef _OPENMP
+    #pragma omp atomic
+    #endif
+            ++progress;
+
+            // push_back to 'single_mtraces' is protected, so threading is ok
+            epdet.detectPeaks(mt_vec[i], single_mtraces);
+            if( !single_mtraces.empty() )
+                out_mtraces.emplace_back( make_pair(mt_vec[i], single_mtraces) );
+        }
+
+        this->endProgress();
+    }
+
 };
 
 int main(int argc, const char** argv)
