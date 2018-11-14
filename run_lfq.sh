38eb09273039c54e425520fd6f3c9f807c2bb889
timosachsenberg <sachsenb@abinode05.Informatik.Uni-Tuebingen.De>
2018-11-14 09:13:35 +0100

    [FIX] basename in script

--- run_lfq.sh
+++ run_lfq.sh
@@ -9,28 +9,30 @@ export DATA_PATH="/home/sachsenb/OpenMS/openms-build/yasset_iPRG2015"
 # perform search, score calibration, and PSM-level q-value/PEP estimation
 for f in ${DATA_PATH}/*.mzML; do
   echo $f
+  fn=${f%.mzML} # filename and path without mzML extension
   # search
-  MSGFPlusAdapter -in ${f} -out ${f}.idXML -database ${DATA_PATH}/iPRG2015_decoy.fasta -executable ${MSGF_PATH} -max_precursor_charge 5 -threads 10
+  MSGFPlusAdapter -in ${f} -out ${fn}.idXML -database ${DATA_PATH}/iPRG2015_decoy.fasta -executable ${MSGF_PATH} -max_precursor_charge 5 -threads 10
   # annotate target/decoy and protein links
-  PeptideIndexer -fasta ${DATA_PATH}/iPRG2015_decoy.fasta  -in ${f}.idXML -out ${f}.idXML
+  PeptideIndexer -fasta ${DATA_PATH}/iPRG2015_decoy.fasta  -in ${fn}.idXML -out ${fn}.idXML -enzyme:specificity none
   # run percolator so we get well calibrated PEPs and q-values
-  PercolatorAdapter -in ${f}.idXML -out ${f}.idXML -percolator_executable percolator -post-processing-tdc -train-best-positive -subset-max-train 100000 
-  FalseDiscoveryRate -in ${f}.idXML -out ${f}.idXML -algorithm:add_decoy_peptides -algorithm:add_decoy_proteins 
+  PSMFeatureExtractor -in ${fn}.idXML -out ${fn}.idXML 
+  PercolatorAdapter -in ${fn}.idXML -out ${fn}.idXML -percolator_executable percolator -post-processing-tdc -subset-max-train 100000 
+  FalseDiscoveryRate -in ${fn}.idXML -out ${fn}.idXML -algorithm:add_decoy_peptides -algorithm:add_decoy_proteins 
   # pre-filter to 5% PSM-level FDR to reduce data
-  IDFilter -in ${f}.idXML -out ${f}.idXML -score:pep 0.05 
+  IDFilter -in ${fn}.idXML -out ${fn}.idXML -score:pep 0.05 
 done
 
 ###########################
 ProteomicsLFQ -in \
-${DATA_PATH}/JD_06232014_sample1_A.mzML  ${DATA_PATH}/JD_06232014_sample1_B.mzML  ${DATA_PATH}/JD_06232014_sample1_C.mzML \
-${DATA_PATH}/JD_06232014_sample2_A.mzML  ${DATA_PATH}/JD_06232014_sample2_B.mzML  ${DATA_PATH}/JD_06232014_sample2_C.mzML \
-${DATA_PATH}/JD_06232014_sample3_A.mzML  ${DATA_PATH}/JD_06232014_sample3_B.mzML  ${DATA_PATH}/JD_06232014_sample3_C.mzML \
-${DATA_PATH}/JD_06232014_sample4_A.mzML  ${DATA_PATH}/JD_06232014_sample4_B.mzML  ${DATA_PATH}/JD_06232014_sample4_C.mzML \
+${DATA_PATH}/JD_06232014_sample1_A.mzML ${DATA_PATH}/JD_06232014_sample1_B.mzML ${DATA_PATH}/JD_06232014_sample1_C.mzML \
+${DATA_PATH}/JD_06232014_sample2_A.mzML ${DATA_PATH}/JD_06232014_sample2_B.mzML ${DATA_PATH}/JD_06232014_sample2_C.mzML \
+${DATA_PATH}/JD_06232014_sample3_A.mzML ${DATA_PATH}/JD_06232014_sample3_B.mzML ${DATA_PATH}/JD_06232014_sample3_C.mzML \
+${DATA_PATH}/JD_06232014_sample4_A.mzML ${DATA_PATH}/JD_06232014_sample4_B.mzML ${DATA_PATH}/JD_06232014_sample4_C.mzML \
 -ids \
-${DATA_PATH}/JD_06232014_sample1_A.idXML  ${DATA_PATH}/JD_06232014_sample1_B.idXML  ${DATA_PATH}/JD_06232014_sample1_C.idXML \
-${DATA_PATH}/JD_06232014_sample2_A.idXML  ${DATA_PATH}/JD_06232014_sample2_B.idXML  ${DATA_PATH}/JD_06232014_sample2_C.idXML \
-${DATA_PATH}/JD_06232014_sample3_A.idXML  ${DATA_PATH}/JD_06232014_sample3_B.idXML  ${DATA_PATH}/JD_06232014_sample3_C.idXML \
-${DATA_PATH}/JD_06232014_sample4_A.idXML  ${DATA_PATH}/JD_06232014_sample4_B.idXML  ${DATA_PATH}/JD_06232014_sample4_C.idXML \
+${DATA_PATH}/JD_06232014_sample1_A.idXML ${DATA_PATH}/JD_06232014_sample1_B.idXML ${DATA_PATH}/JD_06232014_sample1_C.idXML \
+${DATA_PATH}/JD_06232014_sample2_A.idXML ${DATA_PATH}/JD_06232014_sample2_B.idXML ${DATA_PATH}/JD_06232014_sample2_C.idXML \
+${DATA_PATH}/JD_06232014_sample3_A.idXML ${DATA_PATH}/JD_06232014_sample3_B.idXML ${DATA_PATH}/JD_06232014_sample3_C.idXML \
+${DATA_PATH}/JD_06232014_sample4_A.idXML ${DATA_PATH}/JD_06232014_sample4_B.idXML ${DATA_PATH}/JD_06232014_sample4_C.idXML \
 -design ${DATA_PATH}/experimental_design.tsv \
 -Alignment:max_rt_shift 0.1 \
 -fasta ${DATA_PATH}/iPRG2015_decoy.fasta -targeted_only "true" \
