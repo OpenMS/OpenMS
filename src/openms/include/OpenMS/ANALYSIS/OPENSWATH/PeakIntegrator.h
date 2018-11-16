10b89b57f76c7bab10fee031825532f9acf04c9c
lars20070 <l.nilse@dunelm.org.uk>
2018-11-16 09:34:02 +0100

    [DOCU] negative areas documented

--- src/openms/include/OpenMS/ANALYSIS/OPENSWATH/PeakIntegrator.h
+++ src/openms/include/OpenMS/ANALYSIS/OPENSWATH/PeakIntegrator.h
@@ -64,6 +64,10 @@ namespace OpenMS
     metrics are supported.
 
     The containers supported by the methods are MSChromatogram and MSSpectrum.
+    
+    NB: integratePeak() using Simpson's rule can result in negative areas despite
+    strictly postive intensities in the input dataset. An example is given in
+    the class test (see area = -665788.77). 
   */
   class OPENMS_DLLAPI PeakIntegrator :
     public DefaultParamHandler
