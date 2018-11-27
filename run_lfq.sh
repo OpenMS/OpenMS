861d48db73b67a73de881c85275471ee6e5288f4
timosachsenberg <sachsenb@abinode05.Informatik.Uni-Tuebingen.De>
2018-11-27 14:37:20 +0100

    [FIX] annotate PEP correctly in bash script

--- run_lfq.sh
+++ run_lfq.sh
@@ -1,17 +1,18 @@
 #!bin/bash
-export PATH="$PATH:/home/sachsenb/OpenMS/openms-build/bin"
+export PATH="$PATH:/home/sachsenb/OpenMS/openms-build-tmp/bin"
 export PATH="$PATH:/home/sachsenb/OpenMS/THIRDPARTY/Linux/64bit/Percolator"
 
 export MSGF_PATH="/home/sachsenb/OpenMS/THIRDPARTY/All/MSGFPlus/MSGFPlus.jar"
 #input data
 export DATA_PATH="/home/sachsenb/OpenMS/openms-build/yasset_iPRG2015"
 
+
 # perform search, score calibration, and PSM-level q-value/PEP estimation
 for f in ${DATA_PATH}/*.mzML; do
   echo $f
   fn=${f%.mzML} # filename and path without mzML extension
-  # search
-  MSGFPlusAdapter -in ${f} -out ${fn}.idXML -database ${DATA_PATH}/iPRG2015_decoy.fasta -executable ${MSGF_PATH} -max_precursor_charge 5 -threads 10
+  # search with default fixed and variable mods
+  MSGFPlusAdapter -in ${f} -out ${fn}.idXML -database ${DATA_PATH}/iPRG2015_decoy.fasta -executable ${MSGF_PATH} -max_precursor_charge 5 -threads 10 
   # annotate target/decoy and protein links
   PeptideIndexer -fasta ${DATA_PATH}/iPRG2015_decoy.fasta  -in ${fn}.idXML -out ${fn}.idXML -enzyme:specificity none
   # run percolator so we get well calibrated PEPs and q-values
@@ -21,7 +22,7 @@ for f in ${DATA_PATH}/*.mzML; do
   # pre-filter to 5% PSM-level FDR to reduce data
   IDFilter -in ${fn}.idXML -out ${fn}.idXML -score:pep 0.05 
   # switch to PEP score
-  IDScoreSwitcher -in ${fn}.idXML -out ${fn}.idXML -old_score q-value -new_score MS:1001493 -new_score_orientation lower_better -new_score "Posterior Error Probability" 
+  IDScoreSwitcher -in ${fn}.idXML -out ${fn}.idXML -old_score q-value -new_score MS:1001493 -new_score_orientation lower_better -new_score_type "Posterior Error Probability" 
 done
 
 ###########################
