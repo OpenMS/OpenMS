e9810b3c5711e139bdb313f128fc1af22a09f3f6
Hannes Roest <hannes.rost@utoronto.ca>
2018-11-30 10:59:11 -0500

    [BUILD] final build script

--- src/pyOpenMS/dist-scripts/create-manylinux.sh
+++ src/pyOpenMS/dist-scripts/create-manylinux.sh
@@ -9,28 +9,25 @@
 
 set -e
 
-# Note that we cannot use git any more as it refuses to communicate with
-# github, but we have a patched version of wget with a newer OpenSSL version
-# capable of downloading the release tar file.
-wget https://github.com/OpenMS/OpenMS/releases/download/Release2.4.0/OpenMS-2.4.0-src.tar.gz -O OpenMS-2.4.0-src.tar.gz
-tar xzf OpenMS-2.4.0-src.tar.gz
-mv OpenMS-2.4.0/ OpenMS
+yum -y install zip
+
+git clone -b Release2.4.0 https://github.com/OpenMS/OpenMS.git
 
 # Apply a patches / fixes
-# 1. Updated readme (due to pypi.org not displaying markdown)
-# 2. Updated version number
+# 1. Update version number
+# 2. Patch setup.py to ensure we use single-threaded version (3.6/3.7 stalling bug)
 # 3. Patch init (since the linked libraries are all in .lib)
 cd OpenMS
 sed -i 's/@CF_OPENMS_PACKAGE_VERSION@/2.4.0/' src/pyOpenMS/env.py.in
+sed -i 's/single_threaded = False/single_threaded = True/' src/pyOpenMS/setup.py
 patch -p0 src/pyOpenMS/pyopenms/__init__.py < /data/manylinux.patch 
 cd /
-
 
 # install Python deps
 for PYBIN in /opt/python/cp27* /opt/python/cp3[4-9]*; do
   "$PYBIN/bin/pip" install -U Cython
   "$PYBIN/bin/pip" install -U setuptools
-  "$PYBIN/bin/pip" install -U wheel
+  "$PYBIN/bin/pip" install wheel==0.31.1 # bug in newer wheel
   "$PYBIN/bin/pip" install -U numpy
   "$PYBIN/bin/pip" install -U nose
   "$PYBIN/bin/pip" install -U autowrap
@@ -44,14 +41,15 @@ for PYBIN in /opt/python/cp27* /opt/python/cp3[4-9]*; do
   PYVER=`basename $PYBIN`
   mkdir /openms-build-$PYVER
   cd /openms-build-$PYVER
-  # configure (no OpenMP as this can lead to issues with Python) 
+  # configure (no OpenMP as this can lead to issues with Python)
   cmake -DCMAKE_PREFIX_PATH="/contrib-build/;/qt/" -DPYOPENMS=On -DPYTHON_EXECUTABLE:FILEPATH=$PYBIN/bin/python \
-    -DMT_ENABLE_OPENMP=OFF \
+    -DMT_ENABLE_OPENMP=OFF -DPY_NO_OUTPUT=Off -DPY_SINGLE_THREADED=On \
     -DQT_QMAKE_EXECUTABLE=/qt/bin/qmake ../OpenMS
   make -j6 pyopenms
 
   # ensure auditwheel can find the libraries
   export LD_LIBRARY_PATH=`pwd`/lib
+
   # strip the libraries
   strip --strip-all lib/libOpenMS.so 
   strip --strip-all lib/libOpenSwathAlgo.so 
@@ -60,13 +58,35 @@ for PYBIN in /opt/python/cp27* /opt/python/cp3[4-9]*; do
   # remove the libraries as auditwheel will take care of linked libs
   rm -rf pyopenms/lib*
   rm -rf build/lib*/pyopenms/lib*
-
   "$PYBIN/bin/pip" wheel . -w wheelhouse_tmp
 
   # Bundle external shared libraries into the wheels
   for whl in wheelhouse_tmp/pyopenms*.whl; do
     auditwheel repair "$whl" -w wheelhouse/
   done
-  mv wheelhouse/* /data/wheelhouse/
+
+  # We need to fix the Qt libraries as auditwheel seems to strip the qt
+  # libraries which produces problems
+  unalias cp
+  for WHEEL in wheelhouse/*.whl; do
+    mkdir -p wheelhouse/fixed
+    mkdir /tmp/pytmp
+    cp $WHEEL /tmp/pytmp/
+    WHEEL=`basename $WHEEL`
+    cp /qt/lib/libQt5Core.so /tmp/pytmp/
+    cp /qt/lib/libQt5Network.so /tmp/pytmp/
+    cd /tmp/pytmp
+    unzip $WHEEL
+    # Replace the Qt libraries with the original ones
+    cp libQt5Core.so pyopenms/.libs/libQt5Core*
+    cp libQt5Network.so pyopenms/.libs/libQt5Network*
+    rm -rf pyopenms/lib*.so
+    zip -r $WHEEL pyopenms/ pyopenms*dist*/
+    cd -
+    cp /tmp/pytmp/$WHEEL wheelhouse/fixed/
+    rm -rf /tmp/pytmp
+  done
+
+  mv wheelhouse/fixed/* /data/wheelhouse/
 done
 
