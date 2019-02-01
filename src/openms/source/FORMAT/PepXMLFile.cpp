24bb0ed595e1d7c375109d73c5eb9b5559df049e
oliveralka <randalthor89@gmx.net>
2019-02-01 11:57:07 +0100

    [FIX] PepXMLFile, Percolator pepe score, LuciphorAdapter

--- src/openms/source/FORMAT/PepXMLFile.cpp
+++ src/openms/source/FORMAT/PepXMLFile.cpp
@@ -577,7 +577,7 @@ namespace OpenMS
           {
             f << "\t\t\t<search_score" << " name=\"" << it->getScoreType() << "\" value=\"" << h.getScore() << "\"" << "/>\n";
           }
-          if (it->getScoreType() == "Posterior Error Probability")
+          if (it->getScoreType() == "Posterior Error Probability" || it->getScoreType() == "pep")
           {
             f << "\t\t\t<search_score" << " name=\"" << it->getScoreType() << "\" value=\"" << h.getScore() << "\"" << "/>\n";
             double probability = 1.0 - h.getScore();
