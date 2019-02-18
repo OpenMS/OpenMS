00bf3a476601ea81c4c55bfff059a2bb910027b6
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2019-02-18 17:18:28 +0100

    Apparently gcc<6 does not support this kind of init. Use make_tuple.
    Signed-off-by: Julianus Pfeuffer <jpfeuffer@fu-berlin.de>

--- src/openms/source/ANALYSIS/ID/BayesianProteinInferenceAlgorithm.cpp
+++ src/openms/source/ANALYSIS/ID/BayesianProteinInferenceAlgorithm.cpp
@@ -232,9 +232,9 @@ namespace OpenMS
 
           auto posteriorFactors = bpie.estimate_posteriors_in_steps(posteriorVars,
               {
-            {std::max<unsigned long>(10000ul, nrEdges*nrEdges*2ul), initDampeningLambda, initConvergenceThreshold},
-            {nrEdges*nrEdges, std::min(0.5,initDampeningLambda*10), std::min(0.01,initConvergenceThreshold*10)},
-            {nrEdges*nrEdges/2ul, std::min(0.5,initDampeningLambda*100), std::min(0.01,initConvergenceThreshold*100)}
+                  std::make_tuple(std::max<unsigned long>(10000ul, nrEdges*nrEdges*2ul), initDampeningLambda, initConvergenceThreshold),
+                  std::make_tuple(nrEdges*nrEdges, std::min(0.5,initDampeningLambda*10), std::min(0.01,initConvergenceThreshold*10)),
+                  std::make_tuple(nrEdges*nrEdges/2ul, std::min(0.5,initDampeningLambda*100), std::min(0.01,initConvergenceThreshold*100))
               });
 
           // TODO move the writing of statistics from IDBoostGraph here and write more stats
