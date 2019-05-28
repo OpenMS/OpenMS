dc39ef9d8451fc7db7edb344dcce93e3dc7dac87
Hannes Roest <hannes.rost@utoronto.ca>
2019-05-28 15:02:54 -0400

    [FIX] docu fix (#4134)

--- src/openms/include/OpenMS/ANALYSIS/RNPXL/RNPxlFragmentAnnotationHelper.h
+++ src/openms/include/OpenMS/ANALYSIS/RNPXL/RNPxlFragmentAnnotationHelper.h
@@ -49,9 +49,10 @@ namespace OpenMS
 namespace Internal
 {
 
-/* @brief Convenience functions to construct appealing fragment annotation strings
-  *       and store them as PeptideHit::PeakAnnotation
- *
+/** 
+   @brief Convenience functions to construct appealing fragment annotation strings
+         and store them as PeptideHit::PeakAnnotation
+ 
  */
 class OPENMS_DLLAPI RNPxlFragmentAnnotationHelper
 {
@@ -87,7 +88,7 @@ class OPENMS_DLLAPI RNPxlFragmentAnnotationHelper
 
   static String getAnnotatedImmoniumIon(char c, const String& fragment_shift_name);
 
-  // conversion of RNPxl annotations to PeptideHit::PeakAnnotation
+  /// conversion of RNPxl annotations to PeptideHit::PeakAnnotation
   static std::vector<PeptideHit::PeakAnnotation> fragmentAnnotationDetailsToPHFA(
     const String& ion_type, 
     std::map<Size, std::vector<FragmentAnnotationDetail_> > ion_annotation_details);
