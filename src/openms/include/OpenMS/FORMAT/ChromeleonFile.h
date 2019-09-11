d281e5ab9ad9a8724025cdf047735bdf65d50a0a
Pasquale Domenico Colaianni <pasdom@biosustain.dtu.dk>
2019-09-11 14:14:06 +0200

    docs: Document removeCommasAndParseDouble()

--- src/openms/include/OpenMS/FORMAT/ChromeleonFile.h
+++ src/openms/include/OpenMS/FORMAT/ChromeleonFile.h
@@ -68,6 +68,13 @@ public:
     */
     void load(const String& filename, MSExperiment& experiment) const;
 
+    /**
+      @brief Remove commas from the string (used as thousands separators) and
+      parse its value
+
+      @param[in] number A string representing a floating-point number
+      @return The value converted to `double`
+    */
     double removeCommasAndParseDouble(String number) const;
   };
 }
