3f03a0eea21d3fe50c9e32d5a8e914a640dc0468
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2019-12-05 14:28:15 +0100

    Revert "Forgot code examples"
    This reverts commit 7e3cf2a6eab1cf1d84f54c2cb79a4f6c95c1781c.

--- doc/code_examples/Tutorial_Element.cpp
+++ doc/code_examples/Tutorial_Element.cpp
@@ -39,7 +39,7 @@ using namespace std;
 
 Int main()
 {
-  const ElementDB * db = getElementDBInstance();
+  const ElementDB * db = ElementDB::getInstance();
 
   // extract carbon element from ElementDB
   // .getResidue("C") would work as well
--- doc/code_examples/Tutorial_EmpiricalFormula.cpp
+++ doc/code_examples/Tutorial_EmpiricalFormula.cpp
@@ -46,7 +46,7 @@ Int main()
   EmpiricalFormula sum = methanol + water;
 
   // get element from ElementDB
-  const Element * carbon = getElementDBInstance()->getElement("Carbon");
+  const Element * carbon = ElementDB::getInstance()->getElement("Carbon");
 
   // output number of carbon atoms and average weight 
   cout << sum << " "
