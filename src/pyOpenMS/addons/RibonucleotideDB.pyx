b4b4da41e388ff054bae78a2e35efdd53d4b8139
Hannes Roest <hannes.rost@utoronto.ca>
2020-01-08 21:45:56 -0500

    [FIX] db fix

--- src/pyOpenMS/addons/RibonucleotideDB.pyx
+++ src/pyOpenMS/addons/RibonucleotideDB.pyx
@@ -2,6 +2,18 @@
 
 
 
+    # NOTE: using shared_ptr for a singleton will lead to segfaults, use raw ptr instead
+    # cdef AutowrapPtrHolder[_RibonucleotideDB] inst
+
+    def __init__(self):
+      self.inst = AutowrapPtrHolder[_RibonucleotideDB](_getInstance_RibonucleotideDB())
+
+    def __dealloc__(self):
+      # Careful here, the wrapped ptr is a single instance and we should not
+      # reset it, therefore use 'wrap-manual-dealloc'
+      pass
+
+
     def getRibonucleotideAlternatives(self, bytes code ):
         """Cython signature: libcpp_pair[const Ribonucleotide *,const Ribonucleotide *] getRibonucleotideAlternatives(const libcpp_string & code)"""
         assert isinstance(code, bytes), 'arg code wrong type'
