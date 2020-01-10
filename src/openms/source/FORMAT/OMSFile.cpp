3c747050d876cc92eda2157ddd986bd77cfae547
Hendrik Weisser <hendrik.weisser@stormtherapeutics.com>
2020-01-10 14:02:11 +0000

    [COMMENT] try disabling constraint checks when writing .oms files (for performance)?

--- src/openms/source/FORMAT/OMSFile.cpp
+++ src/openms/source/FORMAT/OMSFile.cpp
@@ -92,6 +92,8 @@ namespace OpenMS
       raiseDBError_(query.lastError(), __LINE__, OPENMS_PRETTY_FUNCTION,
                     "error configuring database");
     }
+    // @TODO: could also try disabling checks ("PRAGMA ignore_check_constraints
+    // = ON"), but limited testing has shown no apparent performance benefit
   }
 
 
