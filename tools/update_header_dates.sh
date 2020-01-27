94790282ebf0b65932da4a34a03f16eace253f77
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2020-01-27 16:39:52 +0100

    Changed copyright year.

--- tools/update_header_dates.sh
+++ tools/update_header_dates.sh
@@ -1,2 +1,3 @@
-find . -type f | xargs sed -i 's/and Freie Universitaet Berlin 2002-20../and Freie Universitaet Berlin 2002-2018/g'
-## For all files and on MacOS: find . -type f ! -path "./.git/*" -exec sed -i '' -e 's/and Freie Universitaet Berlin 2002-20../and Freie Universitaet Berlin 2002-2018/g' {} \;
+#find . -type f | xargs sed -i 's/and Freie Universitaet Berlin 2002-2020/and Freie Universitaet Berlin 2002-2020/g'
+## For all files and on MacOS: 
+LC_ALL=C find . -type f ! -path "./.git/*" -exec grep -q "Freie Universitaet Berlin 2002-20" {} \; -exec sed -i '' -e 's/and Freie Universitaet Berlin 2002-2020/and Freie Universitaet Berlin 2002-2020/g' {} \;
