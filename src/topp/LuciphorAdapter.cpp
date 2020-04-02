ba75a4affbfd69ba7dd7a3eca9053bae76c14d1e
timosachsenberg <sachsenb@abinode05.Informatik.Uni-Tuebingen.De>
2020-04-02 14:51:17 +0200

    map spectra to ids by native ids in Luciphor

--- src/topp/LuciphorAdapter.cpp
+++ src/topp/LuciphorAdapter.cpp
@@ -613,7 +613,18 @@ protected:
     
     for (vector<PeptideIdentification>::iterator pep_id = pep_ids.begin(); pep_id != pep_ids.end(); ++pep_id)
     {
-      Size scan_idx = lookup.findByRT(pep_id->getRT());
+      Size scan_idx;
+      const String& ID_native_ids = pep_id->getMetaValue("spectrum_reference");
+      try
+      {
+        scan_idx = lookup.findByNativeID(ID_native_ids);
+      }
+      catch (Exception::ElementNotFound&)
+      {
+        // fall-back if native ids are missing
+        writeLog_("Unable to map native ID of identification to spectrum native ID. " + ID_native_ids);
+        scan_idx = lookup.findByRT(pep_id->getRT());
+      }
 
       vector<PeptideHit> scored_peptides;
       if (!pep_id->getHits().empty())
