fccdfa8424114c1fa38db30b81682451240b2880
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2020-05-04 18:58:26 +0200

    [Feature] refactor PeakPickers (#4695)
    * add auto mode and moves to PPHiRes (lowmem mode)
    
    * removed standalone lowmem version of PP
    
    * remove tests. One used the same files, the other was just indexedMzML which is also already
    covered.

--- src/topp/PeakPickerHiRes.cpp
+++ src/topp/PeakPickerHiRes.cpp
@@ -135,7 +135,7 @@ protected:
   public:
 
     PPHiResMzMLConsumer(String filename, const PeakPickerHiRes& pp) :
-      MSDataWritingConsumer(filename),
+      MSDataWritingConsumer(std::move(filename)),
       ms_levels_(pp.getParameters().getValue("ms_levels").toIntList())
     {
       pp_ = pp;
@@ -143,18 +143,25 @@ protected:
 
     void processSpectrum_(MapType::SpectrumType& s) override
     {
-      if (!ListUtils::contains(ms_levels_, s.getMSLevel())) {return;}
+      if (ms_levels_.empty()) //auto mode
+      {
+        if (s.getType() == SpectrumSettings::CENTROID) return;
+      }
+      else if (!ListUtils::contains(ms_levels_, s.getMSLevel()))
+      {
+        return;
+      }
 
       MapType::SpectrumType sout;
       pp_.pick(s, sout);
-      s = sout;  // todo: swap? (requires implementation)
+      s = std::move(sout);
     }
 
     void processChromatogram_(MapType::ChromatogramType & c) override
     {
       MapType::ChromatogramType c_out;
       pp_.pick(c, c_out);
-      c = c_out;
+      c = std::move(c_out);
     }
 
   private:
