eb43f613da0856ace328716cbfea4182b6245ac8
Leon Bichmann <bichmann@informatik.uni-tuebingen.de>
2020-05-07 15:44:17 +0200

    update log

--- src/topp/PercolatorAdapter.cpp
+++ src/topp/PercolatorAdapter.cpp
@@ -1215,12 +1215,14 @@ protected:
 
       if (!peptide_level_fdrs)
       {
-      OPENMS_LOG_INFO << "Suitable PSM Hits for " << cnt << " of " << all_peptide_ids.size() <<  " PSMs found." << endl;
+      OPENMS_LOG_INFO << "PSM-level FDR: The all PSMs per Peptide should be returned by percolator. Reannotating the all PSMs in input data with percolator output." << endl;
       }
       else
       {
-      OPENMS_LOG_INFO << "Suitable PeptideHits for " << cnt << " of " << all_peptide_ids.size() <<  " PSMs found." << endl;
+      OPENMS_LOG_INFO << "Peptide-level FDR: Only the best PSM per Peptide was returned by percolator. Reannotating the best PSM in input data with percolator output." << endl;
       }
+      OPENMS_LOG_INFO << "Scores of all other PSMs will be set to 1" << endl;
+      OPENMS_LOG_INFO << cnt << " suitable PeptideHits of " << all_peptide_ids.size() <<  " PSMs were reannotated." << endl;
 
       // TODO: There should only be 1 ProteinIdentification element in this vector, no need for a for loop
       for (vector<ProteinIdentification>::iterator it = all_protein_ids.begin(); it != all_protein_ids.end(); ++it)
