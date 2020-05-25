626c021c64a187d0df2873047fcae264279a9e96
Anton Kriese <anton.kriese@gmx.de>
2020-05-25 15:08:03 +0200

    explicitly check for nullptr

--- src/openms/source/FORMAT/HANDLERS/MzMLHandler.cpp
+++ src/openms/source/FORMAT/HANDLERS/MzMLHandler.cpp
@@ -3449,10 +3449,10 @@ namespace OpenMS
 
         if (*key == "GO cellular component" || *key == "brenda source tissue")
         {
-          // the CVTerm info is in the value
+          // the CVTerm info is in the meta value
           const ControlledVocabulary::CVTerm* c = cv_.checkAndGetTermByName(meta.getMetaValue(*key));
 
-          if (c)
+          if (c != nullptr)
           {
             // TODO: validate CV, we currently cannot do this as the relations in the BTO and GO are not captured by our CV impl
             cvParams.push_back(writeCV_(*c, DataValue::EMPTY));
@@ -3462,7 +3462,7 @@ namespace OpenMS
         {
           bool writtenAsCVTerm = false;
           const ControlledVocabulary::CVTerm* c = cv_.checkAndGetTermByName(*key);
-          if (c)
+          if (c != nullptr)
           {
             if (validateCV_(*c, path, validator))
             {
