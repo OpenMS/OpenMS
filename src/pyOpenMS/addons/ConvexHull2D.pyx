d533744dfa31834411ac14b94ccfd784f149f2a5
Shekhar Shukla <nk.shekhar24@gmail.com>
2020-08-19 21:31:23 +0530

    Revert "Update function names in addons/ConvexHull2D.pyx"
    This reverts commit 633b977f33ff5699e087adae6089cf3c8c4ea0b7.

--- src/pyOpenMS/addons/ConvexHull2D.pyx
+++ src/pyOpenMS/addons/ConvexHull2D.pyx
@@ -3,26 +3,15 @@ from DPosition cimport DPosition2 as _DPosition2
 from DBoundingBox cimport DBoundingBox2 as _DBoundingBox2
 
 
-    def enclosesXY(self, float x, float y):
-        """
-        Parameters:
-        x (float)
-        y (float)
-        
-        Returns:
-        int
-        """
+    def encloses(self, float x, float y):
+
         cdef _DPosition2 pos
         pos[0] = x
         pos[1] = y
         return self.inst.get().encloses(pos)
 
 
-    def getHullPointsNPY(self):
-        """
-        Returns:
-        result (np.ndarray[np.float32_t, ndim=2])
-        """
+    def getHullPoints(self):
         cdef libcpp_vector[_DPosition2] points = self.inst.get().getHullPoints()
         cdef np.ndarray[np.float32_t, ndim=2] result
         cdef n = points.size()
@@ -36,11 +25,7 @@ from DBoundingBox cimport DBoundingBox2 as _DBoundingBox2
             i += 1
         return result
 
-    def setHullPointsNPY(self, np.ndarray[np.float32_t, ndim=2] points):
-        """
-        Parameters:
-        points (np.ndarray[np.float32_t, ndim=2])
-        """
+    def setHullPoints(self, np.ndarray[np.float32_t, ndim=2] points):
         cdef _ConvexHull2D * hull = self.inst.get()
         cdef int N = points.shape[0]
         cdef int i
@@ -52,32 +37,19 @@ from DBoundingBox cimport DBoundingBox2 as _DBoundingBox2
             vec.push_back(p)
         self.inst.get().setHullPoints(vec)
 
-    def getBoundingBox2D(self):
-        """
-        Returns:
-        ((double,double),(double,double))
-        """
+    def getBoundingBox(self):
         cdef _DBoundingBox2 box = self.inst.get().getBoundingBox()
         cdef _DPosition2 minp = box.minPosition()
         cdef _DPosition2 maxp = box.maxPosition()
         return (minp[0], minp[1]), (maxp[0], maxp[1])
 
-    def addPointXY(self, x, y):
-        """
-        Parameters:
-        x (double)
-        y (double)
-        """
+    def addPoint(self, x, y):
         cdef _DPosition2 p
         p[0] = x
         p[1] = y
         self.inst.get().addPoint(p)
 
-    def addPointsNPY(self, np.ndarray[np.float32_t, ndim=2] points):
-        """
-        Parameters:
-        points (np.ndarray[np.float32_t, ndim=2])
-        """
+    def addPoints(self, np.ndarray[np.float32_t, ndim=2] points):
         cdef _ConvexHull2D * hull = self.inst.get()
         cdef int N = points.shape[0]
         cdef int i
@@ -88,4 +60,5 @@ from DBoundingBox cimport DBoundingBox2 as _DBoundingBox2
             p[1] = points[i,1]
             vec.push_back(p)
         self.inst.get().addPoints(vec)
+
 
