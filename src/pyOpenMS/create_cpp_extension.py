32709e48338230450d0acc2f38dcf1bba9629d3b
jpfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2020-09-09 16:35:37 +0200

    revert import change

--- src/pyOpenMS/create_cpp_extension.py
+++ src/pyOpenMS/create_cpp_extension.py
@@ -210,7 +210,7 @@ print("created pyopenms.cpp")
 
 with open("pyopenms/all_modules.py", "w") as fp:
     for modname in mnames:
-        fp.write("from pyopenms.%s import *\n" % modname)
+        fp.write("from .%s import *\n" % modname)
 
 
 # create version information
