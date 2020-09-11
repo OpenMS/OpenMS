090f8b158993851103857a62d2a069083b579af9
Chris Bielow <chris.bielow@fu-berlin.de>
2020-09-11 15:37:58 +0200

    * some convenience functions for FileTypes    * avoid std::map include

--- src/openms/source/FORMAT/FileTypes.cpp
+++ src/openms/source/FORMAT/FileTypes.cpp
@@ -29,137 +29,141 @@
 //
 // --------------------------------------------------------------------------
 // $Maintainer: Timo Sachsenberg $
-// $Authors: Stephan Aiche, Andreas Bertsch, Marc Sturm $
+// $Authors: Stephan Aiche, Andreas Bertsch, Marc Sturm, Chris Bielow $
 // --------------------------------------------------------------------------
 
 #include <OpenMS/FORMAT/FileTypes.h>
 
+#include <OpenMS/CONCEPT/Exception.h>
+
+#include <array>
+
 namespace OpenMS
 {
+
+  static FileTypes::FileTypeList<1> test_type_list({ FileTypes::MZML });
+  /// connect the type to some other information
+  /// We could also use paired arrays, but this way, its less likely to have mismatches if a new type is added
+  struct TypeNameBinding
+  {
+    FileTypes::Type type;
+    String name;
+    String description;
+    TypeNameBinding(FileTypes::Type type, String name, String description)
+      : type(type), name(name), description(description)
+    {
+    }
+  };
+  
+  /// Maps the FileType::Type to the preferred extension.
+  static const std::array<TypeNameBinding, FileTypes::SIZE_OF_TYPE> type_with_annotation__ =
+  {
+    TypeNameBinding(FileTypes::UNKNOWN, "unknown", "unknown file extension"),
+    TypeNameBinding(FileTypes::DTA, "dta", "dta raw data file"),
+    TypeNameBinding(FileTypes::DTA2D, "dta2d", "dta2d raw data file"),
+    TypeNameBinding(FileTypes::MZDATA, "mzData", "mzData raw data file"),
+    TypeNameBinding(FileTypes::MZXML, "mzXML", "mzXML raw data file"),
+    TypeNameBinding(FileTypes::FEATUREXML, "featureXML", "OpenMS feature map"),
+    TypeNameBinding(FileTypes::IDXML, "idXML", "OpenMS peptide identification  file"),
+    TypeNameBinding(FileTypes::CONSENSUSXML, "consensusXML", "OpenMS consensus feature map"),
+    TypeNameBinding(FileTypes::MGF, "mgf", "mascot generic format file"),
+    TypeNameBinding(FileTypes::INI, "ini", "OpenMS parameter file"),
+    TypeNameBinding(FileTypes::TOPPAS, "toppas", "OpenMS TOPPAS pipeline"),
+    TypeNameBinding(FileTypes::TRANSFORMATIONXML, "trafoXML", "RT transformation file"),
+    TypeNameBinding(FileTypes::MZML, "mzML", "mzML raw data file"),
+    TypeNameBinding(FileTypes::CACHEDMZML, "cachedMzML", "cachedMzML raw data file"),
+    TypeNameBinding(FileTypes::MS2, "ms2", "ms2 file"),
+    TypeNameBinding(FileTypes::PEPXML, "pepXML", "pepXML file"),
+    TypeNameBinding(FileTypes::PROTXML, "protXML", "protXML file"),
+    TypeNameBinding(FileTypes::MZIDENTML, "mzid", "mzIdentML file"),
+    TypeNameBinding(FileTypes::MZQUANTML, "mzq", "mzQuantML file"),
+    TypeNameBinding(FileTypes::QCML, "qcml", "quality control file"),
+    TypeNameBinding(FileTypes::GELML, "gelML", "gelML file"),
+    TypeNameBinding(FileTypes::TRAML, "traML", "transition file"),
+    TypeNameBinding(FileTypes::MSP, "msp", "NIST spectra library file format"),
+    TypeNameBinding(FileTypes::OMSSAXML, "omssaXML", "omssaXML file"),
+    TypeNameBinding(FileTypes::MASCOTXML, "mascotXML", "mascotXML file"),
+    TypeNameBinding(FileTypes::PNG, "png", "portable network graphics file"),
+    TypeNameBinding(FileTypes::XMASS, "fid", "XMass analysis file"),
+    TypeNameBinding(FileTypes::TSV, "tsv", "tab-separated file"),
+    TypeNameBinding(FileTypes::MZTAB, "mzTab", "mzTab file"),
+    TypeNameBinding(FileTypes::PEPLIST, "peplist", "SpecArray file"),
+    TypeNameBinding(FileTypes::HARDKLOER, "hardkloer", "hardkloer file"),
+    TypeNameBinding(FileTypes::KROENIK, "kroenik", "kroenik file"),
+    TypeNameBinding(FileTypes::FASTA, "fasta", "FASTA file"),
+    TypeNameBinding(FileTypes::EDTA, "edta", "enhanced dta file"),
+    TypeNameBinding(FileTypes::CSV, "csv", "comma-separated values file"),
+    TypeNameBinding(FileTypes::TXT, "txt", "generic text file"),
+    TypeNameBinding(FileTypes::OBO, "obo", "controlled vocabulary file"),
+    TypeNameBinding(FileTypes::HTML, "html", "any HTML file"),
+    TypeNameBinding(FileTypes::ANALYSISXML, "analysisXML", "analysisXML file"),
+    TypeNameBinding(FileTypes::XSD, "xsd", "XSD schema format"),
+    TypeNameBinding(FileTypes::PSQ, "psq", "NCBI binary blast db"),
+    TypeNameBinding(FileTypes::MRM, "mrm", "SpectraST MRM list"),
+    TypeNameBinding(FileTypes::SQMASS, "sqMass", "SqLite format for mass and chromatograms"),
+    TypeNameBinding(FileTypes::PQP, "pqp", "pqp file"),
+    TypeNameBinding(FileTypes::MS, "ms", "SIRIUS file"),
+    TypeNameBinding(FileTypes::OSW, "osw", "OpenSwath output files"),
+    TypeNameBinding(FileTypes::PSMS, "psms", "Percolator tab-delimited output (PSM level)"),
+    TypeNameBinding(FileTypes::PIN, "pin", "Percolator tab-delimited input (PSM level)"),
+    TypeNameBinding(FileTypes::PARAMXML, "paramXML", "OpenMS internal XML file"),
+    TypeNameBinding(FileTypes::SPLIB, "splib", "SpectraST binary spectral library file"),
+    TypeNameBinding(FileTypes::NOVOR, "novor", "Novor custom parameter file"),
+    TypeNameBinding(FileTypes::XQUESTXML, "xquest.xml", "xquest.xml file"),
+    TypeNameBinding(FileTypes::SPECXML, "spec.xml", "spec.xml file"),
+    TypeNameBinding(FileTypes::JSON, "json", "JavaScript Object Notation file"),
+    TypeNameBinding(FileTypes::RAW, "raw", "(Thermo) Raw data file"),
+    TypeNameBinding(FileTypes::EXE, "exe", "Windows executable"),
+    TypeNameBinding(FileTypes::BZ2, "bz2", "bzip2 compressed file"),
+    TypeNameBinding(FileTypes::GZ, "gz", "gzip compressed file"),
+    TypeNameBinding(FileTypes::XML, "xml", "any XML file")  // make sure this comes last, since the name is a suffix of other formats and should only be matched last
+  };
+
+  
   String FileTypes::typeToName(FileTypes::Type type)
   {
-    std::map<Type, String>::const_iterator it = name_of_types_.find(type);
-    if (it != name_of_types_.end())
+    for (const auto& t_info : type_with_annotation__)
     {
-      return it->second;
+      if (t_info.type == type) return t_info.name;
     }
-    else
-    {
-      return "";
-    }
+    throw Exception::InvalidValue(__FILE__, __LINE__, OPENMS_PRETTY_FUNCTION, "Type has no name!", String(type));
   }
 
-  String FileTypes::typeToMZML(FileTypes::Type type)
+  String FileTypes::typeToDescription(Type type)
   {
-    std::map<Type, String>::const_iterator it = name_of_MZMLtypes_.find(type);
-    if (it != name_of_MZMLtypes_.end())
+    for (const auto& t_info : type_with_annotation__)
     {
-      return it->second;
+      if (t_info.type == type) return t_info.description;
     }
-    else
-    {
-      return "";
-    }
+    throw Exception::InvalidValue(__FILE__, __LINE__, OPENMS_PRETTY_FUNCTION, "Type has no description!", String(type));
   }
+
 
   FileTypes::Type FileTypes::nameToType(const String& name)
   {
-    String tmp = name;
-    tmp.toUpper();
-    String tmp2;
-
-    for (int i = 0; i < FileTypes::SIZE_OF_TYPE; ++i)
+    String name_upper = String(name).toUpper();
+    
+    for (const auto& t_info : type_with_annotation__)
     {
-      tmp2 = FileTypes::typeToName((FileTypes::Type)i);
-      tmp2.toUpper();
-      if (tmp == tmp2)
-      {
-        return (FileTypes::Type)i;
-      }
+      if (String(t_info.name).toUpper() == name_upper) return t_info.type;
     }
 
     return FileTypes::UNKNOWN;
   }
 
-  const std::map<FileTypes::Type, String> FileTypes::name_of_types_ = FileTypes::initializeMap_();
-  const std::map<FileTypes::Type, String> FileTypes::name_of_MZMLtypes_ = FileTypes::initializeMZMLMap_();
 
-  std::map<FileTypes::Type, String> FileTypes::initializeMap_()
+  String FileTypes::typeToMZML(FileTypes::Type type)
   {
-    std::map<Type, String> targetMap;
-    targetMap[FileTypes::UNKNOWN] = "unknown";
-    targetMap[FileTypes::DTA] = "dta";
-    targetMap[FileTypes::DTA2D] = "dta2d";
-    targetMap[FileTypes::MZDATA] = "mzData";
-    targetMap[FileTypes::MZXML] = "mzXML";
-    targetMap[FileTypes::FEATUREXML] = "featureXML";
-    targetMap[FileTypes::IDXML] = "idXML";
-    targetMap[FileTypes::CONSENSUSXML] = "consensusXML";
-    targetMap[FileTypes::MGF] = "mgf";
-    targetMap[FileTypes::INI] = "ini";
-    targetMap[FileTypes::TOPPAS] = "toppas";
-    targetMap[FileTypes::TRANSFORMATIONXML] = "trafoXML";
-    targetMap[FileTypes::MZML] = "mzML";
-    targetMap[FileTypes::CACHEDMZML] = "cachedMzML";
-    targetMap[FileTypes::MS2] = "ms2";
-    targetMap[FileTypes::PEPXML] = "pepXML";
-    targetMap[FileTypes::PROTXML] = "protXML";
-    targetMap[FileTypes::MZIDENTML] = "mzid";
-    targetMap[FileTypes::MZQUANTML] = "mzq";
-    targetMap[FileTypes::QCML] = "qcml";
-    targetMap[FileTypes::GELML] = "gelML";
-    targetMap[FileTypes::TRAML] = "traML";
-    targetMap[FileTypes::MSP] = "msp";
-    targetMap[FileTypes::OMSSAXML] = "omssaXML";
-    targetMap[FileTypes::MASCOTXML] = "mascotXML";
-    targetMap[FileTypes::PNG] = "png";
-    targetMap[FileTypes::XMASS] = "fid";
-    targetMap[FileTypes::TSV] = "tsv";
-    targetMap[FileTypes::MZTAB] = "mzTab";
-    targetMap[FileTypes::PEPLIST] = "peplist";
-    targetMap[FileTypes::HARDKLOER] = "hardkloer";
-    targetMap[FileTypes::KROENIK] = "kroenik";
-    targetMap[FileTypes::FASTA] = "fasta";
-    targetMap[FileTypes::EDTA] = "edta";
-    targetMap[FileTypes::CSV] = "csv";
-    targetMap[FileTypes::TXT] = "txt";
-    targetMap[FileTypes::OBO] = "obo";
-    targetMap[FileTypes::HTML] = "html";
-    targetMap[FileTypes::XML] = "xml";
-    targetMap[FileTypes::ANALYSISXML] = "analysisXML";
-    targetMap[FileTypes::XSD] = "xsd";
-    targetMap[FileTypes::PSQ] = "psq";
-    targetMap[FileTypes::MRM] = "mrm";
-    targetMap[FileTypes::SQMASS] = "sqMass";
-    targetMap[FileTypes::PQP] = "pqp";
-    targetMap[FileTypes::MS] = "ms";
-    targetMap[FileTypes::OSW] = "osw";
-    targetMap[FileTypes::PSMS] = "psms";
-    targetMap[FileTypes::PIN] = "pin";
-    targetMap[FileTypes::SPLIB] = "splib";
-    targetMap[FileTypes::NOVOR] = "novor";
-    targetMap[FileTypes::PARAMXML] = "paramXML";
-    targetMap[FileTypes::SPECXML] = "spec.xml";
-    targetMap[FileTypes::XQUESTXML] = "xquest.xml";
-    targetMap[FileTypes::JSON] = "json";
-    targetMap[FileTypes::RAW] = "raw";
-    targetMap[FileTypes::EXE] = "exe";
-
-    return targetMap;
+    switch (type)
+    {
+      case FileTypes::DTA: return "DTA file";
+      case FileTypes::DTA2D: return "DTA file"; // technically not correct, but closer than just a random CV term (currently mzData) - entry cannot be left empty
+      case FileTypes::MZML: return "mzML file";
+      case FileTypes::MZDATA: return "PSI mzData file";
+      case FileTypes::MZXML: return "ISB mzXML file";
+      case FileTypes::MGF: return "Mascot MGF file";
+      case FileTypes::XMASS: return "Bruker FID file";
+      default: return "";
+    }
   }
-
-  std::map<FileTypes::Type, String> FileTypes::initializeMZMLMap_()
-  {
-    std::map<Type, String> targetMap;
-    targetMap[FileTypes::DTA] = "DTA file";
-    targetMap[FileTypes::DTA2D] = "DTA file"; // technically not correct, but closer than just a random CV term (currently mzData) - entry cannot be left empty
-    targetMap[FileTypes::MZML] = "mzML file";
-    targetMap[FileTypes::MZDATA] = "PSI mzData file";
-    targetMap[FileTypes::MZXML] = "ISB mzXML file";
-    targetMap[FileTypes::MGF] = "Mascot MGF file";
-    targetMap[FileTypes::XMASS] = "Bruker FID file";
-
-    return targetMap;
-  }
-
 }
