ebe2e6a52f7dba359eb5ff8e447d4080611c0eb9
kyowonjeong <kyowonjeong@gmail.com>
2020-11-26 21:20:27 +0100

    Comment added to all .h (Except for QScore.h - what should I do with this will be determined soon...)

--- src/openms/include/OpenMS/ANALYSIS/TOPDOWN/FLASHIda.h
+++ src/openms/include/OpenMS/ANALYSIS/TOPDOWN/FLASHIda.h
@@ -69,7 +69,15 @@ namespace OpenMS
     /// assignment operator
     FLASHIda &operator=(const FLASHIda &fd) = default;
 
-    ///
+    /**
+           @brief get peak groups from input spectrum, specified by mzs and intensities (due to C# interface it is necessary)
+           @param mzs mz values of the input spectrum
+           @param ints intensities of the input spectrum
+           @param length length of mzs and ints
+           @param rt Retention time
+           @param msLevel ms level
+           @param spectrum name
+      */
     int getPeakGroups(double *mzs,
                       double *ints,
                       int length,
@@ -77,32 +85,44 @@ namespace OpenMS
                       int msLevel,
                       char *name);
 
+    /**
+           @brief get isolation windows
+           @param wstart window start mzs
+           @param wend windo end mzs
+           @param qScores q scores of windows
+           @param charges charges of windows
+           @avgMasses average masses of windows
+      */
     void getIsolationWindows(double *wstart, double *wend, double *qScores, int *charges, double *avgMasses);
 
   private:
+
+    /// PeakGroup comparator for soring by QScore
     struct
     {
-      bool operator()(PeakGroup a, PeakGroup b) const
+      bool operator()(const PeakGroup& a, const PeakGroup& b) const
       {
         return a.getQScore() > b.getQScore();
       }
     } QscoreComparator;
 
+    /// Selected integer masses - necessary for mass exclusion
     std::map<int, std::vector<double>> selected; // int mass, rt, qscore
+    /// precalculated averagine for fast selection
     PrecalculatedAveragine avg;
-
+    /// discard peak groups using mass exclusion
     void filterPeakGroupsUsingMassExclusion(MSSpectrum &spec, int msLevel);
-
+    /// generate MSSpectrum class using mzs and intensities
     static MSSpectrum makeMSSpectrum(double *mzs, double *ints, int length, double rt, int msLevel, char *name);
-
+    /// deconvoluted spectrum that contains the peak groups
     DeconvolutedSpectrum deconvolutedSpectrum;
+    /// FLASHDeconvAlgorithm class for deconvolution
     FLASHDeconvAlgorithm fd;
+    /// q score threshold - determined from C# side
     double qScoreThreshold;
+    /// retention time window - determined from C# side
     double RTwindow;
+    /// how many masses will be selected per ms level? - determined from C# side
     IntList maxMassCount;
-
-
-
-
   };
 }
