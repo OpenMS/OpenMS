0281ac1253a6e652d7bb932524afddf5e3f1208f
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2021-01-24 18:27:25 +0100

    [TESTS] double overwrites??

--- src/tests/class_tests/openms/source/DIAPrescoring_test.cpp
+++ src/tests/class_tests/openms/source/DIAPrescoring_test.cpp
@@ -66,7 +66,6 @@ END_SECTION
 
 START_SECTION ( test score function with perfect first transition )
 {
-
   OpenSwath::LightTransition mock_tr1;
   mock_tr1.product_mz = 500.;
   mock_tr1.fragment_charge = 1;
@@ -85,12 +84,10 @@ START_SECTION ( test score function with perfect first transition )
   OpenSwath::BinaryDataArrayPtr data2(new OpenSwath::BinaryDataArray);
 
   static const double arr1[] = {
-
       10, 20, 50, 100, 50, 20, 10, // peak at 499
       3, 7, 15, 30, 15, 7, 3,      // peak at 500
       1, 3, 9, 15, 9, 3, 1,        // peak at 501
       3, 9, 3,                     // peak at 502
-
 
       10, 20, 50, 100, 50, 20, 10, // peak at 600
       3, 7, 15, 30, 15, 7, 3,      // peak at 601
@@ -99,7 +96,6 @@ START_SECTION ( test score function with perfect first transition )
   };
   std::vector<double> intensity (arr1, arr1 + sizeof(arr1) / sizeof(double) );
   static const double arr2[] = {
-
       499.97, 499.98, 499.99, 500.0, 500.01, 500.02, 500.03,
       500.97, 500.98, 500.99, 501.0, 501.01, 501.02, 501.03,
       501.97, 501.98, 501.99, 502.0, 502.01, 502.02, 502.03,
@@ -138,7 +134,6 @@ END_SECTION
 
 START_SECTION ( test score function missing first transition )
 {
-
   OpenSwath::LightTransition mock_tr1;
   mock_tr1.product_mz = 500.;
   mock_tr1.fragment_charge = 1;
@@ -210,7 +205,6 @@ END_SECTION
 
 START_SECTION ( test score function with shifted first transition )
 {
-
   OpenSwath::LightTransition mock_tr1;
   mock_tr1.product_mz = 500.;
   mock_tr1.fragment_charge = 1;
@@ -229,12 +223,10 @@ START_SECTION ( test score function with shifted first transition )
   OpenSwath::BinaryDataArrayPtr data2(new OpenSwath::BinaryDataArray);
 
   static const double arr1[] = {
-
       10, 20, 50, 100, 50, 20, 10, // peak at 499
       3, 7, 15, 30, 15, 7, 3,      // peak at 500
       1, 3, 9, 15, 9, 3, 1,        // peak at 501
       3, 9, 3,                     // peak at 502
-
 
       10, 20, 50, 100, 50, 20, 10, // peak at 600
       3, 7, 15, 30, 15, 7, 3,      // peak at 601
@@ -243,7 +235,6 @@ START_SECTION ( test score function with shifted first transition )
   };
   std::vector<double> intensity (arr1, arr1 + sizeof(arr1) / sizeof(double) );
   static const double arr2[] = {
-
       498.97, 498.98, 498.99, 499.0, 499.01, 499.02, 499.03,
       499.97, 499.98, 499.99, 500.0, 500.01, 500.02, 500.03,
       500.97, 500.98, 500.99, 501.0, 501.01, 501.02, 501.03,
@@ -279,7 +270,6 @@ START_SECTION ( test score function with shifted first transition )
   TEST_REAL_SIMILAR(manhattan, 0.557433222328531)
 }
 END_SECTION
-
 
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
