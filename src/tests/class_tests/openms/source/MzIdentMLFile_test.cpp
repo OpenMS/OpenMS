e3899e7eeaf36e668d2e432fb7c934e36861335b
bielow <chris.bielow@fu-berlin.de>
2021-01-27 16:14:20 +0100

    adding a test ...

--- src/tests/class_tests/openms/source/MzIdentMLFile_test.cpp
+++ src/tests/class_tests/openms/source/MzIdentMLFile_test.cpp
@@ -72,8 +72,7 @@ START_SECTION(void load(const String& filename, std::vector<ProteinIdentificatio
 {
   std::vector<ProteinIdentification> protein_ids;
   std::vector<PeptideIdentification> peptide_ids;
-  std::vector<String> fm;
-  fm.push_back("Carbamidomethyl (C)");
+  std::vector<String> fm{"Carbamidomethyl (C)", "Xlink:DTSSP[88] (Protein N-term)"};
   MzIdentMLFile().load(OPENMS_GET_TEST_DATA_PATH("MzIdentMLFile_msgf_mini.mzid"), protein_ids, peptide_ids);
 
   TEST_EQUAL(protein_ids.size(),2)
