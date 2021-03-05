fb18872f4fcc07bc6dfe75059c20ae4506154b66
Tom Waschischeck <tom.waschischeck@web.de>
2021-03-05 17:00:44 +0100

    PSMCorrectness docu

--- src/openms/include/OpenMS/QC/PSMCorrectness.h
+++ src/openms/include/OpenMS/QC/PSMCorrectness.h
@@ -62,8 +62,45 @@ namespace OpenMS
       double variance_correctness = 0;
     };
 
+    /**
+     * @brief computes PSMCorrectness (only of the first PeptideHit of each PepID)
+     *
+     * To calculate PSMCorrectness the theoretical spectrum is generated and matched with the original one.
+     * After that: PSMCorrectness = sum of matched peaks intensity / total intensity
+     *
+     * Stores average and variance of PSMCorrectness as a struct and stores it in the results vector (can be accessed by getResults()).
+     * Each PSMCorrectness is also stored in the first PeptideHit of the corresponding PeptideIdentification as metavalue "PSM_correctness".
+     *
+     * @param fmap Input FeatureMap for annotation and data for theoretical spectra
+     * @param exp Input MSExperiment for MS2 spectra; spectra should be sorted (ascending RT)
+     * @param map_to_spectrum Map to find index of spectrum given by meta value at PepID
+     * @param tolerance Search window for matching peaks; distance has to be lower than tolerance value
+     * @param tolerance_unit Tolerance in ppm or Dalton (if auto was chosen, the unit and value will taken from FeatureMap metadata)
+     * @throws Exceptions::MissingInformation If fragment mass tolerance is missing in metadata of FeatureMap (& no ToleranceUnit is given)
+     * @throws Exception::InvalidParameter PeptideID is missing meta value 'spectrum_reference'
+     * @throws Exception::IllegalArgument Spectrum for a PepID has ms-level of 1
+     * @throws Exception::MissingInformation If PSMCorrectness couldn't be calculated for any spectrum. (i.e. all spectrums are: empty, contain only peaks with intensity 0 or the matching pep_id has no hits)
+     * @throws Exception::InvalidParameter If the fragmentation method is not ECD, ETD, CID or HCD
+     */
     void compute(FeatureMap& fmap, const MSExperiment& exp, const QCBase::SpectraMap& map_to_spectrum, ToleranceUnit tolerance_unit = ToleranceUnit::AUTO, double tolerance = 20);
 
+    /**
+     * @brief computes PSMCorrectness (only of the first PeptideHit of each PepID)
+     *
+     * Same as above, but with PeptideIdentification + SearchParameter input instead of FeatureMap
+     *
+     * @param pep_ids Input peptide identifications for annotation and data for theoretical spectra
+     * @param search_params Input search parameters from ID-search that generated the peptide identifications from @pep_ids
+     * @param exp Input MSExperiment for MS2 spectra; spectra should be sorted (ascending RT)
+     * @param map_to_spectrum Map to find index of spectrum given by meta value at PepID
+     * @param tolerance Search window for matching peaks; distance has to be lower than tolerance value
+     * @param tolerance_unit Tolerance in ppm or Dalton (if auto was chosen, the unit and value will taken from FeatureMap metadata)
+     * @throws Exceptions::MissingInformation If fragment mass tolerance is missing in metadata of FeatureMap (& no ToleranceUnit is given)
+     * @throws Exception::InvalidParameter PeptideID is missing meta value 'spectrum_reference'
+     * @throws Exception::IllegalArgument Spectrum for a PepID has ms-level of 1
+     * @throws Exception::MissingInformation If PSMCorrectness couldn't be calculated for any spectrum. (i.e. all spectrums are: empty, contain only peaks with intensity 0 or the matching pep_id has no hits)
+     * @throws Exception::InvalidParameter If the fragmentation method is not ECD, ETD, CID or HCD
+     */
     void compute(std::vector<PeptideIdentification> & pep_ids, const ProteinIdentification::SearchParameters& search_params, const MSExperiment& exp, const QCBase::SpectraMap& map_to_spectrum, ToleranceUnit tolerance_unit = ToleranceUnit::AUTO, double tolerance = 20);
 
     /// returns the name of the metric
