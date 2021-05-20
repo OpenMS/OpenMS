5ab8334328b2a5c5d75d65ea120a7afa22ee470b
Nora Wild <noraw61@redwood.imp.fu-berlin.de>
2021-05-20 12:18:17 +0200

    executables.cmake

--- src/tests/class_tests/openms/executables.cmake
+++ src/tests/class_tests/openms/executables.cmake
@@ -528,6 +528,7 @@ set(analysis_executables_list
   MetaboliteFeatureDeconvolution_test
   MetaboliteSpectralMatching_test
   ModifiedPeptideGenerator_test
+  NeedlemanWunsch_test
   OfflinePrecursorIonSelection_test
   PeptideIndexing_test
   PeptideAndProteinQuant_test
