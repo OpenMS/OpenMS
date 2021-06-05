983b0311270c897783125cee171a4aed3a4ae8fb
Dhanmoni Nath <dhanmoni@users.noreply.github.com>
2021-06-05 19:28:21 +0530

    plotly graph feature added and graphs can be plotted from user input

--- src/openms_gui/include/OpenMS/VISUAL/PlotlyGraph.h
+++ src/openms_gui/include/OpenMS/VISUAL/PlotlyGraph.h
@@ -16,13 +16,22 @@ namespace OpenMS
   class OPENMS_GUI_DLLAPI PlotlyGraph : public QWidget
   {
     Q_OBJECT
+    //Q_PROPERTY(QString data MEMBER m_data NOTIFY dataChanged)
+    //Q_PROPERTY(int num MEMBER m_num NOTIFY dataChanged)
 
   public:
     PlotlyGraph(QWidget* parent = nullptr);
     ~PlotlyGraph();
+  public slots:
+    //void jscallme(const QString& datafromjs);
+    void on_AddLineBtn_clicked();
+  signals:
+    void dataChanged(const QJsonObject& val);
 
 
   private:
     Ui::PlotlyGraph* ui;
+    //QString m_data;
+    //int m_num;
   };
 }// namespace OpenMS
--- src/openms_gui/source/VISUAL/PlotlyGraph.cpp
+++ src/openms_gui/source/VISUAL/PlotlyGraph.cpp
@@ -3,6 +3,11 @@
 
 #include <QtWebEngineWidgets/QWebEngineView>
 #include <QtWidgets/QPushButton>
+#include <QWebChannel>
+#include <QDebug>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
 
 namespace OpenMS
 {
@@ -11,12 +16,46 @@ namespace OpenMS
   {
     ui->setupUi(this);
     QWebEngineView* view = new QWebEngineView(parent);
+    QWebChannel* channel = new QWebChannel(this);
+    view->page()->setWebChannel(channel);
+    channel->registerObject(QString("webobj"), this);
     view->load(QUrl("qrc:/new/prefix/plotly_graph"));
     ui->gridLayout->addWidget(view);
   }
   PlotlyGraph::~PlotlyGraph()
   {
     delete ui;
+  }
+
+  void PlotlyGraph::on_AddLineBtn_clicked()
+  {
+    
+    QJsonArray x_val;
+    QJsonArray y_val;
+
+    qDebug() << "add line button clicked!" << ui->xValues->text();
+    QStringList xData = ui->xValues->text().split(",");
+    QStringList yData = ui->yValues->text().split(",");
+
+    foreach (QString num, xData)
+    {
+      x_val.push_back(num.toInt());
+    }
+    foreach (QString num, yData)
+    {
+      y_val.push_back(num.toInt());
+    }
+
+    QJsonObject xy_vals;
+    xy_vals["x_val"] = x_val;
+    xy_vals["y_val"] = y_val;
+
+
+    QJsonObject obj;
+    obj["plottingData"] = xy_vals;
+
+    qDebug() << "emitting signal";
+    emit dataChanged(obj);
   }
 
 }// namespace OpenMS
