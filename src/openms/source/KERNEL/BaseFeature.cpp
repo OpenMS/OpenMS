69aa1cac0781414e86b97bfd850937e0dea15c9b
Aman Kumar <54955755+Arraxx@users.noreply.github.com>
2021-09-01 13:50:34 +0530

    code arrangement changes #9 (#5486)
    * code arrange changes #9
    
    * edited
    
    * Update IMDataConverter.cpp
    
    * Update PepXMLFile.cpp
    
    * Update PepXMLFile.cpp
    
    * Update PepXMLFile.cpp
    
    Co-authored-by: Arraxx <kumaramanjha2901@gmail.com>
    Co-authored-by: Timo Sachsenberg <sachsenb@informatik.uni-tuebingen.de>

--- src/openms/source/KERNEL/BaseFeature.cpp
+++ src/openms/source/KERNEL/BaseFeature.cpp
@@ -152,9 +152,13 @@ namespace OpenMS
               [](PeptideIdentification& p1, PeptideIdentification& p2)
               {p1.sort();p2.sort();
               if (p1.empty())
+              {
                 return true;
+              }
               if (p2.empty())
+              {
                 return false;
+              }
               if (p1.isHigherScoreBetter())
               {
                 return p1.getHits()[0].getScore() < p2.getHits()[0].getScore();
@@ -167,9 +171,14 @@ namespace OpenMS
 
   BaseFeature::AnnotationState BaseFeature::getAnnotationState() const
   {
-    if (peptides_.size() == 0) return FEATURE_ID_NONE;
+    if (peptides_.size() == 0)
+    {
+      return FEATURE_ID_NONE;
+    }
     else if (peptides_.size() == 1 && peptides_[0].getHits().size() > 0)
+    {
       return FEATURE_ID_SINGLE;
+    }
     else
     {
       std::set<String> seqs;
@@ -182,10 +191,18 @@ namespace OpenMS
           seqs.insert(id_tmp.getHits()[0].getSequence().toString());
         }
       }
-      if (seqs.size() == 1) return FEATURE_ID_MULTIPLE_SAME; // hits have identical seqs
+      if (seqs.size() == 1)
+      {
+        return FEATURE_ID_MULTIPLE_SAME; // hits have identical seqs
+      }
       else if (seqs.size() > 1)
+      {
         return FEATURE_ID_MULTIPLE_DIVERGENT;                        // multiple different annotations ... probably bad mapping
-      else /*if (seqs.size()==0)*/ return FEATURE_ID_NONE;   // very rare case of empty hits
+      }
+      else /*if (seqs.size()==0)*/
+      {
+        return FEATURE_ID_NONE;   // very rare case of empty hits
+      }
     }
   }
 
