fc56dd28906e2e94fbfe2c1849e1a4036df61c8e
matteopilz <matteo.pilz@uni-tuebingen.de>
2021-09-15 13:27:28 +0200

    Updated Spell-Check

---
+++ src/spellcheck_test2.py
@@ -0,0 +1,36 @@
+# TestA
+
+""" TestB """
+
+''' TestC '''
+
+nottest = 'a' # TestC
+
+nottest = 'a' #TestD
+
+"""TestE
+"""
+
+"""
+TestF"""
+
+"""
+TestG
+"""
+
+'''TestH
+'''
+
+'''
+TestI'''
+
+'''
+TestJ
+'''
+
+'''TestL''' """TestM""" """TestN""" '''TestO'''
+
+nottest = '"""' + 'nottest' + '"""'
+
+nottest = '#' + 'nottest' # TestP
+
