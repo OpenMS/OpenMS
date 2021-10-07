6b65527ee07161b1d3ae7e180086d9f4e94fb18b
kyowonjeong <kyowonjeong@gmail.com>
2021-10-07 08:30:42 +0200

    Isobaric reporter ion option removed again... MS2 polished

--- src/openms/include/OpenMS/ANALYSIS/TOPDOWN/FLASHDeconvAlgorithm.h
+++ src/openms/include/OpenMS/ANALYSIS/TOPDOWN/FLASHDeconvAlgorithm.h
@@ -205,7 +205,7 @@ namespace OpenMS
     int ms_level_;
 
     /// high and low charges are differently deconvoluted. This vale determines the (includisve) threshold for low charge.
-    const int low_charge_ = 6; // inclusive
+    const int low_charge_ = 8; // inclusive
 
     /** @brief static function that converts bin to value
         @param bin bin number
