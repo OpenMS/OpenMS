79f9ccaba070f8e62a57369a8769f6382ceb31ce
Julianus Pfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2021-10-27 23:51:09 +0200

    Update MSFraggerAdapter.cpp

--- src/utils/MSFraggerAdapter.cpp
+++ src/utils/MSFraggerAdapter.cpp
@@ -903,7 +903,7 @@ protected:
     for (auto it = protein_identifications.begin(); it != protein_identifications.end(); it++)
     { 
         it->setSearchEngine("MSFragger");
-	//Whatever the pepXML says, overwrite origin as the input mzML
+		//Whatever the pepXML says, overwrite origin as the input mzML
         it->setPrimaryMSRunPath({this->getStringOption_(TOPPMSFraggerAdapter::in)}, exp);
     }
 
