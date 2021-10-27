37a4a49b152e27480398c885b13fa4a398aefc7f
Julianus Pfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2021-10-27 21:31:25 +0200

    Fix msfragger file origin

--- src/utils/MSFraggerAdapter.cpp
+++ src/utils/MSFraggerAdapter.cpp
@@ -903,6 +903,8 @@ protected:
     for (auto it = protein_identifications.begin(); it != protein_identifications.end(); it++)
     { 
         it->setSearchEngine("MSFragger");
+	//Whatever the pepXML says, overwrite origin as the input mzML
+        it->setPrimaryMSRunPath({input_file}, exp);
     }
 
     // write all (!) parameters as metavalues to the search parameters
