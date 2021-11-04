ec458fbc965e944e316860ee8083373f509e7771
Julianus Pfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2021-11-04 16:41:07 +0100

    [DOC] update documentation

--- src/pyOpenMS/pyopenms/dataframes.py
+++ src/pyOpenMS/pyopenms/dataframes.py
@@ -11,11 +11,13 @@ class ConsensusMapDF(ConsensusMap):
         super().__init__(*args, **kwargs)
 
     def get_intensity_df(self):
-        '''Generates a pandas DataFrame with feature intensities from each sample.
+        """Generates a pandas DataFrame with feature intensities from each sample in long format (over files).
+        For labelled analyses channel intensities will be in one row, therefore resulting in a semi-long/block format.
+        Resulting DataFrame can be joined with result from get_metadata_df by their index 'id'.
 
         Returns:
         pandas.DataFrame: intensity DataFrame
-        '''
+        """
         labelfree = self.getExperimentType() == "label-free"
         filemeta = self.getColumnHeaders()  # type: dict[int, ColumnHeader]
 
@@ -55,29 +57,31 @@ class ConsensusMapDF(ConsensusMap):
 
             return pd.DataFrame(intyarr).set_index('id')
 
-        # Specialized for LabelFree which has to have only one channel
-        def extract_row_blocks_channel_long_file_wide_LF(f: ConsensusFeature):
-            subfeatures = f.getFeatureList()  # type: list[FeatureHandle]
-            row = [0.] * len(files)
+        else:
+            # Specialized for LabelFree which has to have only one channel
+            def extract_row_blocks_channel_long_file_wide_LF(f: ConsensusFeature):
+                subfeatures = f.getFeatureList()  # type: list[FeatureHandle]
+                row = [0.] * len(files)
 
-            for fh in subfeatures:
-                header = filemeta[fh.getMapIndex()]
-                row[file_to_idx[header.filename]] = fh.getIntensity()
+                for fh in subfeatures:
+                    header = filemeta[fh.getMapIndex()]
+                    row[file_to_idx[header.filename]] = fh.getIntensity()
 
-            yield tuple([f.getUniqueId()] + row)
+                yield tuple([f.getUniqueId()] + row)
 
-        dtypes = [('id', np.dtype('uint64'))] + list(zip(files, ['f'] * len(files)))
+            dtypes = [('id', np.dtype('uint64'))] + list(zip(files, ['f'] * len(files)))
 
-        intyarr = np.fromiter(iter=gen(self, extract_row_blocks_channel_long_file_wide_LF), dtype=dtypes, count=self.size())
+            intyarr = np.fromiter(iter=gen(self, extract_row_blocks_channel_long_file_wide_LF), dtype=dtypes, count=self.size())
 
-        return pd.DataFrame(intyarr).set_index('id')
+            return pd.DataFrame(intyarr).set_index('id')
 
     def get_metadata_df(self):
-        '''Generates a pandas DataFrame with feature meta data (sequence, charge, mz, RT, quality).
+        """Generates a pandas DataFrame with feature meta data (sequence, charge, mz, RT, quality).
+        Resulting DataFrame can be joined with result from get_intensity_df by their index 'id'.
 
         Returns:
-        pandas.DataFrame: meta data DataFrame
-        '''
+        pandas.DataFrame: DataFrame with metadata for each feature (such as: best identified sequence, charge, centroid RT/mz, fitting quality)
+        """
 
         def gen(cmap: ConsensusMap, fun):
             for f in cmap:
@@ -109,27 +113,31 @@ class ConsensusMapDF(ConsensusMap):
         return pd.DataFrame(mdarr).set_index('id')
     
     def get_df(self):
-        '''Generates a pandas DataFrame with feature meta data and intensities from each sample.
+        """Generates a pandas DataFrame with both consensus feature meta data and intensities from each sample.
 
         Returns:
         pandas.DataFrame: meta data and intensity DataFrame
-        '''
+        """
         return pd.concat([self.get_metadata_df(), self.get_intensity_df()], axis=1)
 
 ConsensusMap = ConsensusMapDF
 
+# TODO tell the advanced user that they could change this, in case they have different needs.
+# TODO check if type could be inferred in the first pass
+# TODO check if max. string lengths could be inferred in the first pass and how this affects runtime
+# TODO check how runtime is affected if we use np.append instead of np.fromiter and use np.dyte = object for strings
 common_meta_value_types = {
-    b'label': 'U30',
+    b'label': 'U50',
     b'spectrum_index': 'i',
     b'score_fit': 'f',
     b'score_correlation': 'f',
     b'FWHM': 'f',
-    b'spectrum_native_id': 'U30',
+    b'spectrum_native_id': 'U100',
     b'max_height': 'f',
     b'num_of_masstraces': 'i',
-    b'masstrace_intensity': 'i',
+    b'masstrace_intensity': 'f', # TODO this is actually a DoubleList. Think about what to do here. For np.fromiter we would need to set the length of the array.
     b'Group': 'U50',
-    b'is_ungrouped_monoisotopic': 'i'
+    b'is_ungrouped_monoisotopic': 'i' # TODO this sounds very boolean to me
 }
 
 class FeatureMapDF(FeatureMap):
@@ -138,14 +146,14 @@ class FeatureMapDF(FeatureMap):
     
     # meta_values = None (default), 'all' or list of meta value names
     def get_df(self, meta_values = None):
-        '''Generates a pandas DataFrame with information contained in the FeatureMap.
+        """Generates a pandas DataFrame with information contained in the FeatureMap.
 
         Parameters:
         meta_values: meta values to include (None, [custom list of meta value names] or 'all')
         
         Returns:
         pandas.DataFrame: feature information stored in a DataFrame
-        '''
+        """
         # get all possible meta value keys in a set
         if meta_values == 'all':
             meta_values = set()
@@ -166,7 +174,7 @@ class FeatureMapDF(FeatureMap):
             pep = f.getPeptideIdentifications()  # type: list[PeptideIdentification]
             bb = f.getConvexHull().getBoundingBox2D()
                 
-            vals = [f.getMetaValue(m) if f.metaValueExists(m) else np.NA for m in meta_values]   # find some NA or None value for numpy
+            vals = [f.getMetaValue(m) if f.metaValueExists(m) else np.NA for m in meta_values]
             
             if len(pep) != 0:
                 hits = pep[0].getHits()
@@ -202,9 +210,17 @@ class MSExperimentDF(MSExperiment):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def get_df(self, melt : bool = False):
+    def get_df(self, long : bool = False):
+        """Generates a pandas DataFrame with all peaks in the MSExperiment
 
-        if melt:
+        Parameters:
+        long: set to True if you want to have a long/expanded/melted dataframe with one row per peak. Faster but
+            replicated RT information. If False, returns rows in the style: rt, np.array(mz), np.array(int)
+        
+        Returns:
+        pandas.DataFrame: feature information stored in a DataFrame
+        """
+        if long:
             cols = ["RT", "mz", "inty"]
             self.updateRanges()
             spectraarrs2d = self.get2DPeakDataLong(self.getMinRT(), self.getMaxRT(), self.getMinMZ(), self.getMaxMZ())
@@ -217,65 +233,64 @@ class MSExperimentDF(MSExperiment):
 MSExperiment = MSExperimentDF
 PeakMap = MSExperimentDF
 
-class DFConverter:
-    '''Contains functions for the conversion of pyOpenMS data structures to a pandas DataFrame.'''
-    
-    def peptide_identifications_to_df(self, peps: List[PeptideIdentification], decode_ontology : bool = True):
-        '''Converts a list of peptide identifications to a pandas DataFrame.
+# TODO think about the best way for such top-level function. IMHO in python, encapsulation in a stateless class in unnecessary.
+#   We should probably not just import this whole submodule without prefix.
+def peptide_identifications_to_df(self, peps: List[PeptideIdentification], decode_ontology : bool = True):
+    """Converts a list of peptide identifications to a pandas DataFrame.
 
-        Parameters:
-        peps (List[PeptideIdentification]): list of PeptideIdentification objects
-        decode_ontology (bool): decode meta value names
-        
-        Returns:
-        pandas.DataFrame: peptide identifications in a DataFrame
-        '''
-        switchDict = {bool: '?', int: 'i', float: 'f', str: 'U100'}
-        metavals = []
-        types = []
-        mainscorename = "score"
-        for pep in peps:
-            hits = pep.getHits()
-            if not len(hits) == 0:
-                hits[0].getKeys(metavals)
-                mainscorename = pep.getScoreType()
-                for k in metavals:
-                    if k == b"target_decoy":
-                        types.append('?')
-                    else:
-                        mv = hits[0].getMetaValue(k)
-                        types.append(switchDict[type(mv)])
-                break
+    Parameters:
+    peps (List[PeptideIdentification]): list of PeptideIdentification objects
+    decode_ontology (bool): decode meta value names
 
-        decodedMVs = [m.decode("utf-8") for m in metavals] if decode_ontology else metavals
-        cv = ControlledVocabulary()
-        cv.loadFromOBO("psims", File.getOpenMSDataPath() + "/CV/psi-ms.obo")
-        clearMVs = [cv.getTerm(m).name if m.startswith("MS:") else m for m in decodedMVs]
-        #cols = ["id", "RT", "mz", "score", "charge"] + decodedMVs
-        clearcols = ["id", "RT", "mz", mainscorename, "charge"] + clearMVs
-        coltypes = ['U100', 'f', 'f', 'f', 'i'] + types
-        dt = list(zip(clearcols, coltypes))
-        def extract(pep):
-            hits = pep.getHits()
-            if not hits:
-                return tuple([pep.getIdentifier().encode('utf-8'), pep.getRT(), pep.getMZ(), np.NA, np.NA] + [np.NA]*len(metavals))
-
-            besthit = hits[0]
-            ret = [pep.getIdentifier().encode('utf-8'), pep.getRT(), pep.getMZ(), besthit.getScore(), besthit.getCharge()]
+    Returns:
+    pandas.DataFrame: peptide identifications in a DataFrame
+    """
+    switchDict = {bool: '?', int: 'i', float: 'f', str: 'U100'}
+    metavals = []
+    types = []
+    mainscorename = "score"
+    for pep in peps:
+        hits = pep.getHits()
+        if not len(hits) == 0:
+            hits[0].getKeys(metavals)
+            mainscorename = pep.getScoreType()
             for k in metavals:
-                if besthit.metaValueExists(k):
-                    val = besthit.getMetaValue(k)
-                    if k == b"target_decoy":
-                        if val[0] == 't':
-                            ret.append(True)
-                        else:
-                            ret.append(False)
-                    else:
-                        ret.append(val)
+                if k == b"target_decoy":
+                    types.append('?')
                 else:
-                    ret.append(np.NA)
-            return tuple(ret)
+                    mv = hits[0].getMetaValue(k)
+                    types.append(switchDict[type(mv)])
+            break
 
-        psmarr = np.fromiter((extract(pep) for pep in peps), dtype=dt, count=len(peps))
+    decodedMVs = [m.decode("utf-8") for m in metavals] if decode_ontology else metavals
+    cv = ControlledVocabulary()
+    cv.loadFromOBO("psims", File.getOpenMSDataPath() + "/CV/psi-ms.obo")
+    clearMVs = [cv.getTerm(m).name if m.startswith("MS:") else m for m in decodedMVs]
+    #cols = ["id", "RT", "mz", "score", "charge"] + decodedMVs
+    clearcols = ["id", "RT", "mz", mainscorename, "charge"] + clearMVs
+    coltypes = ['U100', 'f', 'f', 'f', 'i'] + types
+    dt = list(zip(clearcols, coltypes))
+    def extract(pep):
+        hits = pep.getHits()
+        if not hits:
+            return tuple([pep.getIdentifier().encode('utf-8'), pep.getRT(), pep.getMZ(), np.NA, np.NA] + [np.NA]*len(metavals))
 
-        return pd.DataFrame(psmarr)
+        besthit = hits[0]
+        ret = [pep.getIdentifier().encode('utf-8'), pep.getRT(), pep.getMZ(), besthit.getScore(), besthit.getCharge()]
+        for k in metavals:
+            if besthit.metaValueExists(k):
+                val = besthit.getMetaValue(k)
+                if k == b"target_decoy":
+                    if val[0] == 't':
+                        ret.append(True)
+                    else:
+                        ret.append(False)
+                else:
+                    ret.append(val)
+            else:
+                ret.append(np.NA)
+        return tuple(ret)
+
+    psmarr = np.fromiter((extract(pep) for pep in peps), dtype=dt, count=len(peps))
+
+    return pd.DataFrame(psmarr)
