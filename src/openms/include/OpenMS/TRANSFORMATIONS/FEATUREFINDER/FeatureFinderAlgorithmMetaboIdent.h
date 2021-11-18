e34d5cbae84e2cbb50da0dffe0c9f5fea93d0823
Timo Sachsenberg <sachsenb@informatik.uni-tuebingen.de>
2021-11-18 13:18:20 +0100

    Update src/openms/include/OpenMS/TRANSFORMATIONS/FEATUREFINDER/FeatureFinderAlgorithmMetaboIdent.h

--- src/openms/include/OpenMS/TRANSFORMATIONS/FEATUREFINDER/FeatureFinderAlgorithmMetaboIdent.h
+++ src/openms/include/OpenMS/TRANSFORMATIONS/FEATUREFINDER/FeatureFinderAlgorithmMetaboIdent.h
@@ -87,7 +87,9 @@ public:
   /// default constructor
   FeatureFinderAlgorithmMetaboIdent();
 
-  /// @brief perform targeted feature extraction of compounds from @param metaboIdentTable and stores them in @param feature
+  /// @brief perform targeted feature extraction of compounds from @param metaboIdentTable and stores them in @param feature.
+  /// If @p spectra_file is provided it will be used as a fall-back to setPrimaryMSRunPath
+  /// in the feature map in case a proper primaryMSRunPath is not annotated in the MSExperiment.
   void run(const std::vector<FeatureFinderMetaboIdentCompound>& metaboIdentTable, FeatureMap& features, String spectra_file = "");
 
   /// @brief Retrieve chromatograms (empty if run was not executed)
