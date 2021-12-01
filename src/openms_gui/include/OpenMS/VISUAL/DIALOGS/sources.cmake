4f45e0fe89463efddba3f9a52eb11180214e9ceb
JeeH-K <jeehyeong91@gmail.com>
2021-12-01 14:16:27 +0100

    FLASHDeconvWizard split from FLASHDeconv PR #5015

--- src/openms_gui/include/OpenMS/VISUAL/DIALOGS/sources.cmake
+++ src/openms_gui/include/OpenMS/VISUAL/DIALOGS/sources.cmake
@@ -5,6 +5,7 @@ set(directory include/OpenMS/VISUAL/DIALOGS)
 set(sources_list_h
 DataFilterDialog.h
 FeatureEditDialog.h
+FLASHDeconvTabWidget.h
 HistogramDialog.h
 LayerStatisticsDialog.h
 ListFilterDialog.h
