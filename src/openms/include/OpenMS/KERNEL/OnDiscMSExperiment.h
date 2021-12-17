6a7a38b0e908e85c9ce54d9d206c020495c910d2
Timo Sachsenberg <sachsenb@informatik.uni-tuebingen.de>
2021-12-17 17:11:35 +0100

    use on disc experiment (#5709)
    * use on disc experiment
    
    * Update GNPSExport.cpp
    
    * Update GNPSExport.cpp

--- src/openms/include/OpenMS/KERNEL/OnDiscMSExperiment.h
+++ src/openms/include/OpenMS/KERNEL/OnDiscMSExperiment.h
@@ -76,7 +76,7 @@ public:
 
       This initializes the object, use openFile to open a file.
     */
-    OnDiscMSExperiment() {}
+    OnDiscMSExperiment() = default;
 
     /**
       @brief Open a specific file on disk.
@@ -87,16 +87,7 @@ public:
       @return Whether the parsing of the file was successful (if false, the
       file most likely was not an indexed mzML file)
     */
-    bool openFile(const String& filename, bool skipMetaData = false)
-    {
-      filename_ = filename;
-      indexed_mzml_file_.openFile(filename);
-      if (!filename.empty() && !skipMetaData)
-      {
-        loadMetaData_(filename);
-      }
-      return indexed_mzml_file_.getParsingSuccess();
-    }
+    bool openFile(const String& filename, bool skipMetaData = false);
 
     /// Copy constructor
     OnDiscMSExperiment(const OnDiscMSExperiment& source) :
@@ -240,16 +231,10 @@ public:
     /**
       @brief returns a single chromatogram
     */
-    OpenMS::Interfaces::ChromatogramPtr getChromatogramById(Size id)
-    {
-      return indexed_mzml_file_.getChromatogramById(id);
-    }
+    OpenMS::Interfaces::ChromatogramPtr getChromatogramById(Size id);
 
     /// sets whether to skip some XML checks and be fast instead
-    void setSkipXMLChecks(bool skip)
-    {
-      indexed_mzml_file_.setSkipXMLChecks(skip);
-    }
+    void setSkipXMLChecks(bool skip);
 
 private:
 
