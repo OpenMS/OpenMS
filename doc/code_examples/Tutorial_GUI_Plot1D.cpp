02a70f08d282cbab9ba3236cc43734fd4e88150b
bielow <chris.bielow@fu-berlin.de>
2022-02-16 10:18:52 +0100

    clang format mostly

--- doc/code_examples/Tutorial_GUI_Plot1D.cpp
+++ doc/code_examples/Tutorial_GUI_Plot1D.cpp
@@ -28,32 +28,36 @@
 // ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 
-#include <QApplication>
-#include <OpenMS/VISUAL/Plot1DWidget.h>
 #include <OpenMS/FORMAT/DTAFile.h>
-#include <OpenMS/KERNEL/StandardTypes.h>
 #include <OpenMS/KERNEL/OnDiscMSExperiment.h>
+#include <OpenMS/KERNEL/StandardTypes.h>
 #include <OpenMS/VISUAL/LayerDataBase.h>
+#include <OpenMS/VISUAL/Plot1DWidget.h>
+#include <OpenMS/VISUAL/Plot2DWidget.h>
+#include <OpenMS/openms_data_path.h>
+#include <QApplication>
 
 using namespace OpenMS;
 using namespace std;
 
-Int main(int argc, const char ** argv)
+Int main(int argc, const char** argv)
 {
-  if (argc < 2) return 1;
-  // the path to the data should be given on the command line
-  String tutorial_data_path(argv[1]);
-  
-  QApplication app(argc, const_cast<char **>(argv));
+  String tutorial_data_path(OPENMS_DOC_PATH + String("/code_examples/data/Tutorial_Spectrum1D.dta"));
+  if (argc >= 2)
+  { // the path to the data can be given on the command line
+    tutorial_data_path = argv[1];
+  }
+
+  QApplication app(argc, const_cast<char**>(argv));
 
   PeakMap exp;
   exp.resize(1);
-  DTAFile().load(tutorial_data_path + "/data/Tutorial_Spectrum1D.dta", exp[0]);
+  DTAFile().load(tutorial_data_path, exp[0]);
   LayerDataBase::ExperimentSharedPtrType exp_sptr(new PeakMap(exp));
   LayerDataBase::ODExperimentSharedPtrType on_disc_exp_sptr(new OnDiscMSExperiment());
-  Plot1DWidget * widget = new Plot1DWidget(Param(), nullptr);
+  auto* widget = new Plot1DWidget(Param(), nullptr);
   widget->canvas()->addLayer(exp_sptr, on_disc_exp_sptr);
   widget->show();
 
   return app.exec();
-} //end of main
+} // end of main
