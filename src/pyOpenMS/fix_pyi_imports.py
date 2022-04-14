c5f5cec1ef27a24e221d49f63d59e2ddeaa52683
Julianus Pfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2022-04-14 22:20:32 +0200

    [FIX,BUILD,PYOMS,TYPING] add import as second line?

--- src/pyOpenMS/fix_pyi_imports.py
+++ src/pyOpenMS/fix_pyi_imports.py
@@ -3,9 +3,8 @@ import sys
 
 if __name__ == '__main__':
     args = sys.argv[1:]
-    with open(args[0], 'r') as contents:
-        save = contents.read()
-    with open(args[0], 'w') as contents:
-        contents.write('from pyopenms import *\n')
-        contents.write(save)
-
+    with open(args[0], 'r') as f:
+        save = f.readlines()
+    save.insert(1, 'from pyopenms import *')
+    with open(args[0], 'w') as f:
+        f.writelines(save)
