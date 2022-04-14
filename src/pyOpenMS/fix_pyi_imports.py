58dc7451230ee224c8df4cc2fe0b8c9045fb4d0b
Julianus Pfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2022-04-14 14:48:42 +0200

    no bup suffix and add newline

--- src/pyOpenMS/fix_pyi_imports.py
+++ src/pyOpenMS/fix_pyi_imports.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     args = sys.argv[1:]
     with open(args[0], 'r') as contents:
         save = contents.read()
-    with open(args[0]+".bup", 'w') as contents:
-        contents.write('from pyopenms import *')
+    with open(args[0], 'w') as contents:
+        contents.write('from pyopenms import *\n')
         contents.write(save)
 
