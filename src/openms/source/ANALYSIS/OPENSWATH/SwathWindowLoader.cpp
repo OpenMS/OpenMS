47fdec696cb375c6b950049e36af2d10e2db6ad5
Joshua <joshuacharkow@gmail.com>
2022-05-03 11:06:24 -0400

    replace std:cerr and std::cout with OPENMS LOG
    when use std::endl instead of \n the OPENMS LOG statements work

--- src/openms/source/ANALYSIS/OPENSWATH/SwathWindowLoader.cpp
+++ src/openms/source/ANALYSIS/OPENSWATH/SwathWindowLoader.cpp
@@ -36,9 +36,9 @@
 
 #include <OpenMS/CONCEPT/Types.h>
 #include <OpenMS/CONCEPT/Exception.h>
+#include <OpenMS/CONCEPT/LogStream.h>
 #include <OpenMS/DATASTRUCTURES/String.h>
 #include <OpenMS/SYSTEM/SysInfo.h>
-#include <OpenMS/CONCEPT/LogStream.h>
 #include <OpenMS/DATASTRUCTURES/StringUtils.h>
 
 #include <fstream>
@@ -72,14 +72,14 @@ namespace OpenMS
 
       if (j >= swath_prec_lower_.size())
       {
-        std::cerr << "Trying to access annotation for SWATH map " << j
+        OPENMS_LOG_FATAL_ERROR << "Trying to access annotation for SWATH map " << j
                   << " but there are only " << swath_prec_lower_.size() << " windows in the"
                   << " swath_windows_file. Please check your input." << std::endl;
         throw Exception::IllegalArgument(__FILE__, __LINE__, OPENMS_PRETTY_FUNCTION,
             "The number of SWATH maps read from the raw data and from the annotation file do not match.");
       }
 
-      std::cout << "Re-annotate from file: SWATH " <<
+      OPENMS_LOG_INFO << "Re-annotate from file: SWATH " <<
         swath_maps[i].lower << " / " << swath_maps[i].upper << " (raw data) is annotated via swath_windows_file with " <<
         swath_prec_lower_[j] << " / " << swath_prec_upper_[j] << std::endl;
       
@@ -90,7 +90,7 @@ namespace OpenMS
                      " Did you forget to apply the sort_swath_maps flag? (override with -force)";
         if (force)
         {
-          std::cerr << err << "\nOverridden with -force.\n";
+          OPENMS_LOG_ERROR << err << "\nOverridden with -force.\n";
         }
         else
         {
@@ -105,7 +105,7 @@ namespace OpenMS
 
     if (j != swath_prec_upper_.size())
     {
-      std::cerr << "The number of SWATH maps read from the raw data (" <<
+      OPENMS_LOG_FATAL_ERROR << "The number of SWATH maps read from the raw data (" <<
         j << ") and from the annotation file (" << swath_prec_upper_.size() << ") do not match." << std::endl;
       throw Exception::IllegalArgument(__FILE__, __LINE__, OPENMS_PRETTY_FUNCTION,
           "The number of SWATH maps read from the raw data and from the annotation file do not match.");
@@ -126,11 +126,11 @@ namespace OpenMS
     try { // If string can be successfully converted to double (excluding initial spaces) then the first line is not a header
       StringUtils::split(line.trim(), ' ', headerSubstrings);
       StringUtils::toDouble(headerSubstrings[0]);
-      std::cout << "Swath Header not found \n";
+      OPENMS_LOG_INFO << "Swath Header not found" << std::endl;
     }
     catch (Exception::ConversionError)
     {
-      std::cout << "Read Swath window header: '" << line << "'\n";
+      OPENMS_LOG_INFO << "Read Swath window header: '" << line << std::endl;
       std::getline(data, line);
     }
 
@@ -151,7 +151,7 @@ namespace OpenMS
     } while (std::getline(data, line));
 
     assert(swath_prec_lower_.size() == swath_prec_upper_.size());
-    std::cout << "Read Swath window file with " << swath_prec_lower_.size() << " SWATH windows." << std::endl;
+    OPENMS_LOG_INFO << "Read Swath window file with " << swath_prec_lower_.size() << " SWATH windows." << std::endl;
   }
 
 }
