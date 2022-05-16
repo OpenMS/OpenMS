6811ffe05803e8eae54ea27979fb6eb4f79e7c0f
Samuel Wein <sam@samwein.com>
2022-05-16 18:33:49 +0200

    [fix] updated some autowrap code for 0.29.0

--- src/openms/include/OpenMS/METADATA/ID/InputFile.h
+++ src/openms/include/OpenMS/METADATA/ID/InputFile.h
@@ -38,6 +38,7 @@
 #include <boost/multi_index/member.hpp>
 #include <boost/multi_index/ordered_index.hpp>
 #include <boost/multi_index_container.hpp>
+#include <OpenMS/METADATA/ID/MetaData.h>
 #include <set>
 
 namespace OpenMS
