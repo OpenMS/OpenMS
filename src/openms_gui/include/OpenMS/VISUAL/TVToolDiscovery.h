764cb04ff5bf9cfab9e1346b0db004b6f25c11ed
Ruben Grünberg <ruben.gruenberg@student.uni-tuebingen.de>
2022-05-17 16:06:13 +0200

    [Feature] dynamic tool load (#5877)
    * add executable function
    
    * add possibility to search for executables in folder
    
    * Rebase onto develop
    
    * Rebase onto develop
    
    * try to load plugins during ini-generation into an array -> FAILED
    
    * fix compiler error in include of ParamValue
    
    * Comments & small fix, plugin names are stored in list
    
    * add example plugin
    
    * remove commented constructor call
    
    * Load Plugins and Tools separately
    
    * Rebase onto develop
    
    * Add tool params with correct prefix
    
    * Save plugin filenames in the Param
    
    * Add initPluginParam python function, create OpenMS_Plugin directory if it does not exist
    
    * Rebase onto develop
    
    * Update the example plugin and small fix
    
    * Revert wrong change
    
    * Add missing isnan
    
    * Github comments, indentation, blank lines
    
    * Add function to create tools list
    
    * Refactor setting the Plugin search path
    
    * Use new syntax for connecting signals/slots
    
    * Update the example plugin
    
    * Remove commented out code
    
    * Remove filesystem dependency, formatting
    
    Co-authored-by: Robert Deibel <deibel.robert@googlemail.com>
    Co-authored-by: Timo Sachsenberg <timo.sachsenberg@uni-tuebingen.de>

--- src/openms_gui/include/OpenMS/VISUAL/TVToolDiscovery.h
+++ src/openms_gui/include/OpenMS/VISUAL/TVToolDiscovery.h
@@ -39,6 +39,7 @@
 #include <future>
 #include <map>
 #include <OpenMS/DATASTRUCTURES/Param.h>
+#include <OpenMS/DATASTRUCTURES/ListUtils.h>
 
 namespace OpenMS
 {
@@ -49,11 +50,11 @@ namespace OpenMS
 
      @code
      TVToolDiscovery scanner;
-     scanner.loadParams();
+     scanner.loadToolParams();
      // Do something else before explicitly waiting for the threads to finish
      ...
      // Wait when convenient. Keeps the GUI responsive while waiting
-     scanner.waitForParams();
+     scanner.waitForToolParams();
      // Access the params. If no special timing for waiting or loading is needed this function can be safely called directly.
      scanner.getToolParams();
      @endcode
@@ -61,41 +62,87 @@ namespace OpenMS
   class OPENMS_GUI_DLLAPI TVToolDiscovery
   {
   public:
-    TVToolDiscovery() {};
+    TVToolDiscovery() :
+      plugin_path_() {};
 
-    TVToolDiscovery(const TVToolDiscovery &) = delete;
+    TVToolDiscovery(const TVToolDiscovery&) = delete;
 
-    TVToolDiscovery &operator=(const TVToolDiscovery &) = delete;
+    TVToolDiscovery& operator=(const TVToolDiscovery&) = delete;
 
-    ~TVToolDiscovery() {};
+    ~TVToolDiscovery() = default;
 
     /// Start creating params for each tool/util asynchronously
-    void loadParams();
+    void loadToolParams();
 
     /**
        @brief Wait for all future params to finish evaluating.
        @details
        While waiting the GUI remains responsive. After waiting it is safe to access the params without further waiting.
      */
-    void waitForParams();
+    void waitForToolParams();
+    void waitForPluginParams();
 
     /**
-       @brief Returns a hash map containing a param for each tool/util.
+       @brief Returns a Param object containing the params for each tool/util.
        @details
        Note that it is possible that not all param futures have been finished (or loaded) yet if this function is called.
-       In that case, the function starts param parsing (loadParam()) and waits for completion (waitForParams())
+       In that case, the function starts param parsing (loadParam()) and waits for completion (waitForToolParams())
        before returning the result.
      */
-    const std::map<std::string, Param> &getToolParams();
+    const Param& getToolParams();
+
+    /**
+       @brief Returns a param containing the params for each plugin.
+       @details
+       This function will ALWAYS trigger a reload of the plugins.
+    */
+    const Param& getPluginParams();
+
+    /// Returns the list of read plugin names as saved in the ini.
+    const std::vector<std::string>& getPlugins();
+
+    /**
+     * @brief Sets the path that will be searched for Plugins
+     * @param path The new path to set
+     * @param create Attempt to create the directory if it does not already exist
+     * @returns False if setting/creating the path fails. True otherwise.
+     */
+    [[maybe_unused]] bool setPluginPath(const String& path, bool create=false);
+
+    /// Returns the current set path to search plugins in
+    const std::string getPluginPath();
+
+    /// Returns the path to the plugin executable or an empty string if the plugin name is unknown
+    std::string findPluginExecutable(const std::string& name);
 
   private:
-    /// Returns param for a given tool/util. This function is thread-safe
-    static Param getParamFromIni_(const std::string &tool_name);
+    /** Returns param for a given tool/util. This function is thread-safe. Additionally inserts names of tools into 
+        plugin list
+     */
+    static Param getParamFromIni_(const String& tool_path, bool plugins=false);
 
-    /// Contains a param future for each tool/util name
-    std::map<std::string, std::future<Param>> param_futures_;
+    /** Start creating params for each plugin in the set plugin path asynchronously
+     *  This should only be called from waitForPluginParams() or the names in the plugins vector are not correct
+     */
+    void loadPluginParams();
 
-    /// Contains a mapping of each tool/util name to its param.
-    std::map<std::string, Param> params_;
+    /// Returns a list of executables that are found at the plugin path
+    const StringList getPlugins_();
+
+    /// The filepath to search pugins in
+    std::string plugin_path_;
+
+    /// The futures for asyncronous loading of the tools/utils and plugins
+    std::vector<std::future<Param>> tool_param_futures_;
+    std::vector<std::future<Param>> plugin_param_futures_;
+
+    /// Contains all the params of the tools/utils
+    Param tool_params_;
+
+    /// Contains all the params of the plugins
+    Param plugin_params_;
+
+    /// The names of all loaded plugins, this is used to add the plugins to the list in the ToolsDialog
+    std::vector<std::string> plugins_;
   };
 }
