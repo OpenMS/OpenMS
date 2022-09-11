6e14b3847e16983b42526fbad91d328e6a5e18b5
rishi2802 <rishithayerragogu28@gmail.com>
2022-09-11 21:17:45 +0530

    auto changed to const auto& in SignalToNoiseEstimatorMeanIterative.h line numbers - 206 and 212

--- src/openms/include/OpenMS/FILTERING/NOISEESTIMATION/SignalToNoiseEstimatorMeanIterative.h
+++ src/openms/include/OpenMS/FILTERING/NOISEESTIMATION/SignalToNoiseEstimatorMeanIterative.h
@@ -1,3 +1,66 @@
+------------
+//                   OpenMS -- Open-Source Mass Spectrometry
+// --------------------------------------------------------------------------
+// Copyright The OpenMS Team -- Eberhard Karls University Tuebingen,
+// ETH Zurich, and Freie Universitaet Berlin 2002-2022.
+//
+// This software is released under a three-clause BSD license:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of any author or any participating institution
+//    may be used to endorse or promote products derived from this software
+//    without specific prior written permission.
+// For a full list of authors, refer to the file AUTHORS.
+// --------------------------------------------------------------------------
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL ANY OF THE AUTHORS OR THE CONTRIBUTING
+// INSTITUTIONS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+erlin 2002-2022.
+//
+//// This software is released under a three-clause BSD license:
+/////  * Redistributions of source code must retain the above copyright
+/////    notice, this list of conditions and the erlin 2002-2022.
+/////
+///// This software is released under a three-clause BSD license:
+/////  * Redistributions of source code must retain the above copyright
+/////    notice, this list of conditions and the // --------------------------------------------------------------------------
+// $Maintainer: Chris Bielow $
+// $Authors: $
+// --------------------------------------------------------------------------
+//
+
+#pragma once
+
+#include <OpenMS/FILTERING/NOISEESTIMATION/SignalToNoiseEstimator.h>
+#include <OpenMS/CONCEPT/Exception.h>
+#include <OpenMS/DATASTRUCTURES/ListUtils.h>
+#include <vector>
+#include <algorithm> //for std::max_element
+
+namespace OpenMS
+{
+  /**
+    @brief Estimates the signal/noise (S/N) ratio of each data point in a scan
+           based on an iterative scheme which discards high intensities
+
+    For each datapoint in the given scan, we collect a range of data points around it (param: <i>win_len</i>).
+    The noise for a datapoint is estimated iteratively by discarding peaks which are more than
+    (<i>stdev_mp</i> * StDev) above the mean value. After three iterations, the mean value is
+    considered to be the noise level. If the number of elements in the current window is not sufficient (param: <i>min_required_elements</i>),
+SignalToNoiseEstimatorMeanIterative.h [unix] (19:31 11/09/2022)                                 1,1 Top
+-- INSERT --
 // --------------------------------------------------------------------------
 //                   OpenMS -- Open-Source Mass Spectrometry
 // --------------------------------------------------------------------------
@@ -45,7 +108,9 @@ namespace OpenMS
 {
   /**
     @brief Estimates the signal/noise (S/N) ratio of each data point in a scan
-           based on an iterative scheme which discards high intensities
+           based on an iterative scheme which discards high intensi
+	   
+	   ties
 
     For each datapoint in the given scan, we collect a range of data points around it (param: <i>win_len</i>).
     The noise for a datapoint is estimated iteratively by discarding peaks which are more than
@@ -406,9 +471,7 @@ protected:
     double max_intensity_;
     /// parameter for initial automatic estimation of "max_intensity_": a stdev multiplier
     double auto_max_stdev_Factor_;
-    /// parameter for initial automatic estimation of "max_intensity_" percentile or a stdev
-    double auto_max_percentile_;
-    /// determines which method shall be used for estimating "max_intensity_". valid are MANUAL=-1, AUTOMAXBYSTDEV=0 or AUTOMAXBYPERCENT=1
+    /// parameter for initial automatic estimation of "max_intens/// determines which method shall be used for estimating "max_intensity_". valid are MANUAL=-1, AUTOMAXBYSTDEV=0 or AUTOMAXBYPERCENT=1
     int    auto_mode_;
     /// range of data points which belong to a window in Thomson
     double win_len_;
