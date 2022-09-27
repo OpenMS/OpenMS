60a6455600bd3ea8c65d7c7b797d47eff15c0116
Hannes Roest <hannes.rost@utoronto.ca>
2022-09-27 00:18:16 -0400

    [FEATURE] allow memory-view access to arrays

--- src/pyOpenMS/addons/OSSpectrum.pyx
+++ src/pyOpenMS/addons/OSSpectrum.pyx
@@ -6,11 +6,21 @@
         cdef list py_result = _vec
         return py_result
 
+    def getMZArray_mv(self):
+        cdef shared_ptr[_OSBinaryDataArray] _r = self.inst.get().getMZArray()
+        cdef double[::1] arr = <double [:_r.get().data.size()]>_r.get().data.data()
+        return arr
+
     def getIntensityArray(self):
         cdef shared_ptr[_OSBinaryDataArray] _r = self.inst.get().getIntensityArray()
         cdef libcpp_vector[double] _vec = _r.get().data
         cdef list py_result = _vec
         return py_result
+
+    def getIntensityArray_mv(self):
+        cdef shared_ptr[_OSBinaryDataArray] _r = self.inst.get().getIntensityArray()
+        cdef double[::1] arr = <double [:_r.get().data.size()]>_r.get().data.data()
+        return arr
 
     def getDriftTimeArray(self):
         cdef shared_ptr[_OSBinaryDataArray] _r = self.inst.get().getDriftTimeArray()
@@ -18,6 +28,12 @@
         cdef libcpp_vector[double] _vec = _r.get().data
         cdef list py_result = _vec
         return py_result
+
+    def getDriftTimeArray_mv(self):
+        cdef shared_ptr[_OSBinaryDataArray] _r = self.inst.get().getDriftTimeArray()
+        if _r.get() == NULL: return None
+        cdef double[::1] arr = <double [:_r.get().data.size()]>_r.get().data.data()
+        return arr
 
     def setMZArray(self, list data):
         assert isinstance(data, list), 'arg transitions wrong type'
