a0862065f6af5647a01331aeb58b7cb2dc3e64c9
Timo Sachsenberg <timo.sachsenberg@uni-tuebingen.de>
2022-10-11 12:28:02 +0200

    Update FeatureFinderMetaboIdent.cpp

--- src/utils/FeatureFinderMetaboIdent.cpp
+++ src/utils/FeatureFinderMetaboIdent.cpp
@@ -261,11 +261,11 @@ protected:
     mzml.setLogType(log_type_);
     mzml.getOptions().addMSLevel(1);
     mzml.load(in, ff_mident.getMSData());
-    if (ff_mident.getMSData().empty())
+    if (ff_mident.getMSData().empty() && !force)
     {
-      OPENMS_LOG_WARNING << "Error: No MS1 scans in '"
-                         << in << "' - aborting." << endl;
-      if(!force) return INCOMPATIBLE_INPUT_DATA;
+      OPENMS_LOG_ERROR << "Error: No MS1 scans in '"
+                       << in << "' - aborting." << endl;
+      return INCOMPATIBLE_INPUT_DATA;
     }
     FeatureMap features;
     ff_mident.run(table, features, in);
