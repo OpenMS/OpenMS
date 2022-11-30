94889908cb32d64b06158c3834b548277ef6ae6f
KyowonJeong <jeong@informatik.uni-tuebingen.de>
2022-11-30 15:08:34 +0100

    PeakGroup_test.cpp bug fixed

--- src/tests/class_tests/openms/source/PeakGroup_test.cpp
+++ src/tests/class_tests/openms/source/PeakGroup_test.cpp
@@ -182,7 +182,15 @@ START_SECTION((float getChargeIntensity(const int abs_charge) const))
 }
 END_SECTION
 
-START_SECTION((std::tuple<double, double> getMaxQScoreMzRange() const))
+START_SECTION((void setRepAbsCharge(const int max_qscore_charge)))
+{
+  sample_pg.setRepAbsCharge(2);
+  int temp_abs = sample_pg.getRepAbsCharge();
+  TEST_EQUAL(temp_abs, 2);
+}
+END_SECTION
+
+START_SECTION((std::tuple<double, double> getRepMzRange() const))
 {
   std::tuple<double, double> tmp_range = sample_pg.getRepMzRange();
   TEST_REAL_SIMILAR(std::get<0>(tmp_range), 1125.5118055019082);
@@ -223,7 +231,6 @@ START_SECTION((float getIsotopeCosine() const))
   TEST_REAL_SIMILAR(temp_iso, 0.3);
 }
 END_SECTION
-
 
 START_SECTION((int getRepAbsCharge() const))
 {
