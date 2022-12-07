1ffcf095ff0a6d233c051ab06d031afaa367517d
KyowonJeong <jeong@informatik.uni-tuebingen.de>
2022-12-07 17:12:00 +0100

    DL recovered

--- src/openms/source/FORMAT/FLASHDeconvSpectrumFile.cpp
+++ src/openms/source/FORMAT/FLASHDeconvSpectrumFile.cpp
@@ -314,6 +314,89 @@ namespace OpenMS
     }
   }
 
+  inline const int z = 1, iso = 11;
+  void FLASHDeconvSpectrumFile::writeDLMatrixHeader(std::fstream& fs)
+  {
+    for(int i=0;i<3;i++)
+    {
+      String prefix = "Set" + std::to_string(i+1) + "_";
+      auto dlm = z*iso;
+
+      for(int j=0; j<dlm;j++)
+      {
+         fs<<prefix<<j<<",";
+      }
+    }
+    fs<<"Class\n";
+  }
+
+  template<class BidiIter >
+  BidiIter random_unique(BidiIter begin, BidiIter end, size_t num_random) {
+    size_t left = std::distance(begin, end);
+    while (num_random--) {
+      BidiIter r = begin;
+      std::advance(r, rand()%left);
+      std::swap(*begin, *r);
+      ++begin;
+      --left;
+    }
+    return begin;
+  }
+
+  void FLASHDeconvSpectrumFile::writeDLMatrix(std::vector<DeconvolvedSpectrum>& dspecs, std::fstream& fs, const FLASHDeconvHelperStructs::PrecalculatedAveragine& avg)
+  {
+    String cns[] = {"T", "D1", "D2", "D3"};
+    int count = 25000;
+    //class,ID,group,data
+    std::vector<std::vector<PeakGroup>> grouped(4);
+
+    for(auto& dspec: dspecs)
+    {
+      for(auto& pg: dspec)
+      {
+         int cl = pg.getDecoyFlag();
+         if(cl<0 || cl>=grouped.size())
+         {
+          continue;
+         }
+         pg.calculateDLMatrices(z, iso, avg);
+         grouped[cl].push_back(pg);
+      }
+    }
+
+    for(auto& g : grouped)
+    {
+      if(g.size() < count)
+      {
+         continue;
+      }
+      random_unique(g.begin(), g.end(), count);
+    }
+
+    for(auto& g : grouped)
+    {
+      int cntr = 0;
+      for (auto& pg : g)
+      {
+         int cl = pg.getDecoyFlag();
+
+         for (int i = 0; i < 3; i++)
+         {
+          auto dlm = pg.getDLMatrix(i).asVector();
+          for (double v : dlm)
+          {
+            fs << v << ",";
+          }
+         }
+         fs << cns[cl] << "\n";
+         if(++cntr >= count)
+         {
+          break;
+         }
+      }
+    }
+  }
+
   void FLASHDeconvSpectrumFile::writeTopFD(const DeconvolvedSpectrum& dspec, std::fstream& fs,
                                            const double snr_threshold, const double decoy_harmonic_factor,
                                            const double decoy_precursor_offset) 
