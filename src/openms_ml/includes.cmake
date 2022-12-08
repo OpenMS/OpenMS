66d987c2abaf02d87f712c78bd193cbffb999b7f
timosachsenberg <sachsenb@ibminode02.Cs.Uni-Tuebingen.De>
2022-12-08 17:23:11 +0100

    init

--- src/openms_ml/includes.cmake
+++ src/openms_ml/includes.cmake
@@ -1,12 +1,12 @@
 set(OpenMSML_sources  CACHE INTERNAL "This variable should hold all OpenMS sources at the end of the config step" )
 
-## added to OpenMSVisual library: ${OpenMSML_sources}
-# TODO include(source/VISUAL/ANNOTATION/sources.cmake)
+## added to OpenMSML library: ${OpenMSML_sources}
+include(source/sources.cmake)
 
 set(OpenMSML_sources_h  CACHE INTERNAL "This variable should hold all OpenMS sources at the end of the config step" )
 
-## added to OpenMSVisual library: ${OpenMSML_sources}
-# TODO include(include/OpenMS/VISUAL/ANNOTATION/sources.cmake)
+## added to OpenMSML library: ${OpenMSML_sources}
+include(include/sources.cmake)
 
 ## merge all headers to sources (for source group view in VS)
 list(APPEND OpenMSML_sources ${OpenMSML_sources_h})
