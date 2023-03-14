de3db20811a764815c5d67d89bd095d3b923b4a0
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2023-03-14 14:50:19 +0100

    try to change exec name inside CTD

--- cmake/knime/change_exec_name_in_ctd.cmake
+++ cmake/knime/change_exec_name_in_ctd.cmake
@@ -28,8 +28,8 @@
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 # --------------------------------------------------------------------------
-# $Maintainer: Stephan Aiche $
-# $Authors: Stephan Aiche $
+# $Maintainer: Julianus Pfeuffer $
+# $Authors: Julianus Pfeuffer $
 # --------------------------------------------------------------------------
 
 cmake_minimum_required(VERSION 3.15 FATAL_ERROR)
@@ -37,36 +37,30 @@ cmake_minimum_required(VERSION 3.15 FATAL_ERROR)
 # include helper functions 
 include ( ${SCRIPT_DIR}common.cmake )
 
-set(required_variables "TOOLNAME;PARAM;CTD_PATH")
+set(required_variables "TOOLNAME;CTD_FILE")
 check_variables(required_variables)
 
-set(filename "${CTD_PATH}/${TOOLNAME}.ctd")
+set(filename ${CTD_FILE})
 
 file(READ "${filename}" contents)
 string(REPLACE "[" "_OPENBRACKET_" contents "${contents}")
 string(REPLACE "]" "_CLOSEBRACKET_" contents "${contents}")
-string(REPLACE ";" "\\;" contents "${contents}")
+string(REPLACE ";" "\\\;" contents "${contents}")
 string(REGEX REPLACE "\n" ";" contents "${contents}")
 
 set(APPEND_TO_FILE FALSE)
 foreach(line ${contents})
-  set(pos -1) 
-  if(line MATCHES ".*name=\"${PARAM}\".*") 
-    string(LENGTH "${CMAKE_MATCH_1}" pos) 
-  endif()
+  string(REGEX REPLACE "(.*tool.*)name=\"([^ ]*)\"(.*)" "\\1executableName=\"\\2\" name=\"${TOOLNAME}\"\\3" line ${line})
   
   string(REPLACE "_OPENBRACKET_" "[" line "${line}")
   string(REPLACE "_CLOSEBRACKET_" "]" line "${line}")
-  
-  # we only write out line that do not contain our parameter
-  if (pos EQUAL -1)
-    if(APPEND_TO_FILE)
-      file(APPEND "${filename}" "${line}\n")
-    else() 
-      # write the first line
-      file(WRITE "${filename}" "${line}\n")
-      # append the rest
-      set(APPEND_TO_FILE TRUE)
-    endif()
+
+  if(APPEND_TO_FILE)
+    file(APPEND "${filename}" "${line}\n")
+  else() 
+    # write the first line
+    file(WRITE "${filename}" "${line}\n")
+    # append the rest
+    set(APPEND_TO_FILE TRUE)
   endif()
 endforeach()
