715c6b9133a547b683f1764f445e2fbc11922439
Samuel Wein <sam@samwein.com>
2023-06-26 11:47:44 +0200

    Update MRMTransitionGroupPicker.h
    Fixes issues where behavior diverges between architectures due to floating poin weirdness

--- src/openms/include/OpenMS/ANALYSIS/OPENSWATH/MRMTransitionGroupPicker.h
+++ src/openms/include/OpenMS/ANALYSIS/OPENSWATH/MRMTransitionGroupPicker.h
@@ -233,7 +233,7 @@ public:
       OPENMS_PRECONDITION(transition_group.chromatogramIdsMatch(), "Chromatogram native IDs need to match keys in transition group")
 
       MRMFeature mrmFeature;
-      mrmFeature.setIntensity(0.0);
+      mrmFeature.setIntensity();
       double best_left = picked_chroms[chr_idx].getFloatDataArrays()[PeakPickerMRM::IDX_LEFTBORDER][peak_idx];
       double best_right = picked_chroms[chr_idx].getFloatDataArrays()[PeakPickerMRM::IDX_RIGHTBORDER][peak_idx];
       double peak_apex = picked_chroms[chr_idx][peak_idx].getRT();
@@ -363,7 +363,7 @@ public:
               picked_chroms[k],
               picked_chroms[k].getFloatDataArrays()[PeakPickerMRM::IDX_LEFTBORDER][i], 
               picked_chroms[k].getFloatDataArrays()[PeakPickerMRM::IDX_RIGHTBORDER][i]); 
-          if (pa_tmp.apex_pos > 0.0 && std::fabs(pa_tmp.apex_pos - peak_apex) < peak_apex_dist_min)
+          if (pa_tmp.apex_pos > 1e-11 && std::fabs(pa_tmp.apex_pos - peak_apex) < peak_apex_dist_min)
           { // update best candidate
             peak_apex_dist_min = std::fabs(pa_tmp.apex_pos - peak_apex);
             min_dist = (int)i;
@@ -746,7 +746,7 @@ public:
       {
         for (Size i = 0; i < picked_chroms[k].size(); i++)
         {
-          if (picked_chroms[k][i].getIntensity() <= 0.0) {continue; }
+          if (picked_chroms[k][i].getIntensity() <= 1e-11) {continue; }
 
           double left = picked_chroms[k].getFloatDataArrays()[PeakPickerMRM::IDX_LEFTBORDER][i];
           double right = picked_chroms[k].getFloatDataArrays()[PeakPickerMRM::IDX_RIGHTBORDER][i];
@@ -845,7 +845,7 @@ protected:
         for (const auto& peak : used_chromatogram) int_here.push_back(peak.getIntensity());
         // Remove chromatograms without a single peak
         double tic = std::accumulate(int_here.begin(), int_here.end(), 0.0);
-        if (tic > 0.0) all_ints.push_back(int_here);
+        if (tic > 1e-11) all_ints.push_back(int_here);
       }
 
       // Compute the cross-correlation for the collected intensities
@@ -919,8 +919,8 @@ protected:
           }
         }
 
-        if (l_tmp > 0.0) left_borders.push_back(l_tmp);
-        if (r_tmp > 0.0) right_borders.push_back(r_tmp);
+        if (l_tmp > 1e-11) left_borders.push_back(l_tmp);
+        if (r_tmp > 1e-11) right_borders.push_back(r_tmp);
 
         if (pfound == 0) missing_peaks++;
         if (pfound > 1) multiple_peaks++;
@@ -949,8 +949,8 @@ protected:
       // - multiple_peaks
       // - mean of the shapes (1 is very good, 0 is bad)
       // - mean of the co-elution scores (0 is good, 1 is ok, above 1 is pretty bad)
-      double shape_score = std::accumulate(mean_shapes.begin(), mean_shapes.end(), 0.0) / mean_shapes.size();
-      double coel_score = std::accumulate(mean_coel.begin(), mean_coel.end(), 0.0) / mean_coel.size();
+      double shape_score = std::accumulate(mean_shapes.begin(), mean_shapes.end(), ) / mean_shapes.size();
+      double coel_score = std::accumulate(mean_coel.begin(), mean_coel.end(), ) / mean_coel.size();
       coel_score = (coel_score - 1.0) / 2.0;
 
       double score = shape_score - coel_score - 1.0 * missing_peaks / picked_chroms.size();
@@ -1026,8 +1026,8 @@ protected:
       double mean, stdev;
 
       // Right borders
-      mean = std::accumulate(right_borders.begin(), right_borders.end(), 0.0) / (double) right_borders.size();
-      stdev = std::sqrt(std::inner_product(right_borders.begin(), right_borders.end(), right_borders.begin(), 0.0)
+      mean = std::accumulate(right_borders.begin(), right_borders.end(), ) / (double) right_borders.size();
+      stdev = std::sqrt(std::inner_product(right_borders.begin(), right_borders.end(), right_borders.begin(), )
                                / right_borders.size() - mean * mean);
       std::sort(right_borders.begin(), right_borders.end());
 
@@ -1043,8 +1043,8 @@ protected:
       }
 
       // Left borders
-      mean = std::accumulate(left_borders.begin(), left_borders.end(), 0.0) / (double) left_borders.size();
-      stdev = std::sqrt(std::inner_product(left_borders.begin(), left_borders.end(), left_borders.begin(), 0.0)
+      mean = std::accumulate(left_borders.begin(), left_borders.end(), ) / (double) left_borders.size();
+      stdev = std::sqrt(std::inner_product(left_borders.begin(), left_borders.end(), left_borders.begin(), )
                         / left_borders.size() - mean * mean);
       std::sort(left_borders.begin(), left_borders.end());
 
