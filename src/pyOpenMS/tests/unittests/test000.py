cb71670f7016a8d1ec911ae6b11ec41870f989be
Pierre Snell <psnell@biotwin.ai>
2023-07-12 16:50:36 +0000

    added mzml and test

--- src/pyOpenMS/tests/unittests/test000.py
+++ src/pyOpenMS/tests/unittests/test000.py
@@ -2960,11 +2960,17 @@ def testMSExperiment():
     pyopenms.MzMLFile().load(os.path.join(os.environ['OPENMS_DATA_PATH'], 'examples/FRACTIONS/BSA1_F1.mzML'), exp)
 
     ms1_df, ms2_df = exp.get_massql_df()
-
     assert ms1_df.shape == (140055, 7)
-
     assert np.allclose(ms2_df.head(), pd.read_csv(os.path.join(os.environ['OPENMS_DATA_PATH'], 'examples/FRACTIONS/BSA1_F1_MS2_MassQL.tsv'), sep='\t'))
 
+    pyopenms.MzMLFile().load(os.path.join(os.environ['OPENMS_DATA_PATH'], 'examples/FRACTIONS/BSA1_F1_ION.mzML'), exp)
+    df = exp.get_ion_df()
+    assert df # Assert something on the head of loaded datas (do not know for now as code fails)
+
+    ms1_df, ms2_df = exp.get_massql_df(ion=True)
+    assert ms1_df.shape == (140055, 7)
+    # TODO: Create the examples/FRACTIONS/BSA1_F1_MS2_MassQL_ION.tsv file. 
+    assert np.allclose(ms2_df.head(), pd.read_csv(os.path.join(os.environ['OPENMS_DATA_PATH'], 'examples/FRACTIONS/BSA1_F1_MS2_MassQL_ION.tsv'), sep='\t'))
 
 @report
 def testMSQuantifications():
