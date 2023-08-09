692a346f76b263f6216a7c1857908d76569f661e
Julianus Pfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2023-08-09 16:53:18 +0200

    finish all steps. just needs testing and potential fixes. minor todos about ssh upload

---
+++ tools/ci/cipackage.cmake
@@ -0,0 +1,33 @@
+##
+## this script REQUIRES the previous execution of the cibuild.cmake script!!
+##
+
+# define build name&co for easier identification on CDash
+set(CTEST_BUILD_NAME "$ENV{BUILD_NAME}")
+set(CTEST_SITE "$ENV{CI_PROVIDER}")
+set(CTEST_SOURCE_DIRECTORY "$ENV{SOURCE_DIRECTORY}")
+set(CTEST_BINARY_DIRECTORY "${CTEST_SOURCE_DIRECTORY}/bld")
+
+# cdash server (fu-berlin) SSL certificate sometimes is revoked. Keeps CI running.
+set (CTEST_CURL_OPTIONS       CURLOPT_SSL_VERIFYHOST_OFF CURLOPT_SSL_VERIFYPEER_OFF )
+
+message(STATUS "CTEST_SOURCE_DIRECTORY: ${CTEST_SOURCE_DIRECTORY}")
+message(STATUS "CTEST_BINARY_DIRECTORY: ${CTEST_BINARY_DIRECTORY}")
+
+ctest_start(APPEND)
+
+# build the dist target for packages
+if("$ENV{ENABLE_STYLE_TESTING}" STREQUAL "OFF")
+  if(NOT "$ENV{PYOPENMS}" STREQUAL "ON")
+    ctest_build(BUILD "${CTEST_BINARY_DIRECTORY}" TARGET "dist" NUMBER_ERRORS _build_errors)
+    ctest_submit(PARTS Build)
+  endif()
+else()
+  set(_build_errors 0)
+endif()
+
+if (_build_errors)
+  message(FATAL_ERROR "Please check the build results at: https://cdash.openms.de/index.php?project=OpenMS&begin=2023-01-01&end=2030-01-01&filtercount=1&field1=buildname&compare1=63&value1=${CTEST_BUILD_NAME}")
+else()
+  message("Please check the build results at: https://cdash.openms.de/index.php?project=OpenMS&begin=2023-01-01&end=2030-01-01&filtercount=1&field1=buildname&compare1=63&value1=${CTEST_BUILD_NAME}")
+endif()
