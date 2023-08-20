b684de680f2811edef2f1d2158696db25b89d39c
Julianus Pfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2023-08-20 16:25:49 +0200

    Update knime_package_support.cmake

--- cmake/knime_package_support.cmake
+++ cmake/knime_package_support.cmake
@@ -356,14 +356,13 @@ if (APPLE) ## On APPLE use our script because the executables' install_names nee
   ) # -p ${PAYLOAD_LIB_PATH}/plugins not applicable for now
   add_custom_command(
           TARGET prepare_knime_payload_libs POST_BUILD
-          COMMAND find ${PAYLOAD_BIN_PATH} -depth 1 -type f -exec ${CMAKE_STRIP} -S {} "\;"
-          COMMAND find ${TP_PAYLOAD_BIN_PATH} -depth 1 -type f -exec ${CMAKE_STRIP} -S {} "\;"
+          COMMAND find "${PAYLOAD_BIN_PATH}" -depth 1 -type f -exec ${CMAKE_STRIP} -S {} "\;"
+          COMMAND find "${TP_PAYLOAD_BIN_PATH}" -depth 1 -type f -exec ${CMAKE_STRIP} -S {} "\;"
   )
   add_custom_command(
           TARGET prepare_knime_payload_libs POST_BUILD
-          COMMAND find "${PAYLOAD_LIB_PATH}" -type f -name "*.dylib" -exec ${CMAKE_STRIP} -x {} \;
-          COMMAND find "${PAYLOAD_LIB_PATH}" -type f -name "Qt*" -exec ${CMAKE_STRIP} -x {} \;
-          VERBATIM
+          COMMAND find "${PAYLOAD_LIB_PATH}" -type f -name "*.dylib" -exec ${CMAKE_STRIP} -x {} "\;"
+          COMMAND find "${PAYLOAD_LIB_PATH}" -type f -name "Qt*" -exec ${CMAKE_STRIP} -x {} "\;"
   )
 elseif(WIN32)
   # on Win everything should be linked statically for distribution except Qt
@@ -389,14 +388,12 @@ else()
   endforeach()
   add_custom_command(
           TARGET prepare_knime_payload_libs POST_BUILD
-          COMMAND find "${PAYLOAD_BIN_PATH}" -depth 1 -type f -exec ${CMAKE_STRIP} -s {} \\;
-          COMMAND find "${TP_PAYLOAD_BIN_PATH}" -depth 1 -type f -exec ${CMAKE_STRIP} -s {} \\;
-          VERBATIM
+          COMMAND find "${PAYLOAD_BIN_PATH}" -depth 1 -type f -exec ${CMAKE_STRIP} -s {} "\;"
+          COMMAND find "${TP_PAYLOAD_BIN_PATH}" -depth 1 -type f -exec ${CMAKE_STRIP} -s {} "\;"
   )
   add_custom_command(
           TARGET prepare_knime_payload_libs POST_BUILD
-          COMMAND find "${PAYLOAD_LIB_PATH}" -type f -name "*.so" -exec ${CMAKE_STRIP} -x {} \\;
-          VERBATIM
+          COMMAND find "${PAYLOAD_LIB_PATH}" -type f -name "*.so" -exec ${CMAKE_STRIP} -x {} "\;"
   )
 endif()
 
