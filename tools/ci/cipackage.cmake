00f6f127dcaf114845d2749079576b7f95493714
Julianus Pfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2023-08-21 21:45:10 +0200

    Update cipackage.cmake

--- tools/ci/cipackage.cmake
+++ tools/ci/cipackage.cmake
@@ -9,6 +9,7 @@ set(CTEST_BUILD_NAME "$ENV{BUILD_NAME}_Package")
 set(CTEST_SITE "$ENV{CI_PROVIDER}")
 set(CTEST_SOURCE_DIRECTORY "$ENV{SOURCE_DIRECTORY}")
 set(CTEST_BINARY_DIRECTORY "${CTEST_SOURCE_DIRECTORY}/bld")
+set(CTEST_CMAKE_GENERATOR "Ninja")
 
 set(CONFIGURE_OPTIONS)
 set(VARS_TO_LOAD PACKAGE_TYPE SEARCH_ENGINES_DIRECTORY SIGNING_IDENTITY SIGNING_EMAIL)
@@ -30,7 +31,7 @@ ctest_start(Nightly GROUP Package)
 # build the dist target for packages
 # we assume the configuration is correct for this. So please no KNIME, PYOPENMS, STYLE or COVERAGE.
 
-ctest_configure(OPTIONS ${CONFIGURE_OPTIONS} RETURN_VALUE _reconfig_package_ret_val)
+ctest_configure(OPTIONS "${CONFIGURE_OPTIONS}" RETURN_VALUE _reconfig_package_ret_val)
 ctest_build(BUILD "${CTEST_BINARY_DIRECTORY}" TARGET "dist" NUMBER_ERRORS _build_errors)
 ctest_submit(PARTS Build)
 
