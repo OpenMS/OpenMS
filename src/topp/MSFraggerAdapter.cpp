e91d75a51249d36c1c2489f72746394400268509
Chris Bielow <chris.bielow@fu-berlin.de>
2023-10-26 11:59:18 +0200

    require MSFragger.jar to exist (or whatever the user specified)

--- src/topp/MSFraggerAdapter.cpp
+++ src/topp/MSFraggerAdapter.cpp
@@ -223,7 +223,7 @@ protected:
     registerIntOption_(TOPPMSFraggerAdapter::java_heapmemory, "<num>", 3500, "Maximum Java heap size (in MB)", false);
 
     // Handle executable
-    registerInputFile_(TOPPMSFraggerAdapter::executable, "<path_to_executable>", "MSFragger.jar", "Path to the MSFragger executable to use; may be empty if the executable is globally available.", false, false, {"is_executable"});
+    registerInputFile_(TOPPMSFraggerAdapter::executable, "<path_to_executable>", "MSFragger.jar", "Path to the MSFragger executable to use; may be empty if the executable is globally available.", true, false, {"is_executable"});
 
     // Input file
     registerInputFile_(TOPPMSFraggerAdapter::in, "<file>", "", "Input File with specta for MSFragger");
