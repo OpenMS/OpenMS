81433a0384e564ebbf199f7b6e1400278e6510be
axelwalter <axel.walter@uni-tuebingen.de>
2023-12-04 10:57:50 +0100

    Update src/openms/source/FORMAT/DATAACCESS/SiriusFragmentAnnotation.cpp
    Co-authored-by: Timo Sachsenberg <timo.sachsenberg@uni-tuebingen.de>

--- src/openms/source/FORMAT/DATAACCESS/SiriusFragmentAnnotation.cpp
+++ src/openms/source/FORMAT/DATAACCESS/SiriusFragmentAnnotation.cpp
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 //
 // --------------------------------------------------------------------------
-// $Maintainer: Oliver Alka., Axel Walter $
+// $Maintainer: Oliver Alka, Axel Walter $
 // $Authors: Oliver Alka $
 // --------------------------------------------------------------------------
 
