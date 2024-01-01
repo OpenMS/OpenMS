663e68aacbcb732d957751ed29c43c352c188c2e
timosachsenberg <timosachsenberg@users.noreply.github.com>
2024-01-01 01:07:00 +0000

    Updated year number

--- cmake/Modules/FindLIBSVM.cmake
+++ cmake/Modules/FindLIBSVM.cmake
@@ -10,7 +10,7 @@
 #
 #=============================================================================
 # Copyright The OpenMS Team -- Eberhard Karls University Tuebingen,
-# ETH Zurich, and Freie Universitaet Berlin 2002-2023.
+# ETH Zurich, and Freie Universitaet Berlin 2002-.
 #
 # This software is released under a three-clause BSD license:
 #  * Redistributions of source code must retain the above copyright
