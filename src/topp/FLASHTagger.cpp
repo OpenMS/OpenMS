b349ed2b6bbcf4bc26b3278c4c487e840866e6dd
KyowonJeong <jeong@informatik.uni-tuebingen.de>
2024-03-22 22:52:50 +0100

    Matrix bug fixed

--- src/topp/FLASHTagger.cpp
+++ src/topp/FLASHTagger.cpp
@@ -24,7 +24,7 @@ using namespace std;
 // Doxygen docu
 //-------------------------------------------------------------
 /**
-@page TOPP_FLASHTagger FLASHTagger 
+@page TOPP_FLASHTagger FLASHTagger
 
 <B>The command line parameters of this tool are:</B>
 @verbinclude TOPPFLASHTagger.cli
