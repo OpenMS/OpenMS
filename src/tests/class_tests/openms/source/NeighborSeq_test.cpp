4b7fe6643d3f0a3e9570c2d503c7fdde95730d63
Chris Bielow <chris.bielow@fu-berlin.de>
2024-08-13 13:25:45 +0200

    extend tests

--- src/tests/class_tests/openms/source/NeighborSeq_test.cpp
+++ src/tests/class_tests/openms/source/NeighborSeq_test.cpp
@@ -2,8 +2,8 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // 
 // --------------------------------------------------------------------------
-// $Maintainer: Chris Bielow $
-// $Authors: Andreas Bertsch $
+// $Maintainer: Chris Bielow, Philipp Wang $
+// $Authors: Chris Bielow, Philipp Wang $
 // --------------------------------------------------------------------------
 #include <OpenMS/FORMAT/FASTAFile.h>
 #include <OpenMS/CONCEPT/ClassTest.h>
@@ -30,30 +30,33 @@ START_SECTION(MSSpectrum generateSpectrum(const String& peptide_sequence))
   MSSpectrum spec_3 = ns.generateSpectrum(AASequence::fromString("VGLPINQR"));
 
   // Test "PEPT"
-  TEST_REAL_SIMILAR(spec_1[0].getPosition()[0], 120.0655);
-  TEST_REAL_SIMILAR(spec_1[1].getPosition()[0], 217.1182);
-  TEST_REAL_SIMILAR(spec_1[2].getPosition()[0], 227.1026);
-  TEST_REAL_SIMILAR(spec_1[3].getPosition()[0], 324.1553);
-  TEST_REAL_SIMILAR(spec_1[4].getPosition()[0], 346.1608);
+  TEST_REAL_SIMILAR(spec_1[0].getMZ(), 98.0600);
+  TEST_REAL_SIMILAR(spec_1[1].getMZ(), 120.0655);
+  TEST_REAL_SIMILAR(spec_1[2].getMZ(), 217.1182);
+  TEST_REAL_SIMILAR(spec_1[3].getMZ(), 227.1026);
+  TEST_REAL_SIMILAR(spec_1[4].getMZ(), 324.1553);
+  TEST_REAL_SIMILAR(spec_1[5].getMZ(), 346.1608);
   
 
   // Test "AR"
-  TEST_REAL_SIMILAR(spec_2[0].getPosition()[0], 175.1189);
+  TEST_REAL_SIMILAR(spec_2[0].getMZ(), 72.04439);
+  TEST_REAL_SIMILAR(spec_2[1].getMZ(), 175.1189);
 
   // Test "VGLPINQR"
-  TEST_REAL_SIMILAR(spec_3[0].getPosition()[0], 157.0971);
-  TEST_REAL_SIMILAR(spec_3[1].getPosition()[0], 175.1189);
-  TEST_REAL_SIMILAR(spec_3[2].getPosition()[0], 270.1812);
-  TEST_REAL_SIMILAR(spec_3[3].getPosition()[0], 303.1775);
-  TEST_REAL_SIMILAR(spec_3[4].getPosition()[0], 367.2339);
-  TEST_REAL_SIMILAR(spec_3[5].getPosition()[0], 417.2204);
-  TEST_REAL_SIMILAR(spec_3[6].getPosition()[0], 480.3180);
-  TEST_REAL_SIMILAR(spec_3[7].getPosition()[0], 530.3045);
-  TEST_REAL_SIMILAR(spec_3[8].getPosition()[0], 594.3609);
-  TEST_REAL_SIMILAR(spec_3[9].getPosition()[0], 627.3578);
-  TEST_REAL_SIMILAR(spec_3[10].getPosition()[0], 722.4195);
-  TEST_REAL_SIMILAR(spec_3[11].getPosition()[0], 740.4413);
-  TEST_REAL_SIMILAR(spec_3[12].getPosition()[0], 797.4628);
+  TEST_REAL_SIMILAR(spec_3[0].getMZ(), 100.0756);
+  TEST_REAL_SIMILAR(spec_3[1].getMZ(), 157.0971);
+  TEST_REAL_SIMILAR(spec_3[2].getMZ(), 175.1189);
+  TEST_REAL_SIMILAR(spec_3[3].getMZ(), 270.1812);
+  TEST_REAL_SIMILAR(spec_3[4].getMZ(), 303.1775);
+  TEST_REAL_SIMILAR(spec_3[5].getMZ(), 367.2339);
+  TEST_REAL_SIMILAR(spec_3[6].getMZ(), 417.2204);
+  TEST_REAL_SIMILAR(spec_3[7].getMZ(), 480.3180);
+  TEST_REAL_SIMILAR(spec_3[8].getMZ(), 530.3045);
+  TEST_REAL_SIMILAR(spec_3[9].getMZ(), 594.3609);
+  TEST_REAL_SIMILAR(spec_3[10].getMZ(), 627.3578);
+  TEST_REAL_SIMILAR(spec_3[11].getMZ(), 722.4195);
+  TEST_REAL_SIMILAR(spec_3[12].getMZ(), 740.4413);
+  TEST_REAL_SIMILAR(spec_3[13].getMZ(), 797.4628);
 
 }
 END_SECTION
@@ -154,37 +157,49 @@ START_SECTION(bool isNeighborPeptide(const AASequence& neighbor_candidate,
                              const double mz_bin_size))
 {
   const AASequence AA_VELQSK = AASequence::fromString("VELQSK");
+  const AASequence AA_SVQELK = AASequence::fromString("SVQELK");
+  const AASequence AA_TVDQLK = AASequence::fromString("TVDQLK");
+
   const AASequence AA_VESQLK = AASequence::fromString("VESQLK");
-  std::vector<AASequence> seqs = {AASequence::fromString("VELQSK"), AASequence::fromString("SVQELK"), AASequence::fromString("TVDQLK"), AASequence::fromString("VGEFK")};
+  std::vector<AASequence> seqs = {AASequence::fromString("VELQSK"),
+                                  AASequence::fromString("SVQELK"),
+                                  AASequence::fromString("TVDQLK"),
+                                  AASequence::fromString("VGEFK")};
   NeighborSeq ns(std::move(seqs));
-  // VELQSK has neighbor VESQLK
-  // SVQELK has neighbor VESQLK
-  // TVDQLK has neighbor VESQLK
+  // VELQSK has neighbor VESQLK // shares 6 ions
+  // SVQELK has neighbor VESQLK // shares 4 ions
+  // TVDQLK has neighbor VESQLK // shares 6 ions
   // VGEFK  has neighbor GLDFK
-  // GIDFK  has neighbor GLDFK
 
   const double pc_tolerance = 0.01;
   const double mz_bin_size = 0.05;
   TEST_TRUE(std::abs(AA_VELQSK.getMonoWeight() - AA_VESQLK.getMonoWeight()) < pc_tolerance)
-  TEST_TRUE(ns.computeSharedIonCount(ns.generateSpectrum(AA_VELQSK), ns.generateSpectrum(AA_VESQLK), mz_bin_size) == 5)
+  TEST_EQUAL(ns.computeSharedIonCount(ns.generateSpectrum(AA_VELQSK), ns.generateSpectrum(AA_VESQLK), mz_bin_size), 6)
+  TEST_EQUAL(ns.computeSharedIonCount(ns.generateSpectrum(AA_SVQELK), ns.generateSpectrum(AA_VESQLK), mz_bin_size), 4)
+  TEST_EQUAL(ns.computeSharedIonCount(ns.generateSpectrum(AA_TVDQLK), ns.generateSpectrum(AA_VESQLK), mz_bin_size), 6)
 
-  TEST_TRUE(ns.isNeighborPeptide(AASequence::fromString("VESQLK"), pc_tolerance, false, 0.25, mz_bin_size))
-  TEST_FALSE(ns.isNeighborPeptide(AASequence::fromString("VESQLK"), pc_tolerance, false, 5*2.0/( (6-1)*2*2) + 0.1, mz_bin_size))
+  // test the overlap threshold:
+  const double shared_ion_fraction = 6 * 2.0 / ( ((AA_VESQLK.size() - 1) * 2 /*b and y*/) * 2);
+  TEST_FALSE(ns.isNeighborPeptide(AASequence::fromString("VESQLK"), pc_tolerance, false, shared_ion_fraction + 0.1, mz_bin_size))
+  // VESQLK matches VELQSK and TVDQLK (but not SVQELK since the overlap is insufficient)
+  TEST_TRUE (ns.isNeighborPeptide(AASequence::fromString("VESQLK"), pc_tolerance, false, shared_ion_fraction - 0.1, mz_bin_size))
+
+  // GLDFK matches to VGEFK
   TEST_TRUE(ns.isNeighborPeptide(AASequence::fromString("GLDFK"), pc_tolerance, false, 0.25, mz_bin_size))
 
   auto stats = ns.getNeighborStats();
   TEST_EQUAL(stats.unfindable_peptides, 0)
-  TEST_EQUAL(stats.findable_no_neighbors, 0)
-  TEST_EQUAL(stats.findable_one_neighbor, 4)
+  TEST_EQUAL(stats.findable_no_neighbors, 1)
+  TEST_EQUAL(stats.findable_one_neighbor, 3)
   TEST_EQUAL(stats.findable_multiple_neighbors, 0)
 
-  // test one neighbor again, which causes 3 candidates to have multiple neighbors
-  TEST_TRUE(ns.isNeighborPeptide(AASequence::fromString("VESQLK"), 0.01, false, 0.25, 0.05))
+  // test VESQLK again, which is a neighbor for 3 ref peptides at threshold 0.25
+  TEST_TRUE(ns.isNeighborPeptide(AASequence::fromString("VESQLK"), pc_tolerance, false, 0.25, mz_bin_size))
   auto stats2 = ns.getNeighborStats();
   TEST_EQUAL(stats2.unfindable_peptides, 0)
   TEST_EQUAL(stats2.findable_no_neighbors, 0)
-  TEST_EQUAL(stats2.findable_one_neighbor, 1)
-  TEST_EQUAL(stats2.findable_multiple_neighbors, 3)
+  TEST_EQUAL(stats2.findable_one_neighbor, 2)
+  TEST_EQUAL(stats2.findable_multiple_neighbors, 2)
 }
 END_SECTION
 
