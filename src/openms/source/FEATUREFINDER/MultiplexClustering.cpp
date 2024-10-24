2f566776511606d2c2d579adf02d50f31adc05da
Timo Sachsenberg <timo.sachsenberg@uni-tuebingen.de>
2024-10-24 19:06:50 +0200

    MzTab: preallocate enough memory instead of allocating in the loops (#7627)
    * add check that otherwise will result in out of memory error
    
    * reduce memory fragmentation on huge files
    
    * Update src/openms/source/FORMAT/MzTab.cpp
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    Co-authored-by: Chris Bielow <chris.bielow@fu-berlin.de>
    
    * Update MultiplexClustering.cpp
    
    ---------
    
    Co-authored-by: timosachsenberg <sachsenb@ibminode06.Cs.Uni-Tuebingen.De>
    Co-authored-by: Chris Bielow <chris.bielow@fu-berlin.de>

--- src/openms/source/FEATUREFINDER/MultiplexClustering.cpp
+++ src/openms/source/FEATUREFINDER/MultiplexClustering.cpp
@@ -86,11 +86,12 @@ namespace OpenMS
     double rt_min = exp.getMinRT();
     double rt_max = exp.getMaxRT();
 
-    if (mz_min < 0.0 || mz_max < mz_min || mz_max > 1.0e12)
+    if (!RangeMZ(0.0, 1.0e12).containsMZ({mz_min, mz_max}) ||
+        !RangeRT(-1.0e12, 1.0e12).containsRT({rt_min, rt_max}) ) 
     {
-      throw Exception::IllegalArgument(__FILE__, __LINE__, OPENMS_PRETTY_FUNCTION, "MinMZ,MaxMZ values outside of sensible value ranges. Are they uninitialized? (" + String(mz_min) + "/" + String(mz_max));
+      throw Exception::IllegalArgument(__FILE__, __LINE__, OPENMS_PRETTY_FUNCTION, "MinMZ,MaxMZ,MinRT,MaxRT values outside of sensible value ranges. Are they uninitialized? (" + String(mz_min) + "/" + String(mz_max) + "/" + String(rt_min) + "/" + String(rt_max));
     }
-
+    
     // extend the grid by a small absolute margin
     double mz_margin = 1e-2;
     double rt_margin = 1e-2;
