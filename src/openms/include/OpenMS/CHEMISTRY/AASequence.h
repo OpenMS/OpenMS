3847c31fba7f0f796cee76e7d26fb5f32f841c64
Timo Sachsenberg <timo.sachsenberg@uni-tuebingen.de>
2024-12-02 14:01:13 +0100

    add final to iterators and remove unneded virtual

--- src/openms/include/OpenMS/CHEMISTRY/AASequence.h
+++ src/openms/include/OpenMS/CHEMISTRY/AASequence.h
@@ -92,7 +92,7 @@ public:
 
         AASequence constant iterator
     */
-    class OPENMS_DLLAPI ConstIterator
+    class OPENMS_DLLAPI ConstIterator final
     {
     public:
       // TODO Iterator constructor for ConstIterator
@@ -129,7 +129,7 @@ public:
       }
 
       /// destructor
-      virtual ~ConstIterator() = default;
+      ~ConstIterator() = default;
 
       //@}
 
@@ -210,7 +210,7 @@ protected:
 
         Mutable iterator for AASequence
     */
-    class OPENMS_DLLAPI Iterator
+    class OPENMS_DLLAPI Iterator final
     {
 public:
 
@@ -239,7 +239,7 @@ public:
       Iterator(const Iterator& rhs) = default;
 
       /// destructor
-      virtual ~Iterator() = default;
+      ~Iterator() = default;
 
       //@}
 
@@ -343,7 +343,7 @@ protected:
     AASequence(AASequence&&) noexcept = default;
 
     /// Destructor
-    virtual ~AASequence() = default;
+    ~AASequence() = default;
     //@}
 
     /// Assignment operator
