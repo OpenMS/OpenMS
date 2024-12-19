ec728f71e1c93165fed2b30b450acb79b9322aa6
Samuel Wein <sam@samwein.com>
2024-12-20 00:33:38 +0100

    remove * prefix

--- src/openms_gui/source/VISUAL/DIALOGS/ToolsDialog.cpp
+++ src/openms_gui/source/VISUAL/DIALOGS/ToolsDialog.cpp
@@ -445,11 +445,13 @@ namespace OpenMS
     // Try to Return the first valid string for the extension on the output parameter
     // If we can't get any valid strings show an error.
     String extension = FileTypes::typeToName(FileTypes::UNKNOWN);
-    auto validStrings = vis_param_.getValidStrings(output_combo_->currentText().remove(QRegularExpression("^\\.\\*")).toStdString()); 
+    auto validStrings = vis_param_.getValidStrings(output_combo_->currentText().toStdString()); 
     // If we have only one valid output type use that
     if (validStrings.size() == 1)
     {
       extension = validStrings[0];
+      // Remove the leading .*
+      extension = extension.suffix(extension.size() - 2);
     }
     // Otherwise the type is unknown
     else 
