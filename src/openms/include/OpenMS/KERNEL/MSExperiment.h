eb2f7c436b2f4684073a823c44f66032693c7ebe
Timo Sachsenberg <timo.sachsenberg@uni-tuebingen.de>
2025-01-15 17:21:02 +0100

    don't cache ms level information

--- src/openms/include/OpenMS/KERNEL/MSExperiment.h
+++ src/openms/include/OpenMS/KERNEL/MSExperiment.h
@@ -1069,17 +1069,20 @@ std::vector<MSChromatogram> extractXICs(
     void updateRanges() override;
 
     /**
-      @brief Updates the m/z, intensity, retention time and MS level ranges of all spectra with a certain ms level
+      @brief Updates the m/z, intensity, and retention time ranges of all spectra with a certain ms level
+      
+      @note This method does not modify the set of MS levels (ms_levels_) stored in the experiment.
+      To update MS levels, use updateRanges() without parameters instead.
 
-      @param ms_level MS level to consider for m/z range , RT range and intensity range (All MS levels if negative)
+      @param ms_level MS level to consider for m/z range, RT range and intensity range (All MS levels if negative)
     */
     void updateRanges(Int ms_level);
 
     /// returns the total number of peaks (spectra and chromatograms included)
     UInt64 getSize() const;
 
-    /// returns an array of MS levels
-    const std::vector<UInt>& getMSLevels() const;
+    /// returns an array of MS levels (calculated on demand)
+    std::vector<UInt> getMSLevels() const;
 
     ///@}
 
@@ -1279,8 +1282,6 @@ std::vector<MSChromatogram> extractXICs(
     bool isIMFrame() const;
 
   protected:
-    /// MS levels of the data
-    std::vector<UInt> ms_levels_;
     /// chromatograms
     std::vector<MSChromatogram > chromatograms_;
     /// spectra
