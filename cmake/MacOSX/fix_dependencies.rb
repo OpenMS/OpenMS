9b89f0f2b9e4e338b0ff21a61b5115eac609e459
Samuel Wein <sam@samwein.com>
2025-01-17 10:03:41 +0000

    try error filtering again

--- cmake/MacOSX/fix_dependencies.rb
+++ cmake/MacOSX/fix_dependencies.rb
@@ -107,7 +107,7 @@ end
 ###############################################################################
 def fixLoadPath(oldpath,libpath,target)
   debug "#{$install_name_tool} -change #{oldpath} #{$executableId}#{libpath} #{target}"
-  `#{$install_name_tool} -change #{oldpath} #{$executableId}#{libpath} #{target}` >(grep -v "changes being made to the file will invalidate the code signature" >/dev/null)
+  `#{$install_name_tool} -change #{oldpath} #{$executableId}#{libpath} #{target}` 2>&1 | grep -v "changes being made to the file will invalidate the code signature"
 end
 
 ###############################################################################
