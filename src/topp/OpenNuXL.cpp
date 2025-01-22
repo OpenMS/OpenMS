a413358b6526bf9955c55950a45d37ebf7934e9b
Timo Sachsenberg <timo.sachsenberg@uni-tuebingen.de>
2025-01-22 16:43:34 +0100

    partial loss score still different

--- src/topp/OpenNuXL.cpp
+++ src/topp/OpenNuXL.cpp
@@ -1178,11 +1178,12 @@ protected:
 
   static double logfactorial_(UInt x)
   {
+    const double dx = static_cast<double>(x);
     if (x < 2) { return 0; }
     double z(0);
-    for (double y = 2; y <= static_cast<double>(x); ++y) { z += log(static_cast<double>(y)); }
-      return z;
-    }
+    for (double y = 2; y <= dx; ++y) { z += std::log(y); }
+    return z;
+  }
 
   // score ions without nucleotide shift
   static void scorePeptideIons_(
@@ -1430,7 +1431,7 @@ protected:
       const double bFact = logfactorial_(b_ion_count);
       const double aFact = logfactorial_(a_ion_count);
       const double yFact = logfactorial_(y_ion_count);
-      hyperScore = log1p(dot_product) + yFact + bFact + aFact;
+      hyperScore = std::log1p(dot_product) + yFact + bFact + aFact;
       MIC = std::accumulate(intensity_sum.begin(), intensity_sum.end(), 0.0);
       for (auto& i : intensity_sum) { i /= TIC; } // scale intensity sum
       MIC /= TIC;
@@ -1984,7 +1985,7 @@ protected:
       const double bFact = logfactorial_(b_ion_count);
       const double aFact = logfactorial_(a_ion_count);
       const double yFact = logfactorial_(y_ion_count);
-      plss_hyperScore = log1p(dot_product) + yFact + bFact + aFact;
+      plss_hyperScore = std::log1p(dot_product) + yFact + bFact + aFact;
       plss_MIC = std::accumulate(intensity_sum.begin(), intensity_sum.end(), 0.0);
       for (auto& i : intensity_sum) { i /= TIC; } // scale intensity sum
       plss_MIC /= TIC;
