b3d050f6473fa975b3d83f6c9a8f4dd47c37ebac
Chandra Pratap Shah <151423552+ChandraDevs1@users.noreply.github.com>
2025-03-04 21:46:34 +0530

    Update setup.py
    Updated `setup.py` to include compatibility with Python 3.13.0

--- src/pyOpenMS/setup.py
+++ src/pyOpenMS/setup.py
@@ -253,7 +253,7 @@ setup(
     package_data= {
         'pyopenms': ['py.typed', '*.pyi']
     },
-    python_requires=">=3.8, <3.13",
+    python_requires=">=3.8, <=3.13",
 	install_requires=[
           'numpy>=1.25.0',
           'pandas',
