325d356fd77e047b1ab61df74319a90bb5d0b743
SANKARSHAN MISHRA <124585946+SankarshanMishra@users.noreply.github.com>
2025-03-19 04:18:16 +0530

    Update EmgFitter1D.h

--- src/openms/include/OpenMS/FEATUREFINDER/EmgFitter1D.h
+++ src/openms/include/OpenMS/FEATUREFINDER/EmgFitter1D.h
@@ -39,55 +39,17 @@ public:
     QualityType fit1d(const RawDataArrayType& range, std::unique_ptr<InterpolationModel>& model) override;
 
 protected:
-    /// Helper struct (contains the size of an area and a raw data container)
-    struct Data
-    {
-      typedef Peak1D PeakType;
-      typedef std::vector<PeakType> RawDataArrayType;
-
-      Size n;
-      RawDataArrayType set;
-    };
-
-    class EgmFitterFunctor :
-      public LevMarqFitter1D::GenericFunctor
-    {
-public:
-      EgmFitterFunctor(int dimensions, const EmgFitter1D::Data* data) :
-        LevMarqFitter1D::GenericFunctor(dimensions,
-                                        static_cast<int>(data->n)),
-        m_data(data)
-      {}
-
-      int operator()(const Eigen::VectorXd& x, Eigen::VectorXd& fvec) const override;
-      // compute Jacobian matrix for the different parameters
-      int df(const Eigen::VectorXd& x, Eigen::MatrixXd& J) const override;
-
-protected:
-      const EmgFitter1D::Data* m_data;
-      static const EmgFitter1D::CoordinateType c;
-      static const EmgFitter1D::CoordinateType sqrt2pi;
-      static const EmgFitter1D::CoordinateType emg_const;
-      static const EmgFitter1D::CoordinateType sqrt_2;
-    };
-
-    /// Compute start parameter
-    virtual void setInitialParameters_(const RawDataArrayType& set);
-    /// Compute start parameters using method of moments (usually reduces nr. of iterations needed at some
-    /// additional one-time costs
+    void setInitialParameters_(const RawDataArrayType& set);
     void setInitialParametersMOM_(const RawDataArrayType& set);
-
-    /// Parameter of emg - peak height
-    CoordinateType height_;
-    /// Parameter of emg - peak width
-    CoordinateType width_;
-    /// Parameter of emg - peak symmetry
-    CoordinateType symmetry_;
-    /// Parameter of emg - peak retention time
-    CoordinateType retention_;
-
     void updateMembers_() override;
-  };
 
+    // Inner classes and member variables
+    struct Data;
+    class EgmFitterFunctor;
+    CoordinateType height_;
+    CoordinateType width_;
+    CoordinateType symmetry_;
+    CoordinateType retention_;
+  };
 }
 
