5efef46afa783c3c5d0d8a1b4be7e030607e469b
Timo Sachsenberg <timo.sachsenberg@uni-tuebingen.de>
2025-03-19 21:30:01 +0100

    fix doc

--- src/topp/IDMerger.cpp
+++ src/topp/IDMerger.cpp
@@ -32,7 +32,7 @@ using namespace std;
 <th ALIGN = "center"> potential successor tools </td>
 </tr>
 <tr>
-<td VALIGN="middle" ALIGN = "center" ROWSPAN=1> @ref TOPP_MascotAdapter (or other ID engines) </td>
+<td VALIGN="middle" ALIGN = "center" ROWSPAN=1> @ref TOPP_CometAdapter (or other ID engines) </td>
 <td VALIGN="middle" ALIGN = "center" ROWSPAN=1> @ref TOPP_ConsensusID </td>
 </tr>
 <tr>
