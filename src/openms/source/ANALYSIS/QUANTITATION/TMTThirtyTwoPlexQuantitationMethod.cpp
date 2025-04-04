5bcc734b352650a78445107468ae3607bc894c27
FreezB11 <yashrajrao4@gmail.com>
2025-04-05 04:34:51 +0530

    TMTThirtyTwoPlexQuantitationMethod updated(fixed)

--- src/openms/source/ANALYSIS/QUANTITATION/TMTThirtyTwoPlexQuantitationMethod.cpp
+++ src/openms/source/ANALYSIS/QUANTITATION/TMTThirtyTwoPlexQuantitationMethod.cpp
@@ -26,10 +26,10 @@ namespace OpenMS{
                                                                                         "131N","131C","131ND","131CD",
                                                                                         "132N","132C","132ND","132CD",
                                                                                         "133N","133C","133ND","133CD",
-                                                                                        "134N" "134ND", "134CD",
+                                                                                        "134N","134ND", "134CD",
                                                                                         "135ND"}
 
-    const std::vector<std::string> TMTThirtyTwoPlexQuantitationMethod::channel_names_ = {""}
+    // const std::vector<std::string> TMTThirtyTwoPlexQuantitationMethod::channel_names_ = {""}
     TMTThirtyTwoPlexQuantitationMethod::TMTThirtyTwoPlexQuantitationMethod()
     {
         setName("TMTThirtyTwoPlexQuantitationMethod");
