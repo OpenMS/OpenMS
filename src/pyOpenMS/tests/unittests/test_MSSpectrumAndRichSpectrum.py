3682ea29c1a7e15f4249f2e790a3a03ae4417b10
ChandraDevs1 <rolexchilli@gmail.com>
2025-04-08 04:18:48 +0530

    added unit test

--- src/pyOpenMS/tests/unittests/test_MSSpectrumAndRichSpectrum.py
+++ src/pyOpenMS/tests/unittests/test_MSSpectrumAndRichSpectrum.py
@@ -1,12 +1,74 @@
 import unittest
 import os
-
 import pyopenms
-
+from pyopenms import Precursor, PeptideIdentification, PeptideHit, AASequence
+import numpy as np
 class TestMSSpectrumAndRichSpectrum(unittest.TestCase):
 
     def setUp(self):
         dirname = os.path.dirname(os.path.abspath(__file__))
+    
+    def test_ion_mobility(self):
+        spec = pyopenms.MSSpectrum()
+        spec.set_peaks(([100.0], [10.0]))
+        
+        # Add ion mobility array
+        fda = spec.getFloatDataArrays().create()
+        fda.setName("Ion Mobility")
+        fda.push_back(0.5)
+        spec.setFloatDataArrays([fda])
+        spec.setDriftTimeUnit(pyopenms.MSSpectrum().DriftTimeUnit.MS_VENDOR)
+
+        data = spec.get_data_dict()
+        self.assertTrue(np.array_equal(data["ion_mobility"], [0.5]))
+        self.assertEqual(data["ion_mobility_unit"][0], "ms")
+
+    def test_precursors(self):
+        spec = pyopenms.MSSpectrum()
+        p = Precursor()
+        p.setMZ(300.0)
+        p.setCharge(2)
+        spec.setPrecursors([p])
+        
+        data = spec.get_data_dict()
+        self.assertEqual(data["precursor_mz"][0], 300.0)
+        self.assertEqual(data["precursor_charge"][0], 2)
+
+    def test_peptide_identifications(self):
+        spec = pyopenms.MSSpectrum()
+        pep_id = PeptideIdentification()
+        hit = PeptideHit()
+        hit.setSequence(AASequence.fromString("PEPTIDE"))
+        pep_id.setHits([hit])
+        spec.setPeptideIdentifications([pep_id])
+        
+        data = spec.get_data_dict()
+        self.assertEqual(data["sequence"][0], "PEPTIDE")
+
+    def test_metadata_handling(self):
+        spec = pyopenms.MSSpectrum()
+        spec.setMetaValue(b"int_val", 5)
+        spec.setMetaValue(b"float_val", 3.14)
+        spec.setMetaValue(b"str_val", "test")
+        spec.setMetaValue(b"bool_val", True)
+        
+        data = spec.get_data_dict()
+        self.assertEqual(data["int_val"][0], 5)
+        self.assertEqual(data["float_val"][0], 3.14)
+        self.assertEqual(data["str_val"][0], "test")
+        self.assertEqual(data["bool_val"][0], True)
+
+    def test_empty_spectrum(self):
+        spec = pyopenms.MSSpectrum()
+        data = spec.get_data_dict()
+        self.assertEqual(len(data["mz"]), 0)
+        self.assertEqual(data["precursor_mz"][0], np.nan)
+
+    def test_export_meta_values_flag(self):
+        spec = pyopenms.MSSpectrum()
+        spec.setMetaValue(b"test", 42)
+        data = spec.get_data_dict(export_meta_values=False)
+        self.assertNotIn("test", data)
 
 
     def testMSSpectrum(self):
