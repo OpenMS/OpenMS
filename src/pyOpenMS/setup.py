90d89ad31c8bb3268fe3d127d3cffc0d40e687e7
Tom David Mueller <tom.mueller@beachouse.de>
2025-04-14 10:51:23 +0200

    rpath to lib

--- src/pyOpenMS/setup.py
+++ src/pyOpenMS/setup.py
@@ -183,9 +183,9 @@ elif sys.platform == "darwin":
         libraries.append("omp")
     # we need to manually link to the Qt Frameworks
     python_version = f"{sys.version_info.major}.{sys.version_info.minor}"
-    pyopenms_path = f"@loader_path/python{python_version}/site-packages/pyopenms"
+    pyopenms_path = f"@loader_path/../lib/python{python_version}/site-packages/pyopenms"
     extra_compile_args = ["-Qunused-arguments", "-fopenmp"]
-    extra_link_args = ["-Wl,-rpath,@loader_path", f"-Wl,-rpath,{pyopenms_path}", "-lomp"]
+    extra_link_args = ["-Wl,-rpath,@loader_path/../lib", f"-Wl,-rpath,{pyopenms_path}", "-lomp"]
 if IS_DEBUG:
     extra_compile_args.append("-g2")
 if OMP and OPENMP_CXX_FLAGS:
