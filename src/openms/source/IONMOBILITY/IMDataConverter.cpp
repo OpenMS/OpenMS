85cc3d40c5da230dffd04590a05d1702bf1dc806
Timo Sachsenberg <timo.sachsenberg@uni-tuebingen.de>
2025-05-06 20:33:03 +0200

    Adds detailed range reporting for MSExperiment data
    Introduces specialized templates for human-readable and machine-readable range reporting in MSExperiment, covering retention time, mass-to-charge, ion mobility, and intensity across combined, spectrum-specific, and chromatogram ranges. Enhances the clarity and granularity of data outputs.

--- src/openms/source/IONMOBILITY/IMDataConverter.cpp
+++ src/openms/source/IONMOBILITY/IMDataConverter.cpp
@@ -206,7 +206,7 @@ namespace OpenMS
         term = &cv.getTerm("MS:1002816");
         break;
       case DriftTimeUnit::VSSC:
-        term = &cv.getTerm("MS:1003008");
+         term = &cv.getTerm("MS:1003008");
         break;
       default:
         throw Exception::InvalidValue(__FILE__, __LINE__, OPENMS_PRETTY_FUNCTION, "Unit cannot be converted into CV term.", toString(unit));
