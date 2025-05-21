8b30bf3c509faddb6c5b69e81623ab7bce168a2f
Timo Sachsenberg <timo.sachsenberg@uni-tuebingen.de>
2025-05-21 08:37:22 +0200

    refactor: Store IDs outside of spectra.  (#7974)
    * Add AnnotatedMSRawData and integrate it instead of MSExperiment where identification is needed
    
    * Various refactorings in order to integrate the new class "AnnotatedMSRawData"
    
    Build target "OpenMS" is successfully compiling now.
    
    * some fixes to tests
    
    * remove remnantsof protein identification at spectra
    
    * Apply suggestions from code review
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * minor
    
    * fix test
    
    * class and tool tests pass
    
    * more fixes
    
    * rename
    
    * removed commented out code
    
    * Update src/openms_gui/source/VISUAL/PlotCanvas.cpp
    
    * Update src/openms/source/ANALYSIS/ID/IDMapper.cpp
    
    * nop
    
    * simplified interface a bit more
    
    * moved file out of kernel
    
    * remove mzML from IDSplitter
    
    * forgot one file
    
    * removed unused mapping class
    
    * compiles again
    
    * some TV fixes
    
    * More identification view fixes
    
    * peak annotations work again
    
    * more safety checks
    
    * more imp to cpp
    
    * mend
    
    * Comment out unused variable declarations
    
    * fix idfilter
    
    * fix doxygen
    
    * reverted ignored setting file
    
    * nop
    
    * merge
    
    * fix warning
    
    ---------
    
    Co-authored-by: dvdvgt <david.voigt1998@gmail.com>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/openms/include/OpenMS/FORMAT/MSPFile.h
+++ src/openms/include/OpenMS/FORMAT/MSPFile.h
@@ -11,11 +11,13 @@
 #include <OpenMS/DATASTRUCTURES/DefaultParamHandler.h>
 #include <OpenMS/METADATA/PeptideIdentification.h>
 #include <OpenMS/KERNEL/StandardTypes.h>
+#include <OpenMS/METADATA/AnnotatedMSRun.h>
 
 #include <vector>
 
 namespace OpenMS
 {
+  class AnnotatedMSRun;
   /**
       @brief File adapter for MSP files (NIST spectra library)
 
@@ -59,11 +61,23 @@ public:
     void load(const String & filename, std::vector<PeptideIdentification> & ids, PeakMap & exp);
 
     /**
+        @brief Loads a map from a MSPFile file.
+
+        @param filename the filename of the experiment
+        @param annot_exp annotated experiment with spectra and ids
+
+        @throw FileNotFound is thrown if the file could not be found
+        @throw ParseError is thrown if the given file could not be parsed
+        @throw ElementNotFound is thrown if a annotated modification cannot be found in ModificationsDB (PSI-MOD definitions)
+    */
+    void load(const String & filename, AnnotatedMSRun & annot_exp);
+
+    /**
         @brief Stores a map in a MSPFile file.
 
         @throw UnableToCreateFile is thrown if the given file could not be created
     */
-    void store(const String & filename, const PeakMap & exp) const;
+    void store(const String & filename, const AnnotatedMSRun & exp) const;
 
 protected:
 
