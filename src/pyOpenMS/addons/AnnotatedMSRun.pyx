d652f5dac9b75be56c69430098ba136a0b97737b
Timo Sachsenberg <timo.sachsenberg@uni-tuebingen.de>
2025-05-22 11:36:24 +0200

    still error in pyx

--- src/pyOpenMS/addons/AnnotatedMSRun.pyx
+++ src/pyOpenMS/addons/AnnotatedMSRun.pyx
@@ -2,22 +2,6 @@
 
     def getMapping(AnnotatedMSRun self, size_t idx):
         """Cython signature: Mapping getMapping(size_t idx)"""
-        #
-        # Get a mapping of spectrum and peptide identification at the given index
-        # Parameters
-        # ----------
-        # idx : size_t
-        #   The index of the mapping to retrieve
-        #     
-        # Returns
-        # -------
-        # Mapping
-        #   A pair containing the spectrum and peptide identification
-        #     
-        # Raises
-        # ------
-        # IndexError
-        #     If the index is out of bounds        
         assert isinstance(idx, (int, long)), 'arg idx wrong type'
         assert idx < self.size(), 'Requested mapping %s does not exist, there are only %s mappings' % (idx, self.size())
         
@@ -36,31 +20,10 @@
         
     def __getitem__(AnnotatedMSRun self, size_t idx):
         """Get a mapping of spectrum and peptide identification at the given index"""
-        # 
-        # Parameters
-        # ----------
-        # idx : size_t
-        #     The index of the mapping to retrieve
-        #     
-        # Returns
-        # -------
-        # tuple
-        #     A tuple containing (MSSpectrum, PeptideIdentification)
-        #     
-        # Raises
-        # ------
-        # IndexError
-        #     If the index is out of bounds
-        # 
         return self.getMapping(idx)
         
     def __iter__(AnnotatedMSRun self):
         """Iterate over all mappings of spectra and peptide identifications"""
-        # 
-        # Returns
-        # -------
-        # iterator
-        #     An iterator yielding tuples of (MSSpectrum, PeptideIdentification)        
         cdef size_t n = self.size()
         for i in range(n):
             yield self.getMapping(i)
