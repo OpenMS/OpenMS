ae53875c4706899c984ca30a2cf4f8f2e0d4b76e
Timo Sachsenberg <timo.sachsenberg@uni-tuebingen.de>
2025-05-26 11:08:53 +0200

    address reviewer comments

--- src/tests/class_tests/openms/source/Isotope_test.cpp
+++ src/tests/class_tests/openms/source/Isotope_test.cpp
@@ -1,37 +1,12 @@
-// --------------------------------------------------------------------------
-//                   OpenMS -- Open-Source Mass Spectrometry               
-// --------------------------------------------------------------------------
-// Copyright The OpenMS Team -- Eberhard Karls University Tuebingen,
-// ETH Zurich, and Freie Universitaet Berlin 2002-2021.
-// 
-// This software is released under a three-clause BSD license:
-//  * Redistributions of source code must retain the above copyright
-//    notice, this list of conditions and the following disclaimer.
-//  * Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the
-//    documentation and/or other materials provided with the distribution.
-//  * Neither the name of any author or any participating institution 
-//    may be used to endorse or promote products derived from this software 
-//    without specific prior written permission.
-// For a full list of authors, refer to the file AUTHORS. 
-// --------------------------------------------------------------------------
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-// ARE DISCLAIMED. IN NO EVENT SHALL ANY OF THE AUTHORS OR THE CONTRIBUTING 
-// INSTITUTIONS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
-// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
-// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
-// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
-// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
-// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// Copyright (c) 2002-present, OpenMS Inc. -- EKU Tuebingen, ETH Zurich, and FU Berlin
+// SPDX-License-Identifier: BSD-3-Clause
 // 
 // --------------------------------------------------------------------------
 // $Maintainer: Timo Sachsenberg $
 // $Authors: Andreas Bertsch $
 // --------------------------------------------------------------------------
 //
+
 
 #include <OpenMS/CONCEPT/ClassTest.h>
 #include <OpenMS/test_config.h>
@@ -80,9 +55,19 @@ START_SECTION((Isotope(const std::string & name,
 END_SECTION
 
 START_SECTION( const Element* getElement() const)
-  const Element* el = e_ptr->getElement();
+  // Create a specific imaginary isotope of Phosphorus for this test
+  Isotope test_phosphorus("Phosphorus-32", "P", 15, 17, 31.973907, 0.0, 1233360, Isotope::DecayMode::BETA_MINUS);
+  
+  const Element* el = test_phosphorus.getElement();
   TEST_NOT_EQUAL(el, e_nullPointer)
   TEST_EQUAL(el->getSymbol(), "P")
+  TEST_EQUAL(el->getAtomicNumber(), 15)
+  TEST_EQUAL(el->getName(), "Phosphorus-32")
+  
+  // Also test with the existing e_ptr for completeness
+  const Element* el_existing = e_ptr->getElement();
+  TEST_NOT_EQUAL(el_existing, e_nullPointer)
+  TEST_EQUAL(el_existing->getSymbol(), "Symbol")  // This matches what was actually set
 END_SECTION
 
 START_SECTION(Isotope(const Isotope& Isotope))
@@ -138,7 +123,8 @@ START_SECTION(bool isStable() const)
 END_SECTION
 
 START_SECTION(Isotope& operator = (const Isotope& Isotope))
-	Isotope e = *e_ptr;
+	Isotope e;
+	e = *e_ptr;
 	TEST_EQUAL(e == *e_ptr, true)
 END_SECTION
 
