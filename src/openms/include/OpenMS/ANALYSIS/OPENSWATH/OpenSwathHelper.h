afe9c1447c5a6b6d0d7c4d1b33eb5a1e5dc18cbb
singjc <justincsing@gmail.com>
2025-08-01 12:11:30 -0400

    Update doc: sampleExperiment

--- src/openms/include/OpenMS/ANALYSIS/OPENSWATH/OpenSwathHelper.h
+++ src/openms/include/OpenMS/ANALYSIS/OPENSWATH/OpenSwathHelper.h
@@ -194,6 +194,8 @@ public:
      * @param[in] seed              If non‐zero, used to seed the RNG (deterministic).
      *                              If zero, will use std::random_device for non-deterministic.
      * @param[in] filter_decoys     Whether to exclude decoys from the sampled targeted experiment. (You likely do not want to sample with decoys if you are using this method for generating iRTs)
+     * @param[in] sort_by_intensity     Whether to sort the assays by the highest cumulative intense transitions. This is useful for sampling the most intense peptides for iRTs.
+     * @param[in] top_fraction     Only sample from the top N fraction of sorted assays to narrow down on only really intense peptides. This is useful for selecting a few "high quality" peptides to use for linear iRTs.
      *
      * @return A new LightTargetedExperiment containing only the sampled
      *         compounds, their transitions, and associated proteins.
