75dd50d4a320a959b090ca4f778f0dd2d8440d01
Timo Sachsenberg <timo.sachsenberg@uni-tuebingen.de>
2025-08-08 11:38:12 +0200

    add test

--- src/openms/source/FEATUREFINDER/FeatureFinderMultiplexAlgorithm.cpp
+++ src/openms/source/FEATUREFINDER/FeatureFinderMultiplexAlgorithm.cpp
@@ -715,16 +715,22 @@ namespace OpenMS
 
   }
 
-  void FeatureFinderMultiplexAlgorithm::generateMapsProfile_(const std::vector<MultiplexIsotopicPeakPattern>& patterns, const std::vector<MultiplexFilteredMSExperiment>& filter_results, const std::vector<std::map<int, GridBasedCluster> >& cluster_results)
+  void FeatureFinderMultiplexAlgorithm::generateMapsProfile_(const std::vector<MultiplexIsotopicPeakPattern>& patterns, const std::vector<MultiplexFilteredMSExperiment>& filter_results, const std::vector<std::map<int, GridBasedCluster> >& cluster_results, bool start_progress)
   {
     // progress logger
     unsigned progress = 0;
-    startProgress(0, patterns.size(), "constructing maps");
+    if (start_progress)
+    {
+      startProgress(0, patterns.size(), "constructing maps");
+    }
 
     // loop over peak patterns
     for (unsigned pattern = 0; pattern < patterns.size(); ++pattern)
     {
-      setProgress(++progress);
+      if (start_progress)
+      {
+        setProgress(++progress);
+      }
 
       // loop over clusters
       for (std::map<int, GridBasedCluster>::const_iterator cluster_it = cluster_results[pattern].begin(); cluster_it != cluster_results[pattern].end(); ++cluster_it)
@@ -872,7 +878,10 @@ namespace OpenMS
 
     }
 
-    endProgress();
+    if (start_progress)
+    {
+      endProgress();
+    }
   }
 
   void FeatureFinderMultiplexAlgorithm::processIndividualPeakmap_(PeakMap& exp, FeatureMap& feature_map_out, ConsensusMap& consensus_map_out, MSExperiment& blacklist_out, bool progress) 
@@ -1036,7 +1045,7 @@ namespace OpenMS
        */
       filtering.getCentroidedExperiment().swap(exp_centroid_local);
       filtering.getPeakBoundaries().swap(boundaries_exp_s);
-      generateMapsProfile_(patterns, filter_results, cluster_results);
+      generateMapsProfile_(patterns, filter_results, cluster_results, false);
     }
 
     // finalize consensus map
