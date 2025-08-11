9e4e7fef1a56c172b401303e9668f8242c19b165
Samuel Wein <sam@samwein.com>
2025-08-11 15:38:04 +0000

    revert changes from the incorrect branch

--- src/openms/include/OpenMS/FORMAT/MascotGenericFile.h
+++ src/openms/include/OpenMS/FORMAT/MascotGenericFile.h
@@ -289,11 +289,6 @@ protected:
               String tmp = line.substr(5);
               spectrum.setMetaValue(Constants::UserParam::MSM_METABOLITE_NAME, tmp);
             }
-            else if (line.hasPrefix("COMPOUND_NAME"))
-            {
-              String tmp = line.substr(14);
-              spectrum.setMetaValue(Constants::UserParam::MSM_METABOLITE_NAME, tmp);
-            }
             else if (line.hasPrefix("INCHI="))
             {
               String tmp = line.substr(6);
