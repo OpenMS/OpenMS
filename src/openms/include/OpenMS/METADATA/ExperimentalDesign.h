f3d1d2f5c955873dd7e3ac8eb335a4a3fd8234c4
Tom David Mueller <tom.mueller@beachouse.de>
2025-08-15 10:06:51 +0200

    Revert "remove danglng br tags"
    This reverts commit 3e1386917f28ba381a044aa5de994ddc7ba20ab3.

--- src/openms/include/OpenMS/METADATA/ExperimentalDesign.h
+++ src/openms/include/OpenMS/METADATA/ExperimentalDesign.h
@@ -200,8 +200,8 @@ namespace OpenMS
     </tr>
     <tr>
         <td>...</td>
-        <td>...</td>
-        <td>...</td>
+        <td>...<br></td>
+        <td>...<br></td>
     </tr>
     <tr>
         <td>22</td>
