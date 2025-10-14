cc39b784a2beb4d84411be93e0b55d0fc4282508
Mohammed AlHigaylan <alhigaylan@macbookpro.home>
2025-10-14 13:02:28 -0400

    reverse deletion of im handling lines in EPD

--- src/openms/source/FEATUREFINDER/ElutionPeakDetection.cpp
+++ src/openms/source/FEATUREFINDER/ElutionPeakDetection.cpp
@@ -488,6 +488,10 @@ namespace OpenMS
         // Create new mass trace, copy smoothed intensities
         MassTrace new_mt(tmp_mt);
         new_mt.setSmoothedIntensities(smoothed_tmp);
+        // copy ion mobility centroid and peak fwhm to split traces
+        new_mt.setCentroidIM(mt.getCentroidIM());
+        new_mt.fwhm_mz_avg = mt.fwhm_mz_avg;
+        new_mt.fwhm_im_avg = mt.fwhm_im_avg;
 
         // check filter criteria
         bool pw_ok = true;
