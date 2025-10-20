3573644960c68c395c54ce1388d9e6f7f90efb2a
singjc <justincsing@gmail.com>
2025-10-19 23:57:40 -0400

    fix: skip peaks that have already been found when using wide boundary selection method

--- src/openms/source/ANALYSIS/OPENSWATH/MRMTransitionGroupPicker.cpp
+++ src/openms/source/ANALYSIS/OPENSWATH/MRMTransitionGroupPicker.cpp
@@ -127,6 +127,9 @@ namespace OpenMS
     {
       for (Size k = 0; k < picked_chroms[i].size(); ++k)
       {
+        // Skip peaks already "consumed" by previous iterations
+        if (picked_chroms[i][k].getIntensity() <= 1e-11) {continue; }
+
         const double left_rt = picked_chroms[i].getFloatDataArrays()[PeakPickerChromatogram::IDX_LEFTBORDER][k];
         const double right_rt = picked_chroms[i].getFloatDataArrays()[PeakPickerChromatogram::IDX_RIGHTBORDER][k];
         const double local_peak_width = right_rt - left_rt;
