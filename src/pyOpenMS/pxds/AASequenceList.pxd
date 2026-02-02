4a1d91c609d7f9893f0167e5cc51f944b622e58a
SK <srikanthkn10@gmail.com>
2026-02-02 15:31:41 +0530

    Reverted back

--- src/pyOpenMS/pxds/AASequenceList.pxd
+++
@@ -1,5 +0,0 @@
-from libcpp.vector cimport vector
-from AASequence cimport AASequence
-
-cdef extern from "<OpenMS/DATASTRUCTURES/ListUtils.h>" namespace "OpenMS":
-    ctypedef vector[AASequence] AASequenceList
--- src/pyOpenMS/tests/unittests/test_SequenceCoverage.py
+++ src/pyOpenMS/tests/unittests/test_SequenceCoverage.py
@@ -4,17 +4,6 @@ import pyopenms as oms
 
 class TestSequenceCoverage(unittest.TestCase):
 
-    def test_peptide_multiple_occurrences(self):
-        """Test peptide mapping multiple times covers all occurrences"""
-        protein = oms.AASequence.fromString("PEPTIDEPEPTIDE")
-        peptides = [
-            oms.AASequence.fromString("PEPTIDE")
-        ]
-        
-        # PEPTIDE occurs twice and covers the entire protein
-        coverage = oms.SequenceCoverage.getCoverage(protein, peptides)
-        self.assertAlmostEqual(coverage, 100.0, places=6)
-
     def test_partial_coverage(self):
         """Partial sequence coverage return 53.846"""
         protein = oms.AASequence.fromString("PEPTIDEAAAAAA")
