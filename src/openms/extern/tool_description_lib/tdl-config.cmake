51ce3bbe3701d6a7928b0a5facb8489714fa3028
Chris Bielow <chris.bielow@fu-berlin.de>
2026-04-01 17:20:31 +0200

    Update src/openms/extern/tool_description_lib/tdl-config.cmake

--- src/openms/extern/tool_description_lib/tdl-config.cmake
+++ src/openms/extern/tool_description_lib/tdl-config.cmake
@@ -32,7 +32,6 @@ if (NOT yaml-cpp_FOUND)
     FetchContent_Declare (
         yaml-cpp_fetch_content
         GIT_REPOSITORY "https://github.com/jbeder/yaml-cpp.git"
-        # !WORKAROUND Points to first commit after 0.8.0: Fixes CMake deprecation warnings (cmake_minimum_required)
         GIT_TAG "56e3bb550c91fd7005566f19c079cb7a503223cf")
     option (YAML_CPP_BUILD_CONTRIB "" OFF)
     option (YAML_CPP_BUILD_TOOLS "" OFF)
