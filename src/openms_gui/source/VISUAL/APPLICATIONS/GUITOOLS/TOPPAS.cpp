06be71ad1d05a3bf86a1efbf8ad54b1d5da2c71f
santhosh teja piridi <santhosh.piridi123@gmail.com>
2026-04-08 10:22:29 +0530

    Fix: restore formatting and finalize JSON error message improvement (#7708)

--- src/openms_gui/source/VISUAL/APPLICATIONS/GUITOOLS/TOPPAS.cpp
+++ src/openms_gui/source/VISUAL/APPLICATIONS/GUITOOLS/TOPPAS.cpp
@@ -205,11 +205,11 @@ int main(int argc, const char** argv)
   }
   catch (Exception::FileNotReadable& e)
   {
-    cout << String("Error: File not readable (") << e.what()  << endl << "Code location: " << e.getFile() << ":" << e.getLine() << endl;
+    cout << String("Error: File not readable (") << e.what() << ")" << endl << "Code location: " << e.getFile() << ":" << e.getLine() << endl;
   }
   catch (Exception::FileEmpty& e)
   {
-    cout << String("Error: File empty (") << e.what()  << endl << "Code location: " << e.getFile() << ":" << e.getLine() << endl;
+    cout << String("Error: File empty (") << e.what() << ")" << endl << "Code location: " << e.getFile() << ":" << e.getLine() << endl;
   }
   catch (Exception::ParseError& e)
   {
