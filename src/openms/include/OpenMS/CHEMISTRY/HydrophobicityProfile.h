c5144928a27ef1e2b22a365c0a618bf39a370da5
NoraHeese <noraheese11@gmail.com>
2026-04-08 16:10:03 +0200

    added HydrophobicityProfile class

---
+++ src/openms/include/OpenMS/CHEMISTRY/HydrophobicityProfile.h
@@ -0,0 +1,43 @@
+// Copyright (c) 2002-present, OpenMS Inc. -- EKU Tuebingen, ETH Zurich, and FU Berlin
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// --------------------------------------------------------------------------
+// $Maintainer:  $
+// $Authors: Markus Apel, Nora Heese $
+// --------------------------------------------------------------------------
+ 
+#pragma once
+ 
+#include <OpenMS/CONCEPT/Types.h>
+#include <OpenMS/CHEMISTRY/AASequence.h>
+#include <functional>
+#include <sstream>
+ 
+namespace OpenMS
+{
+    class HydrophobicityProfile {
+public:
+    // Per-residue and windowed profiles
+    static std::vector<double> computeProfile(
+        const AASequence& seq, 
+        const HydrophobicityScaleNumber scale = KYTEDOOLITTLE
+    );
+    
+    static std::vector<double> computeWindowedProfile(
+        const AASequence& seq,
+        Size window_size = 7,
+        const HydrophobicityScaleNumber scale = KYTEDOOLITTLE
+    );
+    
+    // GRAVY score
+    static double computeGRAVY(const AASequence& seq);
+    
+    // Hydrophobic moment (Eisenberg et al., 1982)
+    static std::vector<double> computeHydrophobicMoment(
+        const AASequence& seq,
+        Size window_size = 11,
+        double angle = 100.0  // degrees; 100=alpha-helix, 160=beta-sheet
+    );
+};
+   //... the actual code goes here ...
+} // namespace OpenMS
---
+++ src/openms/source/CHEMISTRY/HydrophobicityProfile.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2002-present, OpenMS Inc. -- EKU Tuebingen, ETH Zurich, and FU Berlin
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// --------------------------------------------------------------------------
+// $Maintainer:  $
+// $Authors: Markus Apel, Nora Heese $
+// --------------------------------------------------------------------------
+ 
+#include <OpenMS/KERNEL/DPeak.h>
+#include <OpenMS/CHEMISTRY/HydrophobicityProfile.h>
+ 
+namespace OpenMS
+{
+   //... the actual code goes here ...
+} // namespace OpenMS
