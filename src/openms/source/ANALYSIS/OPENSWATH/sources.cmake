7c4573f4f899220effa6e3b799077de2eafaa6a3
singjc <justincsing@gmail.com>
2026-05-07 23:33:34 -0400

    feat(sources.cmake): add GeneInference, LevelContextInference, PeptideInference, PeptidoformInference, and ProteinInference to sources list

--- src/openms/source/ANALYSIS/OPENSWATH/sources.cmake
+++ src/openms/source/ANALYSIS/OPENSWATH/sources.cmake
@@ -10,7 +10,9 @@ set(sources_list
   DIAHelper.cpp
   DIAPrescoring.cpp
   DIAScoring.cpp
+  GeneInference.cpp
   IonMobilityScoring.cpp
+  LevelContextInference.cpp
   MasstraceCorrelator.cpp
   MRMAssay.cpp
   MRMDecoy.cpp
@@ -35,6 +37,9 @@ set(sources_list
   PeakIntegrator.cpp
   PeakPickerChromatogram.cpp
   PeakPickerMobilogram.cpp
+  PeptideInference.cpp
+  PeptidoformInference.cpp
+  ProteinInference.cpp
   SwathMapMassCorrection.cpp
   SwathWindowLoader.cpp
   SwathQC.cpp
