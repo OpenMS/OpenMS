5d51b1a2f3f08b845457d9e6f45829dd6b2c5885
Nidhish Chauhan <solemnsquire@gmail.com>
2026-08-13 12:15:23 +0530

    Addressed coderrabbitai nitpick comments

--- vcpkg-overlays/ports/eol-bspline/portfile.cmake
+++ vcpkg-overlays/ports/eol-bspline/portfile.cmake
@@ -1,5 +1,11 @@
 set(SOURCE_PATH "${CURRENT_PORT_DIR}/src")
 
+vcpkg_apply_patches(
+    SOURCE_PATH "${SOURCE_PATH}"
+    PATCHES
+        fix-missing-includes.patch
+)
+
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
--- vcpkg-overlays/ports/isospec/portfile.cmake
+++ vcpkg-overlays/ports/isospec/portfile.cmake
@@ -10,4 +10,14 @@ vcpkg_cmake_config_fixup(PACKAGE_NAME isospec CONFIG_PATH share/isospec)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/IsoSpec/LICENSE")
+vcpkg_install_copyright(
+    FILE_LIST "${SOURCE_PATH}/IsoSpec/LICENSE"
+    COMMENT
+        "IsoSpec itself is licenses under BSD-2-Clause (see LICENSE)
+
+        Additionally, this library bundles some files with their own licenses:
+
+        - mman.h: MIT License. Comes from the repository (https://github.com/witwall/mman-win32). No explicit license file is included, but stated in file header's comment.
+
+        - btrd.h: Boost Software License. Adapted from boost random/binomial_distribution.hpp header file, at version 1.71. Copyright Steven Watanabe 2010. Full license text: http://www.boost.org/LICENSE_1_0.txt"
+)
