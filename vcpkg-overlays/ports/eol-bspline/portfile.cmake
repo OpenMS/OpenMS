21e59f054d61ca4f4e1db76390a5e8bc53a57ae4
Nidhish Chauhan <solemnsquire@gmail.com>
2026-08-14 20:08:44 +0530

    changes source of vcpkg isospec and eol-bspline to extern

--- vcpkg-overlays/ports/eol-bspline/portfile.cmake
+++ vcpkg-overlays/ports/eol-bspline/portfile.cmake
@@ -1,12 +1,24 @@
-set(SOURCE_PATH "${CURRENT_PORT_DIR}/src")
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+set(EOL_BSPLINE_EXTERN_DIR "${CURRENT_PORT_DIR}/../../../src/openms/extern/eol-bspline")
+get_filename_component(EOL_BSPLINE_EXTERN_DIR "${EOL_BSPLINE_EXTERN_DIR}" ABSOLUTE)
+
+if(NOT EXISTS "${EOL_BSPLINE_EXTERN_DIR}/BSpline/BSpline.h")
+    message(FATAL_ERROR "eol-bspline sources not found at ${EOL_BSPLINE_EXTERN_DIR}")
+endif()
+
+set(SOURCE_PATH "${CURRENT_BUILDTREES_DIR}/src/eol-bspline-staged")
+file(REMOVE_RECURSE "${SOURCE_PATH}")
+file(MAKE_DIRECTORY "${SOURCE_PATH}")
+file(COPY "${EOL_BSPLINE_EXTERN_DIR}/" DESTINATION "${SOURCE_PATH}")
 
 vcpkg_apply_patches(
     SOURCE_PATH "${SOURCE_PATH}"
     PATCHES
+        eol-bspline-standalone-cmake.patch
         fix-missing-includes.patch
 )
 
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
 )
